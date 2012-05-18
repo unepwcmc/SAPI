729a5aa0f7a4da74bc094833614987e0190ade2c
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-05-18 12:13:39 +0100

    Annotated new models

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: countries
+#
+#  id         :integer         not null, primary key
+#  iso_name   :string(255)     not null
+#  iso2_code  :string(255)
+#  iso3_code  :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#  region_id  :integer
+#
+
 class Country < ActiveRecord::Base
   has_many :distribution_components, :as => :component
   belongs_to :region
