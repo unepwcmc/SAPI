a852322ccd7342860f323942a4ac283e9ef8a226
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-05-21 13:12:52 +0100

    simplified the taxon trees index action

--- app/controllers/taxon_trees_controller.rb
+++ app/controllers/taxon_trees_controller.rb
@@ -1,6 +1,5 @@
 class TaxonTreesController < ApplicationController
   def index
-    roots = TaxonConcept.where(:parent_id => nil)
-    render :json => roots.map{ |r| TaxonTree.new(r) }
+    render :json => TaxonConcept.roots.map{ |r| TaxonTree.new(r) }
   end
 end
