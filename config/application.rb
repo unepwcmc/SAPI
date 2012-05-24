56350628ccdb3942387b1469b8d213fcd2027430
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-05-24 15:34:45 +0100

    disabled asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -57,9 +57,6 @@ module SAPI
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.enabled = false
   end
 end
