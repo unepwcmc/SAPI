df254e4fedcca080679aaf77fe5ab27749da3578
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-06-08 14:24:12 +0100

    Added exception notification to application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ SAPI::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #Exception Notification
+  config.middleware.use ExceptionNotifier, :email_prefix => "[SAPI Exception] ",
+    :sender_address => %{"Exception Notifier" <support@unep-wcmc.org>},
+    :exception_recipients => %w{simao.belchior@unep-wcmc.org agnieszka.figiel@unep-wcmc.org}
 end
