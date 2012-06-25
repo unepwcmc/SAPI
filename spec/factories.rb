07ef4048298b41b344ccacc7396662ed45517a67
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-06-25 11:14:06 +0100

    added spec for rank and parent names

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,37 +5,31 @@ FactoryGirl.define do
   factory :cites_designation, parent: :designation, class: Designation do |d|
     d.name 'CITES'
   end
+
   factory :rank do |r|
     r.name 'rank'
   end
-  factory :genus_rank, parent: :rank, class: Rank do |r|
-    r.name 'GENUS'
-  end
-  factory :species_rank, parent: :rank, class: Rank do |r|
-    r.name 'SPECIES'
-  end
-  factory :subspecies_rank, parent: :rank, class: Rank do |r|
-    r.name 'SUBSPECIES'
-  end
+
   factory :taxon_name do |tn|
     tn.scientific_name 'lupus'
   end
+
   factory :taxon_concept do |tc|
     tc.association :designation
     tc.association :rank
     tc.association :taxon_name
     tc.data {}
   end
-  factory :genus, parent: :taxon_concept, class: TaxonConcept do |tc|
-    tc.association :designation, factory: :cites_designation
-    tc.association :rank, factory: :genus_rank
+  
+  %w(kingdom phylum class order family genus species subspecies).each do |rank_name|
+    factory :"#{rank_name}_rank", parent: :rank, class: Rank do |r|
+      r.name rank_name.upcase
+    end
+
+    factory :"#{rank_name}", parent: :taxon_concept, class: TaxonConcept do |tc|
+      tc.association :designation, factory: :cites_designation
+      tc.association :rank, factory: :"#{rank_name}_rank"
+    end
   end
-  factory :species, parent: :taxon_concept, class: TaxonConcept do |tc|
-    tc.association :designation, factory: :cites_designation
-    tc.association :rank, factory: :species_rank
-  end
-  factory :subspecies, parent: :taxon_concept, class: TaxonConcept do |tc|
-    tc.association :designation, factory: :cites_designation
-    tc.association :rank, factory: :subspecies_rank
-  end
+
 end
--- spec/models/taxon_concept_spec.rb
+++ spec/models/taxon_concept_spec.rb
@@ -1,14 +1,36 @@
 require 'spec_helper'
 
 describe TaxonConcept do
-  before(:each) do
-
-  end
-  describe :full_name do
-    before do
+  context "when importing taxonomy data" do
+    before(:all) do
+      @kingdom = create(
+        :kingdom,
+        :taxon_name => create(:taxon_name, :scientific_name => 'Fauna')
+      )
+      @phylum = create(
+        :phylum,
+        :taxon_name => create(:taxon_name, :scientific_name => 'Chordata'),
+        :parent => @kingdom
+      )      
+      @klass = create(
+        :class,
+        :taxon_name => create(:taxon_name, :scientific_name => 'Reptilia'),
+        :parent => @phylum
+      )
+      @order = create(
+        :order,
+        :taxon_name => create(:taxon_name, :scientific_name => 'Serpentes'),
+        :parent => @klass
+      )
+      @family = create(
+        :family,
+        :taxon_name => create(:taxon_name, :scientific_name => 'Boidae'),
+        :parent => @order
+      )
       @genus = create(
         :genus,
-        :taxon_name => create(:taxon_name, :scientific_name => 'Boa')
+        :taxon_name => create(:taxon_name, :scientific_name => 'Boa'),
+        :parent => @family        
       )
       @species = create(
         :species,
@@ -22,14 +44,34 @@ describe TaxonConcept do
       )
       Sapi::rebuild
     end
-    it "should be trinomen for subspecies" do
-      @subspecies.reload.full_name.should == 'Boa constrictor occidentalis'
-    end
-    it "should be binomen for species" do
-      @species.reload.full_name.should == 'Boa constrictor'
-    end
-    it "should be scientific_name on its own in other cases" do
-      @genus.reload.full_name.should == 'Boa'
+    context "Boa constrictor" do
+      describe :full_name do
+        it "should be trinomen for subspecies: Boa constrictor occidentalis" do
+          @subspecies.reload.full_name.should == 'Boa constrictor occidentalis'
+        end
+        it "should be binomen for species: Boa constrictor" do
+          @species.reload.full_name.should == 'Boa constrictor'
+        end
+        it "should be single name for genus: Boa" do
+          @genus.reload.full_name.should == 'Boa'
+        end
+      end
+      describe :rank do
+        it "should be SPECIES" do
+          @species.reload.rank_name.should == 'SPECIES'
+        end
+      end
+      describe :parents do
+        it "should have Boidae as family" do
+          @species.reload.family_name == 'Boidae'
+        end
+        it "should have Serpentes as order" do
+          @species.reload.order_name == 'Serpentes'
+        end
+        it "should have Reptilia as class" do
+          @species.reload.class_name == 'Reptilia'
+        end
+      end
     end
   end
 end
