0be33c5ed6aaeaf14deedf10e9bd0491d6fae137
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-06-28 08:50:54 +0100

    optimized query which inserts listing deletion records between addition records

--- db/structure.sql
+++ db/structure.sql
@@ -49,24 +49,23 @@ CREATE FUNCTION fix_cites_listing_changes() RETURNS void
       INSERT INTO listing_changes 
       (taxon_concept_id, species_listing_id, change_type_id, effective_at, created_at, updated_at)
       SELECT 
-      qq.taxon_concept_id, qq.species_listing_id, (SELECT id FROM change_types WHERE name = 'DELETION' LIMIT 1), qq.effective_at - time '00:00:01', NOW(), NOW()
+      qq.taxon_concept_id, qq.species_listing_id, (SELECT id FROM change_types WHERE name = 'DELETION' LIMIT 1),
+      qq.effective_at - time '00:00:01', NOW(), NOW()
       FROM (
-              WITH q AS (
-                      SELECT taxon_concept_id, species_listing_id, change_type_id, change_types.name, effective_at, 
+             WITH q AS (
+                      SELECT taxon_concept_id, species_listing_id, change_type_id, effective_at, change_types.name AS change_type_name,
                       ROW_NUMBER() OVER(ORDER BY taxon_concept_id, effective_at) AS row_no
                       FROM listing_changes
                       LEFT JOIN change_types on change_type_id = change_types.id
-                      WHERE change_type_id IN (SELECT id FROM change_types WHERE name IN ('ADDITION','DELETION'))
-                      AND species_listing_id IN (
-                        SELECT species_listings.id FROM species_listings
-                        LEFT JOIN designations ON designations.id = species_listings.designation_id
-                        WHERE designations.name = 'CITES'
-                      )
+                      LEFT JOIN species_listings on species_listing_id = species_listings.id
+                      LEFT JOIN designations ON designations.id = species_listings.designation_id
+                      WHERE change_types.name IN ('ADDITION','DELETION')
+                      AND designations.name = 'CITES'
               )
               SELECT q1.taxon_concept_id, q1.species_listing_id, q2.effective_at
-              FROM q q1 LEFT JOIN q q2 ON (q1.taxon_concept_id =q2.taxon_concept_id AND q2.row_no = q1.row_no - 1)
+              FROM q q1 LEFT JOIN q q2 ON (q1.taxon_concept_id = q2.taxon_concept_id AND q2.row_no = q1.row_no + 1)
               WHERE q2.taxon_concept_id IS NOT NULL
-              AND q1.change_type_id = q2.change_type_id AND q1.change_type_id = (SELECT id FROM change_types WHERE name = 'ADDITION' LIMIT 1)
+              AND q1.change_type_id = q2.change_type_id AND q1.change_type_name = 'ADDITION'
       ) qq;
       END;
       $$;
@@ -1819,3 +1818,5 @@ INSERT INTO schema_migrations (version) VALUES ('20120626140446');
 INSERT INTO schema_migrations (version) VALUES ('20120627120930');
 
 INSERT INTO schema_migrations (version) VALUES ('20120627133057');
+
+INSERT INTO schema_migrations (version) VALUES ('20120628072610');
