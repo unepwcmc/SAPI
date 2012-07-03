907a0ede374aab941e1f082c4f507eee1dbaa063
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-07-03 08:14:38 +0100

    fixed errors in staging migrations (create extension requires superuser)

--- db/migrate/20120530135534_setup_hstore.rb
+++ db/migrate/20120530135534_setup_hstore.rb
@@ -1,9 +1,17 @@
 class SetupHstore < ActiveRecord::Migration
   def self.up
-    execute "CREATE EXTENSION hstore"
+    if Rails.env.staging? or Rails.env.production?
+      puts "Please add extension by hand: CREATE EXTENSION hstore"
+    else
+      execute "CREATE EXTENSION hstore"
+    end
   end
 
   def self.down
-    execute "DROP EXTENSION hstore"
+    if Rails.env.staging? or Rails.env.production?
+      puts "Please drop extension by hand: DROP EXTENSION hstore"
+    else
+      execute "DROP EXTENSION hstore"
+    end
   end
 end
--- db/plpgsql/000_hstore.sql
+++ db/plpgsql/000_hstore.sql
@@ -2,12 +2,12 @@
 -- Name: hstore; Type: EXTENSION; Schema: -; Owner: -
 --
 
-CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
+-- CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
 
 
 --
 -- Name: EXTENSION hstore; Type: COMMENT; Schema: -; Owner: -
 --
 
-COMMENT ON EXTENSION hstore IS 'data type for storing sets of (key, value) pairs';
+-- COMMENT ON EXTENSION hstore IS 'data type for storing sets of (key, value) pairs';
 
