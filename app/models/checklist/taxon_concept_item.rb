ef387b67c6758b30c9f955d5190722aeea23ae99
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-10-01 10:14:47 +0100

    deleted unused class

--- app/models/checklist/taxon_concept_item.rb
+++
@@ -1,6 +0,0 @@
-class Checklist::TaxonConceptItem < Checklist::ChecklistItem
-  def initialize(options)
-    @item_type = 'TaxonConcept'#TODO class name would do, if as_json would work
-    super(options)
-  end
-end
