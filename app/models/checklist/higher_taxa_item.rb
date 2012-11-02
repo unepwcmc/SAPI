3c07d9f12ed35c432725e26a9bcb8d41f385f26f
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-11-02 15:33:24 +0000

    added higher taxa common names

--- app/models/checklist/higher_taxa_item.rb
+++ app/models/checklist/higher_taxa_item.rb
@@ -1,26 +1,21 @@
 class Checklist::HigherTaxaItem
-  attr_reader :phylum_name, :class_name, :order_name, :family_name, :rank_name,
-    :full_name, :english_names_list, :spanish_names_list, :french_names_list
-  def initialize(options)
-    options.each do |name, value|
-      instance_variable_set("@#{name}", value)
-    end
+  attr_reader :rank_name, :full_name, :english_names, :spanish_names, :french_names
+
+  def initialize(taxon_concept)
     @item_type = 'HigherTaxa'#TODO class name would do, if as_json would work
+    @taxon_concept = taxon_concept
+    [
+      :rank_name, :full_name, :english_names, :spanish_names, :french_names
+    ].each do |attribute|
+    instance_variable_set("@#{attribute}", @taxon_concept.send(attribute))
+  end
     taxa = ['PHYLUM', 'CLASS', 'ORDER', 'FAMILY']
     current_idx = taxa.index(@rank_name) || 0
     @ancestors_path = 0.upto(current_idx).map do |i|
       taxa[i]
     end.map do |rank|
-      send("#{rank.downcase}_name")
+      taxon_concept.send("#{rank.downcase}_name")
     end.join(',')
   end
-  def english_names
-    ['TODO']
-  end
-  def spanish_names
-    ['TODO']
-  end
-  def french_names
-    ['TODO']
-  end
+
 end
