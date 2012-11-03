e93873d6d25582bbd78d03ef2d6c43ce48fb48f3
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-11-03 22:11:13 +0000

    fixed web taxonomic layout

--- app/models/checklist/higher_taxa_item.rb
+++ app/models/checklist/higher_taxa_item.rb
@@ -1,23 +1,37 @@
 class Checklist::HigherTaxaItem
-  attr_reader :rank_name, :full_name, :english_names, :spanish_names, :french_names,
-    :kingdom_id, :phylum_id, :class_id, :order_id, :family_id
 
   def initialize(taxon_concept)
     @item_type = 'HigherTaxa'#TODO class name would do, if as_json would work
     @taxon_concept = taxon_concept
-    [
-      :rank_name, :full_name, :english_names, :spanish_names, :french_names,
-      :kingdom_id, :phylum_id, :class_id, :order_id, :family_id
-    ].each do |attribute|
-    instance_variable_set("@#{attribute}", @taxon_concept.send(attribute))
   end
+
+  def ancestors_path
     taxa = ['PHYLUM', 'CLASS', 'ORDER', 'FAMILY']
-    current_idx = taxa.index(@rank_name) || 0
-    @ancestors_path = 0.upto(current_idx).map do |i|
+    current_idx = taxa.index(rank_name) || 0
+    0.upto(current_idx).map do |i|
       taxa[i]
     end.map do |rank|
-      taxon_concept.send("#{rank.downcase}_name")
+      send("#{rank.downcase}_name")
     end.join(',')
+  end
+
+  #use method_missing to delegate taxon concept methods
+  def method_missing(method_sym, *arguments, &block)
+    # the first argument is a Symbol, so you need to_s it if you want to pattern match
+    if @taxon_concept.respond_to? method_sym
+      @taxon_concept.send(method_sym)
+    else
+      super
+    end
+  end
+
+  def as_json(options)
+    {
+      :id => id,
+      :item_type => @item_type,
+      :rank_name => rank_name,
+      :ancestors_path => ancestors_path
+    }
   end
 
 end
