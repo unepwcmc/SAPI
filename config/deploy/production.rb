d1be3bc861e29eed5257b4e409b2eddabed9d04a
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-12-05 17:53:19 +0000

    Updated production deploy script to use 009 as the db server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,8 @@ set :rails_env, "production"
 # Primary domain name of your application. Used in the Apache configs
 set :domain, "unepwcmc-004.vm.brightbox.net"
 ## List of servers
-server "unepwcmc-004.vm.brightbox.net", :app, :web, :db, :primary => true
+#server "unepwcmc-004.vm.brightbox.net", :app, :web, :db, :primary => true
+
+role :web, "unepwcmc-004.vm.brightbox.net"
+role :app, "unepwcmc-004.vm.brightbox.net"
+role :db, "unepwcmc-009.vm.brightbox.net", :primary => true
