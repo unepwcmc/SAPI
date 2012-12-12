02ac0e111df8c09763fe92cab9214275ceb62a47
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-12-12 18:44:15 +0000

    added description to trade codes

--- db/migrate/20121212085024_create_trade_codes.rb
+++ db/migrate/20121212085024_create_trade_codes.rb
@@ -1,9 +1,10 @@
 class CreateTradeCodes < ActiveRecord::Migration
   def change
     create_table :trade_codes do |t|
-      t.string :code
-      t.string :name
-      t.string :type
+      t.string :code, :null => false
+      t.string :name, :null => false
+      t.string :description
+      t.string :type, :null => false
 
       t.timestamps
     end
