b380f3f264aa1c838e2a50015450d5624f98a46f
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-12-17 19:40:07 +0000

    [#41219809] Updated new species_listings' form

--- app/controllers/admin/species_listings_controller.rb
+++ app/controllers/admin/species_listings_controller.rb
@@ -1,6 +1,11 @@
 class Admin::SpeciesListingsController < Admin::AdminController
   inherit_resources
 
+  def index
+    @designations = Designation.order(:name)
+    index!
+  end
+
   protected
     def collection
       @species_listings ||= end_of_association_chain.order('designation_id, name')
--- app/controllers/api/species_listings_controller.rb
+++ app/controllers/api/species_listings_controller.rb
@@ -1,4 +1,11 @@
 class Api::SpeciesListingsController < ApplicationController
   respond_to :json
   inherit_resources
+
+  def create
+    create! do |success, failure|
+      success.json { render :json => @species_listing }
+      failure.json { render :json => { :errors => @species_listing.errors } }
+    end
+  end
 end
