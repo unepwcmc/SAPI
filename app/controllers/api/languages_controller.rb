e47f37948eba7ff7cd65136b96597a2ad79db6da
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-12-17 19:26:41 +0000

    [#41220701] Updated new language form.

--- app/controllers/api/languages_controller.rb
+++ app/controllers/api/languages_controller.rb
@@ -1,4 +1,11 @@
 class Api::LanguagesController < ApplicationController
   respond_to :json
   inherit_resources
+
+  def create
+    create! do |success, failure|
+      success.json { render :json => @language }
+      failure.json { render :json => { :errors => @language.errors } }
+    end
+  end
 end
