b8ee229076746e4b9aa418809502947b32004adb
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-12-17 18:56:22 +0000

    [#41220701 #41220691 #41219797 #41219809] Replaced integer with Designations lists for species_listings edit. Fixed form to use necessary classes to match the javascript

--- app/controllers/api/designations_controller.rb
+++ app/controllers/api/designations_controller.rb
@@ -1,4 +1,10 @@
 class Api::DesignationsController < ApplicationController
   respond_to :json
   inherit_resources
+
+  protected
+
+  def collection
+    @designations ||= end_of_association_chain.order(:name).select([:id, :name]).map{|d| {value: d.id, text: d.name}}
+  end
 end
