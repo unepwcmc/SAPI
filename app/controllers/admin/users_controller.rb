6c7e9db4198d788c78786e63499669338a08759e
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-12-19 14:57:37 +0000

    [#41220691] added deletion

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,9 @@
-class Admin::UsersController < Admin::AdminController
+class Admin::UsersController < Admin::SimpleCrudController
   inherit_resources
 
   protected
     def collection
-      @users ||= end_of_association_chain.order(:name)
+      @users ||= end_of_association_chain.order(:name).page(params[:page])
     end
 end
 
