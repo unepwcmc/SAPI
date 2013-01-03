8683139dc72f92411d759b89746481a2879e28a9
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-01-03 19:55:13 +0000

    removed trade code descriptions

--- app/models/trade_code.rb
+++ app/models/trade_code.rb
@@ -17,9 +17,8 @@
 
 class TradeCode < ActiveRecord::Base
   attr_accessible :code, :type,
-    :name_en, :name_es, :name_fr,
-    :description_en, :description_es, :description_fr
-  translates :name, :description
+    :name_en, :name_es, :name_fr
+  translates :name
 
   validates :code, :presence => true, :uniqueness => {:scope => :type}
 
