6cccd6b2d3fae48d1f793ecab3749a41320a90dc
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-01-08 17:38:39 +0000

    Added is_inter_designational to taxon_relationship_types. Added necessary methos to the model and updated the seeds

--- app/models/taxon_relationship_type.rb
+++ app/models/taxon_relationship_type.rb
@@ -2,15 +2,23 @@
 #
 # Table name: taxon_relationship_types
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)      not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :integer          not null, primary key
+#  name                   :string(255)      not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  is_inter_designational :boolean
 #
 
 class TaxonRelationshipType < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :is_inter_designational
 
   include Dictionary
-  build_dictionary :equal_to, :contains, :has_synonym
+  build_dictionary :equal_to, :includes, :included_in, :overlaps, :disjunct, :has_homonym, :has_synonym
+
+  def inter_designational
+    where(:is_inter_designational => true)
+  end
+  def intra_designational
+    where(:is_inter_designational => false)
+  end
 end
