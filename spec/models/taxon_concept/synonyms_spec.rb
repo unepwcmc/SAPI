736f3e3c133c08e6b7faab34db8cd514c96f1a4a
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-01-16 13:36:11 +0000

    improved spec

--- spec/models/taxon_concept/synonyms_spec.rb
+++ spec/models/taxon_concept/synonyms_spec.rb
@@ -4,13 +4,14 @@ describe TaxonConcept do
   describe :accepted_taxon_concept do
     let(:accepted_tc){ create(:species) }
     let(:synonym_tc){
-      synonym = create(:species, :name_status => 'S')
+      create(:species, :name_status => 'S')
+    }
+    let!(:synonym_rel){
       create(
         :has_synonym,
         :taxon_concept => accepted_tc,
-        :other_taxon_concept => synonym
+        :other_taxon_concept => synonym_tc
       )
-      synonym
     }
 
     specify{ synonym_tc.accepted_taxon_concept.should == accepted_tc }
