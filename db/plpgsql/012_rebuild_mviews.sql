10e88c1869ddd6c39a17763afe2881f5fb5ee223
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-01-16 11:01:43 +0000

    applied a more structured approach to taxon concepts triggers

--- db/plpgsql/012_rebuild_mviews.sql
+++ db/plpgsql/012_rebuild_mviews.sql
@@ -1,1 +1,27 @@
+DROP FUNCTION IF EXISTS taxon_concepts_refresh_row(id INTEGER);
+CREATE OR REPLACE FUNCTION taxon_concepts_refresh_row(row_id INTEGER) RETURNS VOID
+SECURITY DEFINER
+LANGUAGE 'plpgsql' AS $$
+BEGIN
+  DELETE
+  FROM taxon_concepts_mview tc
+  WHERE tc.id = row_id;
 
+  INSERT INTO taxon_concepts_mview
+  SELECT *, FALSE, NULL
+  FROM taxon_concepts_view tc
+  WHERE tc.id = row_id;
+END
+$$;
+
+DROP FUNCTION IF EXISTS taxon_concepts_invalidate_row(id INTEGER);
+CREATE OR REPLACE FUNCTION taxon_concepts_invalidate_row(row_id INTEGER) RETURNS VOID
+SECURITY DEFINER
+LANGUAGE 'plpgsql' AS $$
+BEGIN
+  UPDATE taxon_concepts_mview tc
+  SET dirty = TRUE
+  WHERE tc.id = row_id;
+  RETURN;
+END
+$$;
