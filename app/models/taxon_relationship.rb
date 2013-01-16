fe85a144251609d036d1f59912a5182c0642a1f6
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-01-16 13:30:09 +0000

    Added Validations to taxon relationships and tests for those

--- app/models/taxon_relationship.rb
+++ app/models/taxon_relationship.rb
@@ -16,11 +16,13 @@ class TaxonRelationship < ActiveRecord::Base
   belongs_to :taxon_concept
   belongs_to :other_taxon_concept, :class_name => 'TaxonConcept'
 
-  validates :taxon_concept_id, :uniqueness => { :scope => [:taxon_relationship_type_id, :other_taxon_concept_id], :message => 'This particular relationship already exists' }
-
   delegate :is_bidirectional?, :to => :taxon_relationship_type
+
   before_destroy :destroy_opposite, :if => Proc.new { self.is_bidirectional? && self.has_opposite? }
   after_create :create_opposite, :if => Proc.new { self.is_bidirectional? && !self.has_opposite? }
+
+  validates :taxon_concept_id, :uniqueness => { :scope => [:taxon_relationship_type_id, :other_taxon_concept_id], :message => 'This particular relationship already exists' }
+  validate :interdesignational_relationship_uniqueness, :if => "taxon_relationship_type.is_interdesignational?"
 
   def opposite
     TaxonRelationship.where(:taxon_concept_id => self.other_taxon_concept_id,
@@ -46,5 +48,14 @@ class TaxonRelationship < ActiveRecord::Base
         :other_taxon_concept_id => self.taxon_concept_id,
         :taxon_relationship_type_id => self.taxon_relationship_type_id).
       delete_all
+  end
+
+  def interdesignational_relationship_uniqueness
+    if TaxonRelationship.where(:taxon_concept_id => self.taxon_concept_id,
+         :other_taxon_concept_id => self.other_taxon_concept_id).
+         joins(:taxon_relationship_type).
+         where(:taxon_relationship_types => { :is_interdesignational => true }).any? == true
+      errors.add(:taxon_concept_id, "these taxon are already related through another relationship")
+    end
   end
 end
