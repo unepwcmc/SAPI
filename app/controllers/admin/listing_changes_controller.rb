b119fc91491bd8df0a52c56f754012bd0709e493
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-01-17 17:36:02 +0000

    After adding a taxon_relationship the page redirects if the type of relationship is different from the tab where the page is. Removed inherit_resources method call from controllers taht inherit from SimpleCrudController

--- app/controllers/admin/listing_changes_controller.rb
+++ app/controllers/admin/listing_changes_controller.rb
@@ -6,6 +6,6 @@ class Admin::ListingChangesController < Admin::SimpleCrudController
   def collection
     @listing_changes ||= end_of_association_chain.
       order('effective_at desc, is_current desc').
-      page(params[:page])
+      page(params[:page]).where(:parent_id => nil)
   end
 end
