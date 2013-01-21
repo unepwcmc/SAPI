2a894e6b8a86e853fd0bfb98a44e09279b01c594
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-01-21 18:57:06 +0000

    Added bootstrap-datepicker files and requires to the project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require bootstrap-editable
+//= require bootstrap-datepicker
 //= require jquery_nested_form
 //= require underscore
 //= require_tree .
