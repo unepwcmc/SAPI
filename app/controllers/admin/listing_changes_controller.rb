d718b5c4e50a89c522efa5e3bfe799a2ad6000ca
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-01-29 16:46:21 +0000

    kind of fixed listing changes controller: preloads dropdowns with change types and species listings applicable to all designations within a taxonomy. might be an idea to change this.

--- app/controllers/admin/listing_changes_controller.rb
+++ app/controllers/admin/listing_changes_controller.rb
@@ -5,12 +5,12 @@ class Admin::ListingChangesController < Admin::SimpleCrudController
   layout 'taxon_concepts'
 
   protected
-  def load_change_types #TODO this method needs to know the designation
+  def load_change_types
     @taxon_concept ||= TaxonConcept.find(params[:taxon_concept_id])
-    @change_types = ChangeType.order(:name).
-      where(:designation_id => @taxon_concept.designation_id)
-    @species_listings = SpeciesListing.order(:abbreviation).
-      where(:designation_id => @taxon_concept.designation_id)
+    @change_types = ChangeType.order(:name).joins(:designation).
+      where(:"designations.taxonomy_id" => @taxon_concept.taxonomy_id)
+    @species_listings = SpeciesListing.order(:abbreviation).joins(:designation).
+      where(:"designations.taxonomy_id" => @taxon_concept.taxonomy_id)
   end
 
   def collection
