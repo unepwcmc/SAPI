2cd4a6797db3bfd58073b9f0eede0e83d515c6b8
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-02-08 11:47:30 +0000

    Added tmp/pids to shared dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,6 +62,7 @@ set :deploy_via, :remote_cache
 # items are symlinked in when the code is updated.
 # set :local_shared_dirs, %w(public/upload)
 set :local_shared_files, %w(config/database.yml)
+set :local_shared_dirs, %w(tmp/pids)
 
 ## Global Shared Area
 # These are the list of files and directories that you want
