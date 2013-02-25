c170f2963c1084803f5d559ac30c82e1b135f8bb
Adam Mulligan <adammulligan@gmail.com>
2013-02-25 11:26:31 +0000

    Fixed reference delete for taxon concepts

--- app/controllers/admin/taxon_concept_references_controller.rb
+++ app/controllers/admin/taxon_concept_references_controller.rb
@@ -28,15 +28,17 @@ class Admin::TaxonConceptReferencesController < Admin::SimpleCrudController
   end
 
   def destroy
-    destroy! do |success, failure|
-      success.html {
-        redirect_to edit_admin_taxon_concept_url(params[:taxon_concept_id]),
-        :notice => 'Operation successful'
-      }
-      failure.html {
-        redirect_to edit_admin_taxon_concept_url(params[:taxon_concept_id]),
-        :notice => 'Operation failed'
-      }
+    taxon_concept_reference = TaxonConceptReference.where(
+      "reference_id = ? AND taxon_concept_id = ?",
+      params[:id], params[:taxon_concept_id]
+    ).first
+
+    if taxon_concept_reference.delete
+      redirect_to edit_admin_taxon_concept_url(params[:taxon_concept_id]),
+      :notice => 'Operation successful'
+    else
+      redirect_to edit_admin_taxon_concept_url(params[:taxon_concept_id]),
+      :notice => 'Operation failed'
     end
   end
 end
