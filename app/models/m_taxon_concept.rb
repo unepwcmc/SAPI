0995e7dcc4bd970c68780b8bfcca32bbbcb4114b
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-02-27 15:06:11 +0000

    Searching for common names in the search box returns the results that match

--- app/models/m_taxon_concept.rb
+++ app/models/m_taxon_concept.rb
@@ -176,8 +176,17 @@ class MTaxonConcept < ActiveRecord::Base
       <<-SQL
       INNER JOIN (
         SELECT id FROM taxon_concepts_mview
-        WHERE full_name >= '#{TaxonName.lower_bound(scientific_name)}'
-          AND full_name < '#{TaxonName.upper_bound(scientific_name)}'
+        WHERE (full_name >= '#{TaxonName.lower_bound(scientific_name)}'
+          AND full_name < '#{TaxonName.upper_bound(scientific_name)}')
+          OR (
+            EXISTS (
+              SELECT * FROM UNNEST(english_names_ary) name WHERE name ILIKE '%#{scientific_name}%'
+              UNION
+              SELECT * FROM UNNEST(french_names_ary) name WHERE name ILIKE '#{scientific_name}%'
+              UNION
+              SELECT * FROM UNNEST(spanish_names_ary) name WHERE name ILIKE '#{scientific_name}%'
+            )
+          )
       ) matches
       ON matches.id IN (taxon_concepts_mview.id, family_id, order_id, class_id, phylum_id)
       SQL
