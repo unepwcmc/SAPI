bc36f3799a17716ba0b5a1f9d589c82e06b3bf55
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-03-05 12:42:57 +0000

    working on copying over listing changes across events

--- app/models/event_observer.rb
+++ app/models/event_observer.rb
@@ -1,5 +1,12 @@
 class EventObserver < ActiveRecord::Observer
   def after_create(event)
-    #TODO copy listing changes
+    unless event.listing_changes_event_id.blank?
+      ActiveRecord::Base.connection.execute <<-SQL
+        SELECT * FROM copy_listing_changes_across_events(
+          #{event.listing_changes_event_id},
+          #{event.id}
+        )
+      SQL
+    end
   end
 end
--- db/plpgsql/016_copy_listing_changes_across_events.sql
+++ db/plpgsql/016_copy_listing_changes_across_events.sql
@@ -12,18 +12,18 @@ CREATE OR REPLACE FUNCTION copy_listing_changes_across_events(
         short_note_en, short_note_es, short_note_fr,
         full_note_en, full_note_es, full_note_fr,
         display_in_index, display_in_footnote,
-        created_at, updated_at
+        created_at, updated_at, source_id
       )
       SELECT symbol, parent_symbol,
         short_note_en, short_note_es, short_note_fr,
         full_note_en, full_note_es, full_note_fr,
         display_in_index, display_in_footnote,
-        current_date, current_date
+        current_date, current_date, source.id
       FROM annotations source
       INNER JOIN listing_changes
         ON listing_changes.annotation_id = source.id
           AND listing_changes.event_id = from_event_id
-      RETURNING id, listing_changes.id AS source_listing_change_id
+      RETURNING id, source_id
     ), copied_hash_annotations AS (
       -- copy hash annotations
       INSERT INTO annotations (
@@ -31,33 +31,35 @@ CREATE OR REPLACE FUNCTION copy_listing_changes_across_events(
         short_note_en, short_note_es, short_note_fr,
         full_note_en, full_note_es, full_note_fr,
         display_in_index, display_in_footnote,
-        created_at, updated_at
+        created_at, updated_at, source_id
       )
       SELECT symbol, parent_symbol,
         short_note_en, short_note_es, short_note_fr,
         full_note_en, full_note_es, full_note_fr,
         display_in_index, display_in_footnote,
-        current_date, current_date
+        current_date, current_date, source.id
       FROM annotations source
       INNER JOIN listing_changes
         ON listing_changes.hash_annotation_id = source.id
           AND listing_changes.event_id = from_event_id
-      RETURNING id, listing_changes.id AS source_listing_change_id
+      RETURNING id, source_id
     ), copied_listing_changes AS (
       -- copy listing_changes
       INSERT INTO listing_changes (
         change_type_id, species_listing_id, annotation_id, hash_annotation_id,
-        parent_id, event_id, effective_at, is_current, created_at, updated_at
+        parent_id, event_id, effective_at, is_current, created_at, updated_at,
+        source_id
       )
       SELECT source.change_type_id, source.species_listing_id,
         copied_annotations.id, copied_hash_annotations.id, source.parent_id,
-        to_event_id, to_event.effective_at, false, current_date, current_date
-      FROM listing_changes source
+        to_event_id, to_event.effective_at, false, current_date, current_date,
+        source.id
+      FROM listing_changes source, to_event
       LEFT JOIN copied_annotations
-        ON source.id = copied_annotations.source_listing_change_id
+        ON source.annotation_id = copied_annotations.source_id
       LEFT JOIN copied_hash_annotations
-        ON source.id = copied_hash_annotations.source_listing_change_id
-      RETURNING id, source.id AS source_id
+        ON source.hash_annotation_id = copied_hash_annotations.source_id
+      RETURNING id, source_id
     )
     INSERT INTO listing_distributions (
       listing_change_id, geo_entity_id, is_party, created_at, updated_at
@@ -70,5 +72,5 @@ CREATE OR REPLACE FUNCTION copy_listing_changes_across_events(
     END;
   $$;
 
-COMMENT ON FUNCTION copy_listing_changes_across_events() IS
+COMMENT ON FUNCTION copy_listing_changes_across_events(from_event_id INTEGER, to_event_id INTEGER) IS
   'Procedure to copy listing changes across two events.'
