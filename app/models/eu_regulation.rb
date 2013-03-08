7158596e91493abc47872e59d205c8371c44d7f5
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-03-08 09:03:00 +0000

    fixed designation validation in eu regulation

--- app/models/eu_regulation.rb
+++ app/models/eu_regulation.rb
@@ -1,8 +1,8 @@
 class EuRegulation < Event
   attr_accessible :listing_changes_event_id
   attr_accessor :listing_changes_event_id
-  validates :designation_id, :presence => true, 
-   :inclusion => { :in => Designation.where(:name => 'EU').map(&:id)}
+  validates :designation_id, :presence => true
+  validate :designation_is_eu
   validates :effective_at, :presence => true
 
   def can_be_activated?
@@ -18,4 +18,13 @@ class EuRegulation < Event
     update_attribute(:is_current, true)
     notify_observers(:after_activate)
   end
+
+  protected
+    def designation_is_eu
+      eu = Designation.find_by_name('EU')
+      unless designation_id && eu && designation_id == eu.id
+        errors.add(:designation_id, 'should be EU')
+      end
+    end
+
 end
