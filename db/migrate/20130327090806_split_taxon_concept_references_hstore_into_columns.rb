1e50ca4f5842c2c2b8497ad53f58fe320576f148
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-04-03 09:11:36 +0100

    do not rebuild mviews in test environment

--- db/migrate/20130327090806_split_taxon_concept_references_hstore_into_columns.rb
+++ db/migrate/20130327090806_split_taxon_concept_references_hstore_into_columns.rb
@@ -3,14 +3,16 @@ class SplitTaxonConceptReferencesHstoreIntoColumns < ActiveRecord::Migration
     add_column :taxon_concept_references, :is_standard, :boolean
     add_column :taxon_concept_references, :is_cascaded, :boolean
     add_column :taxon_concept_references, :excluded_taxon_concepts_ids, 'INTEGER[]'
-    Sapi::disable_triggers
-    execute <<-SQL
-    UPDATE taxon_concept_references SET
-    is_standard = COALESCE((data->'usr_is_std_ref')::BOOLEAN, FALSE),
-    is_cascaded = COALESCE((data->'cascade')::BOOLEAN, TRUE),
-    excluded_taxon_concepts_ids = (data->'exclusions')::INT[]
-    SQL
-    Sapi::enable_triggers
+    unless Rails.env.test? do
+      Sapi::disable_triggers
+      execute <<-SQL
+      UPDATE taxon_concept_references SET
+      is_standard = COALESCE((data->'usr_is_std_ref')::BOOLEAN, FALSE),
+      is_cascaded = COALESCE((data->'cascade')::BOOLEAN, TRUE),
+      excluded_taxon_concepts_ids = (data->'exclusions')::INT[]
+      SQL
+      Sapi::enable_triggers
+    end
     change_column :taxon_concept_references, :is_standard, :boolean, :null => false, :default => false
     change_column :taxon_concept_references, :is_cascaded, :boolean, :null => false, :default => false
     remove_column :taxon_concept_references, :data
