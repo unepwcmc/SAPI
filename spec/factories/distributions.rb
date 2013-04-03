2a1955f48e91f2753059317aaf63b064caef54e1
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-04-03 12:41:26 +0100

    added nodes_and_descendants method to GeoEntity

--- spec/factories/distributions.rb
+++ spec/factories/distributions.rb
@@ -1,10 +1,20 @@
 FactoryGirl.define do
 
+  factory :geo_relationship_type do
+    sequence(:name) { |n| "CONTAINS#{n}" }
+  end
+
+  factory :geo_relationship do
+    geo_relationship_type
+    geo_entity
+    related_geo_entity
+  end
+
   factory :geo_entity_type do
     name 'Country'
   end
 
-  factory :geo_entity do
+  factory :geo_entity, :aliases => [:related_geo_entity] do
     geo_entity_type
     name 'Wonderland'
     sequence(:iso_code2) {|n| "#{n}X"}
