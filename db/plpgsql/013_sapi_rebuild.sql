6a12111d0978b65ec4874f6a18abdb4e46f289d7
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-04-08 13:54:58 +0100

    added cites / eu procedure to calculate ancestors listing

--- db/plpgsql/013_sapi_rebuild.sql
+++ db/plpgsql/013_sapi_rebuild.sql
@@ -22,7 +22,8 @@ CREATE OR REPLACE FUNCTION sapi_rebuild() RETURNS void
           PERFORM rebuild_descendant_cites_listing();
           PERFORM rebuild_descendant_eu_listing();
           --RAISE NOTICE 'ancestor listings';
-          PERFORM rebuild_ancestor_listings();
+          PERFORM rebuild_ancestor_cites_listing();
+          PERFORM rebuild_ancestor_eu_listing();
           PERFORM rebuild_cites_accepted_flags();
           PERFORM rebuild_cites_show_flags();
         END;
