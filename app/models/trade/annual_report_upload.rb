aabbbdbebc3567741db5a63079c3bf6bd3a2e615
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-05-13 15:45:02 +0100

    If Rails.env is not test or development scp the uploaded file to database server

--- app/models/trade/annual_report_upload.rb
+++ app/models/trade/annual_report_upload.rb
@@ -3,13 +3,31 @@ class Trade::AnnualReportUpload < ActiveRecord::Base
   def save_temp_file(uploaded_file)
     @original_filename = uploaded_file.original_filename
     @length = uploaded_file.tempfile.length
+    if ["development", "test"].include?(Rails.env)
+      save_in_tmp uploaded_file
+    else
+      copy_to_db_server uploaded_file
+    end
+    #Trade::AnnualReportCopyWorker.perform_async(self)
+    copy_to_sandbox
+  end
+
+  def save_in_tmp uploaded_file
     directory = "tmp/uploads"
     # create the file path
     @path = File.join(directory, @original_filename)
     # write the file
     File.open(@path, "wb") { |f| f.write(uploaded_file.read) }
-    #Trade::AnnualReportCopyWorker.perform_async(self)
-    copy_to_sandbox
+  end
+
+  def copy_to_db_server uploaded_file
+    require 'net/scp'
+    directory = '/home/rails/sapi/current/tmp/uploads/'
+    @path = File.join(directory, @original_filename)
+    destiny = Rails.configuration.database_configuration[Rails.env]["host"]
+    Net::SCP.start(destiny, 'rails') do |scp|
+      scp.upload! uploaded_file.tempfile.path, @path
+    end
   end
 
   def copy_to_sandbox
