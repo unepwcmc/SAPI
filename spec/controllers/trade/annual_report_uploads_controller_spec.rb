ba6e3bde2a645dc591c9885a460155dad4fc0c02
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-05-13 14:59:58 +0100

    fixed spec for annual report uploads

--- spec/controllers/trade/annual_report_uploads_controller_spec.rb
+++ spec/controllers/trade/annual_report_uploads_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe Trade::AnnualReportsController do
-  let(:trade_annual_report){ create(:trade_annual_report) }
+describe Trade::AnnualReportUploadsController do
+  let(:trade_annual_report_upload){ create(:trade_annual_report_upload) }
   describe "GET index" do
     it "should return success" do
       get :index, format: :json
@@ -11,7 +11,7 @@ describe Trade::AnnualReportsController do
 
   describe "GET show" do
     it "should return success" do
-      get :show, id: trade_annual_report.id, format: :json
+      get :show, id: trade_annual_report_upload.id, format: :json
       response.should be_success
     end
   end
--- spec/models/trade/annual_report_upload_spec.rb
+++ spec/models/trade/annual_report_upload_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe Trade::AnnualReport do
+describe Trade::AnnualReportUpload do
   pending "add some examples to (or delete) #{__FILE__}"
 end
