218f80a9dca4105b788b5d98f48f40cf476e6d7d
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-05-13 14:24:19 +0100

    store upload info fields

--- app/models/trade/annual_report_upload.rb
+++ app/models/trade/annual_report_upload.rb
@@ -1,25 +1,18 @@
 class Trade::AnnualReportUpload < ActiveRecord::Base
-  attr_reader :path, :original_filename, :length
-
-  # TODO store that
-  def number_of_rows
-    sandbox_shipments.size
-  end
+  TMP_DIR = 'tmp/uploads'
+  attr_reader :path
+  attr_accessible :original_filename, :number_of_rows
 
   def save_temp_file(uploaded_file)
     @original_filename = uploaded_file.original_filename
-    @length = uploaded_file.tempfile.length
-    directory = "tmp/uploads"
-    # create the file path
-    @path = File.join(directory, @original_filename)
-    # write the file
+    @path = File.join(TMP_DIR, @original_filename)
     File.open(@path, "wb") { |f| f.write(uploaded_file.read) }
-    #Trade::AnnualReportCopyWorker.perform_async(self)
     copy_to_sandbox
   end
 
   def copy_to_sandbox
     sandbox.copy
+    update_attributes({:original_filename => @original_filename, :number_of_rows => sandbox_shipments.size})
   end
 
   # object that represents the particular sandbox table linked to this annual
