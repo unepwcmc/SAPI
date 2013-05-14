e4b6b5c29613fa27de975c0ea368fd7113fe9848
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-05-14 20:44:32 +0100

    added specs for the uploads controller

--- spec/controllers/trade/annual_report_uploads_controller_spec.rb
+++ spec/controllers/trade/annual_report_uploads_controller_spec.rb
@@ -15,4 +15,23 @@ describe Trade::AnnualReportUploadsController do
       response.should be_success
     end
   end
+
+  describe "POST create" do
+    def correct_csv
+      test_document = File.join(Rails.root, 'spec', 'support', 'annual_report_upload_correct.csv')
+      Rack::Test::UploadedFile.new(test_document, "text/csv")
+    end
+    it "should return success in jQuery File Upload way" do
+      xhr :post, :create, :csv_source_file => correct_csv, :format => 'json'
+      parse_json(response.body, "files/0")['id'].should_not be_blank
+    end
+    def incorrect_csv
+      test_document = File.join(Rails.root, 'spec', 'support', 'annual_report_upload_incorrect.csv')
+      Rack::Test::UploadedFile.new(test_document, "text/csv")
+    end
+    it "should return error in jQuery File Upload way" do
+      xhr :post, :create, :csv_source_file => incorrect_csv, :format => 'json'
+      parse_json(response.body, "files/0")['id'].should be_blank
+    end
+  end
 end
