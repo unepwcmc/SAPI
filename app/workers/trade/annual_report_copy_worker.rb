8935f6aa1b3c7c8059a18840c85bd8bd04005bcf
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-05-14 20:44:09 +0100

    removed redundant file

--- app/workers/trade/annual_report_copy_worker.rb
+++
@@ -1,7 +0,0 @@
-class Trade::AnnualReportCopyWorker
-  include Sidekiq::Worker
-  sidekiq_options :queue => :trade, :backtrace => true
-  def perform(annual_report_upload)
-    annual_report_upload.copy_to_sandbox
-  end
-end
