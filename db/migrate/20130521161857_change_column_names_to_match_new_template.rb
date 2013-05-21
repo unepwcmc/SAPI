dad020647acc69f1e2c0a373fefbafbd1a8b74f8
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-05-21 19:57:00 +0100

    this should not have been commented out

--- db/migrate/20130521161857_change_column_names_to_match_new_template.rb
+++ db/migrate/20130521161857_change_column_names_to_match_new_template.rb
@@ -1,7 +1,7 @@
 class ChangeColumnNamesToMatchNewTemplate < ActiveRecord::Migration
   def change
     change_table(:trade_sandbox_template) do |t|
-      #t.rename :appendix_no, :appendix
+      t.rename :appendix_no, :appendix
       t.rename :taxon_check, :species_name
       t.rename :trading_partner_code, :trading_partner
       t.rename :origin_country_code, :country_of_origin
