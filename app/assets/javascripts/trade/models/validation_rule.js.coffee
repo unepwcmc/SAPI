a725a099f62a9bee22313093411f833e35084cba
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-05-21 20:40:33 +0100

    added fixed run order to validation rules

--- app/assets/javascripts/trade/models/validation_rule.js.coffee
+++ app/assets/javascripts/trade/models/validation_rule.js.coffee
@@ -1,5 +1,6 @@
 Trade.ValidationRule = DS.Model.extend
   type: DS.attr('string')
+  runOrder: DS.attr('number')
   columnNames: DS.attr('array')
   validValuesView: DS.attr('string')
   formatRe: DS.attr('string')
--- app/serializers/trade/validation_rule_serializer.rb
+++ app/serializers/trade/validation_rule_serializer.rb
@@ -1,3 +1,3 @@
 class Trade::ValidationRuleSerializer < ActiveModel::Serializer
-  attributes :id,  :type, :column_names, :created_at, :updated_at
+  attributes :id,  :type, :column_names, :run_order, :created_at, :updated_at
 end
