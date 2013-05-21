430412b210c8656d44a3f9a71a84000bea5bca20
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-05-21 08:41:55 +0100

    remove the limit on number of returned validations

--- app/controllers/trade/validation_rules_controller.rb
+++ app/controllers/trade/validation_rules_controller.rb
@@ -2,9 +2,6 @@ class Trade::ValidationRulesController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Trade::ValidationRule.limit(10)
-    #render :text => {:validation_rules => Trade::ValidationRule.all}.to_json
-    #render :text => {:validation_rules => [{:id => 2, :type => 'Trade::PresenceValidationRule', :column_names => [:appendix_no]}]}
-    #render :text => '{"validation_rules": [{"id": 2, "type": "Trade::PresenceValidationRule", "column_names": "{appendix_no}"}]}'
+    respond_with Trade::ValidationRule.all
   end
 end
