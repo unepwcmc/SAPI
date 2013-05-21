8f250d4cc58d8d76c94beb3c55a8ce1a4316d707
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-05-21 10:13:53 +0100

    fix for materialized view dependencies

--- db/views/valid_taxon_check_view.sql
+++ db/views/valid_taxon_check_view.sql
@@ -1,4 +1,5 @@
 DROP VIEW IF EXISTS valid_taxon_check_view;
 CREATE VIEW valid_taxon_check_view AS
-SELECT full_name AS taxon_check FROM taxon_concepts_mview
-WHERE taxonomy_is_cites_eu;
+SELECT full_name AS taxon_check FROM taxon_concepts
+JOIN taxonomies ON taxonomies.id = taxon_concepts.taxonomy_id
+WHERE taxonomies.name = 'CITES_EU';
