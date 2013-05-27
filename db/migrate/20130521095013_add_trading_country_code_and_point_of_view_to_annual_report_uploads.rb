f5058e7c2f3b546cb26dfcca66a5d7fae0317969
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2013-05-27 23:49:05 +0100

    another fix to this annoying migration

--- db/migrate/20130521095013_add_trading_country_code_and_point_of_view_to_annual_report_uploads.rb
+++ db/migrate/20130521095013_add_trading_country_code_and_point_of_view_to_annual_report_uploads.rb
@@ -4,12 +4,16 @@ class AddTradingCountryCodeAndPointOfViewToAnnualReportUploads < ActiveRecord::M
       add_column :trade_annual_report_uploads, :trading_country_id, :integer, :null => false
     else
       add_column :trade_annual_report_uploads, :trading_country_id, :integer
-      # set to first available country before enforcing a not null constraint
-      Trade::AnnualReportUpload.update_all(
-        :trading_country_id =>
-        GeoEntity.joins(:geo_entity_type).
-          where(:"geo_entity_types.name" => GeoEntityType::COUNTRY).first.id
-      )
+      any_country = GeoEntity.joins(:geo_entity_type).
+          where(:"geo_entity_types.name" => GeoEntityType::COUNTRY).first
+      unless any_country
+        Trade::AnnualReportUpload.delete_all
+      else
+        # set to first available country before enforcing a not null constraint
+        Trade::AnnualReportUpload.update_all(
+          :trading_country_id => any_country.id
+        )
+      end
       execute 'ALTER TABLE trade_annual_report_uploads ALTER trading_country_id SET NOT NULL'
     end
     add_column :trade_annual_report_uploads, :point_of_view, :string,
