a28cbae8411f9814631a365bf428a91eba116de4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2013-06-09 22:28:11 +0100

    moved autocomplete query to a prefix matcher object

--- spec/controllers/checklist/taxon_concepts_controller_spec.rb
+++ spec/controllers/checklist/taxon_concepts_controller_spec.rb
@@ -7,15 +7,6 @@ describe Checklist::TaxonConceptsController do
         xhr :get, :autocomplete, :format => 'json',
           :scientific_name => 'Arctocephalus townsendi'
         response.body.should have_json_size(1)
-        parse_json(response.body, "0/full_name").should == 'Arctocephalus townsendi' 
-      end
-    end
-    context "when searching by common name" do
-      it "returns 1 result" do
-        xhr :get, :autocomplete, :format => 'json',
-          :scientific_name => 'Guadalupe Fur Seal'
-        response.body.should have_json_size(1)
-        parse_json(response.body, "0/full_name").should == 'Arctocephalus townsendi' 
       end
     end
   end
