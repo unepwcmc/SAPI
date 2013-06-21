60eca4707915f87d1b1250eff40c428f75163071
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-06-21 11:57:11 +0100

    merged migrations

--- db/migrate/20130513100925_add_is_done_to_trade_annual_report_uploads.rb
+++
@@ -1,5 +0,0 @@
-class AddIsDoneToTradeAnnualReportUploads < ActiveRecord::Migration
-  def change
-    add_column :trade_annual_report_uploads, :is_done, :boolean, :default => false
-  end
-end
--- db/migrate/20130524094413_add_id_pkey_to_trade_sandbox_template.rb
+++
@@ -1,7 +0,0 @@
-class AddIdPkeyToTradeSandboxTemplate < ActiveRecord::Migration
-  def change
-    execute <<-SQL
-      ALTER TABLE trade_sandbox_template ADD COLUMN id BIGSERIAL PRIMARY KEY;
-    SQL
-  end
-end
