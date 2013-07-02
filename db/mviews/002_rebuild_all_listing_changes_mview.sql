1d335a379fcb5053e2ff4858684febced786848e
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-07-02 10:28:48 +0100

    trying my luck at fixing the build

--- db/mviews/002_rebuild_all_listing_changes_mview.sql
+++ db/mviews/002_rebuild_all_listing_changes_mview.sql
@@ -1,7 +1,7 @@
-CREATE OR REPLACE FUNCTION higher_or_equal_ranks_names(in_rank_name character varying)
+CREATE OR REPLACE FUNCTION higher_or_equal_ranks_names(in_rank_name VARCHAR(255))
   RETURNS TEXT[]
-AS
-$BODY$
+  LANGUAGE sql IMMUTABLE
+  AS $$
     WITH ranks_in_order(row_no, rank_name) AS (
       SELECT ROW_NUMBER() OVER(), *
       FROM UNNEST(ARRAY[
@@ -11,8 +11,7 @@ $BODY$
     )
     SELECT ARRAY_AGG(rank_name) FROM ranks_in_order
     WHERE row_no >= (SELECT row_no FROM ranks_in_order WHERE rank_name = in_rank_name);
-  $BODY$
-  LANGUAGE sql IMMUTABLE;
+  $$;
 
 CREATE OR REPLACE FUNCTION rebuild_all_listing_changes_mview() RETURNS void
   LANGUAGE plpgsql
