bd2cb7908b0ea13ac06b47b010745c24faf7bc28
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-07-02 08:24:05 +0100

    install cron jobs to rebuild the materialized views

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,15 @@
-every :day, :at => '4:30am' do
-  rake "downloads:cache:rotate"
+every :day, :at => '2:42am' do
+  rake "db:migrate:rebuild_taxon_concepts_mview"
 end
 
-every :day, :at => '5:30am' do
+every :day, :at => '3:42am' do
+  rake "db:migrate:rebuild_taxon_concepts_mview"
+end
+
+every :day, :at => '4:42am' do
   rake "downloads:cache:update"
+end
+
+every :day, :at => '5:42am' do
+  rake "downloads:cache:rotate"
 end
