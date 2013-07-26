760140092ec37649183754d7966e6e411d73caa7
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-07-26 12:15:03 +0100

    Missing attribute for subspecies_info

--- app/serializers/species/eu_listing_change_serializer.rb
+++ app/serializers/species/eu_listing_change_serializer.rb
@@ -1,7 +1,7 @@
 class Species::EuListingChangeSerializer < ActiveModel::Serializer
   attributes :is_current, :species_listing_name, :party_full_name,
    :effective_at_formatted, :short_note_en, :full_note_en,
-   :event_name, :hash_full_note_en, :hash_display
+   :event_name, :hash_full_note_en, :hash_display, :subspecies_info
 
   def hash_display
     return "" unless object.hash_ann_parent_symbol.present?
