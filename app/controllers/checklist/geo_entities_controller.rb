ee44960a1edbb00fab15bdc1d002fc13b31c1951
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-08-07 18:05:06 +0100

    allow to filter geo entities by more than one type

--- app/controllers/checklist/geo_entities_controller.rb
+++ app/controllers/checklist/geo_entities_controller.rb
@@ -6,19 +6,25 @@ class Checklist::GeoEntitiesController < ApplicationController
 
   #override the serializer by using render :text, old ember-data won't handle json root
   def index
-    geo_entity_type = (
+    geo_entity_types = (
       GeoEntityType.dict &
-      [params[:geo_entity_type] && params[:geo_entity_type].upcase]
-    ).first
+      (
+        if params[:geo_entity_types] 
+          params[:geo_entity_types].map(&:upcase)
+        else
+          [params[:geo_entity_type] && params[:geo_entity_type].upcase]
+        end
+      )
+    )
     designation = (
       Designation.dict &
       [params[:designation] && params[:designation].upcase]
     ).first
     @geo_entities = GeoEntity.current.order(:name_en)
-    if geo_entity_type
+    if geo_entity_types
       @geo_entities = @geo_entities.
         joins(:geo_entity_type).
-        where(:"geo_entity_types.name" => geo_entity_type)
+        where(:"geo_entity_types.name" => geo_entity_types)
     end
     if designation
       @geo_entities = @geo_entities.
