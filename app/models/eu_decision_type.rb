a5534f3831a3bf69ccb3102c30a7012238a674a9
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-08-08 14:28:02 +0100

    Updated database structure to have eu_decision_types

---
+++ app/models/eu_decision_type.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: eu_decision_types
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  tooltip    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class EuDecisionType < ActiveRecord::Base
+  attr_accessible :name, :tooltip
+end
--- app/models/eu_suspension.rb
+++ app/models/eu_suspension.rb
@@ -2,21 +2,21 @@
 #
 # Table name: eu_decisions
 #
-#  id               :integer          not null, primary key
-#  is_current       :boolean
-#  notes            :text
-#  internal_notes   :text
-#  taxon_concept_id :integer
-#  geo_entity_id    :integer
-#  start_date       :datetime
-#  start_event_id   :integer
-#  end_date         :datetime
-#  end_event_id     :integer
-#  type             :string(255)
-#  restriction      :string(255)
-#  conditions_apply :boolean
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id                  :integer          not null, primary key
+#  is_current          :boolean
+#  notes               :text
+#  internal_notes      :text
+#  taxon_concept_id    :integer
+#  geo_entity_id       :integer
+#  start_date          :datetime
+#  start_event_id      :integer
+#  end_date            :datetime
+#  end_event_id        :integer
+#  type                :string(255)
+#  conditions_apply    :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  eu_decision_type_id :integer
 #
 
 class EuSuspension < EuDecision
