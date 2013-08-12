4983d8817aa1f4f571eee9f51036d30edf5a6274
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-08-12 14:03:57 +0100

    EuDecisionConfirmation now linked to eu_decisions instead of eu_decision_parts, which no longer exist

--- app/models/eu_decision_confirmation.rb
+++ app/models/eu_decision_confirmation.rb
@@ -2,13 +2,13 @@
 #
 # Table name: eu_decision_confirmations
 #
-#  id                  :integer          not null, primary key
-#  eu_decision_part_id :integer
-#  event_id            :integer
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
+#  id             :integer          not null, primary key
+#  eu_decision_id :integer
+#  event_id       :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
 #
 
 class EuDecisionConfirmation < ActiveRecord::Base
-  attr_accessible :eu_decision_part_id, :event_id
+  attr_accessible :eu_decision_id, :event_id
 end
--- spec/models/eu_decision_confirmation_spec.rb
+++ spec/models/eu_decision_confirmation_spec.rb
@@ -2,11 +2,11 @@
 #
 # Table name: eu_decision_confirmations
 #
-#  id                  :integer          not null, primary key
-#  eu_decision_part_id :integer
-#  event_id            :integer
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
+#  id             :integer          not null, primary key
+#  eu_decision_id :integer
+#  event_id       :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
 #
 
 require 'spec_helper'
