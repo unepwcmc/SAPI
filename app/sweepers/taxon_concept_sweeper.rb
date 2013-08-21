06d8b20ae3aeb6285e31ffb3813223f2e28cea76
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-08-21 11:09:47 +0100

    Removes action caching from show. Leaving just the Serializer cache for the show page

--- app/sweepers/taxon_concept_sweeper.rb
+++ app/sweepers/taxon_concept_sweeper.rb
@@ -13,15 +13,10 @@ class TaxonConceptSweeper < ActionController::Caching::Sweeper
     expire_cache(tc)
   end
 
-  def after_touch(tc)
-    expire_cache(tc)
-  end
-
   private
 
   def expire_cache(tc)
     expire_action(:controller => "checklist/taxon_concepts", :action => "index")
     expire_action(:controller => "api/v1/taxon_concepts", :action => "index")
-    expire_action(:controller => "api/v1/taxon_concepts", :action => "show")
   end
 end
