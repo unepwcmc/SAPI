a9664737d8a958509c529362422f1e5fe0d2d681
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-09-02 14:33:21 +0100

    cascade subfamily listings

--- db/mviews/002_rebuild_all_listing_changes_mview.sql
+++ db/mviews/002_rebuild_all_listing_changes_mview.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE FUNCTION higher_or_equal_ranks_names(in_rank_name VARCHAR(255)
     WITH ranks_in_order(row_no, rank_name) AS (
       SELECT ROW_NUMBER() OVER(), *
       FROM UNNEST(ARRAY[
-      'VARIETY', 'SUBSPECIES', 'SPECIES', 'GENUS',
+      'VARIETY', 'SUBSPECIES', 'SPECIES', 'GENUS', 'SUBFAMILY',
       'FAMILY', 'ORDER', 'CLASS', 'PHYLUM', 'KINGDOM'
       ])
     )
