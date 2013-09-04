1389b84c3202a9f97dacf7d624a4d8604064daed
Decio Battaglia <decio.battaglia@gmail.com>
2013-09-04 14:06:20 +0100

    terms static page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   layout 'pages'
+  
   def about
-    
+  end
+
+  def terms_of_use
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SAPI::Application.routes.draw do
 
   match 'about' => 'pages#about'
+  match 'terms-of-use' => 'pages#terms_of_use'
 
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
