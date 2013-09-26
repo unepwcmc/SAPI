b4c013635f3377eccb3d2902a96549f84680ed4c
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2013-09-26 21:16:48 +0100

    added failing spec

---
+++ spec/models/taxon_concept/dalbergia_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe TaxonConcept do
+  context "Dalbergia" do
+    include_context "Dalbergia"
+
+    context "LISTING" do
+      describe :cites_listing do
+        context 'for species Dalbergia abbreviata' do
+          specify { @species1.cites_listing.should == 'NC' }
+        end
+        context 'for species Dalbergia abrahamii' do
+          specify { @species2.cites_listing.should == 'II' }
+        end
+      end
+
+      describe :cites_listed do
+        context "for species Dalbergia abbreviata" do
+          specify { @species1.cites_listed.should be_nil }
+        end
+        context "for species Dalbergia abrahamii" do
+          specify { @species2.cites_listed.should == false }
+        end
+      end
+    end
+  end
+end
