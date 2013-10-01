e86c2ba886a139aa0f9f10e3ea522f5c2788ad50
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-10-01 13:16:31 +0100

    [Fixes #57747626] inclusion superceded by deletion should be considered not current (Notomys aquilo)

---
+++ spec/shared/notomys_aquilo.rb
@@ -0,0 +1,64 @@
+shared_context "Notomys aquilo" do
+  before(:all) do
+    @order = create_cites_eu_order(
+      :taxon_name => create(:taxon_name, :scientific_name => 'Rodentia'),
+      :parent => cites_eu_mammalia
+    )
+    @family = create_cites_eu_family(
+      :taxon_name => create(:taxon_name, :scientific_name => 'Muridae'),
+      :parent => @order
+    )
+    @genus = create_cites_eu_genus(
+      :taxon_name => create(:taxon_name, :scientific_name => 'Notomys'),
+      :parent => @family
+    )
+    @species = create_cites_eu_species(
+      :taxon_name => create(:taxon_name, :scientific_name => 'aquilo'),
+      :parent => @genus,
+      :name_status => 'A'
+    )
+
+    create_cites_I_addition(
+     :taxon_concept => @species,
+     :effective_at => '1975-07-01'
+    )
+    create_eu_A_addition(
+     :taxon_concept => @species,
+     :effective_at => '1975-07-01'
+    )
+
+    create_cites_II_addition(
+     :taxon_concept => @species,
+     :effective_at => '1979-06-28',
+     :inclusion_taxon_concept_id => @genus.id,
+     :is_current => true
+    )
+    create_eu_B_addition(
+     :taxon_concept => @species,
+     :effective_at => '1979-06-28',
+     :inclusion_taxon_concept_id => @genus.id,
+     :is_current => true
+    )
+
+    cites_del = create_cites_II_deletion(
+      :taxon_concept => @genus,
+      :effective_at => '1987-10-22',
+      :is_current => true
+    )
+    eu_del = create_eu_B_deletion(
+      :taxon_concept => @genus,
+      :effective_at => '1987-10-22',
+      :is_current => true
+    )
+
+    cms_designation
+    Sapi::rebuild(:except => [:taxonomy])
+    self.instance_variables.each do |t|
+      var = self.instance_variable_get(t)
+      if var.kind_of? TaxonConcept
+        self.instance_variable_set(t,MTaxonConcept.find(var.id))
+        self.instance_variable_get(t).reload
+      end
+    end
+  end
+end
