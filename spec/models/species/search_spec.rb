1f5cf2d63becf16cfdae2e9831075265ed651f31
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-10-02 13:47:43 +0100

    [Fixes #58067058]fix for SQL injection issue in Species+ search

---
+++ spec/models/species/search_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+describe Species::Search do
+  include_context "Canis lupus"
+  describe :results do
+    context "when searching by scientific name" do
+      context "when regular query" do
+        subject { Species::Search.new({:taxon_concept_query => 'canis'}).results }
+        specify { subject.should include(@species)}
+      end
+      context "when malicious query" do
+        subject { Species::Search.new({:taxon_concept_query => 'canis\''}).results }
+        specify { subject.should be_empty}
+      end
+      context "when leading whitespace" do
+        subject { Species::Search.new({:taxon_concept_query => ' canis'}).results }
+        specify { subject.should include(@species)}
+      end      
+      context "when trailing whitespace" do
+        subject { Species::Search.new({:taxon_concept_query => 'canis '}).results }
+        specify { subject.should include(@species)}
+      end 
+    end
+  end
+end
