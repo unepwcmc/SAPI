0db288b15226ec6512b7c1a41051e63ab38bf14f
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-10-04 10:48:38 +0100

    [#58227244] Allow associating CITES Suspensions with Territories

--- app/controllers/admin/cites_suspensions_controller.rb
+++ app/controllers/admin/cites_suspensions_controller.rb
@@ -38,7 +38,9 @@ class Admin::CitesSuspensionsController < Admin::SimpleCrudController
     @sources = Source.order(:code)
     @purposes = Purpose.order(:code)
     @geo_entities = GeoEntity.order(:name_en).joins(:geo_entity_type).
-      where(:is_current => true, :geo_entity_types => {:name => 'COUNTRY'})
+      where(:is_current => true,
+            :geo_entity_types => {:name => [GeoEntityType::COUNTRY,
+                                            GeoEntityType::TERRITORY]})
     @suspension_notifications = CitesSuspensionNotification.
       select([:id, :name]).
       order('effective_at DESC')
