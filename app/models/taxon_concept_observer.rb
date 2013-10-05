0c45f139c9c1405a6c84a064cd47192e2bc1b609
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2013-10-05 23:12:34 +0100

    added before create action to taxon concept observer

--- app/models/taxon_concept_observer.rb
+++ app/models/taxon_concept_observer.rb
@@ -1,5 +1,31 @@
 class TaxonConceptObserver < ActiveRecord::Observer
 
+  #initializes data and full name with values from parent
+  def before_create(taxon_concept)
+    if taxon_concept.rank && taxon_concept.parent
+      rank_name = taxon_concept.rank.name
+      parent_full_name = taxon_concept.parent.full_name
+      name = taxon_concept.taxon_name && taxon_concept.taxon_name.scientific_name
+      taxon_concept.full_name = if [Rank::SPECIES, Rank::SUBSPECIES].include? rank_name
+         "#{parent_full_name} #{name.downcase}"
+      elsif rank_name == Rank::VARIETY
+        "#{parent_full_name} var. #{name.downcase}"
+      else
+        name
+      end
+      taxon_concept.data = taxon_concept.parent.data.slice(
+        :kingdom_id, :kingdom_name, :phylum_id, :phylum_name, :class_id,
+        :class_name, :order_id, :order_name, :family_id, :family_name,
+        :subfamily_id, :subfamily_name, :genus_id, :genus_name, :species_id,
+        :species_name
+      ).merge({
+        :rank_name => rank_name
+      })
+    else
+      taxon_concept.full_name = taxon_concept.taxon_name && taxon_concept.taxon_name.scientific_name
+    end
+  end
+
   def after_create(taxon_concept)
     ensure_species_touched(taxon_concept)
     Species::Search.increment_cache_iterator
