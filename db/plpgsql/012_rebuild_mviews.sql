43cfa6a4f532b23687abec4f33b92b879ec4257b
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2013-10-05 22:39:40 +0100

    removed listing changes mview triggers

--- db/plpgsql/012_rebuild_mviews.sql
+++ db/plpgsql/012_rebuild_mviews.sql
@@ -24,30 +24,3 @@ BEGIN
   RETURN;
 END
 $$;
-
-CREATE OR REPLACE FUNCTION listing_changes_refresh_row(row_id INTEGER) RETURNS VOID
-SECURITY DEFINER
-LANGUAGE 'plpgsql' AS $$
-BEGIN
-  DELETE
-  FROM listing_changes_mview lc
-  WHERE lc.id = row_id;
-
-  INSERT INTO listing_changes_mview
-  SELECT *, FALSE, NULL
-  FROM listing_changes_view lc
-  WHERE lc.id = row_id;
-END
-$$;
-
-DROP FUNCTION IF EXISTS listing_changes_invalidate_row(id INTEGER);
-CREATE OR REPLACE FUNCTION listing_changes_invalidate_row(row_id INTEGER) RETURNS VOID
-SECURITY DEFINER
-LANGUAGE 'plpgsql' AS $$
-BEGIN
-  UPDATE listing_changes_mview lc
-  SET dirty = TRUE
-  WHERE lc.id = row_id;
-  RETURN;
-END
-$$;
