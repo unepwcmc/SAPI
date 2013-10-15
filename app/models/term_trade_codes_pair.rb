f852e0d553e93e5e0cf4688ecb95e4e4f94a4a02
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-10-15 14:50:39 +0100

    [#58694332] Renamed other_trade_code to trade_code

--- app/models/term_trade_codes_pair.rb
+++ app/models/term_trade_codes_pair.rb
@@ -2,14 +2,14 @@
 #
 # Table name: term_trade_codes_pairs
 #
-#  id                    :integer          not null, primary key
-#  term_id               :integer
-#  other_trade_code_id   :integer
-#  other_trade_code_type :string(255)
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
+#  id              :integer          not null, primary key
+#  term_id         :integer
+#  trade_code_id   :integer
+#  trade_code_type :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 
 class TermTradeCodesPair < ActiveRecord::Base
-  attr_accessible :other_trade_code_id, :other_trade_code_type, :term_id
+  attr_accessible :trade_code_id, :trade_code_type, :term_id
 end
--- spec/models/term_trade_codes_pair_spec.rb
+++ spec/models/term_trade_codes_pair_spec.rb
@@ -2,12 +2,12 @@
 #
 # Table name: term_trade_codes_pairs
 #
-#  id                    :integer          not null, primary key
-#  term_id               :integer
-#  other_trade_code_id   :integer
-#  other_trade_code_type :string(255)
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
+#  id              :integer          not null, primary key
+#  term_id         :integer
+#  trade_code_id   :integer
+#  trade_code_type :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 
 require 'spec_helper'
