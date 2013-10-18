2af18e7454c3895a75c1ef21901ea7362c11ea03
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-10-18 13:11:03 +0100

    [#58694182] Improved performance and removed synonyms query to validate taxon_concept's species name and term code

--- db/views/valid_taxon_concept_term_view.sql
+++ db/views/valid_taxon_concept_term_view.sql
@@ -1,20 +1,21 @@
 DROP VIEW IF EXISTS valid_taxon_concept_term_view;
 CREATE VIEW valid_taxon_concept_term_view AS
-WITH RECURSIVE self_and_descendants(original_id, id, species_name, term_code) AS (
-  SELECT taxon_concepts.id AS original_id, taxon_concepts.id, full_name, terms.code FROM taxon_concepts
-  INNER JOIN trade_taxon_concept_code_pairs 
-    ON trade_taxon_concept_code_pairs.taxon_concept_id = taxon_concepts.id
-  INNER JOIN trade_codes AS terms
-    ON terms.id = trade_taxon_concept_code_pairs.trade_code_id
-    AND trade_taxon_concept_code_pairs.trade_code_type = 'Term'
+WITH RECURSIVE self_and_descendants(id, species_name, pair_id) AS (
+  SELECT taxon_concepts.id, full_name, trade_taxon_concept_code_pairs.id 
+    FROM trade_taxon_concept_code_pairs
+    INNER JOIN taxon_concepts
+      ON trade_taxon_concept_code_pairs.taxon_concept_id = taxon_concepts.id
+    WHERE name_status = 'A'
 
   UNION
 
-  SELECT d.original_id, hi.id, hi.full_name, terms.code FROM taxon_concepts hi
-  JOIN self_and_descendants d ON d.id = hi.parent_id
-  JOIN trade_taxon_concept_code_pairs td ON td.taxon_concept_id =  d.original_id
-  JOIN trade_codes AS terms
-    ON terms.id = td.trade_code_id
-    AND td.trade_code_type = 'Term'
+  SELECT hi.id, hi.full_name, d.pair_id FROM taxon_concepts hi
+    JOIN self_and_descendants d ON d.id = hi.parent_id
+    WHERE name_status = 'A'
 )
-SELECT species_name, term_code FROM self_and_descendants
+SELECT species_name, terms.code AS term_code FROM self_and_descendants
+INNER JOIN trade_taxon_concept_code_pairs
+  ON trade_taxon_concept_code_pairs.id = self_and_descendants.pair_id
+INNER JOIN trade_codes AS terms
+  ON terms.id = trade_taxon_concept_code_pairs.trade_code_id
+  AND trade_taxon_concept_code_pairs.trade_code_type = 'Term'
