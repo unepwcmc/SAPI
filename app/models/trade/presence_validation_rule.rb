21de6cd1f81cc4226ffb8a5feae38f896c5f611c
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-10-22 12:59:01 +0100

    fixed presence validation to check for empty string as well

--- app/models/trade/presence_validation_rule.rb
+++ app/models/trade/presence_validation_rule.rb
@@ -27,7 +27,7 @@ class Trade::PresenceValidationRule < Trade::ValidationRule
   # using AND.
   def matching_records(table_name)
     s = Arel::Table.new(table_name)
-    arel_nodes = column_names.map{|c| s[c].eq(nil)}
+    arel_nodes = column_names.map{|c| s[c].eq(nil).or(s[c].eq(''))}
     conditions = arel_nodes.shift
     arel_nodes.each{ |n| conditions = conditions.and(n) }
     Trade::SandboxTemplate.select('*').from(table_name).where(conditions)
