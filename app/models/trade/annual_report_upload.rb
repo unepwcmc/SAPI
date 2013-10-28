187dc3aff81ee29440df24184c9beeb748db0a0e
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-10-28 10:13:09 +0100

    [#59317114] Disable csv file validation on update

--- app/models/trade/annual_report_upload.rb
+++ app/models/trade/annual_report_upload.rb
@@ -20,7 +20,7 @@ class Trade::AnnualReportUpload < ActiveRecord::Base
   attr_accessible :csv_source_file, :trading_country_id, :point_of_view
   mount_uploader :csv_source_file, Trade::CsvSourceFileUploader
   belongs_to :trading_country, :class_name => GeoEntity, :foreign_key => :trading_country_id
-  validates :csv_source_file, :csv_column_headers => true
+  validates :csv_source_file, :csv_column_headers => true, :on => :create
 
   def copy_to_sandbox
     sandbox.copy
