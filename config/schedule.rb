ca565d3daa19dbd447066baec60b6daa05491079
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-11-05 13:30:06 +0000

    [#59491252 #60153966] redirect cron output to log file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+set :output, 'log/cron.log'
+
 every :day, :at => '2:42am' do
   rake "db:migrate:rebuild"
 end
@@ -7,7 +9,7 @@ every :day, :at => '4:42am' do
 end
 
 every :day, :at => '5:12am' do
-  rake "downloads:cache:update_species_downloads"	
+  rake "downloads:cache:update_species_downloads"
 end
 
 every :day, :at => '5:42am' do
