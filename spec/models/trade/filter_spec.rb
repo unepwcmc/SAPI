145fc8cfb4882936757624717ec05071249a1900
migtorres <miguel.torres@unep-wcmc.org>
2013-11-12 18:17:59 +0000

    added more rspec tests

--- spec/models/trade/filter_spec.rb
+++ spec/models/trade/filter_spec.rb
@@ -11,6 +11,21 @@ describe Trade::Filter do
         :taxon_name => create(:taxon_name, :scientific_name => 'totalus'),
         :parent => create_cites_eu_genus(:taxon_name => create(:taxon_name, :scientific_name => 'Nullificus'))
       )
+
+      @argentina = create(:geo_entity,
+        :geo_entity_type => create(:geo_entity_type, :name => 'COUNTRY'),
+        :name => 'Argentina',
+        :iso_code2 => 'AR'
+      )
+
+      @portugal = create(:geo_entity,
+        :geo_entity_type => create(:geo_entity_type, :name => 'COUNTRY'),
+        :name => 'Portugal',
+        :iso_code2 => 'PT'
+      )
+
+
+
       @term = create(:term, :code => 'CAV')
       @unit = create(:unit, :code => 'KIL')
       @purpose = create(:purpose, :code => 'T')
@@ -26,9 +41,12 @@ describe Trade::Filter do
         :source => @source,
         :term => @term,
         :unit => @unit,
+        :importer => @argentina,
+        :exporter => @portugal,
+        :country_of_origin => @argentina,
         :year => 2012,
         :import_permit => @import_permit,
-        :export_permits=> [@export_permit1, @export_permit2]
+        :export_permits=> [@export_permit1, @export_permit2],
       )
       @shipment2 = create(
         :shipment,
@@ -38,6 +56,9 @@ describe Trade::Filter do
         :source => @source,
         :term => @term,
         :unit => @unit,
+        :importer => @portugal,
+        :exporter => @argentina,
+        :country_of_origin => @portugal,
         :year => 2013
       )
     end
@@ -51,27 +72,87 @@ describe Trade::Filter do
       specify { subject.should include(@shipment1) }
       specify { subject.length.should == 1 }
     end
-    context "when searching by year" do
-      context "when time range specified" do
-        subject { Trade::Filter.new({:time_range_start => 2013, :time_range_end => 2015}).results }
-        specify { subject.should include(@shipment2) }
-        specify { subject.length.should == 1 }
-      end
-      context "when time range specified incorrectly" do
-        subject { Trade::Filter.new({:time_range_start => 2013, :time_range_end => 2012}).results }
-        specify { subject.length.should == 0 }
-      end
-      context "when time range start specified" do
-        subject { Trade::Filter.new({:time_range_start => 2012}).results }
-        specify { subject.should include(@shipment1) }
-        specify { subject.length.should == 2 }
-      end
-      context "when time range end specified" do
-        subject { Trade::Filter.new({:time_range_end => 2012}).results }
-        specify { subject.should include(@shipment1) }
-        specify { subject.length.should == 1 }
-      end
+
+    context "when searching for terms_ids" do
+      subject { Trade::Filter.new({:terms_ids => [@term.id]}).results }
+      specify { subject.should include(@shipment1) }
+      specify { subject.length.should == 2 }
     end
+
+    
+    context "when searching for units_ids" do
+      subject { Trade::Filter.new({:units_ids => [@unit.id]}).results }
+      specify { subject.should include(@shipment1) }
+      specify { subject.length.should == 2 }
+    end
+
+    
+    context "when searching for purposes_ids" do
+      subject { Trade::Filter.new({:purposes_ids => [@purpose.id]}).results }
+      specify { subject.should include(@shipment1) }
+      specify { subject.length.should == 2 }
+    end
+
+    
+    context "when searching for sources_ids" do
+      subject { Trade::Filter.new({:sources_ids => [@source.id]}).results }
+      specify { subject.should include(@shipment1) }
+      specify { subject.length.should == 2 }
+    end
+
+    
+    context "when searching for importers_ids" do
+      subject { Trade::Filter.new({:importers_ids => [@argentina.id]}).results }
+      specify { subject.should include(@shipment1) }
+      specify { subject.length.should == 1 }
+    end
+
+    context "when searching for exporters_ids" do
+      subject { Trade::Filter.new({:exporters_ids => [@argentina.id]}).results }
+      specify { subject.should include(@shipment1) }
+      specify { subject.length.should == 1 }
+    end
+
+  context "when searching for country_of_origin_ids" do
+      subject { Trade::Filter.new({:country_of_origin_ids => [@argentina.id]}).results }
+      specify { subject.should include(@shipment2) }
+      specify { subject.length.should == 1 }
+    end
+
+
+
+
+
+  context "when searching by year" do
+    context "when time range specified" do
+      subject { Trade::Filter.new({:time_range_start => 2013, :time_range_end => 2015}).results }
+      specify { subject.should include(@shipment2) }
+      specify { subject.length.should == 1 }
+    end
+    context "when time range specified incorrectly" do
+      subject { Trade::Filter.new({:time_range_start => 2013, :time_range_end => 2012}).results }
+      specify { subject.length.should == 0 }
+    end
+    context "when time range start specified" do
+      subject { Trade::Filter.new({:time_range_start => 2012}).results }
+      specify { subject.should include(@shipment1) }
+      specify { subject.length.should == 2 }
+    end
+    context "when time range end specified" do
+      subject { Trade::Filter.new({:time_range_end => 2012}).results }
+      specify { subject.should include(@shipment1) }
+      specify { subject.length.should == 1 }
+    end
+  end
+
+  context "when searching by reporter_type" do
+    context "when reporter type is not I or E" do
+      subject { Trade::Filter.new(['K']).results }
+      specify { subject.length.should == 0 }
+    end
+  end
+
+
     context "when searching by permit" do
       subject { Trade::Filter.new({:permits_ids => [@export_permit1.id]}).results }
       specify { subject.should include(@shipment1) }
