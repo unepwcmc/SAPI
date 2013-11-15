f8ed418a2fa53c20b34f187d93631f2488ae59c6
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-11-15 15:04:21 +0000

    fixed search response fro array params

--- app/models/trade/search_params.rb
+++ app/models/trade/search_params.rb
@@ -9,8 +9,6 @@ class Trade::SearchParams < Hash
         params[:taxon_concepts_ids].blank? ? [] : params[:taxon_concepts_ids].sort,
       :appendices =>
         params[:appendices].blank? ? [] : params[:appendices].sort,
-      :time_range_start => params[:time_range_start].to_i,
-      :time_range_end => params[:time_range_end].to_i,
       :terms_ids => params[:terms_ids].blank? ? [] : params[:terms_ids].sort,
       :units_ids => params[:units_ids].blank? ? [] : params[:units_ids].sort,
       :purposes_ids =>
@@ -23,14 +21,44 @@ class Trade::SearchParams < Hash
         params[:exporters_ids].blank? ? [] : params[:exporters_ids].sort,
       :countries_of_origin_ids=>
         params[:countries_of_origin_ids].blank? ? [] : params[:countries_of_origin_ids].sort,
-      :reporter_type => params[:reporter_type].blank? ? nil : params[:reporter_type].upcase,
       :permits_ids => params[:permits_ids].blank? ? [] : params[:permits_ids].sort,
+      :reporter_type => params[:reporter_type].blank? ? nil : params[:reporter_type].upcase,
+      :time_range_start => params[:time_range_start],
+      :time_range_end => params[:time_range_end],
       :quantity => params[:quantity],
       :page => params[:page] && params[:page].to_i > 0 ? params[:page].to_i : 1,
       :per_page => params[:per_page] && params[:per_page].to_i > 0 ? params[:per_page].to_i : 25
     }
     unless ['I', 'E'].include? sanitized_params[:reporter_type]
       sanitized_params[:reporter_type] = nil
+    end
+    # make sure quantity is numeric
+    begin
+      sanitized_params[:quantity] = Float(sanitized_params[:quantity])
+    rescue ArgumentError, TypeError
+      sanitized_params[:quantity] = nil
+    end
+    [:time_range_start, :time_range_end].each do |integer_param|
+      # make sure is integer
+      begin
+        sanitized_params[:integer_param] = Integer(sanitized_params[:integer_param])
+      rescue ArgumentError, TypeError
+        sanitized_params[:integer_param] = nil
+      end
+    end
+    [
+      :taxon_concepts_ids, :terms_ids, :units_ids, :purposes_ids,
+      :sources_ids, :importers_ids, :exporters_ids, :countries_of_origin_ids,
+      :permits_ids
+    ].each do |integer_array_param|
+      # make sure is array
+      sanitized_params[integer_array_param] = [] unless sanitized_params[integer_array_param].is_a?(Array)
+      # make sure elements are integers
+      begin
+        !sanitized_params[integer_array_param].empty? && Integer(sanitized_params[integer_array_param][0])
+      rescue ArgumentError, TypeError
+        sanitized_params[integer_array_param] = []
+      end
     end
     super(sanitized_params)
     self.merge!(sanitized_params)
