6d2681ee922b6bb8392197a42ccbde2d38b9ec26
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-11-21 14:21:14 +0000

    define func if someone is running migrations from scratch

--- db/migrate/20131119133659_remove_reported_values_columns.rb
+++ db/migrate/20131119133659_remove_reported_values_columns.rb
@@ -1,5 +1,24 @@
 class RemoveReportedValuesColumns < ActiveRecord::Migration
   def change
+    #redefine drop sandboxes in case someone is migrating from scratch
+    execute "
+    CREATE OR REPLACE FUNCTION drop_trade_sandboxes() RETURNS void
+      LANGUAGE plpgsql
+      AS $$
+      DECLARE
+        current_table_name TEXT;
+      BEGIN
+        FOR current_table_name IN SELECT table_name FROM information_schema.tables
+        WHERE table_name LIKE 'trade_sandbox%'
+          AND table_name != 'trade_sandbox_template'
+          AND table_type != 'VIEW'
+        LOOP
+          EXECUTE 'DROP TABLE ' || current_table_name || ' CASCADE';
+        END LOOP;
+        RETURN;
+      END;
+      $$;
+    "
     execute "select * from drop_trade_sandboxes()"
     Trade::AnnualReportUpload.where(:is_done => false).each do |aru|
       aru.destroy
