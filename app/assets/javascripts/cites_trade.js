abcb04cbb4617320339fae569f21c31f63b5023a
deciob <decio.battaglia@gmail.com>
2013-11-25 14:43:43 +0000

    [#61376720] citestrade added js dependency and set paths in css for images

--- app/assets/javascripts/cites_trade.js
+++ app/assets/javascripts/cites_trade.js
@@ -1,9 +1,12 @@
+//= require underscore
+
 //= require cites_trade/jquery.jgrowl_minimized
 //= require cites_trade/select2
 //= require cites_trade/jquery.qtip.min
 //= require cites_trade/jquery.tipTip.minified
 //= require cites_trade/chosen.jquery.min
 //= require cites_trade/jquery.colorize-2.0.0
+//= require cites_trade/jquery.noty
 
 //= require cites_trade/application
 
