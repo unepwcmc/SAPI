b038e9f7a31eda49c12f3b7fb985bdd84dc792bf
deciob <decio.battaglia@gmail.com>
2013-11-25 12:37:24 +0000

    [#61376720] Basic page with main client side original functionality and style

---
+++ app/assets/javascripts/cites_trade.js
@@ -0,0 +1,10 @@
+//= require cites_trade/jquery.jgrowl_minimized
+//= require cites_trade/select2
+//= require cites_trade/jquery.qtip.min
+//= require cites_trade/jquery.tipTip.minified
+//= require cites_trade/chosen.jquery.min
+//= require cites_trade/jquery.colorize-2.0.0
+
+//= require cites_trade/application
+
+//= require_self
