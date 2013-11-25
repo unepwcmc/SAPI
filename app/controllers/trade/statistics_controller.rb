f0f2f1c815738f07830d0b83fdc008757269ff38
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-11-25 12:21:11 +0000

    [#61231354] trade statistics view

--- app/controllers/trade/statistics_controller.rb
+++ app/controllers/trade/statistics_controller.rb
@@ -1,8 +1,9 @@
 class Trade::StatisticsController < ApplicationController
-  layout 'trade'
+  layout 'admin'
 
   def index
-
+    @years = (1975..Date.today.year).to_a.reverse
+    @total_shipments = Trade::Shipment.count
   end
 
 end
