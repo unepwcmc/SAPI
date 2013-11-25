af9c796b5d9a7bba4970596f1a80c499139c1a6f
migtorres <miguel.torres@unep-wcmc.org>
2013-11-25 14:02:56 +0000

    added table 1 stats to controller

--- app/controllers/trade/statistics_controller.rb
+++ app/controllers/trade/statistics_controller.rb
@@ -4,6 +4,10 @@ class Trade::StatisticsController < ApplicationController
   def index
     @years = (1975..Date.today.year).to_a.reverse
     @total_shipments = Trade::Shipment.count
+    @last_updated = Trade::Shipment.maximum(:updated_at).year
+    @shipments_uploaded_2013 = Trade::Shipment.where(:created_at => '2013-01-01 00:00:00'..'2013-12-31 23:59:59').count
+    @shipments_amended_2013 = Trade::Shipment.where(:updated_at => '2013-01-01 00:00:00'..'2013-12-31 23:59:59').count
+    @taxon_concepts_in_trade = Trade::Shipment.count(:taxon_concept_id, :distinct => true)
   end
 
 end
