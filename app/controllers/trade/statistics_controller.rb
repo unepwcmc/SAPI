9931a55a06e76d03cf43ef05f9d6d59a15a193b6
migtorres <miguel.torres@unep-wcmc.org>
2013-11-26 12:05:02 +0000

    added form to choose dates

--- app/controllers/trade/statistics_controller.rb
+++ app/controllers/trade/statistics_controller.rb
@@ -2,11 +2,13 @@ class Trade::StatisticsController < ApplicationController
   layout 'admin'
 
   def index
+  	@start_date = params[:stats_start_date] || Date.today.strftime("01/01/%Y")
+  	@end_date = params[:stats_end_date] || Date.today.strftime("%d/%m/%Y")
     @years = (1975..Date.today.year).to_a.reverse
     @total_shipments = Trade::Shipment.count
-    @last_updated = Trade::Shipment.maximum(:updated_at).year
-    @shipments_uploaded_2013 = Trade::Shipment.where(:created_at => '2013-01-01 00:00:00'..'2013-12-31 23:59:59').count
-    @shipments_amended_2013 = Trade::Shipment.where(:updated_at => '2013-01-01 00:00:00'..'2013-12-31 23:59:59').count
+    @last_updated = Trade::Shipment.maximum(:updated_at)
+    @shipments_uploaded = Trade::Shipment.where(:created_at => params[:stats_start_date]..params[:stats_end_date]).count
+    @shipments_amended = Trade::Shipment.where(:updated_at => params[:stats_start_date]..params[:stats_end_date]).count
     @taxon_concepts_in_trade = Trade::Shipment.count(:taxon_concept_id, :distinct => true)
   end
 
