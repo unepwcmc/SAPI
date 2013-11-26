c86460863b99248ca456a45fd4a96816438758f9
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-11-26 15:42:21 +0000

    small simplifications

--- app/controllers/trade/statistics_controller.rb
+++ app/controllers/trade/statistics_controller.rb
@@ -6,9 +6,9 @@ class Trade::StatisticsController < ApplicationController
   	@end_date = params[:stats_end_date] || Date.today.strftime("%d/%m/%Y")
     @years = (1975..Date.today.year).to_a.reverse
     @total_shipments = Trade::Shipment.count
-    @last_updated = Trade::Shipment.maximum(:updated_at)
-    @shipments_uploaded = Trade::Shipment.where(:created_at => params[:stats_start_date]..params[:stats_end_date]).count
-    @shipments_amended = Trade::Shipment.where(:updated_at => params[:stats_start_date]..params[:stats_end_date]).count
+    @last_updated = Trade::Shipment.maximum(:updated_at).strftime("%d/%m/%Y %H:%M")
+    @shipments_uploaded = Trade::Shipment.where(:created_at => @start_date..@end_date).count
+    @shipments_amended = Trade::Shipment.where(:updated_at => @start_date..@end_date).count
     @taxon_concepts_in_trade = Trade::Shipment.count(:taxon_concept_id, :distinct => true)
   end
 
