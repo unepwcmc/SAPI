5865193811a9eef53818e983aeb8a8e393b7f1a2
deciob <decio.battaglia@gmail.com>
2013-11-27 15:33:27 +0000

    resetting compress js in staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,7 +12,7 @@ SAPI::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
@@ -47,7 +47,7 @@ SAPI::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin.js admin.css trade.js trade.css species.js species.css
-                                pages.css cites_trade.js cites_trade.css)
+                                pages.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
