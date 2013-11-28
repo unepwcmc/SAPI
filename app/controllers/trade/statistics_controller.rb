7d1f74ad216f818c20b666110191daff528afdd8
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-11-28 14:38:19 +0000

    [Fixes #61231354] only include shipments with created_at != updated_at in amended shipments count

--- app/controllers/trade/statistics_controller.rb
+++ app/controllers/trade/statistics_controller.rb
@@ -9,7 +9,7 @@ class Trade::StatisticsController < ApplicationController
     @total_shipments = Trade::Shipment.count
     @last_updated = Trade::Shipment.maximum(:updated_at).strftime("%d/%m/%Y %H:%M")
     @shipments_uploaded = Trade::Shipment.where(:created_at => @start_date..@end_date).count
-    @shipments_amended = Trade::Shipment.where(:updated_at => @start_date..@end_date).count
+    @shipments_amended = Trade::Shipment.where('created_at != updated_at').where(:updated_at => @start_date..@end_date).count
     @taxon_concepts_in_trade = Trade::Shipment.count(:taxon_concept_id, :distinct => true)
     @transactions = Statistics.get_total_transactions_per_year
   end
