d290e916a5ab1bbb4cd1877d685595c7bdeae03d
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-11-29 10:38:24 +0000

    Removed option to edit or add new listing changes from EU Regulation list of listing changes

--- app/controllers/admin/listing_changes_controller.rb
+++ app/controllers/admin/listing_changes_controller.rb
@@ -1,49 +1,8 @@
 class Admin::ListingChangesController < Admin::SimpleCrudController
-  before_filter :load_lib_objects, :only => [:new, :edit]
   belongs_to :eu_regulation
 
-  def create
-    create! do |success, failure|
-      success.html {
-        redirect_to admin_listing_changes_url,
-        :notice => 'Operation successful'
-      }
-      failure.html {
-        load_lib_objects
-        render 'new'
-      }
-    end
-  end
-
-  def update
-    update! do |success, failure|
-      success.html {
-        redirect_to admin_listing_changes_url,
-        :notice => 'Operation successful'
-      }
-      failure.html {
-        load_lib_objects
-        render 'edit'
-      }
-    end
-  end
 
   protected
-
-  def load_lib_objects
-    @units = Unit.order(:code)
-    @terms = Term.order(:code)
-    @sources = Source.order(:code)
-    @purposes = Purpose.order(:code)
-    @geo_entities = GeoEntity.order(:name_en).joins(:geo_entity_type).
-      where(:is_current => true,
-            :geo_entity_types => {:name => [GeoEntityType::COUNTRY,
-                                            GeoEntityType::TERRITORY]})
-    @suspension_notifications = CitesSuspensionNotification.
-      select([:id, :name]).
-      order('effective_at DESC')
-  end
-
   def collection
     @listing_changes ||= end_of_association_chain.
       includes([
--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ SAPI::Application.routes.draw do
     resources :events
     resources :eu_regulations do
       post :activate, :on => :member
-      resources :listing_changes, :only => [:index, :new]
+      resources :listing_changes, :only => [:index, :destroy]
     end
     resources :listing_changes, :only => [:create, :update, :destroy]
     resources :cites_cops
