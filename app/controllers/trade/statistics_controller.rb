33a698cbe1e9a63b805aa2d53a8a8d74777e2bb7
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-11-29 11:22:16 +0000

    [Fixes #61231354] convert timestamps to dates for comparison

--- app/controllers/trade/statistics_controller.rb
+++ app/controllers/trade/statistics_controller.rb
@@ -8,8 +8,13 @@ class Trade::StatisticsController < ApplicationController
     @years = (1975..Date.today.year).to_a.reverse
     @total_shipments = Trade::Shipment.count
     @last_updated = Trade::Shipment.maximum(:updated_at).strftime("%d/%m/%Y %H:%M")
-    @shipments_uploaded = Trade::Shipment.where(:created_at => @start_date..@end_date).count
-    @shipments_amended = Trade::Shipment.where('created_at != updated_at').where(:updated_at => @start_date..@end_date).count
+    @shipments_uploaded = Trade::Shipment.
+      where("created_at::DATE BETWEEN ? AND ?", @start_date, @end_date).
+      count
+    @shipments_amended = Trade::Shipment.
+      where("created_at::DATE BETWEEN ? AND ?", @start_date, @end_date).
+      where('created_at != updated_at').
+      count
     @taxon_concepts_in_trade = Trade::Shipment.count(:taxon_concept_id, :distinct => true)
     @transactions = Statistics.get_total_transactions_per_year
   end
