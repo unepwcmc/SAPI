6d3b75dcdd5baab6972e4748189f644f2f831fb7
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-12-01 11:13:27 +0000

    Fixes bug with adding new eu_suspension_regulation. Was using a non existing attr_accessor.

--- app/models/eu_suspension_regulation.rb
+++ app/models/eu_suspension_regulation.rb
@@ -19,5 +19,8 @@
 #
 
 class EuSuspensionRegulation < Event
+  attr_accessible :eu_suspensions_event_id
+  attr_accessor :eu_suspensions_event_id
+
   has_many :eu_suspensions, :foreign_key => :start_event_id
 end
