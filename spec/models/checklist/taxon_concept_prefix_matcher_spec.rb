a6b6b112e8f8669ee17c639b7295a4fb77b8dbe1
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-12-11 17:29:01 +0000

    adapted old checklist matcher spec for species matcher and it fails

--- spec/models/checklist/taxon_concept_prefix_matcher_spec.rb
+++ spec/models/checklist/taxon_concept_prefix_matcher_spec.rb
@@ -1,29 +1,29 @@
 require 'spec_helper'
 
-describe Checklist::TaxonConceptPrefixMatcher do
+describe Species::TaxonConceptPrefixMatcher do
   include_context "Caiman latirostris"
   describe :taxon_concepts do
     context "when query in capital letters" do
       subject{
-        Checklist::TaxonConceptPrefixMatcher.new({:scientific_name => 'CAI'})
+        Species::TaxonConceptPrefixMatcher.new({:taxon_concept_query => 'CAI', :from_checklist => true})
       }
       specify{ subject.results.size.should == 2 }
     end
     context "when match on accepted name" do
       subject{
-        Checklist::TaxonConceptPrefixMatcher.new({:scientific_name => 'cai'})
+        Species::TaxonConceptPrefixMatcher.new({:taxon_concept_query => 'cai', :from_checklist => true})
       }
       specify{ subject.results.size.should == 2 }
     end
     context "when match on synonym" do
       subject{
-        Checklist::TaxonConceptPrefixMatcher.new({:scientific_name => 'alligator'})
+        Species::TaxonConceptPrefixMatcher.new({:taxon_concept_query => 'alligator', :from_checklist => true})
       }
       specify{ subject.results.size.should == 2 }
     end
     context "when match on common name" do
       subject{
-        Checklist::TaxonConceptPrefixMatcher.new({:scientific_name => 'broad'})
+        Species::TaxonConceptPrefixMatcher.new({:taxon_concept_query => 'broad', :from_checklist => true})
       }
       specify{ subject.results.size.should == 1 }
     end
