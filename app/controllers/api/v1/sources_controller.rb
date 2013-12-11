ca5cc1783520e7debf8dd79b3bb307bc0f8044d2
deciob <decio.battaglia@gmail.com>
2013-12-11 10:09:47 +0000

    [#62134992] - removing forgot debugger

--- app/controllers/api/v1/sources_controller.rb
+++ app/controllers/api/v1/sources_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::SourcesController < ApplicationController
   caches_action :index
   def index
     locale = params['locale'] || I18n.locale
-    debugger
     @sources = Source.all(:order => "name_#{locale}")
     render :json => @sources,
       :each_serializer => Species::SourceSerializer,
