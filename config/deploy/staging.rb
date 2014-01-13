dd51306e58128a9b26c0860d967185a03ef38b47
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-01-13 16:35:15 +0000

    Fixed identation

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -20,54 +20,54 @@ set :default_environment, {
 
 desc "Configure VHost"
 task :config_vhost do
-vhost_config =<<-EOF
-server {
-  listen 80;
-  client_max_body_size 4G;
-  server_name #{application}.unepwcmc-013.vm.brightbox.net #{application}.sw02.matx.info;
-  keepalive_timeout 5;
-  root #{deploy_to}/current/public;
-  passenger_enabled on;
-  rails_env staging;
+  vhost_config =<<-EOF
+    server {
+      listen 80;
+      client_max_body_size 4G;
+      server_name #{application}.unepwcmc-013.vm.brightbox.net #{application}.sw02.matx.info;
+      keepalive_timeout 5;
+      root #{deploy_to}/current/public;
+      passenger_enabled on;
+      rails_env staging;
 
-  add_header 'Access-Control-Allow-Origin' *;
-  add_header 'Access-Control-Allow-Methods' "GET, POST, PUT, DELETE, OPTIONS";
-  add_header 'Access-Control-Allow-Headers' "X-Requested-With, X-Prototype-Version";
-  add_header 'Access-Control-Max-Age' 1728000;
+      add_header 'Access-Control-Allow-Origin' *;
+      add_header 'Access-Control-Allow-Methods' "GET, POST, PUT, DELETE, OPTIONS";
+      add_header 'Access-Control-Allow-Headers' "X-Requested-With, X-Prototype-Version";
+      add_header 'Access-Control-Max-Age' 1728000;
 
-  # Enable serving files through nginx
-  passenger_set_cgi_param HTTP_X_ACCEL_MAPPING /home/rails/sapi/shared/public/downloads/=/downloads/;
-  passenger_pass_header X-Accel-Redirect;
+      # Enable serving files through nginx
+      passenger_set_cgi_param HTTP_X_ACCEL_MAPPING /home/rails/sapi/shared/public/downloads/=/downloads/;
+      passenger_pass_header X-Accel-Redirect;
 
-  location ~ ^/downloads/(.*)$ {
-    alias /home/rails/sapi/shared/public/downloads/$1;
-    internal;
-  }
+      location ~ ^/downloads/(.*)$ {
+        alias /home/rails/sapi/shared/public/downloads/$1;
+        internal;
+      }
 
-  gzip on;
-  location ^~ /assets/ {
-    expires max;
-    add_header Cache-Control public;
-  }
+      gzip on;
+      location ^~ /assets/ {
+        expires max;
+        add_header Cache-Control public;
+      }
 
-  if (-f $document_root/system/maintenance.html) {
-    return 503;
-  }
+      if (-f $document_root/system/maintenance.html) {
+        return 503;
+      }
 
-  error_page 500 502 504 /500.html;
-  location = /500.html {
-    root #{deploy_to}/public;
-  }
+      error_page 500 502 504 /500.html;
+      location = /500.html {
+        root #{deploy_to}/public;
+      }
 
-  error_page 503 @maintenance;
-  location @maintenance {
-    rewrite  ^(.*)$  /system/maintenance.html break;
-  }
-}
-EOF
-put vhost_config, "/tmp/vhost_config"
-sudo "mv /tmp/vhost_config /etc/nginx/sites-available/#{application}"
-sudo "ln -s /etc/nginx/sites-available/#{application} /etc/nginx/sites-enabled/#{application}"
+      error_page 503 @maintenance;
+      location @maintenance {
+        rewrite  ^(.*)$  /system/maintenance.html break;
+      }
+    }
+  EOF
+  put vhost_config, "/tmp/vhost_config"
+  sudo "mv /tmp/vhost_config /etc/nginx/sites-available/#{application}"
+  sudo "ln -s /etc/nginx/sites-available/#{application} /etc/nginx/sites-enabled/#{application}"
 end
  
 after "deploy:setup", :config_vhost
