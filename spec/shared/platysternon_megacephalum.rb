8bf36cc64e81006b168b97f6104b36f10f0f56fd
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-01-15 14:46:08 +0000

    [#57749648] spec for Platysternon megacephalum

---
+++ spec/shared/platysternon_megacephalum.rb
@@ -0,0 +1,71 @@
+#Encoding: utf-8
+shared_context 'Platysternon megacephalum' do
+  let(:reg2012){
+    create(:eu_regulation, :name => 'No 1158/2012', :designation => eu)
+  }
+  let(:reg2013){
+    create(:eu_regulation, :name => 'No 750/2013', :designation => eu)
+  }
+  before(:all) do
+    @klass = cites_eu_reptilia
+    @order = create_cites_eu_order(
+      :taxon_name => create(:taxon_name, :scientific_name => 'Testudines'),
+      :parent => @klass
+    )
+    @family = create_cites_eu_family(
+      :taxon_name => create(:taxon_name, :scientific_name => 'Platysternidae'),
+      :parent => @order
+    )
+    @genus = create_cites_eu_genus(
+      :taxon_name => create(:taxon_name, :scientific_name => 'Platysternon'),
+      :parent => @family
+    )
+    @species = create_cites_eu_species(
+      :taxon_name => create(:taxon_name, :scientific_name => 'megacephalum'),
+      :parent => @genus
+    )
+
+    create_cites_II_addition(
+     :taxon_concept => @species,
+     :effective_at => '2003-02-13',
+     :is_current => false
+    )
+
+    create_eu_B_addition(
+     :taxon_concept => @species,
+     :effective_at => '2012-12-15',
+     :event => reg2012,
+     :is_current => false
+    )
+
+    create_cites_I_addition(
+     :taxon_concept => @family,
+     :effective_at => '2013-06-12',
+     :is_current => true
+    )
+
+    create_eu_A_addition(
+     :taxon_concept => @family,
+     :effective_at => '2013-08-10',
+     :event => reg2013,
+     :is_current => true
+    )
+
+    create_cites_I_addition(
+     :taxon_concept => @species,
+     :effective_at => '2013-06-12',
+     :inclusion_taxon_concept_id => @family.id,
+     :is_current => true
+    )
+
+    cms_designation
+    Sapi.rebuild
+    self.instance_variables.each do |t|
+      var = self.instance_variable_get(t)
+      if var.kind_of? TaxonConcept
+        self.instance_variable_set(t,MTaxonConcept.find(var.id))
+        self.instance_variable_get(t).reload
+      end
+    end
+  end
+end
