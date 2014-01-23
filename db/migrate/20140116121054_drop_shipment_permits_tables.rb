abf53fab652b8c014918e5b0b58b86af9dad378d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-01-23 23:12:54 +0100

    drop view first in migration

--- db/migrate/20140116121054_drop_shipment_permits_tables.rb
+++ db/migrate/20140116121054_drop_shipment_permits_tables.rb
@@ -1,5 +1,6 @@
 class DropShipmentPermitsTables < ActiveRecord::Migration
   def change
+  	execute 'DROP VIEW IF EXISTS trade_shipments_view'
   	execute 'DROP TABLE IF EXISTS trade_shipment_import_permits'
   	execute 'DROP TABLE IF EXISTS trade_shipment_export_permits'
   	execute 'DROP TABLE IF EXISTS trade_shipment_origin_permits'
