6a00717c03f8f887d80dd08ec844ecd996233bab
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-02-11 12:17:44 +0000

    Set the index to be actually unique

--- db/migrate/20140210121403_add_unique_index_to_trade_permits.rb
+++ db/migrate/20140210121403_add_unique_index_to_trade_permits.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToTradePermits < ActiveRecord::Migration
   def change
-    add_index "trade_permits", :number, :name => "trade_permits_number_idx"
+    add_index "trade_permits", :number, :name => "trade_permits_number_idx", :unique => true
   end
 end
