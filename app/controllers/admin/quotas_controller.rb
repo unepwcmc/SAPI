93b076136c609e6f6c524543b730c707b3a83a47
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-02-13 15:48:17 +0000

    [#62053520] Added taxon_concepts and exception taxon_concepts to the duplication form

--- app/controllers/admin/quotas_controller.rb
+++ app/controllers/admin/quotas_controller.rb
@@ -8,19 +8,32 @@ class Admin::QuotasController < Admin::SimpleCrudController
     @geo_entities = GeoEntity.joins(:quotas).order(:name_en).uniq
   end
 
+  def duplicate
+    debugger
+    true
+  end
+
   def count
     @count = Quota.where([
         "EXTRACT(year from start_date) = :year
-        #{ if params[:excluded_geo_entities].present?
+        #{ if params[:excluded_geo_entities_ids].present?
            "AND geo_entity_id NOT IN (:excluded_geo_entities)"
           end}
-        #{ if params[:included_geo_entities].present?
+        #{ if params[:included_geo_entities_ids].present?
            "AND geo_entity_id IN (:included_geo_entities)"
+          end}
+        #{ if params[:excluded_taxon_concepts_ids].present?
+           "AND taxon_concept_id NOT IN (:excluded_taxon_concepts)"
+          end}
+        #{ if params[:included_taxon_concepts_ids].present?
+           "AND taxon_concept_id IN (:included_taxon_concepts)"
           end}
         ",
         :year => params[:year],
-        :excluded_geo_entities => params[:excluded_geo_entities],
-        :included_geo_entities => params[:included_geo_entities]]).
+        :excluded_geo_entities => params[:excluded_geo_entities_ids],
+        :included_geo_entities => params[:included_geo_entities_ids],
+        :excluded_taxon_concepts => params[:excluded_taxon_concepts_ids],
+        :included_taxon_concepts => params[:included_taxon_concepts_ids]]).
         count
     render :json => @count.to_json
   end
