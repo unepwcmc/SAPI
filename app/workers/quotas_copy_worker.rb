5e531886f378722830dec6a7315affb393fe82e8
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-02-14 10:51:50 +0000

    [#62053520] Fixed plpgsql query to use CTE in the update and insert call. Fixed encoding that was breaking rails migrations. Tidy up quotas_copy_worker and sanitizing params

--- app/workers/quotas_copy_worker.rb
+++ app/workers/quotas_copy_worker.rb
@@ -1,27 +1,29 @@
 class QuotasCopyWorker
   include Sidekiq::Worker
   def perform(options)
-    ActiveRecord::Base.connection.execute <<-SQL
-      SELECT * FROM copy_quotas_across_years(
-        #{options["from_year"].to_i},
-        '#{options["start_date"]}'::DATE,
-        '#{options["end_date"]}'::DATE,
-        '#{options["publication_date"]}'::DATE,
-        ARRAY[#{options["excluded_taxon_concepts_ids"]}]::integer[],
-        ARRAY[#{options["included_taxon_concepts_ids"]}]::integer[],
-        #{if options["excluded_geo_entities_ids"]
-            "ARRAY#{options["excluded_geo_entities_ids"].map(&:to_i)}"
-          else
-            "ARRAY[]::integer[]"
-          end
-        },
-        #{if options["included_geo_entities_ids"]
-            "ARRAY#{options["included_geo_entities_ids"].map(&:to_i)}"
-          else
-            "ARRAY[]::integer[]"
-          end
-        }
-      )
-    SQL
+    sql = <<-SQL
+        SELECT * FROM copy_quotas_across_years(
+          :from_year,
+          :start_date::DATE,
+          :end_date::DATE,
+          :publication_date::DATE,
+          ARRAY[:excluded_taxon_concepts_ids]::integer[],
+          ARRAY[:included_taxon_concepts_ids]::integer[],
+          ARRAY[:excluded_geo_entities_ids]::integer[],
+          ARRAY[:included_geo_entities_ids]::integer[]
+        )
+      SQL
+    ActiveRecord::Base.connection.execute(
+      ActiveRecord::Base.send(:sanitize_sql_array, [sql,
+        :from_year => options["from_year"], :start_date => options["start_date"],
+        :end_date =>options["end_date"],
+        :publication_date => options["publication_date"],
+        :excluded_taxon_concepts_ids => options["excluded_taxon_concepts_ids"].presence,
+        :included_taxon_concepts_ids => options["included_taxon_concepts_ids"].presence,
+        :excluded_geo_entities_ids => (options["excluded_geo_entities_ids"] && 
+                                       options["excluded_geo_entities_ids"].join(",")),
+        :included_geo_entities_ids => (options["included_geo_entities_ids"] &&
+                                       options["included_geo_entities_ids"].join(","))
+    ]))
   end
 end
--- db/plpgsql/013_copy_quotas_across_years.sql
+++ db/plpgsql/013_copy_quotas_across_years.sql
@@ -1,8 +1,8 @@
-﻿CREATE OR REPLACE FUNCTION copy_quotas_across_years(
+CREATE OR REPLACE FUNCTION copy_quotas_across_years(
   from_year INTEGER, new_start_date DATE, new_end_date DATE, new_publication_date DATE,
   excluded_taxon_concepts_ids INTEGER[], included_taxon_concepts_ids INTEGER[],
-   excluded_geo_entities_ids INTEGER[], included_geo_entities_ids INTEGER[])
-RETURNS void
+   excluded_geo_entities_ids INTEGER[], included_geo_entities_ids INTEGER[]
+  ) RETURNS VOID
   LANGUAGE plpgsql
   AS $$
 DECLARE
@@ -11,10 +11,8 @@ DECLARE
    included_geo_entities INTEGER[];
    excluded_geo_entities INTEGER[];
    cites_taxonomy_id INTEGER;
-   sql TEXT;
    updated_rows INTEGER;
 BEGIN
-
     SELECT id into cites_taxonomy_id FROM taxonomies WHERE name = 'CITES_EU';
 
     -- fetch included_taxon_concepts
@@ -68,33 +66,31 @@ BEGIN
         AND geo_relationship_types.name = 'CONTAINS'
     ) AS matches;
 
+    WITH original_current_quotas AS (
+      SELECT *
+      FROM trade_restrictions
+      WHERE type = 'Quota' AND EXTRACT(year FROM start_date) =  from_year AND is_current = true
+      AND (ARRAY_LENGTH(excluded_taxon_concepts, 1) IS NULL OR NOT excluded_taxon_concepts @> ARRAY[taxon_concept_id])
+      AND (ARRAY_LENGTH(included_taxon_concepts, 1) IS NULL OR included_taxon_concepts @> ARRAY[taxon_concept_id])	
+      AND (ARRAY_LENGTH(excluded_geo_entities, 1) IS NULL OR NOT excluded_geo_entities @> ARRAY[geo_entity_id])
+      AND (ARRAY_LENGTH(included_geo_entities, 1) IS NULL OR included_geo_entities  @> ARRAY[geo_entity_id])
+    ), updated_quotas AS (
+      UPDATE trade_restrictions
+      SET is_current = false
+      FROM original_current_quotas
+      WHERE trade_restrictions.id = original_current_quotas.id
+    )
     INSERT INTO trade_restrictions(type, is_current, start_date, end_date, geo_entity_id, quota,
     publication_date, notes, unit_id, taxon_concept_id, public_display, url, created_at, updated_at,
     excluded_taxon_concepts_ids)
     SELECT 'Quota', is_current, new_start_date, new_end_date, geo_entity_id, quota,
-      new_publication_date, notes, unit_id, taxon_concept_id, public_display, url, current_date,
-      current_date, trade_restrictions.excluded_taxon_concepts_ids
-    FROM trade_restrictions
-    WHERE type = 'Quota' AND EXTRACT(year FROM start_date) =  from_year AND is_current = true
-    AND (ARRAY_LENGTH(excluded_taxon_concepts, 1) IS NULL OR NOT excluded_taxon_concepts @> ARRAY[taxon_concept_id])
-    AND (ARRAY_LENGTH(included_taxon_concepts, 1) IS NULL OR included_taxon_concepts @> ARRAY[taxon_concept_id])	
-    AND (ARRAY_LENGTH(excluded_geo_entities, 1) IS NULL OR NOT excluded_geo_entities @> ARRAY[geo_entity_id])
-    AND (ARRAY_LENGTH(included_geo_entities, 1) IS NULL OR included_geo_entities  @> ARRAY[geo_entity_id]);
-    -- still need to set the old quotas as not current
+    new_publication_date, notes, unit_id, taxon_concept_id, public_display, url, current_date,
+    current_date, trade_restrictions.excluded_taxon_concepts_ids
+    FROM original_current_quotas AS trade_restrictions;
+
 
     GET DIAGNOSTICS updated_rows = ROW_COUNT;
-    RAISE INFO '[%] Inserted % quotas', 'trade_transactions', updated_rows;
-
-    UPDATE trade_restrictions
-    SET is_current = false
-    WHERE type = 'Quota' AND EXTRACT(year FROM start_date) =  from_year AND is_current = true
-    AND (ARRAY_LENGTH(excluded_taxon_concepts, 1) IS NULL OR NOT excluded_taxon_concepts @> ARRAY[taxon_concept_id])
-    AND (ARRAY_LENGTH(included_taxon_concepts, 1) IS NULL OR included_taxon_concepts @> ARRAY[taxon_concept_id])	
-    AND (ARRAY_LENGTH(excluded_geo_entities, 1) IS NULL OR NOT excluded_geo_entities @> ARRAY[geo_entity_id])
-    AND (ARRAY_LENGTH(included_geo_entities, 1) IS NULL OR included_geo_entities  @> ARRAY[geo_entity_id]);
-
-    GET DIAGNOSTICS updated_rows = ROW_COUNT;
-    RAISE INFO '[%] Set is_current to false for % quotas', 'trade_transactions', updated_rows;
+    RAISE INFO '[%] Copied % quotas', 'trade_transactions', updated_rows;
   END;
 $$;
 
@@ -103,4 +99,4 @@ COMMENT ON FUNCTION copy_quotas_across_years(
   new_publication_date DATE, excluded_taxon_concepts_ids INTEGER[],
   included_taxon_concepts_ids INTEGER[], excluded_geo_entities_ids INTEGER[],
   included_geo_entities_ids INTEGER[]) IS
-  'Procedure to copy quotas across two years with some filtering parameters.'
+  'Procedure to copy quotas across two years with some filtering parameters.';
