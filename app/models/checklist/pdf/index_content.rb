91e7638cd3c563e575e4ac94b0f16933fcd9dda9
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-02-14 11:31:17 +0000

    [#65555870] translated the non-hash annotations

--- app/models/checklist/pdf/index_content.rb
+++ app/models/checklist/pdf/index_content.rb
@@ -13,10 +13,11 @@ module Checklist::Pdf::IndexContent
 
   def annotations_key
     tex = "\\parindent 0in"
-    tex << "\\cpart{Annotations key}\n"
+    tex << "\\cpart{#{LatexToPdf.escape_latex(I18n.t('pdf.annotations_key'))}}\n"
     cites = Designation.find_by_name(Designation::CITES)
+    full_note_field = "full_note_#{I18n.locale}"
     taxa_with_index_annotations = MTaxonConcept.
-      select("taxon_concepts_mview.*, annotations.symbol, annotations.full_note_en").
+      select("taxon_concepts_mview.*, annotations.symbol, annotations.#{full_note_field}").
       joins( <<-SQL
         JOIN listing_changes
         ON listing_changes.taxon_concept_id = taxon_concepts_mview.id
@@ -33,11 +34,11 @@ module Checklist::Pdf::IndexContent
       ).
       order("annotations.symbol::INT")
 
-    tex << "\\section*{Annotations not preceded by \"\\#\"}\n"
+    tex << "\\section*{#{LatexToPdf.escape_latex(I18n.t('pdf.non_hash_annotations'))}}\n"
     taxa_with_index_annotations.each do |tc|
         box_colour = (tc.kingdom_name == 'Animalia' ? 'orange' : 'green')
         tex << "\\cfbox{#{box_colour}}{\\superscript{#{tc['symbol']}} \\textbf{#{taxon_name_at_rank(tc)}}}\n\n"
-        tex << "#{LatexToPdf.html2latex(tc['full_note_en'])}\n\n"
+        tex << "#{LatexToPdf.html2latex(tc[full_note_field])}\n\n"
       end
     tex << "\\parindent -0.1in"
     tex
