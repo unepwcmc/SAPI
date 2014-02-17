46b187b46520bfcda23f76fff206bb1630ee2830
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-02-17 15:43:36 +0000

    [#65555822] localised short_note and full_note

--- app/serializers/checklist/index_listing_change_serializer.rb
+++ app/serializers/checklist/index_listing_change_serializer.rb
@@ -2,6 +2,6 @@ class Checklist::IndexListingChangeSerializer < ActiveModel::Serializer
   attributes :id,
     :species_listing_name, :party_iso_code, :party_full_name,
     :change_type_name, :effective_at_formatted, :is_current,
-    :hash_ann_symbol, :hash_full_note_en, :auto_note,
-    :full_note_en, :short_note_en, :short_note_es, :short_note_fr
+    :hash_ann_symbol, :hash_full_note, :auto_note,
+    :full_note, :short_note, :short_note, :short_note
 end
