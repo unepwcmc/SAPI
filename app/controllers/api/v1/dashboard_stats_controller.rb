1979c24ae46c489db0cd4ecb917faffca972f30f
deciob <decio.battaglia@gmail.com>
2014-02-19 14:23:07 +0000

    better error message on dashboard_stats

--- app/controllers/api/v1/dashboard_stats_controller.rb
+++ app/controllers/api/v1/dashboard_stats_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::DashboardStatsController < ApplicationController
 
   def index
-    iso_code = params['iso_code'].upcase
-    geo_entity = GeoEntity.where(:iso_code2 => iso_code).first
+    iso_code = params['iso_code']
+    geo_entity = GeoEntity.where(:iso_code2 => iso_code.upcase).first
     if geo_entity
       stats = DashboardStats.new(geo_entity, params['kingdom'], params['trade_limit'])
       render :json => stats, :serializer => DashboardStatsSerializer
