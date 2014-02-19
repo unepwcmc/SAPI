80eabbb1914dba469488851d2a805d0c0e665fd3
deciob <decio.battaglia@gmail.com>
2014-02-19 14:20:11 +0000

    better error message on dashboard_stats

--- app/controllers/api/v1/dashboard_stats_controller.rb
+++ app/controllers/api/v1/dashboard_stats_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::DashboardStatsController < ApplicationController
 
   def index
-    iso_code = params['iso_code']
+    iso_code = params['iso_code'].upcase
     geo_entity = GeoEntity.where(:iso_code2 => iso_code).first
     if geo_entity
       stats = DashboardStats.new(geo_entity, params['kingdom'], params['trade_limit'])
       render :json => stats, :serializer => DashboardStatsSerializer
     else
-      render :json => 'Non valid iso2 code!'
+      render :json => { "error" => "#{iso_code} is not a valid iso2 code!" },
+             :status => :unprocessable_entity
     end
   end
 
