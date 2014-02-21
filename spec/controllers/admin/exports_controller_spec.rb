6fc5d8b2c97fc3ae79032166b2015268ec221bc8
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-02-21 18:35:28 +0000

    [#64037166] Amended specs to test downloading taxon concepts distribution. Added methods to downloads_cache to clear taxon_concepts_distributions. Added both taxon_concepts_distributions and taxon_concepts_names to general cache clear

--- spec/controllers/admin/exports_controller_spec.rb
+++ spec/controllers/admin/exports_controller_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe Admin::ExportsController do
-  after(:each) do
-    DownloadsCache.clear_taxon_concepts_names
-  end
   describe "GET index" do
     it "renders the index template" do
       get :index
@@ -11,17 +8,20 @@ describe Admin::ExportsController do
       response.should render_template("layouts/admin")
     end
   end
-  describe "GET download" do
+  describe "GET download with data_type=Names" do
+    after(:each) do
+      DownloadsCache.clear_taxon_concepts_names
+    end
     context "all" do
       it "returns taxon concepts names file" do
         create(:taxon_concept)
         Species::TaxonConceptsNamesExport.any_instance.stub(:public_file_name).and_return('taxon_concepts_names.csv')
-        get :download
+        get :download, :data_type => "Names"
         response.content_type.should eq("text/csv")
         response.headers["Content-Disposition"].should eq("attachment; filename=\"taxon_concepts_names.csv\"")
       end
       it "redirects when no results" do
-        get :download
+        get :download, :data_type => "Names"
         response.should redirect_to(admin_exports_path)
       end
     end
@@ -29,12 +29,12 @@ describe Admin::ExportsController do
       it "returns CITES_EU taxon concepts names file" do
         create_cites_eu_species
         Species::TaxonConceptsNamesExport.any_instance.stub(:public_file_name).and_return('taxon_concepts_names.csv')
-        get :download, :filters => {:taxonomy => 'CITES_EU'}
+        get :download, :data_type => "Names", :filters => {:taxonomy => 'CITES_EU'}
         response.content_type.should eq("text/csv")
         response.headers["Content-Disposition"].should eq("attachment; filename=\"taxon_concepts_names.csv\"")
       end
       it "redirects when no results" do
-        get :download, :filters => {:taxonomy => 'CITES_EU'}
+        get :download, :data_type => "Names", :filters => {:taxonomy => 'CITES_EU'}
         response.should redirect_to(admin_exports_path)
       end
     end
@@ -42,12 +42,45 @@ describe Admin::ExportsController do
       it "returns CMS taxon concepts names file" do
         create_cms_species
         Species::TaxonConceptsNamesExport.any_instance.stub(:public_file_name).and_return('taxon_concepts_names.csv')
-        get :download, :filters => {:taxonomy => 'CMS'}
+        get :download, :data_type => "Names", :filters => {:taxonomy => 'CMS'}
         response.content_type.should eq("text/csv")
         response.headers["Content-Disposition"].should eq("attachment; filename=\"taxon_concepts_names.csv\"")
       end
       it "redirects when no results" do
-        get :download, :filters => {:taxonomy => 'CMS'}
+        get :download, :data_type => "Names", :filters => {:taxonomy => 'CMS'}
+        response.should redirect_to(admin_exports_path)
+      end
+    end
+  end
+  describe "GET download with data_type=Distributions" do
+    after(:each) do
+      DownloadsCache.clear_taxon_concepts_distributions
+    end
+    context "all" do
+      it "returns taxon concepts distributions file" do
+        tc = create(:taxon_concept)
+        create(:distribution, :taxon_concept_id => tc.id)
+        Species::TaxonConceptsDistributionsExport.any_instance.stub(:public_file_name).and_return('taxon_concepts_distributions.csv')
+        get :download, :data_type => "Distributions"
+        response.content_type.should eq("text/csv")
+        response.headers["Content-Disposition"].should eq("attachment; filename=\"taxon_concepts_distributions.csv\"")
+      end
+      it "redirects when no results" do
+        get :download, :data_type => "Distributions"
+        response.should redirect_to(admin_exports_path)
+      end
+    end
+    context "CITES_EU" do
+      it "returns CITES_EU taxon concepts distributions file" do
+        tc = create_cites_eu_species
+        create(:distribution, :taxon_concept_id => tc.id)
+        Species::TaxonConceptsDistributionsExport.any_instance.stub(:public_file_name).and_return('taxon_concepts_distributions.csv')
+        get :download, :data_type => "Distributions", :filters => {:taxonomy => 'CITES_EU'}
+        response.content_type.should eq("text/csv")
+        response.headers["Content-Disposition"].should eq("attachment; filename=\"taxon_concepts_distributions.csv\"")
+      end
+      it "redirects when no results" do
+        get :download, :data_type => "Distributions", :filters => {:taxonomy => 'CITES_EU'}
         response.should redirect_to(admin_exports_path)
       end
     end
