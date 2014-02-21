c712c57b42a46ec1c3ec813de94ffb143ade9933
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-02-21 14:28:55 +0000

    Starting to develop the export of the species distributions for the admin tool

---
+++ app/models/species/taxon_concepts_distributions_export.rb
@@ -0,0 +1,41 @@
+require 'digest/sha1'
+require 'csv'
+class Species::TaxonConceptsDistributionsExport < Species::CsvExport
+
+  def initialize(filters)
+    @filters = filters || {}
+    @taxonomy = @filters[:taxonomy] && Taxonomy.find_by_name(filters[:taxonomy])
+  end
+
+  def query
+    rel = TaxonConcept.select(sql_columns).from(table_name).
+      order('name_status, taxonomic_position')
+    rel = rel.where("#{table_name}.taxonomy_id" => @taxonomy.id) if @taxonomy
+    rel
+  end
+
+private
+
+  def resource_name
+    'taxon_concepts_names'
+  end
+
+  def table_name
+    'taxon_concepts_names_view'
+  end
+
+  def sql_columns
+    columns = [
+      :id, :legacy_id, :kingdom_name, :phylum_name, :class_name, :order_name, :family_name,
+      :genus_name, :species_name, :full_name, :author_year, :rank_name, :name_status, :taxonomy_name
+    ]
+  end
+
+  def csv_column_headers
+    headers = [
+      'Id', 'Legacy id', 'Kingdom', 'Phylum', 'Class', 'Order', 'Family',
+      'Genus', 'Species', 'Scientific Name', 'Author', 'Rank', 'Name status', 'Taxonomy'
+    ]
+  end
+
+end
