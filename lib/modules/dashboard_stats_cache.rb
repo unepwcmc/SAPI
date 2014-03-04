361a497be1f23f12fde14194889193dbff4d22ee
deciob <decio.battaglia@gmail.com>
2014-03-04 16:51:27 +0000

    updated geo_entity query in warm_dashboard_cache

--- lib/modules/dashboard_stats_cache.rb
+++ lib/modules/dashboard_stats_cache.rb
@@ -3,7 +3,8 @@ module DashboardStatsCache
   def self.update_dashboard_stats
     puts "Updating Dashboard Stats"
     DashboardStatsSerializer.cache.clear
-    countries = GeoEntity.where(:geo_entity_type_id => 1)
+    countries = GeoEntity.where(:is_current => true).joins(:geo_entity_type).
+      where("geo_entity_types.name in ('COUNTRY','TERRITORY')")
     countries.each do |country|
       stats = DashboardStats.new(country, 'Animalia', 6)
       DashboardStatsSerializer.new(stats).serializable_hash
