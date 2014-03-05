00bab18de296bfe25ae4a719601a739caecae607
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-03-05 11:43:27 +0000

    Added some output, to see how things are going

--- lib/modules/dashboard_stats_cache.rb
+++ lib/modules/dashboard_stats_cache.rb
@@ -6,6 +6,7 @@ module DashboardStatsCache
     countries = GeoEntity.where(:is_current => true).joins(:geo_entity_type).
       where("geo_entity_types.name in ('COUNTRY','TERRITORY')")
     countries.each do |country|
+      puts "Warming up #{country.name_en}"
       stats = DashboardStats.new(country, 'Animalia', 6)
       DashboardStatsSerializer.new(stats).serializable_hash
     end
