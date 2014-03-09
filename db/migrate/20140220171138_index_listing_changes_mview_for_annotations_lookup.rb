881265493f5d4984cc46d001f64a16df8c0d828c
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-03-09 23:29:00 +0000

    This migration was failling on my test environment, when I dropped and created the database. With the IF EXISTS this doesn't occur anymore

--- db/migrate/20140220171138_index_listing_changes_mview_for_annotations_lookup.rb
+++ db/migrate/20140220171138_index_listing_changes_mview_for_annotations_lookup.rb
@@ -1,6 +1,6 @@
 class IndexListingChangesMviewForAnnotationsLookup < ActiveRecord::Migration
   def up
-    execute "DROP INDEX annotations_display_in_index_symbol_idx"
+    execute "DROP INDEX IF EXISTS annotations_display_in_index_symbol_idx"
     execute "CREATE INDEX listing_changes_mview_display_in_index ON listing_changes_mview (is_current, display_in_index, designation_id)"
   end
 
