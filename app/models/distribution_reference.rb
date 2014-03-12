709b07610af843cef1d7bfae95938eac4e2b6bb4
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-03-12 18:01:24 +0000

    Added timestamps to distribution references

--- app/models/distribution_reference.rb
+++ app/models/distribution_reference.rb
@@ -5,6 +5,8 @@
 #  id              :integer          not null, primary key
 #  distribution_id :integer          not null
 #  reference_id    :integer          not null
+#  created_at      :datetime
+#  updated_at      :datetime
 #
 
 class DistributionReference < ActiveRecord::Base
