37af0fed4e4cfd452e6ede3629b19a742d3c980f
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-03-12 14:31:05 +0000

    fix the error if a tuple to be updated or deleted has already been updated or deleted by a BEFORE trigger

--- db/plpgsql/008c_rebuild_ancestor_cms_listing.sql
+++ db/plpgsql/008c_rebuild_ancestor_cms_listing.sql
@@ -1,106 +1,150 @@
-CREATE OR REPLACE FUNCTION cms_aggregate_children_listing(
+DROP FUNCTION IF EXISTS cms_aggregate_children_listing(
   node_id INTEGER, cascade_to_children BOOLEAN
-  ) RETURNS HSTORE
+  );
+
+CREATE OR REPLACE FUNCTION cms_leaf_listing(node_id INT)
+  RETURNS HSTORE
+  LANGUAGE sql STABLE
+  AS $$
+    SELECT hstore(
+      'cms_listing',
+      ARRAY_TO_STRING(
+        -- unnest to filter out the nulls
+        ARRAY(
+          SELECT * FROM UNNEST(
+            ARRAY[
+              taxon_concepts.listing -> 'cms_I',
+              taxon_concepts.listing -> 'cms_II',
+              taxon_concepts.listing -> 'cms_not_listed'
+            ]
+          ) s WHERE s IS NOT NULL
+        ), '/'
+      )
+    )
+    FROM taxon_concepts
+    WHERE id = node_id;
+  $$;
+
+CREATE OR REPLACE FUNCTION cms_aggregate_children_listing(node_id INT)
+  RETURNS HSTORE
+  LANGUAGE sql STABLE
+  AS $$
+    WITH aggregated_children_listing AS (
+      SELECT
+      -- this to be used in the timelines: if there are explicitly listed
+      -- descendants, the timeline might differ from the current listing
+      -- and a note should be displayed to inform the user
+      hstore('cms_listed_descendants', BOOL_OR(
+        (listing -> 'cms_status_original')::BOOLEAN
+        OR (listing -> 'cms_listed_descendants')::BOOLEAN
+      )::VARCHAR) ||
+      hstore('cms_I', MAX((listing -> 'cms_I')::VARCHAR)) ||
+      hstore('cms_II', MAX((listing -> 'cms_II')::VARCHAR)) ||
+      hstore('cms_NC', MAX((listing -> 'cms_not_listed')::VARCHAR)) ||
+      hstore('cms_listing', ARRAY_TO_STRING(
+        -- unnest to filter out the nulls
+        ARRAY(SELECT * FROM UNNEST(
+          ARRAY[
+            (MAX(listing -> 'cms_I')::VARCHAR),
+            (MAX(listing -> 'cms_II')::VARCHAR),
+            (MAX(listing -> 'cms_not_listed')::VARCHAR)
+          ]) s WHERE s IS NOT NULL),
+          '/'
+        )
+      ) AS listing
+      FROM taxon_concepts
+      WHERE
+        -- aggregate children's listings
+        parent_id = $1
+        -- as well as parent if they're explicitly listed
+        OR (
+          id = $1
+          AND (listing->'cms_status_original')::BOOLEAN
+        )
+        -- as well as parent if they are species
+        -- the assumption being they will have subspecies
+        -- which are not listed in their own right and
+        -- should therefore inherit the cascaded listing
+        -- if one exists
+        -- this should fix Lutrinae species, which should be I/II
+        -- even though subspecies in the db are on I
+        OR (
+          id = $1
+          AND data->'rank_name' = 'SPECIES'
+        )
+    )
+    SELECT listing
+    FROM aggregated_children_listing;
+  $$;
+
+CREATE OR REPLACE FUNCTION rebuild_ancestor_cms_listing_recursively_for_node(node_id integer)
+  RETURNS void
   LANGUAGE plpgsql
   AS $$
     DECLARE
-      tmp HSTORE;
+      children_node_ids INTEGER[];
+      tmp_node_id INT;
     BEGIN
-      -- this will update the listing only if node is a leaf
-      WITH updated AS (
-        UPDATE taxon_concepts SET listing = taxon_concepts.listing ||
-        hstore(
-          'cms_listing',
-          ARRAY_TO_STRING(
-            -- unnest to filter out the nulls
-            ARRAY(
-              SELECT * FROM UNNEST(
-                ARRAY[
-                  taxon_concepts.listing -> 'cms_I',
-                  taxon_concepts.listing -> 'cms_II',
-                  taxon_concepts.listing -> 'cms_not_listed'
-                ]
-              ) s WHERE s IS NOT NULL
-            ), '/'
-          )
-        )
-        FROM taxon_concepts leaves
-        WHERE NOT EXISTS (
-          SELECT id FROM taxon_concepts WHERE taxon_concepts.parent_id = leaves.id
-        ) AND taxon_concepts.id = leaves.id AND leaves.id = node_id
-        RETURNING taxon_concepts.id, taxon_concepts.listing
-      ) SELECT listing INTO tmp FROM updated LIMIT 1;
-      IF FOUND THEN
-        RETURN tmp;
-      END IF;
-      -- if it's not a leaf
-      IF cascade_to_children THEN
-        UPDATE taxon_concepts SET listing = taxon_concepts.listing ||
-          cms_aggregate_children_listing(id, TRUE)
-        WHERE taxon_concepts.parent_id = node_id;
-      END IF;
+      SELECT ARRAY_AGG_NOTNULL(id) INTO children_node_ids
+      FROM taxon_concepts
+      WHERE parent_id = node_id;
+      -- if there are children, rebuild their aggregated listing first
+      FOREACH tmp_node_id IN ARRAY children_node_ids
+      LOOP
+        PERFORM rebuild_ancestor_cms_listing_recursively_for_node(tmp_node_id);
+      END LOOP;
 
-        WITH updated AS (
-          WITH aggregated_children_listing AS (
-            SELECT
-            hstore('cms_I', MAX((listing -> 'cms_I')::VARCHAR)) ||
-            hstore('cms_II', MAX((listing -> 'cms_II')::VARCHAR)) ||
-            hstore('cms_not_listed', MAX((listing -> 'cms_not_listed')::VARCHAR)) ||
-            hstore('cms_listing', ARRAY_TO_STRING(
-              -- unnest to filter out the nulls
-              ARRAY(SELECT * FROM UNNEST(
-                ARRAY[
-                  (MAX(listing -> 'cms_I')::VARCHAR),
-                  (MAX(listing -> 'cms_II')::VARCHAR),
-                  (MAX(listing -> 'cms_not_listed')::VARCHAR)
-                ]) s WHERE s IS NOT NULL),
-                '/'
-              )
-            ) AS listing
-            FROM taxon_concepts WHERE parent_id = node_id
-              OR (id = node_id AND (listing->'cms_status_original')::BOOLEAN)
-          )
-          UPDATE taxon_concepts SET listing = taxon_concepts.listing || aggregated_children_listing.listing
-          FROM aggregated_children_listing
-          WHERE taxon_concepts.id = node_id
-          RETURNING taxon_concepts.listing
-        )
-        SELECT listing INTO tmp FROM updated;
-
-      RETURN tmp;
+      -- update this node's aggregated listing
+      IF ARRAY_UPPER(children_node_ids, 1) IS NULL THEN
+        UPDATE taxon_concepts
+        SET listing = listing || cms_leaf_listing(node_id)
+        WHERE id = node_id;
+      ELSE
+        UPDATE taxon_concepts
+        SET listing = listing || cms_aggregate_children_listing(node_id)
+        WHERE id = node_id;
+      END IF;
     END;
   $$;
 
-CREATE OR REPLACE FUNCTION rebuild_ancestor_cms_listing_for_node(node_id integer) RETURNS void
+CREATE OR REPLACE FUNCTION rebuild_ancestor_cms_listing_for_node(node_id integer)
+  RETURNS void
   LANGUAGE plpgsql
   AS $$
     DECLARE
-      cms_id int;
       ancestor_node_ids INTEGER[];
       tmp_node_id int;
     BEGIN
-    SELECT id INTO cms_id FROM taxonomies WHERE name = 'CMS';
-    IF node_id IS NOT NULL THEN
-      PERFORM cms_aggregate_children_listing(node_id, TRUE);
+      IF node_id IS NULL THEN
+        FOR tmp_node_id IN SELECT taxon_concepts.id FROM taxon_concepts
+          JOIN taxonomies ON taxon_concepts.taxonomy_id = taxonomies.id
+          AND taxonomies.name = 'CMS'
+          WHERE parent_id IS NULL
+        LOOP
+          PERFORM rebuild_ancestor_cms_listing_for_node(tmp_node_id);
+        END LOOP;
+        RETURN;
+      END IF;
+      PERFORM rebuild_ancestor_cms_listing_recursively_for_node(node_id);
+      -- if we're not starting from root, we need to update ancestors
+      -- up till root
       ancestor_node_ids := ancestor_node_ids_for_node(node_id);
       FOREACH tmp_node_id IN ARRAY ancestor_node_ids
       LOOP
-        PERFORM cms_aggregate_children_listing(tmp_node_id, FALSE);
+        UPDATE taxon_concepts
+        SET listing  = listing || cms_aggregate_children_listing(tmp_node_id)
+        WHERE id = tmp_node_id;
       END LOOP;
-    ELSE
-      UPDATE taxon_concepts SET listing = taxon_concepts.listing ||
-        cms_aggregate_children_listing(id, TRUE)
-      WHERE parent_id IS NULL AND taxonomy_id = cms_id;
-    END IF;
     END;
   $$;
+
 
 CREATE OR REPLACE FUNCTION rebuild_ancestor_cms_listing() RETURNS void
   LANGUAGE plpgsql
   AS $$
     BEGIN
-    PERFORM rebuild_ancestor_cms_listing_for_node(NULL);
+      PERFORM rebuild_ancestor_cms_listing_for_node(NULL);
     END;
   $$;
 
-COMMENT ON FUNCTION rebuild_ancestor_cms_listing() IS 'Procedure to rebuild EU ancestor listings in taxon_concepts.';
+COMMENT ON FUNCTION rebuild_ancestor_cms_listing() IS 'Procedure to rebuild CITES ancestor listings in taxon_concepts.';
