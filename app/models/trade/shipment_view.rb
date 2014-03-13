e1aded71d4ece361d1c7149fd436c78771c3ad70
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-03-13 10:09:19 +0000

    rewritten the queries for speed and reorganised the code for readability

--- app/models/trade/shipment_view.rb
+++ app/models/trade/shipment_view.rb
@@ -44,5 +44,4 @@
 class Trade::ShipmentView < ActiveRecord::Base
   self.table_name = 'trade_shipments_view'
   belongs_to :taxon_concept
-  belongs_to :m_taxon_concept, :foreign_key => :taxon_concept_id
 end
