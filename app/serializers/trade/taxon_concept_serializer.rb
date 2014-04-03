24b5fd83a2adbbdbaf6118cb654b29ac9f5cad40
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-04-03 17:34:33 +0100

    Include name_status on the trade admin interface

--- app/serializers/trade/taxon_concept_serializer.rb
+++ app/serializers/trade/taxon_concept_serializer.rb
@@ -1,3 +1,7 @@
 class Trade::TaxonConceptSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :author_year, :rank_name
+
+  def full_name
+    object.full_name + " (#{object.name_status})"
+  end
 end
