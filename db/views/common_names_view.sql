42c009ab90f29915532c541061c3b33842c231c6
deciob <decio.battaglia@gmail.com>
2014-04-10 15:47:08 +0100

    basic functionality for Admin can download Common Names data

--- db/views/common_names_view.sql
+++ db/views/common_names_view.sql
@@ -1,19 +1,22 @@
 DROP VIEW IF EXISTS common_names_view;
 CREATE VIEW common_names_view AS
 SELECT
+  st.name_status,
   st.id,
   a.data->'phylum_name' AS accepted_phylum_name,
   a.data->'class_name' AS accepted_class_name,
   a.data->'order_name' AS accepted_order_name,
   a.data->'family_name' AS accepted_family_name,
-  st.full_name AS scientific_name,
+  st.full_name AS full_name,
   st.author_year,
-  a.data->'rank_name' AS accepted_rank_name,
+  st.data->'rank_name' AS rank_name,
   n.name AS common_name,
   l.name_en AS common_name_language,
   taxonomies.name AS taxonomy_name,
   to_char(st.created_at, 'DD/MM/YYYY') AS created_at,
-  'TODO' AS created_by
+  'TODO' AS created_by,
+  taxonomies.id AS taxonomy_id
+
 FROM taxon_concepts st
 JOIN taxonomies ON taxonomies.id = st.taxonomy_id
 LEFT JOIN taxon_relationships
