6b9bef38fa7457baa65ac0cb387113cd460c01f6
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-04-14 17:05:00 +0200

    added users controller spec

---
+++ spec/controllers/admin/users_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+describe Admin::UsersController do
+  login_admin
+
+  describe "index" do
+    describe "GET index" do
+      it "renders the index template" do
+        get :index
+        response.should render_template("index")
+      end
+    end
+  end
+
+  describe "XHR POST create" do
+    it "renders create when successful" do
+      xhr :post, :create, user: FactoryGirl.attributes_for(:user)
+      response.should render_template("create")
+    end
+    it "renders new when not successful" do
+      xhr :post, :create, user: { :name => nil }
+      response.should render_template("new")
+    end
+  end
+
+  describe "XHR GET edit" do
+    let(:user){ create(:user) }
+    it "renders the edit template" do
+      xhr :get, :edit, :id => user.id
+      response.should render_template('new')
+    end
+    it "assigns the hybrid_relationship variable" do
+      xhr :get, :edit, :id => user.id
+      assigns(:user).should_not be_nil
+    end
+  end
+
+  describe "XHR PUT update JSON" do
+    let(:user){ create(:user) }
+    it "responds with 200 when successful" do
+      xhr :put, :update, :format => 'json', :id => user.id, :user => { :name => 'ZZ' }
+      response.should be_success
+    end
+    it "responds with json when not successful" do
+      xhr :put, :update, :format => 'json', :id => user.id, :user => { :name => nil }
+      JSON.parse(response.body).should include('errors')
+    end
+  end
+
+  describe "DELETE destroy" do
+    let(:user){ create(:user) }
+    it "redirects after delete" do
+      delete :destroy, :id => user.id
+      response.should redirect_to(admin_users_url)
+    end
+  end
+
+end
