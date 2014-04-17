ef81c0939968b97e5c6196a40b9b156922358b54
deciob <decio.battaglia@gmail.com>
2014-04-17 11:00:14 +0100

    reverted acceptedSubspecies change

--- app/serializers/species/autocomplete_taxon_concept_serializer.rb
+++ app/serializers/species/autocomplete_taxon_concept_serializer.rb
@@ -1,15 +1,8 @@
 class Species::AutocompleteTaxonConceptSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :rank_name, :matching_names, :accepted_subspecies
+  attributes :id, :full_name, :rank_name, :matching_names
 
   def matching_names
     (object.synonyms + object.english_names +
       object.french_names + object.spanish_names).sort
-  end
-
-  def accepted_subspecies
-    taxon_concept = MTaxonConcept.find(object.id)
-    object.rank_name == 'SUBSPECIES' && 
-    (taxon_concept.taxonomy_is_cites_eu? && taxon_concept.cites_listed?) ||
-    taxon_concept.cms_listed?
   end
 end
