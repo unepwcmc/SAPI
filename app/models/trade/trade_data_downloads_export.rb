6c65f88eb2df1f5a84b3c3f4d44a34b60473d228
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-04-17 16:20:13 +0200

    trade downloads data implemented without using the postgres copy gem

---
+++ app/models/trade/trade_data_downloads_export.rb
@@ -0,0 +1,40 @@
+class Trade::TradeDataDownloadsExport < Species::CsvCopyExport
+
+  def query
+    Trade::TradeDataDownload.order(:created_at)
+  end
+
+private
+
+  def resource_name
+    'trade_download_stats'
+  end
+
+  def sql_columns
+    [
+      :user_ip,
+      :report_type,
+      :year_from,
+      :year_to,
+      :taxon,
+      :appendix,
+      :importer,
+      :exporter,
+      :origin,
+      :term,
+      :unit,
+      :source,
+      :purpose,
+      :number_of_rows,
+      :city,
+      :country,
+      :organization,
+      :created_at
+    ]
+  end
+
+  def csv_column_headers
+    sql_columns.map(&:to_s).map(&:humanize)
+  end
+
+end
