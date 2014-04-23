7abc0a2f66dff0819f176031b550edf6213bb313
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-04-23 11:03:20 +0100

    spec for Amazona festiva festiva (never listed subspecies)

--- spec/models/taxon_concept/psittaciformes_spec.rb
+++ spec/models/taxon_concept/psittaciformes_spec.rb
@@ -19,13 +19,16 @@ describe TaxonConcept do
           specify { @species1_1.cites_listing.should == 'I' }
         end
         context "for species Amazona aestiva" do
-          specify { @species2_2.cites_listing.should == 'II' }
+          specify { @species2_2_1.cites_listing.should == 'II' }
         end
         context "for species Agapornis roseicollis (DEL II, not listed, not shown)" do
           specify { @species2_1.cites_listing.should == 'NC' }
         end
         context "for species Psittacula krameri (DEL III, not listed, not shown)" do
           specify { @species2_3.cites_listing.should == 'NC' }
+        end
+        context "for subspecies Amazona festiva festiva" do
+          specify { @subspecies2_2_2_1.cites_listing.should == 'II' }
         end
       end
 
@@ -43,13 +46,16 @@ describe TaxonConcept do
           specify { @species1_1.eu_listing.should == 'A' }
         end
         context "for species Amazona aestiva" do
-          specify { @species2_2.eu_listing.should == 'B' }
+          specify { @species2_2_1.eu_listing.should == 'B' }
         end
         context "for species Agapornis roseicollis (DEL II, not listed, not shown)" do
           specify { @species2_1.eu_listing.should == 'NC' }
         end
         context "for species Psittacula krameri (DEL III, not listed, not shown)" do
           specify { @species2_3.eu_listing.should == 'NC' }
+        end
+        context "for subspecies Amazona festiva festiva" do
+          specify { @subspecies2_2_2_1.eu_listing.should == 'B' }
         end
       end
 
@@ -69,6 +75,9 @@ describe TaxonConcept do
         context "for species Cacatua ducorpsi" do
           specify { @species1_2_2.cites_listed.should == false }
         end
+        context "for subspecies Amazona festiva festiva" do
+          specify { @subspecies2_2_2_1.cites_listed.should == false }
+        end
       end
 
       describe :eu_listed do
@@ -87,6 +96,9 @@ describe TaxonConcept do
         context "for species Cacatua ducorpsi" do
           specify { @species1_2_2.eu_listed.should == false }
         end
+        context "for subspecies Amazona festiva festiva" do
+          specify { @subspecies2_2_2_1.eu_listed.should == false }
+        end
       end
 
       describe :cites_show do
@@ -94,7 +106,7 @@ describe TaxonConcept do
           specify { @species2_1.cites_show.should be_true }
         end
         context "for species Amazona aestiva" do
-          specify { @species2_2.cites_show.should be_true }
+          specify { @species2_2_1.cites_show.should be_true }
         end
         context "for species Psittacula krameri (DEL III)" do
           specify { @species2_3.cites_show.should be_true }
@@ -107,6 +119,18 @@ describe TaxonConcept do
         end
         context "for species Psittacula krameri (DEL III)" do
           specify { @species2_3.cites_status.should == 'EXCLUDED' }
+        end
+      end
+
+      describe :show_in_checklist_ac do
+        context "for subspecies Amazona festiva festiva" do
+          specify { @subspecies2_2_2_1.show_in_checklist_ac.should be_false }
+        end
+      end
+
+      describe :show_in_species_plus_ac do
+        context "for subspecies Amazona festiva festiva" do
+          specify { @subspecies2_2_2_1.show_in_species_plus_ac.should be_false }
         end
       end
 
