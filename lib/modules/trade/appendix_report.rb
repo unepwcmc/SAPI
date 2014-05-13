ee60d9122afe6adf0d30f2e6c491d5029101c860
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-05-13 14:27:03 +0100

    fix duplicated auto appendix eoutput e.g. I/I/I

--- lib/modules/trade/appendix_report.rb
+++ lib/modules/trade/appendix_report.rb
@@ -3,26 +3,32 @@ class Trade::AppendixReport
 
   def initialize(shipments_rel)
     @query = shipments_rel.except(:select).select([
-      'trade_shipments.id', :legacy_shipment_number,
-      'taxon_concepts.id', 'taxon_concepts.full_name',
-      :year, 'trade_shipments.appendix',
-      'ARRAY_TO_STRING(ARRAY_AGG_NOTNULL(m.appendix), \'/\')'
-    ]).joins(:taxon_concept).
-    joins(<<-SQL
+      'trade_shipments.id', :legacy_shipment_number, :year,
+      'trade_shipments.taxon_concept_id', 'trade_shipments.appendix',
+      'm.appendix AS auto_appendix'
+    ]).joins(<<-SQL
       LEFT JOIN valid_taxon_concept_appendix_year_mview m
-      ON EXTRACT(year FROM m.effective_from) <= trade_shipments.year
+      ON DATE_PART('year', m.effective_from) <= trade_shipments.year
       AND (
-        m.effective_to IS NULL
-        OR EXTRACT(year FROM m.effective_to) >= trade_shipments.year
+        DATE_PART('year', m.effective_to) IS NULL
+        OR DATE_PART('year', m.effective_to) >= trade_shipments.year
       )
       AND m.taxon_concept_id = trade_shipments.taxon_concept_id
       SQL
-    ).group([
-      'trade_shipments.id', :legacy_shipment_number,
-      :year, 'taxon_concepts.id', 'trade_shipments.appendix'
-    ])
+    ).uniq
+
+    @query = Trade::Shipment.from("(#{@query.to_sql}) s").
+    select(['s.id', :legacy_shipment_number, :taxon_concept_id,
+      :full_name, :year, :appendix,
+      'ARRAY_TO_STRING(ARRAY_AGG_NOTNULL(auto_appendix ORDER BY auto_appendix), \'/\')'
+    ]).
+    joins('JOIN taxon_concepts ON s.taxon_concept_id = taxon_concepts.id').
+    group(['s.id', :legacy_shipment_number, :taxon_concept_id,
+      'taxon_concepts.full_name', :year, :appendix
+    ]).order([:full_name, :year, :appendix])
+
     @diff_query = @query.having(<<-SQL
-      NOT ARRAY_AGG_NOTNULL(m.appendix) @> ARRAY[trade_shipments.appendix]
+      NOT ARRAY_AGG_NOTNULL(auto_appendix) @> ARRAY[appendix]
       SQL
     )
   end
