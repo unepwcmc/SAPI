812a15e1f69a927a75ac6d3430656d9e6cbeca71
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-05-23 16:36:56 +0100

    Updated controller and views to display iucn_mappings

--- app/controllers/admin/iucn_mappings_controller.rb
+++ app/controllers/admin/iucn_mappings_controller.rb
@@ -2,9 +2,14 @@ class Admin::IucnMappingsController < Admin::SimpleCrudController
 
   def index
     @totals = {
-      :with_match => Admin::IucnMapping.where('iucn_taxon_id IS NOT NULL').count,
-      :without_match => Admin::IucnMapping.where(:iucn_taxon_id => nil).count,
-      :all => Admin::IucnMapping.count
+      :matching => Admin::IucnMapping.filter("MATCHING").count,
+      :full_match => Admin::IucnMapping.filter("FULL_MATCH").count,
+      :name_match => Admin::IucnMapping.filter("NAME_MATCH").count,
+      :full_synonym_match => Admin::IucnMapping.filter("FULL_SYNONYM_MATCH").count,
+      :synonym_match => Admin::IucnMapping.filter("SYNONYM_MATCH").count,
+      :non_matching => Admin::IucnMapping.filter("NON_MATCHING").count,
+      :all => Admin::IucnMapping.count,
+
     }
   end
 
@@ -12,6 +17,7 @@ class Admin::IucnMappingsController < Admin::SimpleCrudController
 
   def collection
     @iucn_mappings ||= end_of_association_chain.order(:taxon_concept_id).
-      page(params[:page]).filter(params[:show]||0)
+      page(params[:page]).filter(params[:show]||"ALL").includes(:taxon_concept).
+      includes(:synonym)
   end
 end
