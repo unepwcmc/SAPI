fcd11fe515eae48aba44f73d7b6add2ef530f735
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-05-27 14:54:12 +0100

    Matching on subspecies and stripping authors to improve matching

--- app/models/admin/iucn_mapping.rb
+++ app/models/admin/iucn_mapping.rb
@@ -42,24 +42,39 @@ class Admin::IucnMapping < ActiveRecord::Base
     config = YAML.load_file(config_location)[Rails.env]
     @token = config['iucn_redlist']
 
-    rank = Rank.where(:name => Rank::SPECIES).first
+    species = Rank.where(:name => Rank::SPECIES).first
+    subspecies = Rank.where(:name => Rank::SUBSPECIES).first
     taxonomy = Taxonomy.where(:name => Taxonomy::CITES_EU).first
 
-    TaxonConcept.where(:rank_id => rank.id, :name_status => 'A',
+    TaxonConcept.where(:rank_id => [species.id, subspecies.id], :name_status => 'A',
                        :taxonomy_id => taxonomy.id).each do |tc|
       map = Admin::IucnMapping.find_or_create_by_taxon_concept_id(tc.id)
-      data = fetch_data_for_name tc.full_name
+      full_name = if tc.rank_id == subspecies.id
+                    tc.full_name.insert(tc.full_name.rindex(/ /), " ssp.")
+                  else
+                    tc.full_name
+                  end
+      data = fetch_data_for_name full_name
       if data["result"].empty?
         puts "#{tc.full_name} NO MATCH trying synonyms"
-        tc.synonyms.each do |syn|
-          data = fetch_data_for_name syn.full_name
-          if data["result"] && !data["result"].empty?
-            map_taxon_concept tc, map, data, syn
-            break
-          end
-        end
+        match_on_synonyms tc, map, subspecies.id
       else
         map_taxon_concept tc, map, data
+      end
+    end
+  end
+
+  def self.match_on_synonyms tc, map, subspecies_rank_id
+    tc.synonyms.each do |syn|
+      full_name = if syn.rank_id == subspecies_rank_id
+                    syn.full_name.insert(syn.full_name.rindex(/ /), " ssp.")
+                  else
+                    syn.full_name
+                  end
+      data = fetch_data_for_name full_name
+      if data["result"] && !data["result"].empty?
+        map_taxon_concept tc, map, data, syn
+        return
       end
     end
   end
@@ -93,17 +108,27 @@ class Admin::IucnMapping < ActiveRecord::Base
 
   def self.type_of_match tc, match, synonym
     if tc.full_name == match["scientific_name"]
-      if tc.author_year == match["authority"]
+      if strip_authors(tc.author_year) == strip_authors(match["authority"])
+        puts "FULL_MATCH!"
         "FULL_MATCH"
       else
+        puts "NAME_MATCH"
         "NAME_MATCH"
       end
     elsif synonym.full_name == match["scientific_name"]
-      if synonym.author_year == match["authority"]
+      if strip_authors(synonym.author_year) == strip_authors(match["authority"])
+        puts "FULL_SYNONYM_MATCH"
         "FULL_SYNONYM_MATCH"
       else
+        puts "SYNONYM_MATCH"
         "SYNONYM_MATCH"
       end
     end
+  end
+
+  def self.strip_authors author
+    author.split(" ").
+      reject{|p| ["and", "&", "&amp;", ","].include?(p)}.
+      join(" ")
   end
 end
