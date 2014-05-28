a7e1d5c3a494a50c95df9a09ebf197fc7fbf6464
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-05-28 09:50:49 +0100

    [Finishes #41220769] Adds authorization. Blocking contributors from deleting data, and accessing some parts of the administration website. Including no access to trade management interface, bulk updates, and core data menu

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,9 @@
 class Admin::AdminController < ApplicationController
   layout 'admin'
   before_filter :authenticate_user!
+  authorize_resource
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to admin_root_path, :alert => exception.message
+  end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,12 +11,12 @@ class Ability
     #     can :read, :all
     #   end
     #
-    # The first argument to `can` is the action you are giving the user 
+    # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
+    # here are :read, :create, :update and :destroy
     #
-    # The second argument is the resource the user can perform the action on. 
+    # The second argument is the resource the user can perform the action on.
     # If you pass :all it will apply to every resource. Otherwise pass a Ruby
     # class of the resource.
     #
@@ -28,5 +28,24 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+
+    user ||= User.new
+
+    if user.is_manager?
+      can :manage, :all
+    else
+      can :read, :all
+      can :update, :all
+      can :create, :all
+      cannot :manage, [
+        Taxonomy, Rank, Designation,
+        Instrument, SpeciesListing,
+        ChangeType, EuDecisionType,
+        Language, GeoEntity, GeoEntityType,
+        TradeCode, Trade::TaxonConceptTermPair,
+        TermTradeCodesPair, Event, CitesSuspension,
+        Quota, EuRegulation, EuSuspensionRegulation
+      ]
+    end
   end
 end
