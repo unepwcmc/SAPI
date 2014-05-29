bf9f48086999ba9f300eda7a2e39890b079702cc
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-05-29 18:40:30 +0100

    If no referrer set, redirect to admin_root

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,7 +3,7 @@ class Admin::AdminController < ApplicationController
   before_filter :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to request.referrer, :alert => case exception.action
+    redirect_to request.referrer || admin_root_path, :alert => case exception.action
       when :destroy
         "You are not authorized to destroy that record"
       else
