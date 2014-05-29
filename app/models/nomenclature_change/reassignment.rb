aac02308d390cfa17f07a4de24940dd302c84f7a
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-05-29 15:43:58 +0100

    children & names

---
+++ app/models/nomenclature_change/reassignment.rb
@@ -0,0 +1,16 @@
+# Represents a reassignment of taxon concept associations, which may be
+# a side effect of a nomenclature change.
+# For example, if the change involves swapping an accepted name and a synonym,
+# the former accepted name's synonyms, distribution, legislation etc. will be
+# assigned to the former synonym.
+# The table uses an STI mechanism to differentiate between different types of
+# associations.
+# A polymorphic association is in place that links this object to the entitity
+# that gets reassigned.
+# For example the reassignable_type might be 'ListingChange'
+class NomenclatureChange::Reassignment < ActiveRecord::Base
+  attr_accessible :type, :reassignable_id, :reassignable_type,
+    :nomenclature_change_input_id, :nomenclature_change_output_id,
+    :is_copy
+  belongs_to :reassignable, :polymorphic => true
+end
--- app/models/nomenclature_change_component.rb
+++
@@ -1,6 +0,0 @@
-class NomenclatureChangeComponent < ActiveRecord::Base
-  track_who_does_it
-  attr_accessible :created_by_id, :is_input, :is_output,
-    :nomenclature_change_id, :taxon_concept_id, :updated_by_id
-  validates_presence_of :taxon_concept_id
-end
