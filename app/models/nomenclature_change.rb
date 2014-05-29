0b790400bd97911cd54fc56da3815fe15bb689e0
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-05-29 08:01:53 +0100

    nested form for inputs / outputs

--- app/models/nomenclature_change.rb
+++ app/models/nomenclature_change.rb
@@ -1,5 +1,13 @@
 class NomenclatureChange < ActiveRecord::Base
   track_who_does_it
-  attr_accessible :created_by_id, :event_id, :updated_by_id
+  attr_accessible :created_by_id, :event_id, :updated_by_id,
+    :nomenclature_change_inputs_attributes,
+    :nomenclature_change_outputs_attributes
   belongs_to :event
+  has_many :nomenclature_change_inputs, :conditions => 'is_input',
+    :class_name => NomenclatureChangeComponent, :dependent => :destroy
+  has_many :nomenclature_change_outputs, :conditions => 'NOT is_input',
+    :class_name => NomenclatureChangeComponent, :dependent => :destroy
+  accepts_nested_attributes_for :nomenclature_change_inputs, :allow_destroy => true
+  accepts_nested_attributes_for :nomenclature_change_outputs, :allow_destroy => true
 end
