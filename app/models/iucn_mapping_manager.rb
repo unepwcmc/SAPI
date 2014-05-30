dc562f9b661a8ca66834b760cbf8c2b0501f8115
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-05-30 18:56:33 +0100

    Reshuffle things a bit. Making it possible to fetch the mapping information for one taxon_concept

--- app/models/iucn_mapping_manager.rb
+++ app/models/iucn_mapping_manager.rb
@@ -9,37 +9,41 @@ class IucnMappingManager
       @url = config['iucn_redlist']['url']
 
       species = Rank.where(:name => Rank::SPECIES).first
-      subspecies = Rank.where(:name => Rank::SUBSPECIES).first
+      @subspecies = Rank.where(:name => Rank::SUBSPECIES).first
       taxonomy = Taxonomy.where(:name => Taxonomy::CITES_EU).first
 
-      TaxonConcept.where(:rank_id => [species.id, subspecies.id], :name_status => 'A',
-                         :taxonomy_id => taxonomy.id).each do |tc|
-        map = IucnMapping.find_or_create_by_taxon_concept_id(tc.id)
-        full_name = if tc.rank_id == subspecies.id
-                      tc.full_name.insert(tc.full_name.rindex(/ /), " ssp.")
-                    else
-                      tc.full_name
-                    end
-        data = fetch_data_for_name full_name
-        if data["result"].empty?
-          puts "#{tc.full_name} NO MATCH trying synonyms"
-          match_on_synonyms tc, map, subspecies.id
-        else
-          map_taxon_concept tc, map, data
-        end
+      TaxonConcept.where(:rank_id => [species.id, @subspecies.id], :name_status => 'A',
+                         :taxonomy_id => taxonomy.id).each do |taxon_concept|
+        sync_taxon_concept taxon_concept
       end
     end
 
-    def match_on_synonyms tc, map, subspecies_rank_id
-      tc.synonyms.each do |syn|
-        full_name = if syn.rank_id == subspecies_rank_id
+    def sync_taxon_concept taxon_concept
+      mapping = IucnMapping.find_or_create_by_taxon_concept_id(taxon_concept.id)
+      full_name = if taxon_concept.rank_id == @subspecies.id
+                    taxon_concept.full_name.insert(taxon_concept.full_name.rindex(/ /), " ssp.")
+                  else
+                    taxon_concept.full_name
+                  end
+      data = fetch_data_for_name full_name
+      if data["result"].empty?
+        puts "#{taxon_concept.full_name} NO MATCH trying synonyms"
+        match_on_synonyms taxon_concept, mapping
+      else
+        map_taxon_concept taxon_concept, mapping, data
+      end
+    end
+
+    def match_on_synonyms taxon_concept, map
+      taxon_concept.synonyms.each do |syn|
+        full_name = if syn.rank_id == @subspecies.id
                       syn.full_name.insert(syn.full_name.rindex(/ /), " ssp.")
                     else
                       syn.full_name
                     end
         data = fetch_data_for_name full_name
         if data["result"] && !data["result"].empty?
-          map_taxon_concept tc, map, data, syn
+          map_taxon_concept taxon_concept, map, data, syn
           return
         end
       end
@@ -50,24 +54,24 @@ class IucnMappingManager
       JSON.parse(RestClient.get(url))
     end
 
-    def map_taxon_concept tc, map, data, synonym=nil
+    def map_taxon_concept taxon_concept, map, data, synonym=nil
       begin
         match = data["result"].first
-        puts "#{tc.full_name} #{tc.author_year} <=>  #{match["scientific_name"]} #{match["authority"]}"
+        puts "#{taxon_concept.full_name} #{taxon_concept.author_year} <=>  #{match["scientific_name"]} #{match["authority"]}"
         map.update_attributes(
           :iucn_taxon_name => match['scientific_name'],
           :iucn_taxon_id => match['taxonid'],
           :iucn_author => match['authority'],
           :iucn_category => match['category'],
           :details => {
-          :match => type_of_match(tc, match, synonym),
+          :match => type_of_match(taxon_concept, match, synonym),
           :no_matches => data["result"].size
         },
           :synonym_id => synonym.try(:id)
         )
       rescue Exception => e
         puts "#######################################################################"
-        puts "########################## EXCEPTION Taxon Concept #{tc.id} ###########"
+        puts "########################## EXCEPTION Taxon Concept #{taxon_concept.id} ###########"
         puts e.message
       end
     end
