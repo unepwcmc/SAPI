246840621e959f4fe56f7638d7322a25a086ee2a
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-05-30 14:11:07 +0100

    Pass search_params to the logger instead of just params

--- app/models/trade/trade_data_download_logger.rb
+++ app/models/trade/trade_data_download_logger.rb
@@ -2,24 +2,22 @@ module Trade::TradeDataDownloadLogger
 
   module_function
 
-  def log_download request, params, rows
+  def log_download request, search_params, rows
     data = {}
-    # !! Not all the params can be sanitized with Trade::SearchParams.sanitize
-    filters = Trade::SearchParams.sanitize(params['filters'])
     data["user_ip"] = request.ip
     data["number_of_rows"] = rows
-    data["report_type"] = params['filters']['report_type']
-    data["year_from"] = filters[:time_range_start]
-    data["year_to"] = filters[:time_range_end]
-    data["appendix"] = params['filters']['appendix']
-    data["unit"] = params['filters']['unit']
-    data["taxon"] = TaxonConcept.where(:id => filters[:taxon_concepts_ids]).
+    data["report_type"] = search_params['report_type']
+    data["year_from"] = search_params[:time_range_start]
+    data["year_to"] = search_params[:time_range_end]
+    data["appendix"] = search_params['appendix']
+    data["unit"] = search_params['unit']
+    data["taxon"] = TaxonConcept.where(:id => search_params[:taxon_concepts_ids]).
       order(:full_name).select(:full_name).map(&:full_name).join(", ")
-    data["term"] = self.get_field_values(filters[:terms_ids], Term)
-    data["purpose"] = self.get_field_values(filters[:purposes_ids], Purpose)
-    data["source"] = self.get_field_values(filters[:sources_ids], Source)
-    data["importer"] = self.get_field_values(filters[:importers_ids], GeoEntity)
-    data["exporter"] = self.get_field_values(filters[:exporters_ids], GeoEntity)
+    data["term"] = self.get_field_values(search_params[:terms_ids], Term)
+    data["purpose"] = self.get_field_values(search_params[:purposes_ids], Purpose)
+    data["source"] = self.get_field_values(search_params[:sources_ids], Source)
+    data["importer"] = self.get_field_values(search_params[:importers_ids], GeoEntity)
+    data["exporter"] = self.get_field_values(search_params[:exporters_ids], GeoEntity)
     data["city"], data["country"] = self.city_country_from(request.ip).map do |s|
       s.force_encoding("ISO-8859-1").encode("UTF-8")
     end
