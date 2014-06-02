028899e95630f4cdd644ed138d6a17f01fb727e1
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-02 10:37:24 +0100

    spacing in distribution lists

--- db/mviews/007c_rebuild_cms_species_listing_mview.sql
+++ db/mviews/007c_rebuild_cms_species_listing_mview.sql
@@ -54,15 +54,15 @@ SELECT
     E'\n'
   ) AS original_taxon_concept_full_note_en,
   taxon_concepts_mview.countries_ids_ary,
-  ARRAY_TO_STRING(taxon_concepts_mview.all_distribution_ary, ',') AS all_distribution,
-  ARRAY_TO_STRING(taxon_concepts_mview.all_distribution_iso_codes_ary, ',') AS all_distribution_iso_codes,
-  ARRAY_TO_STRING(taxon_concepts_mview.native_distribution_ary, ',') AS native_distribution,
-  ARRAY_TO_STRING(taxon_concepts_mview.introduced_distribution_ary, ',') AS introduced_distribution,
-  ARRAY_TO_STRING(taxon_concepts_mview.introduced_uncertain_distribution_ary, ',') AS introduced_uncertain_distribution,
-  ARRAY_TO_STRING(taxon_concepts_mview.reintroduced_distribution_ary, ',') AS reintroduced_distribution,
-  ARRAY_TO_STRING(taxon_concepts_mview.extinct_distribution_ary, ',') AS extinct_distribution,
-  ARRAY_TO_STRING(taxon_concepts_mview.extinct_uncertain_distribution_ary, ',') AS extinct_uncertain_distribution,
-  ARRAY_TO_STRING(taxon_concepts_mview.uncertain_distribution_ary, ',') AS uncertain_distribution
+  ARRAY_TO_STRING(taxon_concepts_mview.all_distribution_ary, ', ') AS all_distribution,
+  ARRAY_TO_STRING(taxon_concepts_mview.all_distribution_iso_codes_ary, ', ') AS all_distribution_iso_codes,
+  ARRAY_TO_STRING(taxon_concepts_mview.native_distribution_ary, ', ') AS native_distribution,
+  ARRAY_TO_STRING(taxon_concepts_mview.introduced_distribution_ary, ', ') AS introduced_distribution,
+  ARRAY_TO_STRING(taxon_concepts_mview.introduced_uncertain_distribution_ary, ', ') AS introduced_uncertain_distribution,
+  ARRAY_TO_STRING(taxon_concepts_mview.reintroduced_distribution_ary, ', ') AS reintroduced_distribution,
+  ARRAY_TO_STRING(taxon_concepts_mview.extinct_distribution_ary, ', ') AS extinct_distribution,
+  ARRAY_TO_STRING(taxon_concepts_mview.extinct_uncertain_distribution_ary, ', ') AS extinct_uncertain_distribution,
+  ARRAY_TO_STRING(taxon_concepts_mview.uncertain_distribution_ary, ', ') AS uncertain_distribution
 FROM "taxon_concepts_mview"
 JOIN cms_listing_changes_mview listing_changes_mview
    ON listing_changes_mview.taxon_concept_id = taxon_concepts_mview.id
