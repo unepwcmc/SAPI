54a0687a1daf123271377f1b50284b6162f51ab6
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-02 10:18:10 +0100

    legislation

--- app/models/nomenclature_change/component.rb
+++ app/models/nomenclature_change/component.rb
@@ -5,10 +5,11 @@
 class NomenclatureChange::Component < ActiveRecord::Base
   track_who_does_it
   attr_accessible :created_by_id, :is_input, :is_output,
-    :nomenclature_change_id, :taxon_concept_id, :updated_by_id,
+    :nomenclature_change_id, :taxon_concept_id, :note, :updated_by_id,
     :input_parent_reassignments_attributes,
     :input_name_reassignments_attributes,
-    :input_distribution_reassignments_attributes
+    :input_distribution_reassignments_attributes,
+    :input_legislation_reassignments_attributes
   belongs_to :taxon_concept
   has_many :input_parent_reassignments,
     :class_name => NomenclatureChange::ParentReassignment,
@@ -19,8 +20,12 @@ class NomenclatureChange::Component < ActiveRecord::Base
   has_many :input_distribution_reassignments,
     :class_name => NomenclatureChange::DistributionReassignment,
     :foreign_key => :nomenclature_change_input_id, :dependent => :destroy
+  has_many :input_legislation_reassignments,
+    :class_name => NomenclatureChange::LegislationReassignment,
+    :foreign_key => :nomenclature_change_input_id, :dependent => :destroy
   validates_presence_of :taxon_concept_id, :if => Proc.new { |c| c.is_input }
   accepts_nested_attributes_for :input_parent_reassignments, :allow_destroy => true
   accepts_nested_attributes_for :input_name_reassignments, :allow_destroy => true
   accepts_nested_attributes_for :input_distribution_reassignments, :allow_destroy => true
+  accepts_nested_attributes_for :input_legislation_reassignments, :allow_destroy => true
 end
