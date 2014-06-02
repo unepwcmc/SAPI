ece8de39c0e15ea9fbf1d45712d3c3b32286053d
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-02 09:32:30 +0100

    distribution

--- app/models/nomenclature_change/component.rb
+++ app/models/nomenclature_change/component.rb
@@ -7,13 +7,20 @@ class NomenclatureChange::Component < ActiveRecord::Base
   attr_accessible :created_by_id, :is_input, :is_output,
     :nomenclature_change_id, :taxon_concept_id, :updated_by_id,
     :input_parent_reassignments_attributes,
-    :input_name_reassignments_attributes
+    :input_name_reassignments_attributes,
+    :input_distribution_reassignments_attributes
   belongs_to :taxon_concept
-  has_many :input_parent_reassignments, :class_name => NomenclatureChange::ParentReassignment,
+  has_many :input_parent_reassignments,
+    :class_name => NomenclatureChange::ParentReassignment,
     :foreign_key => :nomenclature_change_input_id, :dependent => :destroy
-  has_many :input_name_reassignments, :class_name => NomenclatureChange::NameReassignment,
+  has_many :input_name_reassignments,
+    :class_name => NomenclatureChange::NameReassignment,
+    :foreign_key => :nomenclature_change_input_id, :dependent => :destroy
+  has_many :input_distribution_reassignments,
+    :class_name => NomenclatureChange::DistributionReassignment,
     :foreign_key => :nomenclature_change_input_id, :dependent => :destroy
   validates_presence_of :taxon_concept_id, :if => Proc.new { |c| c.is_input }
   accepts_nested_attributes_for :input_parent_reassignments, :allow_destroy => true
   accepts_nested_attributes_for :input_name_reassignments, :allow_destroy => true
+  accepts_nested_attributes_for :input_distribution_reassignments, :allow_destroy => true
 end
