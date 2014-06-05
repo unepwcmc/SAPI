bb3fa88ba547a9c43eff2036e2ecd7086b510e3b
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-05 22:33:27 +0100

    handle multiple reassignment targets

--- app/models/nomenclature_change/parent_reassignment.rb
+++ app/models/nomenclature_change/parent_reassignment.rb
@@ -1,2 +1,6 @@
 class NomenclatureChange::ParentReassignment < NomenclatureChange::Reassignment
+  attr_accessible :reassignment_target_attributes
+  has_one :reassignment_target, :class_name => NomenclatureChange::ReassignmentTarget,
+    :foreign_key => :nomenclature_change_reassignment_id
+  accepts_nested_attributes_for :reassignment_target, :allow_destroy => true
 end
