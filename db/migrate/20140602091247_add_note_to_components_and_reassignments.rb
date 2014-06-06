e88c722d6a55b250ea655b066cdc9370bdb961ee
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-06 13:48:49 +0100

    split components into inputs and outputs

--- db/migrate/20140602091247_add_note_to_components_and_reassignments.rb
+++
@@ -1,6 +0,0 @@
-class AddNoteToComponentsAndReassignments < ActiveRecord::Migration
-  def change
-    add_column :nomenclature_change_components, :note, :text
-    add_column :nomenclature_change_reassignments, :note, :text
-  end
-end
