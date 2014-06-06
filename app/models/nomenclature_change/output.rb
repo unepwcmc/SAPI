b8a22634b28ca9c72572054549e8e8b96745b118
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-06 14:57:48 +0100

    added progress bar

--- app/models/nomenclature_change/output.rb
+++ app/models/nomenclature_change/output.rb
@@ -5,6 +5,8 @@ class NomenclatureChange::Output < ActiveRecord::Base
   track_who_does_it
   attr_accessible :created_by_id, :new_author_year, :new_full_name, :new_name_status, :new_parent_id, :new_rank_id, :new_taxon_concept_id, :nomenclature_change_id, :note, :taxon_concept_id, :updated_by_id
   belongs_to :taxon_concept
+  has_many :reassignment_targets, :class_name => NomenclatureChange::ReassignmentTarget,
+    :foreign_key => :nomenclature_change_output_id, :dependent => :destroy
   validates_presence_of :new_full_name, :if => Proc.new { |c| c.taxon_concept_id.blank? }
   validates_presence_of :new_author_year, :if => Proc.new { |c| c.taxon_concept_id.blank? }
   validates_presence_of :new_name_status, :if => Proc.new { |c| c.taxon_concept_id.blank? }
