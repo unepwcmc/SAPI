e42b8144fa4cdffbb5a9731d5a0ee9212b50bdd3
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-06 23:03:31 +0100

    started work on processing nomenclature changes

--- app/models/nomenclature_change/parent_reassignment.rb
+++ app/models/nomenclature_change/parent_reassignment.rb
@@ -1,6 +1,12 @@
+# Reassignable is a taxon concept that is reassigned to a new parent
 class NomenclatureChange::ParentReassignment < NomenclatureChange::Reassignment
   attr_accessible :reassignment_target_attributes
   has_one :reassignment_target, :class_name => NomenclatureChange::ReassignmentTarget,
     :foreign_key => :nomenclature_change_reassignment_id
   accepts_nested_attributes_for :reassignment_target, :allow_destroy => true
+
+  def process
+    # TODO handle name changes
+  end
+
 end
