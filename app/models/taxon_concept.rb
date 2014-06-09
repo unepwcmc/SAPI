4b5b5cfe92644a0b74b4b205b33b859cc84839ee
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-09 08:33:24 +0100

    simplified parent validations

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -135,8 +135,8 @@ class TaxonConcept < ActiveRecord::Base
 
   validates :taxonomy_id, :presence => true
   validates :rank_id, :presence => true
-  validate :parent_in_same_taxonomy
-  validate :parent_at_immediately_higher_rank
+  validate :parent_in_same_taxonomy, :if => lambda { |tc| tc.parent }
+  validate :parent_at_immediately_higher_rank, :if => lambda { |tc| tc.parent }
   validates :taxon_name_id, :presence => true,
     :unless => lambda { |tc| tc.taxon_name.try(:valid?) }
   validates :taxon_name_id, :uniqueness => { :scope => [:taxonomy_id, :parent_id, :name_status, :author_year] }
@@ -279,7 +279,6 @@ class TaxonConcept < ActiveRecord::Base
   end
 
   def parent_in_same_taxonomy
-    return true unless parent
     if taxonomy_id != parent.taxonomy_id
       errors.add(:parent_id, "must be in same taxonomy")
       return false
@@ -287,7 +286,6 @@ class TaxonConcept < ActiveRecord::Base
   end
 
   def parent_at_immediately_higher_rank
-    return true unless parent
     return true if (parent.rank.name == 'KINGDOM' && parent.full_name == 'Plantae' && rank.name == 'ORDER')
     unless parent.rank.taxonomic_position >= rank.parent_rank_lower_bound &&
       parent.rank.taxonomic_position < rank.taxonomic_position
