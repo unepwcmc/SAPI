51df29ef27aad817963b086f11fec4b8a4b0433a
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-06-10 14:13:32 +0100

    [#49202187] Adds before_validation callback to ensure that common names are not duplicated. And to avoid propagating changes made to a common name to other taxon concepts' common names

--- app/models/taxon_common.rb
+++ app/models/taxon_common.rb
@@ -18,4 +18,23 @@ class TaxonCommon < ActiveRecord::Base
   belongs_to :common_name
   belongs_to :taxon_concept
   accepts_nested_attributes_for :common_name
+
+  before_validation do
+    cname = self.common_name
+    return unless cname.valid?
+
+    if cname.new_record?
+      #check if it exists or use the provided details
+      self.common_name = CommonName.find_by_name_and_language_id(
+        cname.name, cname.language_id) || cname
+    elsif cname.changed? && TaxonCommon.where(:common_name_id => cname.id).count > 1
+      #if changed and associated with multiple taxonConcepts, create a new
+      #common name
+      self.common_name = CommonName.create(
+        :name => cname.name,
+        :language_id => cname.language_id
+      )
+    end
+    self.touch if cname.new_record? || cname.changed?
+  end
 end
