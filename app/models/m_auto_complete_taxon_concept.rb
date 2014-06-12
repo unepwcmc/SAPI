48dec3ca3532bf5167690db668d56da5371d14ed
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-13 00:06:53 +0100

    sync changes in autocomplete

--- app/models/m_auto_complete_taxon_concept.rb
+++ app/models/m_auto_complete_taxon_concept.rb
@@ -23,6 +23,7 @@ class MAutoCompleteTaxonConcept < ActiveRecord::Base
   self.primary_key = :id
   scope :by_cites_eu_taxonomy, where(:taxonomy_is_cites_eu => true)
   scope :by_cms_taxonomy, where(:taxonomy_is_cites_eu => false)
+  translates :rank_display_name
   def matching_names
     parse_pg_array(read_attribute(:matching_names_ary))
   end
