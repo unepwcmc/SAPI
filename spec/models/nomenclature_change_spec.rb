030df427f8d0260d55ac1180d6fae5d3232a52d3
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-13 13:38:03 +0100

    validate required split inputs / outputs

--- spec/models/nomenclature_change_spec.rb
+++ spec/models/nomenclature_change_spec.rb
@@ -8,5 +8,53 @@ describe NomenclatureChange do
       }
       specify { expect(nomenclature_change).not_to be_valid }
     end
+    context "when previous status=submitted" do
+      let(:nomenclature_change){
+        nc = create(:nomenclature_change, :status => NomenclatureChange::SUBMITTED)
+        nc.status = NomenclatureChange::NEW
+        nc
+      }
+      specify { expect(nomenclature_change).not_to be_valid }
+    end
+    context "when previous status=closed" do
+      let(:nomenclature_change){
+        nc = create(:nomenclature_change, :status => NomenclatureChange::CLOSED)
+        nc.status = NomenclatureChange::SUBMITTED
+        nc
+      }
+      specify { expect(nomenclature_change).not_to be_valid }
+    end
+  end
+  describe :submitting? do
+    context "when new object with status=submitted" do
+      let(:nomenclature_change){
+        build(:nomenclature_change, :status => NomenclatureChange::SUBMITTED)
+      }
+      specify{ expect(nomenclature_change).to be_submitting }
+    end
+    context "when updating object with status new -> submitted" do
+      let(:nomenclature_change){
+        nc = create(:nomenclature_change, :status => NomenclatureChange::NEW)
+        nc.status = NomenclatureChange::SUBMITTED
+        nc
+      }
+      specify{ expect(nomenclature_change).to be_submitting }
+    end
+    context "when updating object with status submitted -> closed" do
+      let(:nomenclature_change){
+        nc = create(:nomenclature_change, :status => NomenclatureChange::SUBMITTED)
+        nc.status = NomenclatureChange::CLOSED
+        nc
+      }
+      specify{ expect(nomenclature_change).not_to be_submitting }
+    end
+    context "when updating object with status closed -> submitted" do
+      let(:nomenclature_change){
+        nc = create(:nomenclature_change, :status => NomenclatureChange::CLOSED)
+        nc.status = NomenclatureChange::SUBMITTED
+        nc
+      }
+      specify{ expect(nomenclature_change).not_to be_submitting }
+    end
   end
 end
