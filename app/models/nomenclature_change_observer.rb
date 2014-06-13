5e1eddf4456b854fffd382b647db9d77e6b860a1
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-13 13:38:03 +0100

    validate required split inputs / outputs

--- app/models/nomenclature_change_observer.rb
+++ app/models/nomenclature_change_observer.rb
@@ -1,9 +1,14 @@
 class NomenclatureChangeObserver < ActiveRecord::Observer
 
   def before_save(nomenclature_change)
-    if nomenclature_change.status == 'submitted' && nomenclature_change.status_changed?
-      processor_klass = "#{nomenclature_change.type}::Processor".constantize
-      processor_klass.new(nomenclature_change).run
+    if nomenclature_change.submitting?
+      begin
+        processor_klass = "#{nomenclature_change.type}::Processor".constantize
+      rescue NameError
+        Rails.logger.warn "No processor found for #{nomenclature_change.type}"
+      else
+        processor_klass.new(nomenclature_change).run
+      end
     end
   end
 
--- lib/modules/dictionary.rb
+++ lib/modules/dictionary.rb
@@ -8,8 +8,21 @@ module Dictionary
     #builds a set of constants like: KEY = 'KEY'
     #as well as a method self.dict that returns all of those constants' values
     def build_dictionary *keys
+      # keys.each do |key|
+      #   const_set key.to_s.upcase, key.to_s.upcase
+      # end
+      # define_singleton_method("dict") { keys.map{|k| k.to_s.upcase } }
+      build_basic_dictionary(*keys){ |key| key.to_s.upcase }
+    end
+
+    def build_basic_dictionary *keys
       keys.each do |key|
-        const_set key.to_s.upcase, key.to_s.upcase
+        const_set key.to_s.upcase,
+        if block_given?
+          yield(key)
+        else
+          key
+        end
       end
       define_singleton_method("dict") { keys.map{|k| k.to_s.upcase } }
     end
