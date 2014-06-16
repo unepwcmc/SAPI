e7a1c8c7b3a2f9a27424333a2ee36f2aeb5efe0e
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-16 09:08:19 +0100

    validate output taxon concept

--- spec/models/nomenclature_change/output_spec.rb
+++ spec/models/nomenclature_change/output_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe NomenclatureChange::Output do
+  before(:each){ cites_eu }
   describe :validate do
     context "when nomenclature change not specified" do
       let(:output){
@@ -8,14 +9,32 @@ describe NomenclatureChange::Output do
       }
       specify { expect(output).not_to be_valid }
     end
-    context "when taxon concept not specified and new_full_name not specified" do
+    context "when taxon concept not specified and new taxon concept attributes not specified" do
       let(:output){
         build(
           :nomenclature_change_output, :taxon_concept_id => nil,
-          :new_full_name => nil
+          :new_full_name => nil,
+          :new_parent_id => nil,
+          :new_rank_id => nil,
+          :new_name_status => nil
         )
       }
-      specify { expect(output).not_to be_valid }
+      specify { expect(output).to have(1).errors_on(:new_full_name) }
+      specify { expect(output).to have(1).errors_on(:new_parent_id) }
+      specify { expect(output).to have(1).errors_on(:new_rank_id) }
+      specify { expect(output).to have(1).errors_on(:new_name_status) }
+    end
+    context "when new taxon concept invalid" do
+      let(:output){
+        build(
+          :nomenclature_change_output, :taxon_concept_id => nil,
+          :new_full_name => 'xxx',
+          :new_parent_id => create_cites_eu_species.id,
+          :new_rank_id => species_rank.id,
+          :new_name_status => nil
+        )
+      }
+      specify { expect(output).to have(1).errors_on(:new_parent_id) }
     end
   end
 end
