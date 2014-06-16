b418e6e73fdf5a8f0e6171f8161fc075da074885
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-16 23:09:55 +0100

    extracted summarizers to separate classes

--- app/models/nomenclature_change/output.rb
+++ app/models/nomenclature_change/output.rb
@@ -37,41 +37,6 @@ class NomenclatureChange::Output < ActiveRecord::Base
 
   end
 
-  def transformations_summary
-    res = []
-    rank_name = new_rank.try(:name) || taxon_concept.try(:rank).try(:name)
-    full_name = display_full_name
-    name_status = new_name_status || taxon_concept.try(:name_status)
-    if taxon_concept.blank?
-      res << "New #{rank_name} #{full_name} (#{name_status}) will be created"
-    elsif taxon_concept.full_name != new_full_name
-      res << "New #{rank_name} #{full_name} (#{name_status}) will be created, based on #{taxon_concept.full_name}"
-      if ['A', 'N', 'H'].include? taxon_concept.name_status
-        res << "#{taxon_concept.full_name} will be turned into a synonym of #{display_full_name}"
-      end
-    else
-      changes = []
-      if new_rank
-        changes << "rank changed from #{taxon_concept.rank.name} to #{new_rank.name}"
-      end
-      if new_parent
-        changes << "parent changed from #{taxon_concept.parent.full_name} to #{new_parent.full_name}"
-      end
-      if new_name_status
-        changes << "name status changed from #{taxon_concept.name_status} to #{new_name_status}"
-      end
-      if new_author_year
-        changes << "author year changed from #{taxon_concept.author_year} to #{new_author_year}"
-      end
-      changes.compact!
-      unless changes.empty?
-        res << "The following changes will be applied to #{taxon_concept.full_name}:"
-        res << changes
-      end
-    end
-    res
-  end
-
   # Returns true when the new taxon has a different name from old one
   def will_create_taxon?
     taxon_concept.nil? ||
