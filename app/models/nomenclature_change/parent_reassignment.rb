d300b37a847f202c254a7a200cdbd6c032bbd846
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-16 23:10:32 +0100

    fixed associations defs & association presence validations

--- app/models/nomenclature_change/parent_reassignment.rb
+++ app/models/nomenclature_change/parent_reassignment.rb
@@ -1,7 +1,8 @@
 # Reassignable is a taxon concept that is reassigned to a new parent
 class NomenclatureChange::ParentReassignment < NomenclatureChange::Reassignment
   attr_accessible :reassignment_target_attributes
-  has_one :reassignment_target, :class_name => NomenclatureChange::ReassignmentTarget,
+  has_one :reassignment_target, :inverse_of => :reassignment,
+    :class_name => NomenclatureChange::ReassignmentTarget,
     :foreign_key => :nomenclature_change_reassignment_id
   accepts_nested_attributes_for :reassignment_target, :allow_destroy => true
 
