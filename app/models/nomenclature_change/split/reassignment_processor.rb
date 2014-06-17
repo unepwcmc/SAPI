dcbe271be5600e8554b1bb11ef38b5f11ea0f799
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-17 23:08:36 +0100

    specs for reassignments

--- app/models/nomenclature_change/split/reassignment_processor.rb
+++ app/models/nomenclature_change/split/reassignment_processor.rb
@@ -15,8 +15,8 @@ class NomenclatureChange::Split::ReassignmentProcessor
   private
 
   # all objects of reassignable_type that are linked to input taxon
-  def reassignables_by_class
-    Object::const_get(@reassignment.reassignable_type).where(
+  def reassignables_by_class(reassignable_type)
+    Object::const_get(reassignable_type).where(
       :taxon_concept_id => @input.taxon_concept.id
     )
   end
@@ -26,20 +26,20 @@ class NomenclatureChange::Split::ReassignmentProcessor
     reassignment.reassignment_targets.select do |target|
       target.output != @input
     end.each do |target|
-      if @reassignment.reassignable_id.blank?
-        reassignables_by_class.each do |reassignable|
+      if reassignment.reassignable_id.blank?
+        reassignables_by_class(reassignment.reassignable_type).each do |reassignable|
           process_target(target, reassignable)
         end
       else
-        process_target(target, @reassignment.reassignable)
+        process_target(target, reassignment.reassignable)
       end
     end
-    unless @nc.outputs.include?(@input)
+    unless @nc.outputs.include?(@input) || reassignment.kind_of?(NomenclatureChange::ParentReassignment)
       # input is not part of the split
       # delete original association
       Rails.logger.warn("Deleting #{reassignment.reassignable_type} (id=#{reassignment.reassignable_id}) from #{@input.taxon_concept.full_name}")
       if reassignment.reassignable_id.blank?
-        reassignables_by_class.each do |reassignable|
+        reassignables_by_class(reassignment.reassignable_type).each do |reassignable|
           reassignable.destroy
         end
       else
@@ -49,18 +49,20 @@ class NomenclatureChange::Split::ReassignmentProcessor
   end
 
   def process_target(target, reassignable)
-    new_object = reassignable.dup
+    new_taxon_concept = target.output.taxon_concept || target.output.new_taxon_concept
+    Rails.logger.debug("Processing reassignment to #{new_taxon_concept.full_name}")
+
     # TODO for listing changes this needs to copy: annotations, listing distributions and exceptions
     # TODO for distributions this needs to copy distribution references
     # TODO for trade restrictions this needs to copy purpose / source / term links
-    Rails.logger.debug "NEW TAXON"
-    Rails.logger.debug target.output.reload.new_taxon_concept_id
-    new_object.taxon_concept_id = target.output.taxon_concept_id ||
-      target.output.reload.new_taxon_concept_id
-    # TODO in case synonyms / subspecies were selected as outputs
-    # this needs to consider the new_taxon_concept_id
-    # so any taxon creations need to be resolved earlier
-    new_object.save
+    if target.reassignment.kind_of? NomenclatureChange::ParentReassignment
+      reassignable.parent_id = new_taxon_concept.id
+      reassignable.save
+    else
+      new_object = reassignable.dup
+      new_object.taxon_concept_id = new_taxon_concept.id
+      new_object.save
+    end
   end
 
 end
---
+++ spec/models/nomenclature_change/split/reassignment_processor_spec.rb
@@ -0,0 +1,122 @@
+require 'spec_helper'
+
+describe NomenclatureChange::Split::ReassignmentProcessor do
+  describe :run do
+    let(:input_species){ create_cites_eu_species }
+    let!(:split){
+      s = create(:nomenclature_change_split)
+      s.update_attributes(
+        :input_attributes => {:taxon_concept_id => input_species.id},
+        :status => NomenclatureChange::Split::INPUTS
+      )
+      s
+    }
+    let(:input){ split.input }
+    let(:output_species){ create_cites_eu_species }
+    let(:output){
+      create(
+        :nomenclature_change_output, :nomenclature_change => split,
+        :taxon_concept_id => output_species.id
+      )
+    }
+    let(:processor){
+      NomenclatureChange::Split::ReassignmentProcessor.new(input, output)
+    }
+
+    context "when children" do
+      let(:input_species_child){
+        create_cites_eu_subspecies(:parent => input_species)
+      }
+      let(:reassignment){
+        create(:nomenclature_change_parent_reassignment,
+          :input => input,
+          :reassignable_id => input_species_child.id
+        )
+      }
+      let!(:reassignment_target){
+        create(:nomenclature_change_reassignment_target,
+          :reassignment => reassignment,
+          :output => output
+        )
+      }
+      before(:each) do
+        processor.run
+      end
+      specify{ expect(input_species_child.reload.parent).to eq(output_species) }
+      specify{ expect(input_species.children.count).to eq(0) }
+    end
+    context "when names" do
+      let(:input_species_synonym){
+        create_cites_eu_species
+      }
+      let(:input_species_synonym_rel){
+        create(:taxon_relationship,
+          :taxon_relationship_type_id => synonym_relationship_type.id,
+          :taxon_concept => input_species,
+          :other_taxon_concept => input_species_synonym
+        )
+      }
+      let(:reassignment){
+        create(:nomenclature_change_name_reassignment,
+          :input => input,
+          :reassignable_id => input_species_synonym_rel.id
+        )
+      }
+      let!(:reassignment_target){
+        create(:nomenclature_change_reassignment_target,
+          :reassignment => reassignment,
+          :output => output
+        )
+      }
+      before(:each) do
+        processor.run
+      end
+      specify{ expect(output_species.synonyms).to include(input_species_synonym) }
+      specify{ expect(input_species.synonyms).not_to include(input_species_synonym) }
+    end
+    context "when listing changes" do
+      let!(:input_species_listing){
+        create_cites_I_addition(:taxon_concept => input_species)
+      }
+      let(:reassignment){
+        create(:nomenclature_change_legislation_reassignment,
+          :input => input,
+          :reassignable_type => 'ListingChange'
+        )
+      }
+      let!(:reassignment_target){
+        create(:nomenclature_change_reassignment_target,
+          :reassignment => reassignment,
+          :output => output
+        )
+      }
+      before(:each) do
+        processor.run
+      end
+      specify{ expect(output_species.listing_changes.count).to eq(1) }
+      specify{ expect(input_species.listing_changes.count).to eq(0) }
+    end
+    context "when common names" do
+      let!(:input_species_common_name){
+        create(:taxon_common, :taxon_concept => input_species)
+      }
+      let(:reassignment){
+        create(:nomenclature_change_reassignment,
+          :input => input,
+          :reassignable_type => 'TaxonCommon'
+        )
+      }
+      let!(:reassignment_target){
+        create(:nomenclature_change_reassignment_target,
+          :reassignment => reassignment,
+          :output => output
+        )
+      }
+      before(:each) do
+        processor.run
+      end
+      specify{ expect(output_species.common_names.count).to eq(1) }
+      specify{ expect(input_species.common_names.count).to eq(0) }
+    end
+  end
+end
