87d4d179df2258e0dd4d016252162332d516a8fa
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-06-18 12:25:05 +0100

    Copied split files as a scaffold for lump.

---
+++ app/models/nomenclature_change/lump/transformation_summarizer.rb
@@ -0,0 +1,50 @@
+class NomenclatureChange::Lump::TransformationSummarizer
+
+  delegate :new_rank, :to => :"@output"
+  delegate :new_parent, :to => :"@output"
+  delegate :new_full_name, :to => :"@output"
+  delegate :new_author_year, :to => :"@output"
+  delegate :new_name_status, :to => :"@output"
+  delegate :display_full_name, :to => :"@output"
+  delegate :taxon_concept, :to => :"@output"
+
+  def initialize(output)
+    @output = output
+  end
+
+  def summary
+    res = []
+    rank_name = new_rank.try(:name) || taxon_concept.try(:rank).try(:name)
+    full_name = display_full_name
+    name_status = new_name_status || taxon_concept.try(:name_status)
+    if taxon_concept.blank?
+      res << "New #{rank_name} #{full_name} (#{name_status}) will be created"
+    elsif taxon_concept.full_name != new_full_name
+      res << "New #{rank_name} #{full_name} (#{name_status}) will be created, based on #{taxon_concept.full_name}"
+      if ['A', 'N', 'H'].include? taxon_concept.name_status
+        res << "#{taxon_concept.full_name} will be turned into a synonym of #{display_full_name}"
+      end
+    else
+      changes = []
+      if new_rank
+        changes << "rank changed from #{taxon_concept.rank.name} to #{new_rank.name}"
+      end
+      if new_parent
+        changes << "parent changed from #{taxon_concept.parent.full_name} to #{new_parent.full_name}"
+      end
+      if new_name_status
+        changes << "name status changed from #{taxon_concept.name_status} to #{new_name_status}"
+      end
+      if new_author_year
+        changes << "author year changed from #{taxon_concept.author_year} to #{new_author_year}"
+      end
+      changes.compact!
+      unless changes.empty?
+        res << "The following changes will be applied to #{taxon_concept.full_name}:"
+        res << changes
+      end
+    end
+    res
+  end
+
+end
