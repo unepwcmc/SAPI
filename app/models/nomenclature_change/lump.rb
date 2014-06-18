2cb48276d1c3eb678c7ec3a02f11c89e80ff30dc
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-06-18 12:25:05 +0100

    Copied split files as a scaffold for lump.

---
+++ app/models/nomenclature_change/lump.rb
@@ -0,0 +1,54 @@
+class NomenclatureChange::Lump < NomenclatureChange
+  STEPS = [:inputs, :outputs, :children, :names, :distribution, :legislation, :notes, :summary]
+  STATUSES = ['new', 'submitted'] + STEPS.map(&:to_s)
+  build_basic_dictionary(*STATUSES)
+  has_one :input, :inverse_of => :nomenclature_change,
+    :class_name => NomenclatureChange::Input,
+    :foreign_key => :nomenclature_change_id,
+    :dependent => :destroy
+  has_many :outputs, :inverse_of => :nomenclature_change,
+    :class_name => NomenclatureChange::Output,
+    :foreign_key => :nomenclature_change_id,
+    :dependent => :destroy
+  accepts_nested_attributes_for :input, :allow_destroy => true
+  accepts_nested_attributes_for :outputs, :allow_destroy => true
+
+  validates :status, inclusion: {
+    in: STATUSES,
+    message: "%{value} is not a valid status"
+  }
+  validate :required_inputs, if: :inputs_or_submitting?
+  validate :required_outputs, if: :outputs_or_submitting?
+  validate :required_ranks, if: :outputs_or_submitting?
+
+  def required_inputs
+    if input.blank?
+      errors.add(:input, "Must have one input")
+      return false
+    end
+  end
+
+  def required_outputs
+    if outputs.size < 2
+      errors.add(:outputs, "Must have at least two outputs")
+      return false
+    end
+  end
+
+  def required_ranks
+    if !(outputs.map{ |o| o.new_rank.try(:name) || o.taxon_concept.try(:rank).try(:name) }.
+      uniq - [input.try(:taxon_concept).try(:rank).try(:name)]).empty?
+      errors.add(:outputs, "Must be at same rank as input")
+      return false
+    end
+  end
+
+  def inputs_or_submitting?
+    status == NomenclatureChange::Lump::INPUTS || submitting?
+  end
+
+  def outputs_or_submitting?
+    status == NomenclatureChange::Lump::OUTPUTS || submitting?
+  end
+
+end
