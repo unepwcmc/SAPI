1e466410f94aefb1cb3d4c70a8fd68f4fa560ad1
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-24 12:39:01 +0100

    status change skeleton

--- app/models/nomenclature_change/status_change/constructor.rb
+++ app/models/nomenclature_change/status_change/constructor.rb
@@ -5,30 +5,46 @@ class NomenclatureChange::StatusChange::Constructor
     @nomenclature_change = nomenclature_change
   end
 
-  def build_outputs
-    @nomenclature_change.outputs.build if @nomenclature_change.outputs.empty?
+  def build_primary_output
+    if @nomenclature_change.primary_output.nil?
+      @nomenclature_change.build_primary_output(
+        :is_primary_output => true
+      )
+    end
+  end
+
+  def build_secondary_output
+    if @nomenclature_change.needs_to_relay_associations? &&
+      @nomenclature_change.secondary_output.nil?
+      @nomenclature_change.build_secondary_output(
+        :is_primary_output => false
+      )
+    end
   end
 
   def build_input
-    output = @nomenclature_change.output_that_needs_reassignments
-    if output
-      input = @nomenclature_change.input
-      if input.nil? || input.taxon_concept_id != output_nr.taxon_concept_id
-        # we need to create an input with same taxon as this output
-        @nomenclature_change.build_input(taxon_concept_id: output.taxon_concept_id)
-      end
-    else
-      #TODO check if necessary, consider swaps
+    input = @nomenclature_change.input
+    output = @nomenclature_change.primary_output
+    if @nomenclature_change.needs_to_relay_associations? && (
+      input.nil? || input.taxon_concept_id != output.taxon_concept_id
+      )
+      # we need to create an input with same taxon as this output
+      @nomenclature_change.build_input(
+        taxon_concept_id: output.taxon_concept_id
+      )
+    elsif @nomenclature_change.needs_to_receive_associations? && input.nil?
       @nomenclature_change.build_input
     end
   end
 
   def build_reassignments
-    return false unless @nomenclature_change.needs_reassignments?
     input = @nomenclature_change.input
-    return false unless input
-    # there needs to be a second output, which will be the reassignment target
-    output = @nomenclature_change.output_that_receives_reassignments
+    output = if @nomenclature_change.needs_to_relay_associations?
+      @nomenclature_change.secondary_output
+    elsif @nomenclature_change.needs_to_receive_associations?
+      @nomenclature_change.primary_output
+    end
+    return false unless input && output
     _build_parent_reassignments(input, output)
     _build_names_reassignments(input, [outputs])
     _build_distribution_reassignments(input, [output])
@@ -39,11 +55,17 @@ class NomenclatureChange::StatusChange::Constructor
 
   def build_output_notes
     event = @nomenclature_change.event
-    @nomenclature_change.outputs_except_inputs.each do |output|
-      if output.note.blank?
-        output.note = "#{output.display_full_name} status change from #{output.taxon_concept.name_status} to #{output.new_name_status} in #{Date.today.year}"
-        output.note << " following taxonomic changes adopted at #{event.try(:name)}" if event
-      end
+
+    if @nomenclature_change.primary_output.note.blank?
+      output = @nomenclature_change.primary_output
+      output.note = "#{output.display_full_name} status change from #{output.taxon_concept.name_status} to #{output.new_name_status} in #{Date.today.year}"
+      output.note << " following taxonomic changes adopted at #{event.try(:name)}" if event
+    end
+
+    if @nomenclature_change.is_swap? && @nomenclature_change.secondary_output.note.blank?
+      output = @nomenclature_change.secondary_output
+      output.note = "#{output.display_full_name} status change from #{output.taxon_concept.name_status} to #{output.new_name_status} in #{Date.today.year}"
+      output.note << " following taxonomic changes adopted at #{event.try(:name)}" if event
     end
   end
 end
