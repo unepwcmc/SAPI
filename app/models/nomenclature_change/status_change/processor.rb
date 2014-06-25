a44ceecee79f5eb55461516cd06671227fc81bb6
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-25 17:38:49 +0100

    added status change processor

---
+++ app/models/nomenclature_change/status_change/processor.rb
@@ -0,0 +1,34 @@
+class NomenclatureChange::StatusChange::Processor
+
+  def initialize(nc)
+    @nc = nc
+    @input = nc.input
+    @primary_output = nc.primary_output
+    @secondary_output = nc.secondary_output
+    @swap = @nc.is_swap?
+  end
+
+  def run
+    Rails.logger.warn("[#{@nc.type}] BEGIN")
+    Rails.logger.debug("[#{@nc.type}] Processing primary output #{@primary_output.display_full_name}")
+    processor = NomenclatureChange::StatusChange::TransformationProcessor.new(@primary_output)
+    processor.run
+    if @swap
+      Rails.logger.debug("[#{@nc.type}] Processing secondary output #{@secondary_output.display_full_name}")
+      processor = NomenclatureChange::StatusChange::TransformationProcessor.new(@secondary_output)
+      processor.run
+    end
+    output = if @nc.needs_to_relay_associations?
+      @secondary_output
+    elsif @nc.needs_to_receive_associations?
+      @primary_output
+    end
+    if @input && output
+      Rails.logger.debug("[#{@nc.type}] Processing reassignments from #{@input.taxon_concept.full_name}")
+      processor = NomenclatureChange::StatusChange::TransformationProcessor.new(@input, output)
+      processor.run
+    end
+    Rails.logger.warn("[#{@nc.type}] END")
+  end
+
+end
--- spec/models/nomenclature_change/status_change/constructor_spec.rb
+++ spec/models/nomenclature_change/status_change/constructor_spec.rb
@@ -1,82 +1,8 @@
 require 'spec_helper'
 
 describe NomenclatureChange::StatusChange::Constructor do
-  let(:input_species){ create_cites_eu_species }
-  let(:status_downgrade_with_primary_output){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: create_cites_eu_species.id,
-      new_name_status: 'S'
-    )
-    s.reload
-  }
-  let(:status_upgrade_with_primary_output){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: create_cites_eu_species(:name_status => 'S').id,
-      new_name_status: 'A'
-    )
-    s.reload
-  }
-  let(:status_downgrade_with_input){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: input_species.id,
-      new_name_status: 'S'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    s
-  }
-  let(:status_downgrade_with_input_and_secondary_output){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: input_species.id,
-      new_name_status: 'S'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: false,
-      taxon_concept_id: create_cites_eu_species.id
-    )
-    s.reload
-  }
-  let(:status_downgrade_with_swap){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: input_species.id,
-      new_name_status: 'S'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: false,
-      taxon_concept_id: create_cites_eu_species.id,
-      new_name_status: 'A'
-    )
-    s.reload
-  }
-  let(:status_upgrade_with_input){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: create_cites_eu_species(:name_status => 'S').id,
-      new_name_status: 'A'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    s.reload
-  }
+  include_context 'status_change_definitions'
+
   let(:constructor){ NomenclatureChange::StatusChange::Constructor.new(status_change) }
   describe :build_primary_output do
     let(:status_change){ create(:nomenclature_change_status_change) }
