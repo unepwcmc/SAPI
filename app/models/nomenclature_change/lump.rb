47646462efb221ec914de8f14e4b7e5c59b0dc51
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-06-26 10:39:08 +0100

    For lumps: multiple inputs, single output

--- app/models/nomenclature_change/lump.rb
+++ app/models/nomenclature_change/lump.rb
@@ -2,16 +2,16 @@ class NomenclatureChange::Lump < NomenclatureChange
   STEPS = [:inputs, :outputs, :children, :names, :distribution, :legislation, :notes, :summary]
   STATUSES = ['new', 'submitted'] + STEPS.map(&:to_s)
   build_basic_dictionary(*STATUSES)
-  has_one :input, :inverse_of => :nomenclature_change,
+  has_one :output, :inverse_of => :nomenclature_change,
     :class_name => NomenclatureChange::Input,
     :foreign_key => :nomenclature_change_id,
     :dependent => :destroy
-  has_many :outputs, :inverse_of => :nomenclature_change,
+  has_many :inputs, :inverse_of => :nomenclature_change,
     :class_name => NomenclatureChange::Output,
     :foreign_key => :nomenclature_change_id,
     :dependent => :destroy
-  accepts_nested_attributes_for :input, :allow_destroy => true
-  accepts_nested_attributes_for :outputs, :allow_destroy => true
+  accepts_nested_attributes_for :inputs, :allow_destroy => true
+  accepts_nested_attributes_for :output, :allow_destroy => true
 
   validates :status, inclusion: {
     in: STATUSES,
@@ -22,23 +22,23 @@ class NomenclatureChange::Lump < NomenclatureChange
   validate :required_ranks, if: :outputs_or_submitting?
 
   def required_inputs
-    if input.blank?
-      errors.add(:input, "Must have one input")
+    if inputs.size < 2
+      errors.add(:inputs, "Must have at least two inputs")
       return false
     end
   end
 
   def required_outputs
-    if outputs.size < 2
-      errors.add(:outputs, "Must have at least two outputs")
+    if output.blank?
+      errors.add(:output, "Must have one output")
       return false
     end
   end
 
   def required_ranks
-    if !(outputs.map{ |o| o.new_rank.try(:name) || o.taxon_concept.try(:rank).try(:name) }.
-      uniq - [input.try(:taxon_concept).try(:rank).try(:name)]).empty?
-      errors.add(:outputs, "Must be at same rank as input")
+    if !(inputs.map{ |i| i.new_rank.try(:name) || i.taxon_concept.try(:rank).try(:name) }.
+      uniq - [output.try(:taxon_concept).try(:rank).try(:name)]).empty?
+      errors.add(:outputs, "Must be at same rank as inputs")
       return false
     end
   end
