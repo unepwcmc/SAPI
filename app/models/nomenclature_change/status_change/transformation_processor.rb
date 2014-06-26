7c5a719dbd2599c413bffd734932560c5472ca08
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-26 11:44:40 +0100

    removed possibly redundant code

--- app/models/nomenclature_change/status_change/transformation_processor.rb
+++ app/models/nomenclature_change/status_change/transformation_processor.rb
@@ -11,18 +11,6 @@ class NomenclatureChange::StatusChange::TransformationProcessor
       Rails.logger.warn "FAILED to save taxon #{tc.errors.inspect}"
       return false
     end
-    # I think this goes into nomenclature change processor really
-    if @nc.needs_to_relay_associations?
-      # mark old accepted name (primary output) as synonym
-      # of new accepted name (secondary output)
-      @nc.primary_output.taxon_relationships.each do |tr|
-        tr.taxon_concept_id = @nc.secondary_output.taxon_concept_id
-      end
-    elsif @nc.needs_to_receive_associations? && @nc.is_swap?
-      @nc.primary_output.inverse_taxon_relationships.each do |tr|
-        tr.other_taxon_concept_id = @nc.secondary_output.taxon_concept_id
-      end
-    end
   end
 
 end
