056273a7e7f447da80d56304122e0ac9f5c67e20
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-06-26 10:11:54 +0100

    unified style of split & status change spec setup

--- spec/models/nomenclature_change/shared/status_change_definitions.rb
+++ spec/models/nomenclature_change/shared/status_change_definitions.rb
@@ -1,97 +1,81 @@
 shared_context 'status_change_definitions' do
   let(:input_species){ create_cites_eu_species }
   let(:status_downgrade_with_primary_output){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: create_cites_eu_species.id,
-      new_name_status: 'S'
-    )
-    s.reload
+    create(:nomenclature_change_status_change,
+      primary_output_attributes: {
+        is_primary_output: true,
+        taxon_concept_id: create_cites_eu_species.id,
+        new_name_status: 'S'
+      },
+      status: NomenclatureChange::StatusChange::PRIMARY_OUTPUT
+    ).reload
   }
   let(:status_upgrade_with_primary_output){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: create_cites_eu_species(name_status: 'S').id,
-      new_name_status: 'A'
-    )
-    s.reload
-  }
-  let(:status_downgrade_with_input){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: create_cites_eu_species.id,
-      new_name_status: 'S'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    s
+    create(:nomenclature_change_status_change,
+      primary_output_attributes: {
+        is_primary_output: true,
+        taxon_concept_id: create_cites_eu_species(name_status: 'S').id,
+        new_name_status: 'A'
+      },
+      status: NomenclatureChange::StatusChange::PRIMARY_OUTPUT
+    ).reload
   }
   let(:status_downgrade_with_input_and_secondary_output){
-    #input_species = create_cites_eu_species
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: input_species.id,
-      new_name_status: 'S'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: false,
-      taxon_concept_id: create_cites_eu_species.id
-    )
-    s.reload
+    create(:nomenclature_change_status_change,
+      primary_output_attributes: {
+        is_primary_output: true,
+        taxon_concept_id: input_species.id,
+        new_name_status: 'S'
+      },
+      input_attributes: { taxon_concept_id: input_species.id },
+      secondary_output_attributes: {
+        is_primary_output: false,
+        taxon_concept_id: create_cites_eu_species.id
+      },
+      status: NomenclatureChange::StatusChange::RELAY_OR_SWAP
+    ).reload
   }
   let(:status_downgrade_with_swap){
-    #input_species = create_cites_eu_species
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: input_species.id,
-      new_name_status: 'S'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: false,
-      taxon_concept_id: create_cites_eu_species(name_status: 'S').id,
-      new_name_status: 'A'
-    )
-    s.reload
+    create(:nomenclature_change_status_change,
+      primary_output_attributes: {
+        is_primary_output: true,
+        taxon_concept_id: create_cites_eu_species.id,
+        new_name_status: 'S'
+      },
+      input_attributes: { taxon_concept_id: input_species.id },
+      secondary_output_attributes: {
+        is_primary_output: false,
+        taxon_concept_id: create_cites_eu_species(name_status: 'S').id,
+        new_name_status: 'A'
+      },
+      status: NomenclatureChange::StatusChange::RELAY_OR_SWAP
+    ).reload
   }
   let(:status_upgrade_with_input){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: create_cites_eu_species(name_status: 'S').id,
-      new_name_status: 'A'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    s.reload
+    create(:nomenclature_change_status_change,
+      primary_output_attributes: {
+        is_primary_output: true,
+        taxon_concept_id: create_cites_eu_species(name_status: 'S').id,
+        new_name_status: 'A'
+      },
+      input_attributes: { taxon_concept_id: input_species.id },
+      status: NomenclatureChange::StatusChange::RECEIVE_OR_SWAP
+    ).reload
   }
   let(:status_upgrade_with_swap){
-    s = create(:nomenclature_change_status_change)
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: true,
-      taxon_concept_id: create_cites_eu_species(name_status: 'S').id,
-      new_name_status: 'A'
-    )
-    create(:nomenclature_change_input, nomenclature_change: s,
-      taxon_concept_id: input_species.id
-    )
-    create(:nomenclature_change_output, nomenclature_change: s,
-      is_primary_output: false,
-      taxon_concept_id: input_species.id,
-      new_name_status: 'S'
-    )
-    s.reload
+    create(:nomenclature_change_status_change,
+      primary_output_attributes: {
+        is_primary_output: true,
+        taxon_concept_id: create_cites_eu_species(name_status: 'S').id,
+        new_name_status: 'A'
+      },
+      input_attributes: { taxon_concept_id: input_species.id },
+      secondary_output_attributes: {
+        is_primary_output: false,
+        taxon_concept_id: input_species.id,
+        new_name_status: 'S'
+      },
+      status: NomenclatureChange::StatusChange::RECEIVE_OR_SWAP
+    ).reload
   }
 end
