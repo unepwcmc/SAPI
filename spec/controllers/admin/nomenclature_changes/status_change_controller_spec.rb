c142fe3b34609527b717b7ed0bb34e4dd98cd9f8
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-01 17:09:53 +0100

    added legislation notes step to status change workflow

---
+++ spec/controllers/admin/nomenclature_changes/status_change_controller_spec.rb
@@ -0,0 +1,112 @@
+require 'spec_helper'
+
+describe Admin::NomenclatureChanges::StatusChangeController do
+  login_admin
+  include_context 'status_change_definitions'
+
+  describe 'GET show' do
+    context :primary_output do
+      before(:each) do
+        @status_change = create(:nomenclature_change_status_change)
+      end
+      it 'renders the primary_output template' do
+        get :show, id: :primary_output, nomenclature_change_id: @status_change.id
+        response.should render_template('primary_output')
+      end
+    end
+    context :receive_or_swap do
+      before(:each) do
+        @status_change = status_upgrade_with_primary_output
+      end
+      it 'renders the receive_or_swap template' do
+        get :show, id: :receive_or_swap, nomenclature_change_id: @status_change.id
+        response.should render_template('receive_or_swap')
+      end
+    end
+    context :relay_or_swap do
+      before(:each) do
+        @status_change = status_downgrade_with_primary_output
+      end
+      it 'renders the relay_or_swap template' do
+        get :show, id: :relay_or_swap, nomenclature_change_id: @status_change.id
+        response.should render_template('relay_or_swap')
+      end
+    end
+    context :notes do
+      before(:each) do
+        @status_change = status_upgrade_with_swap
+      end
+      it 'renders the notes template' do
+        get :show, id: :notes, nomenclature_change_id: @status_change.id
+        response.should render_template('notes')
+      end
+    end
+    context :reassignments do
+      before(:each) do
+        @status_change = status_upgrade_with_swap
+      end
+      context "when legislation present" do
+        before(:each) do
+          create_cites_I_addition(taxon_concept: input_species)
+        end
+        it 'renders the legislation template' do
+          get :show, id: :legislation, nomenclature_change_id: @status_change.id
+          response.should render_template('legislation')
+        end
+      end
+      context "when no legislation" do
+        it 'redirects to next step' do
+          get :show, id: :legislation, nomenclature_change_id: @status_change.id
+          response.should redirect_to(admin_nomenclature_change_status_change_url(
+            nomenclature_change_id: assigns(:nomenclature_change).id, :id => 'summary'
+          ))
+        end
+      end
+    end
+    context :summary do
+      before(:each) do
+        @status_change = status_upgrade_with_swap
+      end
+      it 'renders the summary template' do
+        get :show, id: :summary, nomenclature_change_id: @status_change.id
+        response.should render_template('summary')
+      end
+    end
+  end
+
+  describe 'POST create' do
+    it 'redirects to status_change wizard' do
+      post :create, nomenclature_change_id: 'new'
+      response.should redirect_to(admin_nomenclature_change_status_change_url(
+        nomenclature_change_id: assigns(:nomenclature_change).id, :id => 'primary_output'
+      ))
+    end
+  end
+
+  describe 'PUT update' do
+    before(:each) do
+      @status_change = create(:nomenclature_change_status_change)
+    end
+    context 'when successful' do
+      it 'redirects to next step' do
+        put :update, nomenclature_change_status_change: {
+          primary_output_attributes: {
+            taxon_concept_id: create_cites_eu_species.id,
+            new_name_status: 'S'
+          }
+        }, nomenclature_change_id: @status_change.id, id: 'primary_output'
+        response.should redirect_to(admin_nomenclature_change_status_change_url(
+          nomenclature_change_id: assigns(:nomenclature_change).id, :id => 'relay_or_swap'
+        ))
+      end
+    end
+    context 'when unsuccessful' do
+      it 're-renders step' do
+        put :update, nomenclature_change_status_change: {},
+          nomenclature_change_id: @status_change.id, id: 'primary_output'
+        response.should render_template('primary_output')
+      end
+    end
+  end
+
+end
