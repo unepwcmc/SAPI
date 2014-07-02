f39566ce860634dd7aa5b7cbb4fb193d2925c6b7
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-02 11:03:59 +0100

    moved the input - detaching process to split processor

--- app/models/nomenclature_change/split/processor.rb
+++ app/models/nomenclature_change/split/processor.rb
@@ -16,6 +16,22 @@ class NomenclatureChange::Split::Processor
       processor = NomenclatureChange::ReassignmentProcessor.new(@input, output)
       processor.run
     end
+    unless @outputs.map(&:taxon_concept).include?(@input.taxon_concept)
+      @input.reassignments.each do |reassignment|
+        unless reassignment.kind_of?(NomenclatureChange::ParentReassignment) # don't delete taxa
+          # input is not part of the split
+          # delete original association
+          Rails.logger.warn("Deleting #{reassignment.reassignable_type} (id=#{reassignment.reassignable_id}) from #{@input.taxon_concept.full_name}")
+          if reassignment.reassignable_id.blank?
+            reassignables_by_class(reassignment.reassignable_type).each do |reassignable|
+              reassignable.destroy
+            end
+          else
+            reassignment.reassignable.destroy
+          end
+        end
+      end
+    end
     Rails.logger.warn("[#{@nc.type}] END")
   end
 
