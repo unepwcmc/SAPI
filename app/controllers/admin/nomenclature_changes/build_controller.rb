3746d373afbb9c6c5833a4921e98a758f98c1134
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-02 11:49:11 +0100

    added taxonomy filter to taxon autocomplete

--- app/controllers/admin/nomenclature_changes/build_controller.rb
+++ app/controllers/admin/nomenclature_changes/build_controller.rb
@@ -17,6 +17,10 @@ class Admin::NomenclatureChanges::BuildController < Admin::AdminController
     @events = CitesCop.order('effective_at DESC')
   end
 
+  def set_taxonomy
+    @taxonomy = Taxonomy.find_by_name(Taxonomy::CITES_EU)
+  end
+
   def skip_or_previous_step
     if params[:back]
       jump_to(previous_step)
