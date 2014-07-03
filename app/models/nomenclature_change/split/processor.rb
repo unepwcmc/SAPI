de329f2a42ae7cde9fc65a9bd210d854c6b0fdb2
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-03 12:40:23 +0100

    added copy vs transfer reassignment processing

--- app/models/nomenclature_change/split/processor.rb
+++ app/models/nomenclature_change/split/processor.rb
@@ -8,29 +8,16 @@ class NomenclatureChange::Split::Processor
 
   def run
     Rails.logger.warn("[#{@nc.type}] BEGIN")
-    @outputs.each do |output|
+    @outputs.each_with_index do |output, idx|
       Rails.logger.debug("[#{@nc.type}] Processing output #{output.display_full_name}")
       processor = NomenclatureChange::Split::TransformationProcessor.new(output)
       processor.run
       Rails.logger.debug("[#{@nc.type}] Processing reassignments from #{@input.taxon_concept.full_name}")
+      # if input is not one of outputs and this is the last output
+      # transfer the associations rather than copy them
+      copy = !@outputs.map(&:taxon_concept).include?(input.taxon_concept) && idx = @outputs.length - 1
       processor = NomenclatureChange::ReassignmentProcessor.new(@input, output)
       processor.run
-    end
-    unless @outputs.map(&:taxon_concept).include?(@input.taxon_concept)
-      @input.reassignments.each do |reassignment|
-        unless reassignment.kind_of?(NomenclatureChange::ParentReassignment) # don't delete taxa
-          # input is not part of the split
-          # delete original association
-          Rails.logger.warn("Deleting #{reassignment.reassignable_type} (id=#{reassignment.reassignable_id}) from #{@input.taxon_concept.full_name}")
-          if reassignment.reassignable_id.blank?
-            reassignables_by_class(reassignment.reassignable_type).each do |reassignable|
-              reassignable.destroy
-            end
-          else
-            reassignment.reassignable.destroy
-          end
-        end
-      end
     end
     Rails.logger.warn("[#{@nc.type}] END")
   end
