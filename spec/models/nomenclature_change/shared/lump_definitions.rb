1eff92a624c98452e9fa8c20eb6d69d9ff697b0f
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-08 16:21:48 +0100

    fixed model specs

--- spec/models/nomenclature_change/shared/lump_definitions.rb
+++ spec/models/nomenclature_change/shared/lump_definitions.rb
@@ -1,12 +1,24 @@
 shared_context 'lump_definitions' do
-  let(:input_species1){ create_cites_eu_species }
+  let(:input_species){ create_cites_eu_species }
+  let(:input_species1){ input_species }
   let(:input_species2){ create_cites_eu_species }
   let(:output_species){ create_cites_eu_species }
+  let(:output_subspecies){
+    create_cites_eu_subspecies(
+      taxon_name: create(:taxon_name, scientific_name: 'fatalus'),
+      parent: create_cites_eu_species(
+        taxon_name: create(:taxon_name, scientific_name: 'fatalus'),
+        parent: create_cites_eu_genus(
+          taxon_name: create(:taxon_name, scientific_name: 'Errorus')
+        )
+      )
+    )
+  }
   let(:lump_with_inputs){
     create(:nomenclature_change_lump,
-      :inputs_attributes => {
-        0 => {:taxon_concept_id => input_species1.id},
-        1 => {:taxon_concept_id => input_species2.id}
+      inputs_attributes: {
+        0 => { taxon_concept_id: input_species1.id },
+        1 => { taxon_concept_id: input_species2.id }
       }
     )
   }
@@ -15,34 +27,34 @@ shared_context 'lump_definitions' do
   }
   let(:lump_with_inputs_and_same_output){
     create(:nomenclature_change_lump,
-      input_attributes: { taxon_concept_id: input_species.id },
-      outputs_attributes: {
-        0 => { taxon_concept_id: output_species1.id },
-        1 => { taxon_concept_id: input_species.id }
+      inputs_attributes: {
+        0 => { taxon_concept_id: input_species1.id },
+        1 => { taxon_concept_id: input_species2.id }
       },
+      output_attributes: { taxon_concept_id: input_species1.id },
       status: NomenclatureChange::Lump::OUTPUTS
     )
   }
   let(:lump_with_inputs_and_output_existing_taxon){
     create(:nomenclature_change_lump,
-      :inputs_attributes => {
-        0 => {:taxon_concept_id => input_species1.id},
-        1 => {:taxon_concept_id => input_species2.id}
+      inputs_attributes: {
+        0 => { taxon_concept_id: input_species1.id },
+        1 => { taxon_concept_id: input_species2.id }
       },
-      :output_attributes => {:taxon_concept_id => output_species.id},
+      output_attributes: { taxon_concept_id: output_species.id },
       status: NomenclatureChange::Lump::OUTPUTS
     )
   }
   let(:lump_with_inputs_and_output_new_taxon){
     create(:nomenclature_change_lump,
-      :inputs_attributes => {
-        0 => {:taxon_concept_id => input_species1.id},
-        1 => {:taxon_concept_id => input_species2.id}
+      inputs_attributes: {
+        0 => { taxon_concept_id: input_species1.id },
+        1 => { taxon_concept_id: input_species2.id }
       },
       output_attributes: {
         new_scientific_name: 'fatalus',
-        :new_parent_id => create_cites_eu_genus(
-          :taxon_name => create(:taxon_name, :scientific_name => 'Errorus')
+        new_parent_id: create_cites_eu_genus(
+          taxon_name: create(:taxon_name, scientific_name: 'Errorus')
         ).id,
         new_rank_id: species_rank.id,
         new_name_status: 'A'
@@ -52,9 +64,9 @@ shared_context 'lump_definitions' do
   }
   let(:lump_with_inputs_and_output_status_change){
     create(:nomenclature_change_lump,
-      :inputs_attributes => {
-        0 => {:taxon_concept_id => input_species1.id},
-        1 => {:taxon_concept_id => input_species2.id}
+      inputs_attributes: {
+        0 => { taxon_concept_id: input_species1.id },
+        1 => { taxon_concept_id: input_species2.id }
       },
       output_attributes: {
         taxon_concept_id: output_species.id,
@@ -63,17 +75,17 @@ shared_context 'lump_definitions' do
       status: NomenclatureChange::Lump::OUTPUTS
     )
   }
-  let(:lump_with_input_and_outputs_name_change){
+  let(:lump_with_inputs_and_output_name_change){
     create(:nomenclature_change_lump,
-      :inputs_attributes => {
-        0 => {:taxon_concept_id => input_species1.id},
-        1 => {:taxon_concept_id => input_species2.id}
+      inputs_attributes: {
+        0 => { taxon_concept_id: input_species1.id },
+        1 => { taxon_concept_id: input_species2.id }
       },
       output_attributes: {
-        taxon_concept_id: output_species.id,
+        taxon_concept_id: output_subspecies.id,
         new_scientific_name: 'lolcatus',
-        :new_parent_id => create_cites_eu_genus(
-          :taxon_name => create(:taxon_name, :scientific_name => 'Errorus')
+        new_parent_id: create_cites_eu_genus(
+          taxon_name: create(:taxon_name, scientific_name: 'Errorus')
         ).id,
         new_rank_id: species_rank.id,
         new_name_status: 'A'
