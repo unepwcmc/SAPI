6c3e39137f046bcadd6a84d5b0be615dd4bfeb01
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-09 16:33:46 +0100

    fixed controller specs

--- spec/controllers/admin/nomenclature_changes/lump_controller_spec.rb
+++ spec/controllers/admin/nomenclature_changes/lump_controller_spec.rb
@@ -34,61 +34,6 @@ describe Admin::NomenclatureChanges::LumpController do
         get :show, id: :notes, nomenclature_change_id: @lump.id
         response.should render_template('notes')
       end
-      context "when children present" do
-        before(:each) do
-          create_cites_eu_subspecies(parent: @input_species)
-        end
-        it 'renders the children template' do
-          get :show, id: :children, nomenclature_change_id: @lump.id
-          response.should render_template('children')
-        end
-      end
-      context "when no children" do
-        it 'redirects to next step' do
-          get :show, id: :children, nomenclature_change_id: @lump.id
-          response.should redirect_to(admin_nomenclature_change_lump_url(
-            nomenclature_change_id: assigns(:nomenclature_change).id, :id => 'names'
-          ))
-        end
-      end
-      context "when names present" do
-        before(:each) do
-          create(:taxon_relationship,
-            taxon_concept: @input_species,
-            other_taxon_concept: create_cites_eu_species(name_status: 'S'),
-            taxon_relationship_type: synonym_relationship_type
-          )
-        end
-        it 'renders the names template' do
-          get :show, id: :names, nomenclature_change_id: @lump.id
-          response.should render_template('names')
-        end
-      end
-      context "when no names" do
-        it 'redirects to next step' do
-          get :show, id: :names, nomenclature_change_id: @lump.id
-          response.should redirect_to(admin_nomenclature_change_lump_url(
-            nomenclature_change_id: assigns(:nomenclature_change).id, :id => 'distribution'
-          ))
-        end
-      end
-      context "when distribution present" do
-        before(:each) do
-          create(:distribution, taxon_concept: @input_species)
-        end
-        it 'renders the distribution template' do
-          get :show, id: :distribution, nomenclature_change_id: @lump.id
-          response.should render_template('distribution')
-        end
-      end
-      context "when no distribution" do
-        it 'redirects to next step' do
-          get :show, id: :distribution, nomenclature_change_id: @lump.id
-          response.should redirect_to(admin_nomenclature_change_lump_url(
-            nomenclature_change_id: assigns(:nomenclature_change).id, :id => 'legislation'
-          ))
-        end
-      end
       context "when legislation present" do
         before(:each) do
           create_cites_I_addition(taxon_concept: @input_species)
@@ -129,7 +74,10 @@ describe Admin::NomenclatureChanges::LumpController do
     context 'when successful' do
       it 'redirects to next step' do
         put :update, nomenclature_change_lump: {
-          input_attributes: {taxon_concept_id: create_cites_eu_species.id}
+          inputs_attributes: {
+            0 => {taxon_concept_id: create_cites_eu_species.id},
+            1 => {taxon_concept_id: create_cites_eu_species.id}
+          }
         }, nomenclature_change_id: @lump.id, id: 'inputs'
         response.should redirect_to(admin_nomenclature_change_lump_url(
           nomenclature_change_id: assigns(:nomenclature_change).id, :id => 'outputs'
@@ -139,7 +87,9 @@ describe Admin::NomenclatureChanges::LumpController do
     context 'when unsuccessful' do
       it 're-renders step' do
         put :update, nomenclature_change_lump: {
-            input_attributes: {taxon_concept_id: nil}
+            inputs_attributes: {
+              0 => {taxon_concept_id: nil}
+            }
           }, nomenclature_change_id: @lump.id, id: 'inputs'
         response.should render_template('inputs')
       end
