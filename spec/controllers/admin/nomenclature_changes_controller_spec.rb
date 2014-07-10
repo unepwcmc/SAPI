6799de537dc79ba091a787d8f2442efcf1676f89
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-10 07:47:00 +0100

    added nomenclature changes controller spec

---
+++ spec/controllers/admin/nomenclature_changes_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Admin::NomenclatureChangesController do
+  login_admin
+
+  describe "GET index" do
+    it "assigns @nomenclature_changes sorted by designation and name" do
+      nomenclature_change1 = create(:nomenclature_change)
+      nomenclature_change2 = create(:nomenclature_change)
+      get :index
+      assigns(:nomenclature_changes).should eq([nomenclature_change2, nomenclature_change1])
+    end
+    it "renders the index template" do
+      get :index
+      response.should render_template("index")
+    end
+  end
+
+  describe "DELETE destroy" do
+    let(:nomenclature_change){ create(:nomenclature_change) }
+    it "redirects after delete" do
+      delete :destroy, :id => nomenclature_change.id
+      response.should redirect_to(admin_nomenclature_changes_url)
+    end
+  end
+
+end
