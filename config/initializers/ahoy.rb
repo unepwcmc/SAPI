9d895fe62c3a19e1306a442f704cc488a6dd2a20
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-10 10:28:52 +0100

    only report exceptions where notifier defined

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,7 +1,6 @@
-require 'exception_notifier'
 class Ahoy::Store < Ahoy::Stores::ActiveRecordStore
   def report_exception(e)
-    ExceptionNotifier.notify_exception(e)
+    ExceptionNotifier.notify_exception(e) if defined? ExceptionNotifier
   end
 
   def visit_model
