c223e6cd42a0dfd7e1a2cd2327286af136ca750e
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-11 16:04:50 +0100

    split transfer and copy processor and added distribution reassignment processor specs

---
+++ app/models/nomenclature_change/reassignment_copy_processor.rb
@@ -0,0 +1,104 @@
+class NomenclatureChange::ReassignmentCopyProcessor
+
+  def initialize(input, output)
+    @input = input
+    @output = output
+  end
+
+  def run
+    @input.reassignments.each do |reassignment|
+      process_reassignment(reassignment)
+    end
+  end
+
+  def process_reassignment(reassignment)
+    Rails.logger.debug("Processing #{reassignment.reassignable_type} reassignment from #{@input.taxon_concept.full_name}")
+    reassignment.reassignment_targets.select do |target|
+      target.output.new_taxon_concept_id &&
+        target.output.new_taxon_concept_id != @input.taxon_concept.id ||
+        target.output.taxon_concept_id != @input.taxon_concept.id
+    end.each do |target|
+      if reassignment.reassignable_id.blank?
+        @input.reassignables_by_class(reassignment.reassignable_type).each do |reassignable|
+          process_copy_to_target(target, reassignable)
+        end
+      else
+        process_copy_to_target(target, reassignment.reassignable)
+      end
+    end
+  end
+
+  def process_copy_to_target(target, reassignable)
+    new_taxon_concept = target.output.taxon_concept || target.output.new_taxon_concept
+    Rails.logger.debug("Processing #{reassignable.class} #{reassignable.id} copy to #{new_taxon_concept.full_name}")
+
+    if target.reassignment.kind_of? NomenclatureChange::ParentReassignment
+      reassignable.parent_id = new_taxon_concept.id
+      reassignable.save
+    elsif reassignable.kind_of? Trade::Shipment
+      reassignable.taxon_concept_id = new_taxon_concept.id
+      reassignable.save
+      # TODO reported taxon concept id
+    else
+      copied_object = reassignable.duplicates({
+        taxon_concept_id: new_taxon_concept.id
+      }).first || reassignable.dup
+      copied_object.taxon_concept_id = new_taxon_concept.id
+      if reassignable.kind_of? Distribution
+        # for distributions this needs to copy distribution references and tags
+        reassignable.distribution_references.each do |distr_ref|
+          !copied_object.new_record? && distr_ref.duplicates({
+            distribution_id: copied_object.id
+          }).first || copied_object.distribution_references.build(distr_ref.comparison_attributes)
+        end
+        # taggings
+        reassignable.taggings.each do |tagging|
+          !copied_object.new_record? && tagging.duplicates({
+            taggable_id: copied_object.id
+          }).first || copied_object.taggings.build(tagging.comparison_attributes)
+        end
+      elsif reassignable.kind_of? ListingChange
+        # for listing changes this needs to copy listing distributions and exceptions
+        reassignable.listing_distributions.each do |listing_distr|
+          !copied_object.new_record? && listing_distr.duplicates({
+            listing_change_id: copied_object.id
+          }).first || copied_object.listing_distributions.build(listing_distr.comparison_attributes)
+        end
+        # party distribution
+        party_listing_distribution = copied_object.party_listing_distribution
+        !copied_object.new_record? && party_listing_distribution &&
+          party_listing_distribution.duplicates({
+            listing_change_id: copied_object.id
+          }).first || party_listing_distribution &&
+            copied_object.build_party_listing_distributions(
+            party_listing_distribution.comparison_attributes
+          )
+        # taxonomic exclusions (population exclusions already duplicated)
+        reassignable.exclusions.where('taxon_concept_id IS NOT NULL').each do |exclusion|
+          !copied_object.new_record? && exclusion.duplicates({
+            listing_change_id: copied_object.id
+          }).first || copied_object.exclusions.build(exclusion.comparison_attributes)
+        end
+      elsif reassignable.kind_of?(Quota) || reassignable.kind_of?(CitesSuspension)
+        # for trade restrictions this needs to copy purpose / source / term links
+        reassignable.trade_restriction_terms.each do |trade_restr_term|
+          !copied_object.new_record? && trade_restr_term.duplicates({
+            trade_restriction_id: copied_object.id
+          }).first || copied_object.trade_restriction_terms.build(trade_restr_term.comparison_attributes)
+        end
+        reassignable.trade_restriction_sources.each do |trade_restr_source|
+          !copied_object.new_record? && trade_restr_source.duplicates({
+            trade_restriction_id: copied_object.id
+          }).first || copied_object.trade_restriction_terms.build(trade_restr_source.comparison_attributes)
+        end
+        reassignable.trade_restriction_purposes.each do |trade_restr_purpose|
+          !copied_object.new_record? && trade_restr_purpose.duplicates({
+            trade_restriction_id: copied_object.id
+          }).first || copied_object.trade_restriction_terms.build(trade_restr_purpose.comparison_attributes)
+        end
+      end
+      copied_object.save # hope that saves the duplicated associations as well
+    end
+  end
+
+end
--- app/models/nomenclature_change/reassignment_processor.rb
+++ app/models/nomenclature_change/reassignment_processor.rb
@@ -1,11 +1,8 @@
 class NomenclatureChange::ReassignmentProcessor
 
-  # if copy == true, dup associations rather than transfer
   def initialize(input, output, copy = false)
     @input = input
     @output = output
-    @nc = output.nomenclature_change
-    @copy = copy
   end
 
   def run
@@ -14,36 +11,20 @@ class NomenclatureChange::ReassignmentProcessor
     end
   end
 
-  private
-
-  # all objects of reassignable_type that are linked to input taxon
-  def reassignables_by_class(reassignable_type)
-    reassignable_type.constantize.where(
-      :taxon_concept_id => @input.taxon_concept.id
-    )
-  end
-
   def process_reassignment(reassignment)
     Rails.logger.debug("Processing #{reassignment.reassignable_type} reassignment from #{@input.taxon_concept.full_name}")
     reassignment.reassignment_targets.select do |target|
-      target.output.taxon_concept != @input.taxon_concept
+      target.output.new_taxon_concept_id &&
+        target.output.new_taxon_concept_id != @input.taxon_concept.id ||
+        target.output.taxon_concept_id != @input.taxon_concept.id
     end.each do |target|
       if reassignment.reassignable_id.blank?
-        reassignables_by_class(reassignment.reassignable_type).each do |reassignable|
-          if @copy
-            process_copy_to_target(target, reassignable)
-          else
-            process_transfer_to_target(target, reassignable)
-          end
+        @input.reassignables_by_class(reassignment.reassignable_type).each do |reassignable|
+          process_transfer_to_target(target, reassignable)
         end
       else
-        if @copy
-          process_copy_to_target(target, reassignment.reassignable)
-        else
-          process_transfer_to_target(target, reassignment.reassignable)
-        end
+        process_transfer_to_target(target, reassignment.reassignable)
       end
-      # TODO delete reassignable from source unless copy or transferred already
     end
   end
 
@@ -68,77 +49,4 @@ class NomenclatureChange::ReassignmentProcessor
       transferred_object.save
     end
   end
-
-  def process_copy_to_target(target, reassignable)
-    new_taxon_concept = target.output.taxon_concept || target.output.new_taxon_concept
-    Rails.logger.debug("Processing #{reassignable.class} #{reassignable.id} copy to #{new_taxon_concept.full_name}")
-
-    if target.reassignment.kind_of? NomenclatureChange::ParentReassignment
-      reassignable.parent_id = new_taxon_concept.id
-      reassignable.save
-    elsif reassignable.class == 'Trade::Shipment'
-      reassignable.taxon_concept_id = new_taxon_concept.id
-      reassignable.save
-      # TODO reported taxon concept id
-    else
-      copied_object = reassignable.duplicates({
-        taxon_concept_id: new_taxon_concept.id
-      }).first || reassignable.dup
-      copied_object.taxon_concept_id = new_taxon_concept.id
-      if reassignable.class == 'Distribution'
-        # for distributions this needs to copy distribution references and tags
-        reassignable.distribution_references.each do |distr_ref|
-          !copied_object.new_record? && distr_ref.duplicates({
-            distribution_id: copied_object.id
-          }).first || copied_object.distribution_references.build(distr_ref.attributes)
-        end
-        # taggings
-        reassignable.taggings.each do |tagging|
-          !copied_object.new_record? && tagging.duplicates({
-            taggable_id: copied_object.id
-          }).first || copied_object.taggings.build(tagging.attributes)
-        end
-      elsif reassignable.class == 'ListingChange'
-        # for listing changes this needs to copy listing distributions and exceptions
-        reassignable.listing_distributions.each do |listing_distr|
-          !copied_object.new_record? && listing_distr.duplicates({
-            listing_change_id: copied_object.id
-          }).first || copied_object.listing_distributions.build(listing_distr.attributes)
-        end
-        # party distribution
-        party_listing_distribution = copied_object.party_listing_distribution
-        !copied_object.new_record? && party_listing_distribution &&
-          party_listing_distribution.duplicates({
-            listing_change_id: copied_object.id
-          }).first || copied_object.build_party_listing_distributions(
-            party_listing_distribution.attributes
-          )
-        # taxonomic exclusions (population exclusions already duplicated)
-        reassignable.exclusions.where('taxon_concept_id IS NOT NULL').each do |exclusion|
-          !copied_object.new_record? && exclusion.duplicates({
-            listing_change_id: copied_object.id
-          }).first || copied_object.exclusions.build(exclusion.attributes)
-        end
-      elsif reassignable.class == 'Quota' || reassignable.class == 'CitesSuspension'
-        # for trade restrictions this needs to copy purpose / source / term links
-        reassignable.trade_restriction_terms.each do |trade_restr_term|
-          !copied_object.new_record? && trade_restr_term.duplicates({
-            trade_restriction_id: copied_object.id
-          }).first || copied_object.trade_restriction_terms.build(trade_restr_term.attributes)
-        end
-        reassignable.trade_restriction_sources.each do |trade_restr_source|
-          !copied_object.new_record? && trade_restr_source.duplicates({
-            trade_restriction_id: copied_object.id
-          }).first || copied_object.trade_restriction_terms.build(trade_restr_source.attributes)
-        end
-        reassignable.trade_restriction_purposes.each do |trade_restr_purpose|
-          !copied_object.new_record? && trade_restr_purpose.duplicates({
-            trade_restriction_id: copied_object.id
-          }).first || copied_object.trade_restriction_terms.build(trade_restr_purpose.attributes)
-        end
-      end
-      copied_object.save # hope that saves the duplicated associations as well
-    end
-  end
-
 end
