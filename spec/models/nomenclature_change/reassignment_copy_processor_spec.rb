932c03a15702ed43a109bd709ab46ec9435b5d6f
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-11 16:04:50 +0100

    split transfer and copy processor and added distribution reassignment processor specs

--- spec/models/nomenclature_change/reassignment_copy_processor_spec.rb
+++ spec/models/nomenclature_change/reassignment_copy_processor_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe NomenclatureChange::ReassignmentProcessor do
+describe NomenclatureChange::ReassignmentCopyProcessor do
   include_context 'split_definitions'
   describe :run do
-    let(:split){ split_with_input_and_output }
+    let(:split){ split_with_input_and_same_output }
     let(:input){ split.input }
     let(:output){ split.outputs.first }
     let(:processor){
-      NomenclatureChange::ReassignmentProcessor.new(input, output)
+      NomenclatureChange::ReassignmentCopyProcessor.new(input, output)
     }
 
     context "when children" do
@@ -15,19 +15,19 @@ describe NomenclatureChange::ReassignmentProcessor do
     end
     context "when names" do
       include_context 'name_reassignments_processor_examples'
-      specify{ expect(input_species.synonyms).to be_empty }
+      specify{ expect(input_species.synonyms).to include(input_species_synonym) }
     end
     context "when distribution" do
       include_context 'distribution_reassignments_processor_examples'
-      specify{ expect(input_species.distributions).to be_empty }
+      specify{ expect(input_species.distributions.count).to eq(2) }
     end
     context "when listing changes" do
       include_context 'legislation_reassignments_processor_examples'
-      specify{ expect(input_species.listing_changes).to be_empty }
+      specify{ expect(input_species.listing_changes.count).to eq(2) }
     end
     context "when common names" do
       include_context 'common_name_reassignments_processor_examples'
-      specify{ expect(input_species.common_names).to be_empty }
+      specify{ expect(input_species.common_names.count).to eq(1) }
     end
   end
 end
