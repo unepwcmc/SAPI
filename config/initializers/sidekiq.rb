9314f8f1c7d7e5bff09a037e87026feea2faf660
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-13 21:21:37 +0100

    removed redundant sidekiq config which might be causing issues in prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,12 +5,10 @@ Sidekiq.configure_client do |config|
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware unless Rails.env.test?
   end
-  config.redis = { :url => 'redis://127.0.0.1:6379/0', :namespace => 'SAPI' }
 end
 
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware
   end
-  config.redis = { :url => 'redis://127.0.0.1:6379/0', :namespace => 'SAPI' }
 end
