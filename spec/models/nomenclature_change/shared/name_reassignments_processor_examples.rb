df6c30bdec0abae6835c17340736043c4ddbce9a
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-15 10:17:37 +0100

    use new hash syntax

--- spec/models/nomenclature_change/shared/name_reassignments_processor_examples.rb
+++ spec/models/nomenclature_change/shared/name_reassignments_processor_examples.rb
@@ -4,21 +4,21 @@ shared_context 'name_reassignments_processor_examples' do
   }
   let(:input_species_synonym_rel){
     create(:taxon_relationship,
-      :taxon_relationship_type_id => synonym_relationship_type.id,
-      :taxon_concept => input_species,
-      :other_taxon_concept => input_species_synonym
+      taxon_relationship_type_id: synonym_relationship_type.id,
+      taxon_concept: input_species,
+      other_taxon_concept: input_species_synonym
     )
   }
   let(:reassignment){
     create(:nomenclature_change_name_reassignment,
-      :input => input,
-      :reassignable_id => input_species_synonym_rel.id
+      input: input,
+      reassignable_id: input_species_synonym_rel.id
     )
   }
   let!(:reassignment_target){
     create(:nomenclature_change_reassignment_target,
-      :reassignment => reassignment,
-      :output => output
+      reassignment: reassignment,
+      output: output
     )
   }
   before(:each) do
--- spec/models/nomenclature_change/shared/parent_reassignments_processor_examples.rb
+++ spec/models/nomenclature_change/shared/parent_reassignments_processor_examples.rb
@@ -1,17 +1,17 @@
 shared_context 'parent_reassignments_processor_examples' do
   let(:input_species_child){
-    create_cites_eu_subspecies(:parent => input_species)
+    create_cites_eu_subspecies(parent: input_species)
   }
   let(:reassignment){
     create(:nomenclature_change_parent_reassignment,
-      :input => input,
-      :reassignable_id => input_species_child.id
+      input: input,
+      reassignable_id: input_species_child.id
     )
   }
   let!(:reassignment_target){
     create(:nomenclature_change_reassignment_target,
-      :reassignment => reassignment,
-      :output => output
+      reassignment: reassignment,
+      output: output
     )
   }
   before(:each) do
