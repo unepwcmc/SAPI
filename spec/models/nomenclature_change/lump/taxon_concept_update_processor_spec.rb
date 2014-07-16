e338ae356b273522674d261e8c6fd4e9098cfd1a
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-16 12:47:41 +0100

    composable subprocessors

--- spec/models/nomenclature_change/lump/taxon_concept_update_processor_spec.rb
+++ spec/models/nomenclature_change/lump/taxon_concept_update_processor_spec.rb
@@ -1,31 +1,30 @@
 require 'spec_helper'
 
-describe NomenclatureChange::Split::TransformationProcessor do
-  include_context 'split_definitions'
+describe NomenclatureChange::TaxonConceptUpdateProcessor do
+  include_context 'lump_definitions'
 
-  before(:each){ synonym_relationship_type }
   describe :run do
     let(:processor){
-      NomenclatureChange::Split::TransformationProcessor.new(output)
+      NomenclatureChange::TaxonConceptUpdateProcessor.new(output)
     }
     before(:each) do
       processor.run
     end
     context "when output is existing taxon" do
-      let(:output){ split_with_input_and_output_existing_taxon.outputs.last }
+      let(:output){ lump_with_inputs_and_output_existing_taxon.output }
       specify{ expect(output.new_taxon_concept).to be_nil }
     end
     context "when output is new taxon" do
-      let(:output){ split_with_input_and_output_new_taxon.outputs.last }
+      let(:output){ lump_with_inputs_and_output_new_taxon.output }
       specify{ expect(output.new_taxon_concept.full_name).to eq('Errorus fatalus') }
     end
     context "when output is existing taxon with new status" do
-      let(:output){ split_with_input_and_outputs_status_change.outputs.last }
+      let(:output){ lump_with_inputs_and_output_status_change.output }
       specify{ expect(output.new_taxon_concept).to be_nil }
       specify{ expect(output.taxon_concept.name_status).to eq('A') }
     end
     context "when output is existing taxon with new name" do
-      let(:output){ split_with_input_and_outputs_name_change.outputs.last }
+      let(:output){ lump_with_inputs_and_output_name_change.output }
       specify{ expect(output.taxon_concept.full_name).to eq('Errorus fatalus fatalus') }
       pending{ expect(output.taxon_concept.reload.name_status).to eq('S') }
       specify{ expect(output.new_taxon_concept.name_status).to eq('A') }
