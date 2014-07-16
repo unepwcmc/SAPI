b740eb0d799b417a8cb8fc30a3530c1e57415566
krln <krlnkmsk88@gmail.com>
2014-07-16 15:26:21 +0200

    Ahoy in Admin

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -1,6 +1,8 @@
 module Ahoy
   class Visit < ActiveRecord::Base
+  	visits = Ahoy::Visit.all
     self.table_name = 'ahoy_visits'
+
 
     has_many :ahoy_events, class_name: 'Ahoy::Event'
     belongs_to :user
