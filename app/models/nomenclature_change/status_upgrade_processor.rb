8dbec3b928f03c34c3a410cd6c04e5a4db4f088f
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-16 12:47:41 +0100

    composable subprocessors

---
+++ app/models/nomenclature_change/status_upgrade_processor.rb
@@ -0,0 +1,41 @@
+class NomenclatureChange::StatusUpgradeProcessor
+
+  def initialize(input_or_output, synonyms = [])
+    @input_or_output = input_or_output
+    @synonyms = synonyms
+    @old_status = @input_or_output.taxon_concept.name_status.dup
+  end
+
+  def run
+    unless @input_or_output.taxon_concept.name_status == 'A'
+      @input_or_output.taxon_concept.update_column(:name_status, 'A')
+    end
+    if @old_status == 'S'
+      # if was S and now is A
+      # remove has_synonym associations
+      @input_or_output.taxon_concept.inverse_synonym_relationships.
+        includes(:taxon_concept).each do |rel|
+        Rails.logger.debug "Removing HAS_SYNONYM relationship with #{rel.taxon_concept.full_name}"
+        rel.destroy
+      end
+      has_synonym_rel_type = TaxonRelationshipType.
+        find_by_name(TaxonRelationshipType::HAS_SYNONYM)
+      # set input_or_input_or_output as accepted name of synonyms
+      @synonyms.each do |synonym|
+        @input_or_output.taxon_concept.synonym_relationships << TaxonRelationship.new(
+          :taxon_relationship_type_id => has_synonym_rel_type.id,
+          :other_taxon_concept_id => synonym.taxon_concept.id
+        )
+      end
+    elsif @old_status == 'T'
+      # if was T and now is A
+      # remove has_trade_name associations
+      @input_or_output.taxon_concept.inverse_trade_name_relationships.
+        includes(:taxon_concept).each do |rel|
+        Rails.logger.debug "Removing HAS_TRADE_NAME relationship with #{rel.taxon_concept.full_name}"
+        rel.destroy
+      end
+    end
+  end
+
+end
