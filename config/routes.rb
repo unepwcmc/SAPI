08e99962d32db0bc1f1f267798ff419be59813a5
krln <krlnkmsk88@gmail.com>
2014-07-16 15:26:21 +0200

    Ahoy in Admin

--- config/routes.rb
+++ config/routes.rb
@@ -81,6 +81,8 @@ SAPI::Application.routes.draw do
 
     resources :iucn_mappings, :only => [:index]
     resources :cms_mappings, :only => [:index]
+    resources :ahoy_visits, :only => [:index]
+    resources :ahoy_events, :only => [:index]
 
     resources :taxon_concepts, :only => [:index, :create, :edit, :update, :destroy] do
       get :autocomplete, :on => :collection
