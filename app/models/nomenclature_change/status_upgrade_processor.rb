101e6d5f7c69afef5980365d49e943beb95f334c
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-17 09:11:26 +0100

    added specs for trade name status changes

--- app/models/nomenclature_change/status_upgrade_processor.rb
+++ app/models/nomenclature_change/status_upgrade_processor.rb
@@ -1,8 +1,8 @@
 class NomenclatureChange::StatusUpgradeProcessor
 
-  def initialize(input_or_output, synonyms = [])
+  def initialize(input_or_output, linked_names = [])
     @input_or_output = input_or_output
-    @synonyms = synonyms
+    @linked_names = linked_names
     @old_status = @input_or_output.taxon_concept.name_status.dup
   end
 
@@ -21,10 +21,10 @@ class NomenclatureChange::StatusUpgradeProcessor
       has_synonym_rel_type = TaxonRelationshipType.
         find_by_name(TaxonRelationshipType::HAS_SYNONYM)
       # set input_or_input_or_output as accepted name of synonyms
-      @synonyms.each do |synonym|
+      @linked_names.each do |linked_name|
         @input_or_output.taxon_concept.synonym_relationships << TaxonRelationship.new(
           :taxon_relationship_type_id => has_synonym_rel_type.id,
-          :other_taxon_concept_id => synonym.taxon_concept.id
+          :other_taxon_concept_id => linked_name.taxon_concept.id
         )
       end
       Rails.logger.debug "Updating shipments where reported taxon concept = #{@input_or_output.taxon_concept.full_name} to have taxon concept = #{@input_or_output.taxon_concept.full_name}"
@@ -40,6 +40,15 @@ class NomenclatureChange::StatusUpgradeProcessor
         Rails.logger.debug "Removing HAS_TRADE_NAME relationship with #{rel.taxon_concept.full_name}"
         rel.destroy
       end
+      has_trade_name_rel_type = TaxonRelationshipType.
+        find_by_name(TaxonRelationshipType::HAS_TRADE_NAME)
+      # set input_or_input_or_output as accepted name of trade_names
+      @linked_names.each do |linked_name|
+        @input_or_output.taxon_concept.synonym_relationships << TaxonRelationship.new(
+          :taxon_relationship_type_id => has_trade_name_rel_type.id,
+          :other_taxon_concept_id => linked_name.taxon_concept.id
+        )
+      end
       Rails.logger.debug "Updating shipments where reported taxon concept = #{@input_or_output.taxon_concept.full_name} to have taxon concept = #{@input_or_output.taxon_concept.full_name}"
       Trade::Shipment.update_all(
         {taxon_concept_id: @input_or_output.id},
@@ -50,10 +59,10 @@ class NomenclatureChange::StatusUpgradeProcessor
 
   def summary
     txt = "#{@input_or_output.taxon_concept.full_name} will be promoted to accepted name"
-    unless @synonyms.empty?
+    unless @linked_names.empty?
       [
-        txt + " with the following synonyms:",
-        @synonyms.map(&:taxon_concept).map(&:full_name)
+        txt + " with the following linked_names:",
+        @linked_names.map(&:taxon_concept).map(&:full_name)
       ]
     end
   end
