bef713393c9f6e959218a9591872bf09d9b78e8b
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-18 13:52:54 +0100

    schema annotations for nomenclature change models

--- spec/models/nomenclature_change_spec.rb
+++ spec/models/nomenclature_change_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: nomenclature_changes
+#
+#  id            :integer          not null, primary key
+#  event_id      :integer
+#  type          :string(255)      not null
+#  status        :string(255)      not null
+#  created_by_id :integer          not null
+#  updated_by_id :integer          not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'spec_helper'
 
 describe NomenclatureChange do
