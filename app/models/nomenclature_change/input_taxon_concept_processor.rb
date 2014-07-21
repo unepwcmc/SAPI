406662b53136710e317dd0c7aeff4369143c253a
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-21 13:33:09 +0100

    processing input / output notes

---
+++ app/models/nomenclature_change/input_taxon_concept_processor.rb
@@ -0,0 +1,19 @@
+class NomenclatureChange::InputTaxonConceptProcessor
+
+  def initialize(input)
+    @input = input
+  end
+
+  def run
+    return false unless @input.taxon_concept
+    Rails.logger.debug("Processing input #{@input.taxon_concept.full_name}")
+    @input.taxon_concept.update_attributes(
+      nomenclature_note_en: "#{@input.taxon_concept.nomenclature_note_en} #{@input.note}",
+      internal_nomenclature_note: "#{@input.taxon_concept.internal_nomenclature_note} #{@input.internal_note}"
+    )
+  end
+
+  def summary
+    ["Will add nomenclature note for input #{@input.taxon_concept.full_name}"]
+  end
+end
