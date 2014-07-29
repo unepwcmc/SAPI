a1cf82e1cf8f285a5952d102a77fb37648190668
krln <krlnkmsk88@gmail.com>
2014-07-29 15:41:16 +0200

    deleted impression.rb & corrected mistakes

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -2,7 +2,7 @@ module Ahoy
   class Visit < ActiveRecord::Base
     self.table_name = 'ahoy_visits'
 
-    has_many :events, class_name: 'Ahoy::Event'
+    has_many :ahoy_events, class_name: 'Ahoy::Event'
     belongs_to :user
     serialize :properties, JSON
   end
