6a33e834e7ca285fd66cd64f53a872a52c61aa39
krln <krlnkmsk88@gmail.com>
2014-07-29 15:41:16 +0200

    deleted impression.rb & corrected mistakes

--- app/controllers/admin/ahoy_events_controller.rb
+++ app/controllers/admin/ahoy_events_controller.rb
@@ -1,8 +1,8 @@
 class Admin::AhoyEventsController < Admin::SimpleCrudController
-	authorize_resource :class => 'Ahoy::Event'
- respond_to :json
- 
- def index
- 	@ahoy_events = Ahoy::Event.page(params[:page])
- end
+  authorize_resource :class => 'Ahoy::Event'
+  respond_to :json
+
+  def index
+    @ahoy_events = Ahoy::Event.page(params[:page])
+  end
 end
