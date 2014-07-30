c21a60e491017ecf6202e56434b88e42d5ba27d2
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-30 15:59:31 +0100

    added specs for the geoip class

--- lib/modules/sapi/geoip.rb
+++ lib/modules/sapi/geoip.rb
@@ -3,34 +3,35 @@ class Sapi::GeoIP
   include Singleton
 
   def initialize
-    @city_db = GeoIP.new(GEO_IP_CONFIG['city_db'])
+    @city_db = ::GeoIP.new(GEO_IP_CONFIG['city_db'])
     @org_db = GEO_IP_CONFIG['org_db'] && ::GeoIP.new(GEO_IP_CONFIG['org_db'])
   end
 
   def resolve(ip)
-    # city and country
+    result = country_and_city(ip).merge(organisation(ip))
+    result.each do |k, v|
+      result[k] = if v.nil?
+        'Unknown'
+      else
+        v.force_encoding("ISO-8859-1").encode("UTF-8")
+      end
+    end
+  end
+
+  def country_and_city(ip)
     cdb_names = @city_db.city(ip)
-    country, city = if cdb_names.nil?
-      ['Unknown', 'Unknown', 'Unknown']
-    else
-      [
-        cdb_names.country_code2.force_encoding("ISO-8859-1").encode("UTF-8"),
-        cdb_names.city_name.force_encoding("ISO-8859-1").encode("UTF-8")
-      ]
-    end
-
-    # organisation (if available)
-    org_names = @org_db && @org_db.organization(ip)
-    org = if org_names.nil?
-      'Unknown'
-    else
-      org_names.isp.force_encoding("ISO-8859-1").encode("UTF-8")
-    end
-
+    country = cdb_names.try(:country_code2)
+    city = cdb_names.try(:city_name)
     {
       country: country,
-      city: city,
-      organization: org
+      city: city
     }
   end
+
+  def organisation(ip)
+    org_names = @org_db && @org_db.organization(ip)
+    org = org_names.try(:isp)
+    { organization: org }
+  end
+
 end
