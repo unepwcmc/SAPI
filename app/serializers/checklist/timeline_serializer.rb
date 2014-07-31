e00e36162df87497c3ab74047bb616b12bd317d7
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-31 12:30:12 +0100

    additional attributes to be sent to the front-end

--- app/serializers/checklist/timeline_serializer.rb
+++ app/serializers/checklist/timeline_serializer.rb
@@ -1,4 +1,5 @@
 class Checklist::TimelineSerializer < ActiveModel::Serializer
-  attributes :id, :appendix, :party, :parties
+  attributes :id, :appendix, :party, :parties, :continues_in_present,
+    :has_nested_timelines
   has_many :timeline_events, :timeline_intervals, :timelines
 end
