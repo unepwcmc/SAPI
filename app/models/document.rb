348788b673ee0cebc25d73dd1f2ca4fcff7fc213
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-08-06 20:21:18 +0100

    document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,8 @@
 class Document < ActiveRecord::Base
-
+  belongs_to :event
+  belongs_to :language
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :is_public, presence: true
+  # TODO validates inclusion of type in available types
 end
