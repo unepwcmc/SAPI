d75252ff22570c3a20b1a936e00f470cfde59529
deciob <decio.battaglia@gmail.com>
2014-08-08 11:41:03 +0100

    added schema information to document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: documents
+#
+#  id               :integer               not null, primary key
+#  title            :text                  not null
+#  filename         :text
+#  date             :date                  not null
+#  type             :character_varying
+#  is_public        :boolean               not null
+#  event_id         :integer
+#  language_id      :integer
+#  legacy_id        :integer
+#  created_by_id    :integer
+#  updated_by_id    :integer
+#
+
 class Document < ActiveRecord::Base
   belongs_to :event
   belongs_to :language
