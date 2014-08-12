7f202a1bc77ef69a699db54d6a9730e4f44318f1
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-08-12 15:14:23 +0100

    is_public flag should default to false

--- app/models/document.rb
+++ app/models/document.rb
@@ -22,7 +22,6 @@ class Document < ActiveRecord::Base
   belongs_to :language
   validates :title, presence: true
   validates :date, presence: true
-  validates :is_public, presence: true
   # TODO validates inclusion of type in available types
   mount_uploader :filename, DocumentFileUploader
 
