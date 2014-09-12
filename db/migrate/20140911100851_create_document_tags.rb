e3b9053671b752f5f1b137973eac9d097f8f23e8
Adam Mulligan <adammulligan@fastmail.fm>
2014-09-12 09:14:47 +0100

    Squashes migrations together
    No need for multiple migrations for such simple tables

--- db/migrate/20140911100851_create_document_tags.rb
+++ db/migrate/20140911100851_create_document_tags.rb
@@ -2,6 +2,7 @@ class CreateDocumentTags < ActiveRecord::Migration
   def change
     create_table :document_tags do |t|
       t.string :name
+      t.string :type
 
       t.timestamps
     end
--- db/migrate/20140912075809_add_type_to_document_tag.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToDocumentTag < ActiveRecord::Migration
-  def change
-    add_column :document_tags, :type, :string
-  end
-end
