0baa50913843eceea00ab01e5badbfada9e64a35
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-09-15 09:19:36 +0100

    updated schema annotations for event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,22 +2,23 @@
 #
 # Table name: events
 #
-#  id             :integer          not null, primary key
-#  name           :string(255)
-#  designation_id :integer
-#  description    :text
-#  url            :text
-#  is_current     :boolean          default(FALSE), not null
-#  type           :string(255)      default("Event"), not null
-#  effective_at   :datetime
-#  published_at   :datetime
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  legacy_id      :integer
-#  end_date       :datetime
-#  subtype        :string(255)
-#  updated_by_id  :integer
-#  created_by_id  :integer
+#  id                   :integer          not null, primary key
+#  name                 :string(255)
+#  designation_id       :integer
+#  description          :text
+#  url                  :text
+#  is_current           :boolean          default(FALSE), not null
+#  type                 :string(255)      default("Event"), not null
+#  effective_at         :datetime
+#  published_at         :datetime
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  legacy_id            :integer
+#  end_date             :datetime
+#  subtype              :string(255)
+#  updated_by_id        :integer
+#  created_by_id        :integer
+#  extended_description :text
 #
 
 class Event < ActiveRecord::Base
