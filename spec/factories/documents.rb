b538b436d64eec18473796c463e45b328e57f4f7
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-09-17 16:59:05 +0100

    extend documents edit form with review phase

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -5,6 +5,10 @@ FactoryGirl.define do
     filename { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'annual_report_upload_exporter.csv')) }
     event
     type 'Document'
+
+    factory :review_of_significant_trade, class: Document::ReviewOfSignificantTrade do
+      type 'Document::ReviewOfSignificantTrade'
+    end
   end
 
   factory :document_citation do
