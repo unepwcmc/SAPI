d5b45f89bca1ebaaec670fca159bb55603f99e07
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-09-22 10:25:52 +0100

    removed obsolete line

--- app/controllers/admin/ahoy_events_controller.rb
+++ app/controllers/admin/ahoy_events_controller.rb
@@ -1,7 +1,6 @@
 class Admin::AhoyEventsController < Admin::SimpleCrudController
   authorize_resource :class => 'Ahoy::Event'
   respond_to :json
-  has_many :toptens
 
   def index
     @ahoy_events = Ahoy::Event.order('time DESC').page(params[:page])
