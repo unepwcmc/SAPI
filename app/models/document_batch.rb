70d0386232596ba88d73b7402c80378e7de06ec9
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-09-24 12:43:09 +0100

    revert to previous version of the file upload field

--- app/models/document_batch.rb
+++ app/models/document_batch.rb
@@ -2,15 +2,20 @@ class DocumentBatch
   extend ActiveModel::Naming
   include ActiveModel::Conversion
   include ActiveModel::Validations
-  attr_accessor :event_id, :language_id, :date, :is_public, :documents
+  attr_reader :event_id, :language_id, :date, :is_public, :documents
+  validates :date, presence: true
+  validates :documents, presence: true, allow_blank: false
 
   def initialize(attributes = {})
-    attributes.each do |name, value|
-      send("#{name}=", value)
-    end
+    @event_id = attributes[:event_id]
+    @language_id = attributes[:language_id]
+    @date = attributes[:date]
+    @is_public = attributes[:is_public]
+    initialize_documents(attributes[:documents_attributes], attributes[:files])
   end
 
   def save
+    return false unless valid?
     success = true
     Document.transaction do
       @documents.each do |d|
@@ -23,36 +28,32 @@ class DocumentBatch
     success
   end
 
-  def self.association association, klass
-    @@attributes ||= {}
-    @@attributes[association] = klass
+  def persisted?
+    false
   end
 
-  association :documents, Document
+  private
 
-  def self.reflect_on_association(association)
-    data = { klass: @@attributes[association] }
-    OpenStruct.new data
+  def initialize_documents(documents_attributes, files)
+    @documents = []
+    if documents_attributes && files
+      for idx in 0..(files.length - 1) do
+        document_params = {
+          type: documents_attributes[idx.to_s][:type],
+          filename: files[idx]
+        }
+        @documents.push(Document.new(common_attributes.merge(document_params)))
+      end
+    end
   end
 
   def common_attributes
     {
-      'event_id' => event_id,
-      'date' => date,
-      'language_id' => language_id,
-      'is_public' => ActiveRecord::ConnectionAdapters::Column.value_to_boolean(is_public)
+      'event_id' => @event_id,
+      'date' => @date,
+      'language_id' => @language_id,
+      'is_public' => ActiveRecord::ConnectionAdapters::Column.value_to_boolean(@is_public)
     }
   end
 
-  def documents_attributes=(attributes)
-    @documents ||= []
-    attributes.each do |_, document_params|
-      destroy = ActiveRecord::ConnectionAdapters::Column.value_to_boolean(document_params.delete(:_destroy))
-      @documents.push(Document.new(common_attributes.merge(document_params))) unless destroy
-    end
-  end
-
-  def persisted?
-    false
-  end
 end
