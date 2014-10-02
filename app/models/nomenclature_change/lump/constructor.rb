1d5aa8e021ba164f406bdf6b4af4b3dade0430ad
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-10-02 10:55:32 +0100

    add multi-lingual public notes to inputs, outputs and reassignments

--- app/models/nomenclature_change/lump/constructor.rb
+++ app/models/nomenclature_change/lump/constructor.rb
@@ -51,80 +51,143 @@ class NomenclatureChange::Lump::Constructor
     end
   end
 
-  def input_lumped_into(input, output)
+  def input_lumped_into(input, output, lng)
     input_html = taxon_concept_html(input.taxon_concept.full_name, input.taxon_concept.rank.name)
     output_html = taxon_concept_html(output.display_full_name, output.display_rank_name)
-    "#{input_html} was lumped into #{output_html} in #{Date.today.year}"
+    case lng
+    when :es
+      "ES #{input_html} was lumped into #{output_html} in #{Date.today.year}"
+    when :fr
+      "FR #{input_html} was lumped into #{output_html} in #{Date.today.year}"
+    else
+      "#{input_html} was lumped into #{output_html} in #{Date.today.year}"
+    end
   end
 
-  def output_lumped_from(output, inputs)
+  def output_lumped_from(output, inputs, lng)
     output_html = taxon_concept_html(output.display_full_name, output.display_rank_name)
     inputs_html = @nomenclature_change.inputs.map do |input|
       taxon_concept_html(input.taxon_concept.full_name, input.taxon_concept.rank.name)
     end.join(', ')
-    "#{output_html} was lumped from #{inputs_html} in #{Date.today.year}"
+    case lng
+    when :es
+      "ES #{output_html} was lumped from #{inputs_html} in #{Date.today.year}"
+    when :fr
+      "FR #{output_html} was lumped from #{inputs_html} in #{Date.today.year}"
+    else
+      "#{output_html} was lumped from #{inputs_html} in #{Date.today.year}"
+    end
   end
 
-  def input_note(input, output, event)
-    note = '<p>'
-    note << input_lumped_into(input, output)
-    note << following_taxonomic_changes(event) if event
-    note << '.</p>'
-    note
+  def multi_lingual_input_note(input, output, event)
+    result = {}
+    [:en, :es, :fr].each do |lng|
+      note = '<p>'
+      note << input_lumped_into(input, output, lng)
+      note << following_taxonomic_changes(event, lng) if event
+      note << '.</p>'
+      result[lng] = note
+    end
+    result
   end
 
-  def output_note(output, inputs, event)
-    note = '<p>'
-    note << output_lumped_from(output, @nomenclature_change.inputs)
-    note << following_taxonomic_changes(event) if event
-    note << '.</p>'
-    note
+  def multi_lingual_output_note(output, inputs, event)
+    result = {}
+    [:en, :es, :fr].each do |lng|
+      note = '<p>'
+      note << output_lumped_from(output, @nomenclature_change.inputs, lng)
+      note << following_taxonomic_changes(event, lng) if event
+      note << '.</p>'
+      result[lng] = note
+    end
+    result
   end
 
   def build_input_and_output_notes
     output = @nomenclature_change.output
     event = @nomenclature_change.event
     @nomenclature_change.inputs_except_outputs.each do |input|
-      if input.note.blank?
-        input.note = input_note(input, output, event)
+      if input.note_en.blank?
+        note = multi_lingual_input_note(input, output, event)
+        input.note_en = note[:en]
+        input.note_es = note[:es]
+        input.note_fr = note[:fr]
       end
     end
-    if output.note.blank?
-      output.note = output_note(output, @nomenclature_change.inputs, event)
+    if output.note_en.blank?
+      note = multi_lingual_output_note(output, @nomenclature_change.inputs, event)
+        output.note_en = note[:en]
+        output.note_es = note[:es]
+        output.note_fr = note[:fr]
     end
   end
 
-  def legislation_note
+  def legislation_note(lng)
     output = @nomenclature_change.output
     input_html = taxon_concept_html('[[input]]', output.display_rank_name)
     output_html = taxon_concept_html(output.display_full_name, output.display_rank_name)
     note = '<p>'
     note << yield(input_html, output_html)
-    note << " following #{@nomenclature_change.event.name}" if @nomenclature_change.event
+    if @nomenclature_change.event
+      note << following_taxonomic_changes(@nomenclature_change.event, lng)
+    end
     note + '.</p>'
   end
 
-  def listing_change_note
-    legislation_note do |input_html, output_html|
-      "Originally listed as #{input_html}, which was lumped into #{output_html}"
-    end
+  def multi_lingual_listing_change_note
+    {
+      en: legislation_note(:en) do |input_html, output_html|
+        "Originally listed as #{input_html}, which was lumped into #{output_html}"
+      end,
+      es: legislation_note(:es) do |input_html, output_html|
+        "ES Originally listed as #{input_html}, which was lumped into #{output_html}"
+      end,
+      fr: legislation_note(:fr) do |input_html, output_html|
+        "FR Originally listed as #{input_html}, which was lumped into #{output_html}"
+      end
+    }
   end
 
-  def suspension_note
-    legislation_note do |input_html, output_html|
-      "Suspension originally formed for #{input_html}, which was lumped into #{output_html}"
-    end
+  def multi_lingual_suspension_note
+    {
+      en: legislation_note(:en) do |input_html, output_html|
+        "Suspension originally formed for #{input_html}, which was lumped into #{output_html}"
+      end,
+      es: legislation_note(:es) do |input_html, output_html|
+        "ES Suspension originally formed for #{input_html}, which was lumped into #{output_html}"
+      end,
+      fr: legislation_note(:fr) do |input_html, output_html|
+        "FR Suspension originally formed for #{input_html}, which was lumped into #{output_html}"
+      end
+    }
   end
 
-  def opinion_note
-    legislation_note do |input_html, output_html|
-      "Opinion originally formed for #{input_html}, which was lumped into #{output_html}"
-    end
+  def multi_lingual_opinion_note
+    {
+      en: legislation_note(:en) do |input_html, output_html|
+        "Opinion originally formed for #{input_html}, which was lumped into #{output_html}"
+      end,
+      es: legislation_note(:es) do |input_html, output_html|
+        "ES Opinion originally formed for #{input_html}, which was lumped into #{output_html}"
+      end,
+      fr: legislation_note(:fr) do |input_html, output_html|
+        "FR Opinion originally formed for #{input_html}, which was lumped into #{output_html}"
+      end
+    }
+    
   end
 
-  def quota_note
-    legislation_note do |input_html, output_html|
-      "Quota originally published for #{input_html}, which was lumped into #{output_html}"
-    end
+  def multi_lingual_quota_note
+    {
+      en: legislation_note(:en) do |input_html, output_html|
+        "Quota originally published for #{input_html}, which was lumped into #{output_html}"
+      end,
+      es: legislation_note(:es) do |input_html, output_html|
+        "ES Quota originally published for #{input_html}, which was lumped into #{output_html}"
+      end,
+      fr: legislation_note(:fr) do |input_html, output_html|
+        "FR Quota originally published for #{input_html}, which was lumped into #{output_html}"
+      end,
+    }
   end
 end
--- app/models/nomenclature_change/name_reassignment.rb
+++ app/models/nomenclature_change/name_reassignment.rb
@@ -7,11 +7,14 @@
 #  type                         :string(255)      not null
 #  reassignable_type            :string(255)
 #  reassignable_id              :integer
-#  note                         :text
+#  note_en                      :text
 #  created_by_id                :integer          not null
 #  updated_by_id                :integer          not null
 #  created_at                   :datetime         not null
 #  updated_at                   :datetime         not null
+#  note_es                      :text
+#  note_fr                      :text
+#  internal_note                :text
 #
 
 # Reassignable is a taxon relationship that is assigned to a new taxon concept
