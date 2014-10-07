92b7501d1bdf75bff4f238216e04e4e964d58a62
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-10-07 16:25:40 +0100

    removed internal_nomenclature_note throughout (use the nomenclature comment on taxon concepts or internal notes on legislation instead)

--- app/models/quota.rb
+++ app/models/quota.rb
@@ -24,10 +24,9 @@
 #  updated_by_id               :integer
 #  created_by_id               :integer
 #  nomenclature_note_en        :text
+#  internal_notes              :text
 #  nomenclature_note_es        :text
 #  nomenclature_note_fr        :text
-#  internal_nomenclature_note  :text
-#  internal_notes              :text
 #
 
 class Quota < TradeRestriction
