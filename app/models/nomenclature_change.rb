d2409c0c3125e3f635afdd85351b4079cede4388
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-10-13 12:37:15 +0100

    disabled mass assignment on user stamping attributes

--- app/models/nomenclature_change.rb
+++ app/models/nomenclature_change.rb
@@ -17,7 +17,7 @@ class NomenclatureChange < ActiveRecord::Base
   include StatusDictionary
   build_steps
   track_who_does_it
-  attr_accessible :created_by_id, :event_id, :updated_by_id, :status
+  attr_accessible :event_id, :status
 
   belongs_to :event
 
