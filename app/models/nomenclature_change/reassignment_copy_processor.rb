2f721df77738b6794a7ab05f4bf734e571fc8fae
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-10-13 12:54:33 +0100

    reduced code duplication

--- app/models/nomenclature_change/reassignment_copy_processor.rb
+++ app/models/nomenclature_change/reassignment_copy_processor.rb
@@ -60,65 +60,11 @@ class NomenclatureChange::ReassignmentCopyProcessor
           target.reassignment.note_with_resolved_placeholders_fr(@input, @output)
       end
       if reassignable.kind_of? Distribution
-        # for distributions this needs to copy distribution references and tags
-        reassignable.distribution_references.each do |distr_ref|
-          !copied_object.new_record? && distr_ref.duplicates({
-            distribution_id: copied_object.id
-          }).first || copied_object.distribution_references.build(distr_ref.comparison_attributes)
-        end
-        # taggings
-        reassignable.taggings.each do |tagging|
-          !copied_object.new_record? && tagging.duplicates({
-            taggable_id: copied_object.id
-          }).first || copied_object.taggings.build(tagging.comparison_attributes)
-        end
+        build_distribution_associations(reassignable, copied_object)
       elsif reassignable.kind_of? ListingChange
-        # for listing changes this needs to copy listing distributions and exceptions
-        reassignable.listing_distributions.each do |listing_distr|
-          !copied_object.new_record? && listing_distr.duplicates({
-            listing_change_id: copied_object.id
-          }).first || copied_object.listing_distributions.build(listing_distr.comparison_attributes)
-        end
-        # party distribution
-        party_listing_distribution = reassignable.party_listing_distribution
-        !copied_object.new_record? && party_listing_distribution &&
-          party_listing_distribution.duplicates({
-            listing_change_id: copied_object.id
-          }).first || party_listing_distribution &&
-            copied_object.build_party_listing_distribution(
-            party_listing_distribution.comparison_attributes
-          )
-        # taxonomic exclusions (population exclusions already duplicated)
-        reassignable.exclusions.where('taxon_concept_id IS NOT NULL').each do |exclusion|
-          !copied_object.new_record? && exclusion.duplicates({
-            listing_change_id: copied_object.id
-          }).first || copied_object.exclusions.build(
-            exclusion.comparison_attributes, :without_protection => true
-          )
-        end
+        build_listing_change_associations(reassignable, copied_object)
       elsif reassignable.kind_of?(Quota) || reassignable.kind_of?(CitesSuspension)
-        # for trade restrictions this needs to copy purpose / source / term links
-        reassignable.trade_restriction_terms.each do |trade_restr_term|
-          !copied_object.new_record? && trade_restr_term.duplicates({
-            trade_restriction_id: copied_object.id
-          }).first || copied_object.trade_restriction_terms.build(
-            trade_restr_term.comparison_attributes, :without_protection => true
-          )
-        end
-        reassignable.trade_restriction_sources.each do |trade_restr_source|
-          !copied_object.new_record? && trade_restr_source.duplicates({
-            trade_restriction_id: copied_object.id
-          }).first || copied_object.trade_restriction_sources.build(
-            trade_restr_source.comparison_attributes, :without_protection => true
-          )
-        end
-        reassignable.trade_restriction_purposes.each do |trade_restr_purpose|
-          !copied_object.new_record? && trade_restr_purpose.duplicates({
-            trade_restriction_id: copied_object.id
-          }).first || copied_object.trade_restriction_purposes.build(
-            trade_restr_purpose.comparison_attributes, :without_protection => true
-          )
-        end
+        build_trade_restriction_associations(reassignable, copied_object)
       end
       copied_object.save # hope that saves the duplicated associations as well
     end
@@ -130,6 +76,66 @@ class NomenclatureChange::ReassignmentCopyProcessor
       to #{@output.display_full_name}",
       NomenclatureChange::ReassignmentSummarizer.new(@input, @output).summary
     ]
+  end
+
+  private
+
+  def build_distribution_associations(reassignable, copied_object)
+    # for distributions this needs to copy distribution references and tags
+    reassignable.distribution_references.each do |distr_ref|
+      !copied_object.new_record? && distr_ref.duplicates({
+        distribution_id: copied_object.id
+      }).first || copied_object.distribution_references.build(distr_ref.comparison_attributes)
+    end
+    # taggings
+    reassignable.taggings.each do |tagging|
+      !copied_object.new_record? && tagging.duplicates({
+        taggable_id: copied_object.id
+      }).first || copied_object.taggings.build(tagging.comparison_attributes)
+    end
+  end
+
+  def build_listing_change_associations(reassignable, copied_object)
+    # for listing changes this needs to copy listing distributions and exceptions
+    reassignable.listing_distributions.each do |listing_distr|
+      !copied_object.new_record? && listing_distr.duplicates({
+        listing_change_id: copied_object.id
+      }).first || copied_object.listing_distributions.build(listing_distr.comparison_attributes)
+    end
+    # party distribution
+    party_listing_distribution = reassignable.party_listing_distribution
+    !copied_object.new_record? && party_listing_distribution &&
+      party_listing_distribution.duplicates({
+        listing_change_id: copied_object.id
+      }).first || party_listing_distribution &&
+        copied_object.build_party_listing_distribution(
+        party_listing_distribution.comparison_attributes
+      )
+    # taxonomic exclusions (population exclusions already duplicated)
+    reassignable.exclusions.where('taxon_concept_id IS NOT NULL').each do |exclusion|
+      !copied_object.new_record? && exclusion.duplicates({
+        listing_change_id: copied_object.id
+      }).first || copied_object.exclusions.build(
+        exclusion.comparison_attributes, :without_protection => true
+      )
+    end
+  end
+
+  def build_trade_restriction_associations(reassignable, copied_object)
+    # for trade restrictions this needs to copy purpose / source / term links
+    [
+      :trade_restriction_terms,
+      :trade_restriction_sources,
+      :trade_restriction_purposes
+    ].each do |trade_restriction_codes|
+      reassignable.send(trade_restriction_codes).each do |trade_restr_code|
+        !copied_object.new_record? && trade_restr_code.duplicates({
+          trade_restriction_id: copied_object.id
+        }).first || copied_object.send(trade_restriction_codes).build(
+          trade_restr_code.comparison_attributes, :without_protection => true
+        )
+      end
+    end
   end
 
 end
