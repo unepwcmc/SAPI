899f227464b63bbb995144240b2466af72fe18aa
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-10-13 09:45:37 +0100

    protect created_by_id and updated_by_id from mass assignment

--- app/models/nomenclature_change/reassignment_target.rb
+++ app/models/nomenclature_change/reassignment_target.rb
@@ -13,8 +13,8 @@
 
 class NomenclatureChange::ReassignmentTarget < ActiveRecord::Base
   track_who_does_it
-  attr_accessible :created_by_id, :nomenclature_change_output_id,
-    :nomenclature_change_reassignment_id, :note, :updated_by_id
+  attr_accessible :nomenclature_change_output_id,
+    :nomenclature_change_reassignment_id, :note
   belongs_to :output, :class_name => NomenclatureChange::Output,
     :foreign_key => :nomenclature_change_output_id
   belongs_to :reassignment, :class_name => NomenclatureChange::Reassignment,
