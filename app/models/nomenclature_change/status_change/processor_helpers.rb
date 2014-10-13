56433601d77cc10ce205757cea3dc9a36c64b101
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-10-13 12:15:50 +0100

    reduced code duplication in nomenclature change processors

--- app/models/nomenclature_change/status_change/processor_helpers.rb
+++ app/models/nomenclature_change/status_change/processor_helpers.rb
@@ -7,6 +7,23 @@ module NomenclatureChange::StatusChange::ProcessorHelpers
     Rails.logger.warn("[#{@nc.type}] END")
   end
 
+  def reassignment_processor(output)
+    return nil unless @input && output
+
+    # if input is not one of outputs, that means it only acts as a template
+    # for associations and reassignment processor should copy rather than
+    # transfer associations
+    transfer = [@primary_output, @secondary_output].compact.map(&:taxon_concept).include?(
+      @input.taxon_concept
+    )
+    if transfer
+      NomenclatureChange::ReassignmentProcessor.new(@input, output)
+    else
+      NomenclatureChange::ReassignmentCopyProcessor.new(@input, output)
+    end
+
+  end
+
   # Generate a summary based on the subprocessors chain
   def summary
     result = []
