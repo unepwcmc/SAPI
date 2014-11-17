bcbb9f16abec832daf2b1b2f90fdff4f217cf800
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2014-11-17 16:33:42 +0000

    Added output_reassignments table and reworked the builders
    Conflicts:
    	app/models/listing_change.rb
    	app/models/nomenclature_change/reassignment_processor.rb

---
+++ app/models/nomenclature_change/output_parent_reassignment.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: nomenclature_change_reassignments
+#
+#  id                           :integer          not null, primary key
+#  nomenclature_change_output_id :integer          not null
+#  type                         :string(255)      not null
+#  reassignable_type            :string(255)
+#  reassignable_id              :integer
+#  note_en                      :text
+#  note_es                      :text
+#  note_fr                      :text
+#  internal_note                :text
+#  created_by_id                :integer          not null
+#  updated_by_id                :integer          not null
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#
+
+# Reassignable is a taxon concept that is reassigned to a new parent
+class NomenclatureChange::OutputParentReassignment < NomenclatureChange::OutputReassignment
+end
