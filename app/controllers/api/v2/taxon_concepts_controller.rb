749215dd62698deba8cbcb5a57571f93d0af471c
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-11-17 17:24:32 +0000

    started scaffolding the taxon concepts index page on api v2

--- app/controllers/api/v2/taxon_concepts_controller.rb
+++ app/controllers/api/v2/taxon_concepts_controller.rb
@@ -1,12 +1,28 @@
 class Api::V2::TaxonConceptsController < ApplicationController
 
   resource_description do
-    short 'Taxon concepts'
     formats ['json']
     api_base_url 'api/v2/taxon_concepts'
+
+    description <<-EOS
+      My controller description.
+    EOS
   end
 
-  api :GET, '/'
+  api :GET, '/', "List of taxon concepts"
+  param :page, Integer, :desc => "Page number"
+  description "Provides a paginated list of taxon concepts, with
+    100 records per page"
+  example <<-EOS
+    'taxon_concepts': [
+      {
+        'id': 1,
+        'scientific_name': 'Loxodonta africana',
+        'author_year': '(Blumenbach, 1797)',
+        'rank': 'SPECIES',
+      }
+    ]
+  EOS
   def index
   end
 end
