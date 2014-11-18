cbf51f0b2807ddbde1a9b2cc00c92ca228e91883
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-11-18 14:16:23 +0000

    Fleshing out API documentation

--- app/controllers/api/v2/taxon_concepts_controller.rb
+++ app/controllers/api/v2/taxon_concepts_controller.rb
@@ -5,24 +5,64 @@ class Api::V2::TaxonConceptsController < ApplicationController
     api_base_url 'api/v2/taxon_concepts'
 
     description <<-EOS
-      My controller description.
+      == About the API
+      == Authenticating your requests
+      == Pagination
+      == Contact
     EOS
   end
 
-  api :GET, '/', "List of taxon concepts"
-  param :page, Integer, :desc => "Page number"
-  description "Provides a paginated list of taxon concepts, with
-    100 records per page"
+  api :GET, '/', "Lists taxon concepts"
+  param :page, Integer, :desc => "Page Number", :required => false
   example <<-EOS
     'taxon_concepts': [
       {
         'id': 1,
         'scientific_name': 'Loxodonta africana',
         'author_year': '(Blumenbach, 1797)',
-        'rank': 'SPECIES',
+        'rank': 'SPECIES'
       }
     ]
   EOS
   def index
+  end
+
+  api :GET, '/:id/names', "Lists synonyms and common names for a given taxon concept"
+  param :id, Integer, :desc => "Taxon Concept ID", :required => true
+  example <<-EOS
+    'synonyms': [
+      {
+        'id': 1,
+        'scientific_name': 'Loxodonta africana'
+      }
+    ],
+    'common_names': [
+      {
+        'id': 1,
+        'scientific_name': 'Loxodonta africana'
+      }
+    ]
+  EOS
+  def show
+  end
+
+  api :GET, '/:id/cites_legislation', "Lists current listings, quotas, and suspensions for a given taxon concept"
+  param :id, Integer, :desc => "Taxon Concept ID", :required => true
+  def show2
+  end
+
+  api :GET, '/:id/eu_legislation', "Lists current listings, opinions, and suspensions for a given taxon concept"
+  param :id, Integer, :desc => "Taxon Concept ID", :required => true
+  def show3
+  end
+
+  api :GET, '/:id/references', "Lists references for a given taxon concept"
+  param :id, Integer, :desc => "Taxon Concept ID", :required => true
+  def show4
+  end
+
+  api :GET, '/:id/distributions', "Lists distributions for a given taxon concept"
+  param :id, Integer, :desc => "Taxon Concept ID", :required => true
+  def show5
   end
 end
--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,15 +1,42 @@
 Apipie.configure do |config|
   config.app_name = "CITES Checklist and Species+ API"
   config.api_base_url = "/api/v2"
-  config.doc_base_url = "/apipie"
-  config.default_version = "2.0"
-  # where is your API defined?
+  config.doc_base_url = "/api/documentation"
+  config.default_version = "v2"
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/v2/*.rb"
 
   config.app_info = <<-EOS
-      ==== Purpose of this API
-      Application Programming Interface (API) to support CITES Parties
-      to increase the accuracy and efficiency of curating CITES species data for permitting purposes.
+      == About the API
+      Application Programming Interface (API) to support CITES Parties to increase the 
+      accuracy and efficiency of curating CITES species data for permitting purposes.
+
+      == Getting Started
+      === Authenticating your requests
+      You can sign up for an API account here ???. Once you have signed up, visit ???? 
+      wj9402u9 an-jkgj and get your token. This token will need to be passed in to all
+      requests as the value of a key named 'token' in the query string as below:
+
+        http://www.speciesplus.net/api/v2/taxon_concepts/?token="abcd1234"
+
+      === Pagination
+      Where the request returns more than 100 objects, the request is paginated, showing 
+      100 objects at a time. To fetch the remaining objects, you will need to make a new 
+      request and pass the optional ‘page’ parameter like as below:
+
+        http://www.speciesplus.net/api/v2/taxon_concepts/?token=abcd1234&page=2
+
+      == Terms of Use
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in arcu suscipit, 
+      egestas eros nec, scelerisque purus. Aenean venenatis lacus placerat euismod auctor. 
+      Sed vitae dui metus. Ut id ex quis tortor suscipit semper vel non nisl. Aliquam erat 
+      volutpat. Donec mattis consequat felis, lobortis pharetra justo faucibus sed.
+
+      == Contact
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in arcu suscipit, 
+      egestas eros nec, scelerisque purus. Aenean venenatis lacus placerat euismod auctor. 
+      Sed vitae dui metus. Ut id ex quis tortor suscipit semper vel non nisl. Aliquam erat 
+      volutpat. Donec mattis consequat felis, lobortis pharetra justo faucibus sed.
     EOS
+
   config.copyright = "&copy; #{Time.now.year} UNEP-WCMC"
 end
