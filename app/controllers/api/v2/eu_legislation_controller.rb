35ab64a94fa5458e43395c54f44b21d1d47543b5
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-11-19 10:44:38 +0000

    Improved api documentation

--- app/controllers/api/v2/eu_legislation_controller.rb
+++ app/controllers/api/v2/eu_legislation_controller.rb
@@ -1,10 +1,10 @@
-class Api::V2::DistributionsController < ApplicationController
+class Api::V2::EuLegislationController < ApplicationController
   resource_description do
     formats ['json']
     api_base_url 'api/v2/taxon_concepts'
   end
 
-  api :GET, '/:id/distributions', "Lists distributions for a given taxon concept"
+  api :GET, '/:id/eu_legislation', "Lists current listings, opinions, and suspensions for a given taxon concept"
   param :id, Integer, :desc => "Taxon Concept ID", :required => true
   
   def index
