44899fd9103eee94c05413da298b120000499ce6
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-11-19 11:52:59 +0000

    fixed missing commas in JSON

--- app/controllers/api/v2/eu_legislation_controller.rb
+++ app/controllers/api/v2/eu_legislation_controller.rb
@@ -16,14 +16,14 @@ class Api::V2::EuLegislationController < ApplicationController
             'party_full_name' : null,
             'effective_at_formatted' : '13/09/2007',
             'short_note_en' : 'All populations except those of BW, NA, ZA, and ZW.',
-            'full_note_en' : 'Included in Appendix I except the population...'
+            'full_note_en' : 'Included in Appendix I except the population...',
             'auto_note' : null,
             'is_inclusion' : null,
             'subspecies_info' : null,
             'inherited_full_note_en' : null,
             'inherited_short_note_en' : null,
-            'event_name' : 'Commission Reg. (EU)'
-            'event_url' : 'http://eur-lex.europa.eu/LexUriServ/'
+            'event_name' : 'Commission Reg. (EU)',
+            'event_url' : 'http://eur-lex.europa.eu/LexUriServ/',
             'hash_full_note_en' : null,
             'hash_display' : ''
           }
