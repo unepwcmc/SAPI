e464cd8dd306d7d02935652a96a80d76bc0b1cf5
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-11-21 14:02:05 +0000

    first drop the view, then alter constraint

--- db/migrate/20141120211023_add_not_null_constraint_to_eu_decisions_geo_entity_id.rb
+++ db/migrate/20141120211023_add_not_null_constraint_to_eu_decisions_geo_entity_id.rb
@@ -1,5 +1,6 @@
 class AddNotNullConstraintToEuDecisionsGeoEntityId < ActiveRecord::Migration
   def change
+    execute 'DROP VIEW eu_decisions_view'
     change_column :eu_decisions, :geo_entity_id, :integer, null: false
   end
 end
