90c70a0eb58edba4507c4c48bf4ddaf81a5899bf
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-11-25 12:51:07 +0000

    specs for output reassignments processing

--- spec/models/nomenclature_change/reassignment_transfer_processor_spec.rb
+++ spec/models/nomenclature_change/reassignment_transfer_processor_spec.rb
@@ -3,40 +3,72 @@ require 'spec_helper'
 describe NomenclatureChange::ReassignmentTransferProcessor do
   include_context 'split_definitions'
   describe :run do
-    let(:split){ split_with_input_and_output }
-    let(:input){ split.input }
-    let(:output){ split.outputs.first }
-    let(:processor){
-      NomenclatureChange::ReassignmentTransferProcessor.new(input, output)
-    }
+    context "input reassignments" do
+      let(:split){ split_with_input_and_output }
+      let(:input){ split.input }
+      let(:output){ split.outputs.first }
+      let(:processor){
+        NomenclatureChange::ReassignmentTransferProcessor.new(input, output)
+      }
+      context "when children" do
+        include_context 'parent_reassignments_processor_examples'
+      end
+      context "when names" do
+        include_context 'name_reassignments_processor_examples'
+        specify{ expect(input_species.synonyms).to be_empty }
+      end
+      context "when distribution" do
+        include_context 'distribution_reassignments_processor_examples'
+        specify{ expect(input_species.distributions).to be_empty }
+      end
+      context "when legislation" do
+        include_context 'legislation_reassignments_processor_examples'
+        specify{ expect(input_species.listing_changes).to be_empty }
+        specify{ expect(input_species.quotas).to be_empty }
+        specify{ expect(input_species.cites_suspensions).to be_empty }
+      end
+      context "when common names" do
+        include_context 'common_name_reassignments_processor_examples'
+        specify{ expect(input_species.common_names).to be_empty }
+      end
+      context "when references" do
+        include_context 'reference_reassignments_processor_examples'
+        specify{ expect(input_species.taxon_concept_references).to be_empty }
+      end
+      context "when shipments" do
+        include_context 'shipment_reassignments_processor_examples'
+      end
+    end
 
-    context "when children" do
-      include_context 'parent_reassignments_processor_examples'
-    end
-    context "when names" do
-      include_context 'name_reassignments_processor_examples'
-      specify{ expect(input_species.synonyms).to be_empty }
-    end
-    context "when distribution" do
-      include_context 'distribution_reassignments_processor_examples'
-      specify{ expect(input_species.distributions).to be_empty }
-    end
-    context "when legislation" do
-      include_context 'legislation_reassignments_processor_examples'
-      specify{ expect(input_species.listing_changes).to be_empty }
-      specify{ expect(input_species.quotas).to be_empty }
-      specify{ expect(input_species.cites_suspensions).to be_empty }
-    end
-    context "when common names" do
-      include_context 'common_name_reassignments_processor_examples'
-      specify{ expect(input_species.common_names).to be_empty }
-    end
-    context "when references" do
-      include_context 'reference_reassignments_processor_examples'
-      specify{ expect(input_species.taxon_concept_references).to be_empty }
-    end
-    context "when shipments" do
-      include_context 'shipment_reassignments_processor_examples'
+    context "output reassignments" do
+      let(:split){ split_with_input_and_outputs_name_change }
+      let(:output){ split.outputs.last }
+      let(:old_output_subspecies){ output.taxon_concept }
+      let(:new_output_species){ output.new_taxon_concept }
+      let(:output_processor){
+        NomenclatureChange::OutputTaxonConceptProcessor.new(output)
+      }
+      let(:processor){
+        NomenclatureChange::ReassignmentTransferProcessor.new(output, output)
+      }
+      context "when names" do
+        include_context 'output_name_reassignments_processor_examples'
+      end
+      context "when distribution" do
+        include_context 'output_distribution_reassignments_processor_examples'
+      end
+      context "when legislation" do
+        include_context 'output_legislation_reassignments_processor_examples'
+      end
+      context "when common names" do
+        include_context 'output_common_name_reassignments_processor_examples'
+      end
+      context "when references" do
+        include_context 'output_reference_reassignments_processor_examples'
+      end
+      context "when shipments" do
+        include_context 'output_shipment_reassignments_processor_examples'
+      end
     end
   end
 end
---
+++ spec/models/nomenclature_change/shared/output_common_names_reassignments_processor_examples.rb
@@ -0,0 +1,13 @@
+shared_context 'output_common_name_reassignments_processor_examples' do
+  before(:each) do
+    create(:nomenclature_change_output_reassignment,
+      output: output,
+      reassignable_type: 'TaxonCommon'
+    )
+    2.times{ create(:taxon_common, taxon_concept: old_output_subspecies) }
+    output_processor.run
+    processor.run
+  end
+  specify{ expect(new_output_species.common_names.count).to eq(2) }
+  specify{ expect(old_output_subspecies.common_names).to be_empty }
+end
