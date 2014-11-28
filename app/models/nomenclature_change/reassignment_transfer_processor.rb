1f3a3e9cc252b085824b8e82310d8bf0126a8475
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-11-28 16:42:42 +0000

    simplify input and output reassignments processing

--- app/models/nomenclature_change/reassignment_transfer_processor.rb
+++ app/models/nomenclature_change/reassignment_transfer_processor.rb
@@ -1,55 +1,38 @@
 class NomenclatureChange::ReassignmentTransferProcessor < NomenclatureChange::ReassignmentProcessor
 
-  # Each reassignable object implements find_duplicate,
-  # which is called from here to make sure we're not adding a duplicate.
-  def process_reassignment_to_target(target, reassignable)
-    new_taxon_concept = @output.taxon_concept
-    if target.output.will_create_taxon?
-      new_taxon_concept = @output.new_taxon_concept
-    end
-    Rails.logger.debug("Processing #{reassignable.class} #{reassignable.id} transfer to #{new_taxon_concept.full_name}")
+  def process_reassignment(reassignment, reassignable)
+    o = transferred_object_before_save(reassignment, reassignable)
+    o if o.save # if save successful return the reassignmed object
+  end
 
-    if target.reassignment.kind_of?(NomenclatureChange::ParentReassignment) ||
-      reassignable.kind_of?(Trade::Shipment)
+  def transferred_object_before_save(reassignment, reassignable)
+    new_taxon_concept = @output.new_taxon_concept || @output.taxon_concept
+    Rails.logger.debug("Processing #{reassignable.class} #{reassignable.id} transfer to #{new_taxon_concept.full_name}")
+    if reassignment.kind_of?(NomenclatureChange::ParentReassignment) ||
+      reassignment.kind_of?(NomenclatureChange::OutputParentReassignment)
       reassignable.parent_id = new_taxon_concept.id
-      reassignable.save
+      reassignable
+    elsif reassignable.kind_of?(Trade::Shipment)
+      reassignable.taxon_concept_id = new_taxon_concept.id
+      reassignable
     else
+      # Each reassignable object implements find_duplicate,
+      # which is called from here to make sure we're not adding a duplicate.
       transferred_object = reassignable.duplicates({
         taxon_concept_id: new_taxon_concept.id
       }).first || reassignable
       transferred_object.taxon_concept_id = new_taxon_concept.id
-      if reassignable.kind_of? ListingChange
+      if reassignable.is_a?(ListingChange)
         transferred_object.inclusion_taxon_concept_id = nil
-        transferred_object.assign_attributes(notes(transferred_object, target.reassignment))
-      elsif reassignable.kind_of?(CitesSuspension) || reassignable.kind_of?(Quota) ||
-        reassignable.kind_of?(EuSuspension) || reassignable.kind_of?(EuOpinion)
-        transferred_object.assign_attributes(notes(transferred_object, target.reassignment))
       end
-      transferred_object.save
-    end
-  end
-
-  def process_reassignment_to_output(reassignment, reassignable)
-    new_taxon_concept = @output.new_taxon_concept || @output.taxon_concept
-    Rails.logger.debug("Processing #{reassignable.class} #{reassignable.id} transfer to #{new_taxon_concept.full_name}")
-
-    if reassignment.kind_of?(NomenclatureChange::OutputParentReassignment) ||
-      reassignable.kind_of?(Trade::Shipment)
-      reassignable.parent_id = new_taxon_concept.id
-      reassignable.save
-    else
-      transferred_object = reassignable.duplicates({
-        "taxon_concept_id" => new_taxon_concept.id
-      }).first || reassignable
-      transferred_object.taxon_concept_id = new_taxon_concept.id
-      if reassignable.kind_of? ListingChange
-        transferred_object.inclusion_taxon_concept_id = nil
-        transferred_object.assign_attributes(notes(transferred_object,reassignment))
-      elsif reassignable.kind_of?(CitesSuspension) || reassignable.kind_of?(Quota) ||
-        reassignable.kind_of?(EuSuspension) || reassignable.kind_of?(EuOpinion)
+      if reassignment.is_a?(NomenclatureChange::Reassignment) && (
+        reassignable.is_a?(ListingChange) ||
+        reassignable.is_a?(CitesSuspension) || reassignable.is_a?(Quota) ||
+        reassignable.is_a?(EuSuspension) || reassignable.is_a?(EuOpinion)
+        )
         transferred_object.assign_attributes(notes(transferred_object, reassignment))
       end
-      transferred_object.save
+      transferred_object
     end
   end
 
