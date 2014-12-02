d7956d1280973fac823923bf71b769c609ce2491
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2014-12-02 14:48:54 +0000

    Fixed full name of reported taxon concepts in shipments

--- app/serializers/trade/shipment_from_view_serializer.rb
+++ app/serializers/trade/shipment_from_view_serializer.rb
@@ -17,7 +17,7 @@ class Trade::ShipmentFromViewSerializer < ActiveModel::Serializer
   def reported_taxon_concept
     {
       id: object.reported_taxon_concept_id, 
-      full_name: object.reported_taxon_concept_full_name + " (#{object.taxon_concept_name_status})",
+      full_name: object.reported_taxon_concept_full_name + " (#{object.reported_taxon_concept_name_status})",
       author_year: object.reported_taxon_concept_author_year
     }
   end
