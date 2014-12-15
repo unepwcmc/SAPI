847559c1d64422bfa9462d12553db917b2a12125
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-12-15 22:21:44 +0000

    check for presence of mviews

--- db/migrate/20141212093310_add_nomenclature_notes_to_listing_changes_mview.rb
+++ db/migrate/20141212093310_add_nomenclature_notes_to_listing_changes_mview.rb
@@ -1,7 +1,9 @@
 class AddNomenclatureNotesToListingChangesMview < ActiveRecord::Migration
   def change
-    add_column :listing_changes_mview, :nomenclature_note_en, :text
-    add_column :listing_changes_mview, :nomenclature_note_fr, :text
-    add_column :listing_changes_mview, :nomenclature_note_es, :text
+    if table_exists? :listing_changes_mview
+      add_column :listing_changes_mview, :nomenclature_note_en, :text
+      add_column :listing_changes_mview, :nomenclature_note_fr, :text
+      add_column :listing_changes_mview, :nomenclature_note_es, :text
+    end
   end
 end
