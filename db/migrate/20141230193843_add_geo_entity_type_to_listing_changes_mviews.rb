78d2ec06ecb558abd3ab838ff2eca61856328163
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-01-06 13:19:11 +0000

    check if column exists first

--- db/migrate/20141230193843_add_geo_entity_type_to_listing_changes_mviews.rb
+++ db/migrate/20141230193843_add_geo_entity_type_to_listing_changes_mviews.rb
@@ -2,8 +2,9 @@ class AddGeoEntityTypeToListingChangesMviews < ActiveRecord::Migration
   def change
     [:cites, :eu, :cms].each do |designation|
       listing_changes_mview = "#{designation}_listing_changes_mview"
-
-      add_column listing_changes_mview, :geo_entity_type, :string
+      unless column_exists? listing_changes_mview, :geo_entity_type
+        add_column listing_changes_mview, :geo_entity_type, :string
+      end
     end
   end
 end
