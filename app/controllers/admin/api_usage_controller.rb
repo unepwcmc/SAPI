f185435de1d0198c126a335dc20d5394d45b53fb
Spencer Dixon <spencerlloyddixon@gmail.com>
2015-01-09 14:50:33 +0000

    Updates to dashboard, renaming axis and adding colours

--- app/controllers/admin/api_usage_controller.rb
+++ app/controllers/admin/api_usage_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ApiUsageController < Admin::AdminController
   def index
-    @last_30_days_requests = ApiRequest.where('created_at > ?', 30.days.ago)
+    @last_30_days_requests = ApiRequest.group(:response_status)
+      .group_by_day(:created_at, range: 30.days.ago.midnight..Time.now).count
+
     @all_requests = ApiRequest.all
   end
 end
