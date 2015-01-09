3a9471f26a8250dffd17de51fe710d7f02725cac
Spencer Dixon <spencerlloyddixon@gmail.com>
2015-01-09 14:50:33 +0000

    Updates to dashboard, renaming axis and adding colours

--- app/helpers/admin/api_usage_helper.rb
+++ app/helpers/admin/api_usage_helper.rb
@@ -3,9 +3,9 @@ module Admin::ApiUsageHelper
   # and anything else with 'Failed' and return newly constructed hash
   def sanitise_hash_for_line_graph(hash)
     new_hash = {}
-    hash.map { |k,v| 
+    hash.map { |k,v|
       n = k[0] == 200 ? 'Successful' : 'Failed'
-      new_hash[[n, k[1]]] = v 
+      new_hash[[n, k[1]]] = v
     }
     new_hash
   end
