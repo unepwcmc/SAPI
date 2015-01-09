e330284efbb0ce53a48e5aeda550e7486aa4a0e8
Spencer Dixon <spencerlloyddixon@gmail.com>
2015-01-09 14:50:33 +0000

    Updates to dashboard, renaming axis and adding colours

---
+++ app/helpers/admin/api_usage_helper.rb
@@ -0,0 +1,12 @@
+module Admin::ApiUsageHelper
+  # Take the hash necessary for the line graph and replace 200 with 'Successful'
+  # and anything else with 'Failed' and return newly constructed hash
+  def sanitise_hash_for_line_graph(hash)
+    new_hash = {}
+    hash.map { |k,v| 
+      n = k[0] == 200 ? 'Successful (200)' : 'Failed (500)'
+      new_hash[[n, k[1]]] = v 
+    }
+    new_hash
+  end
+end
