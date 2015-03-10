ac066e9131a64a0c4de791d217b861b49487c7e5
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-03-10 13:25:17 +0000

    copying listing change exclusions

--- spec/workers/event_listing_changes_copy_worker_spec.rb
+++ spec/workers/event_listing_changes_copy_worker_spec.rb
@@ -8,9 +8,24 @@ describe EventListingChangesCopyWorker do
       :is_current => true
     )
   }
+  let(:eu_regulation){
+    create_eu_regulation(
+      :name => 'REGULATION 2.0',
+      :listing_changes_event_id => prev_eu_regulation.id,
+      :designation => eu,
+      :is_current => true
+    )
+  }
+  let(:species){
+    create_cites_eu_species
+  }
+  let(:subspecies){
+    create_cites_eu_subspecies(parent: species)
+  }
   let!(:listing_change){
     create_eu_A_addition(
-      :event_id => prev_eu_regulation.id
+      :event_id => prev_eu_regulation.id,
+      :taxon_concept_id => species.id
     )
   }
 
@@ -29,17 +44,33 @@ describe EventListingChangesCopyWorker do
   end
 
   context "when copy into current regulation" do
-    let(:eu_regulation){
-      create_eu_regulation(
-        :name => 'REGULATION 2.0',
-        :listing_changes_event_id => prev_eu_regulation.id,
-        :designation => eu,
-        :is_current => true
-      )
-    }
     before { EventListingChangesCopyWorker.new.perform(prev_eu_regulation.id, eu_regulation.id) }
     specify { eu_regulation.listing_changes.reload.count.should == 1 }
     specify { eu_regulation.listing_changes.first.is_current.should be_true }
+  end
+
+  context "when exclusion" do
+    let!(:taxonomic_exclusion){
+      create_eu_A_exception(
+        :parent_id => listing_change.id,
+        :taxon_concept_id => subspecies.id
+      )
+    }
+    let!(:geographic_exclusion){
+      create_eu_A_exception(
+        :parent_id => listing_change.id,
+        :taxon_concept_id => species.id
+      )
+    }
+    let!(:exclusion_distribution){
+      create(:listing_distribution, listing_change_id: geographic_exclusion.id)
+    }
+
+    before { EventListingChangesCopyWorker.new.perform(prev_eu_regulation.id, eu_regulation.id) }
+    specify { eu_regulation.listing_changes.reload.count.should == 1 }
+    specify { eu_regulation.listing_changes.first.exclusions.count.should == 2 }
+    specify { eu_regulation.listing_changes.first.taxonomic_exclusions.count.should == 1 }
+    specify { eu_regulation.listing_changes.first.geographic_exclusions.count.should == 1 }
   end
 
 end
