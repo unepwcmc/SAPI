ba04c166af9b5800f90ea1da9dc663a74e51359d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-04-21 10:25:46 +0100

    updated schema annotations

--- spec/models/nomenclature_change/output_reassignment_spec.rb
+++ spec/models/nomenclature_change/output_reassignment_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: nomenclature_change_output_reassignments
+#
+#  id                            :integer          not null, primary key
+#  nomenclature_change_output_id :integer          not null
+#  type                          :string(255)      not null
+#  reassignable_type             :string(255)
+#  reassignable_id               :integer
+#  note_en                       :text
+#  created_by_id                 :integer          not null
+#  updated_by_id                 :integer          not null
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
+#  note_es                       :text
+#  note_fr                       :text
+#  internal_note                 :text
+#
+
 require 'spec_helper'
 
 describe NomenclatureChange::OutputReassignment do
