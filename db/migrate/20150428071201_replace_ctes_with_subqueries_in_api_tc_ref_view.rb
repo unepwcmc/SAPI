f5875b8e095ecea4802ac602cc37c2ceaa06123d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-04-28 08:19:46 +0100

    replace CTEs with subqueries in taxon concept references view

---
+++ db/migrate/20150428071201_replace_ctes_with_subqueries_in_api_tc_ref_view.rb
@@ -0,0 +1,11 @@
+class ReplaceCtesWithSubqueriesInApiTcRefView < ActiveRecord::Migration
+  def up
+    execute "DROP VIEW IF EXISTS api_taxon_references_view"
+    execute "CREATE VIEW api_taxon_references_view AS #{view_sql('20150428071201', 'api_taxon_references_view')}"
+  end
+
+  def down
+    execute "DROP VIEW IF EXISTS api_taxon_references_view"
+    execute "CREATE VIEW api_taxon_references_view AS #{view_sql('20150402131608', 'api_taxon_references_view')}"
+  end
+end
