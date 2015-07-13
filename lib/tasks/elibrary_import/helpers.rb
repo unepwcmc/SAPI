9766ea2a6f99af429b84e0db5d0ba39ca8f948d3
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-07-13 13:20:15 +0100

    added script to import events

---
+++ lib/tasks/elibrary_import/helpers.rb
@@ -0,0 +1,31 @@
+def check_file_provided(task_name)
+  if ENV['FILE'].blank?
+    fail "Usage: FILE=/abs/path/to/file rake elibrary:import:#{task_name}"
+  end
+end
+
+def drop_table_if_exists(table_name)
+  ActiveRecord::Base.connection.execute "DROP TABLE IF EXISTS #{table_name} CASCADE"
+  puts "Table removed"
+end
+
+def create_table_from_column_array(table_name, db_columns_with_type)
+  drop_table_if_exists(table_name)
+  ActiveRecord::Base.connection.execute "CREATE TABLE #{table_name} (#{db_columns_with_type.join(', ')})"
+  puts "Table created"
+end
+
+def copy_from_csv(path_to_file, table_name, db_columns)
+  require 'psql_command'
+  puts "Copying data from #{path_to_file} into #{table_name}"
+  cmd = <<-PSQL
+SET DateStyle = \"ISO,DMY\";
+\\COPY #{table_name} (#{db_columns.join(', ')})
+FROM '#{Rails.root + path_to_file}'
+WITH DELIMITER ','
+ENCODING 'utf-8'
+CSV HEADER
+PSQL
+  PsqlCommand.new(cmd).execute
+  puts "Data copied to tmp table"
+end
