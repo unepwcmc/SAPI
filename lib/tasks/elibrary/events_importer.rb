ffcfc8bd2fca15aa226718838b27d27e22ed26df
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-07-15 14:58:36 +0100

    fixed duplication of EC SRG events

--- lib/tasks/elibrary/events_importer.rb
+++ lib/tasks/elibrary/events_importer.rb
@@ -47,6 +47,18 @@ class Elibrary::EventsImporter
       WHERE events.id = cops_to_update.id
     SQL
     ActiveRecord::Base.connection.execute(sql)
+
+    sql = <<-SQL
+      WITH srgs_to_update AS (
+        #{srgs_to_update_sql}
+      )
+      UPDATE events
+      SET elib_legacy_id = srgs_to_update.elib_legacy_id,
+        published_at = srgs_to_update.published_at
+      FROM srgs_to_update
+      WHERE events.id = srgs_to_update.id
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
   end
 
   def run_queries
@@ -69,13 +81,12 @@ class Elibrary::EventsImporter
   end
 
   def all_rows_sql
-    cites = fetch_designation_or_fail('CITES')
-    eu = fetch_designation_or_fail('EU')
+    cites = Designation.find_by_name('CITES')
     sql = <<-SQL
       SELECT
         EventID,
         CASE
-          WHEN splus_event_type = 'EcSrg' THEN #{eu.id}
+          WHEN splus_event_type = 'EcSrg' THEN NULL
           ELSE #{cites.id}
         END AS designation_id,
         BTRIM(EventName) AS EventName,
@@ -100,12 +111,26 @@ class Elibrary::EventsImporter
     SQL
   end
 
+  # EC SRGs are special, because they pre-existed in the system
+  # need to update them with the elib_legacy_id & published_at date
+  # matching depends on the event name & type
+  def srgs_to_update_sql
+    sql = <<-SQL
+      SELECT id, e.designation_id, name, type, ne.EventID AS elib_legacy_id, ne.EventDate AS published_at
+      FROM (#{all_rows_sql}) ne
+      JOIN events e -- designation_id left empty for EC SRGs
+      ON UPPER(e.name) = UPPER(ne.EventName)
+        AND UPPER(e.type) = UPPER(ne.splus_event_type) AND ne.splus_event_type = 'EcSrg'
+      WHERE e.published_at IS NULL OR e.elib_legacy_id IS NULL
+    SQL
+  end
+
   def rows_to_insert_sql
     sql = <<-SQL
       SELECT * FROM (
         #{all_rows_sql}
       ) all_rows_in_table_name
-      WHERE EventDate IS NOT NULL
+      WHERE splus_event_type IS NOT NULL AND EventDate IS NOT NULL
       EXCEPT
       SELECT elib_legacy_id, e.designation_id, name, published_at, type FROM (
         #{all_rows_sql}
@@ -122,14 +147,6 @@ class Elibrary::EventsImporter
 
   def print_post_import_stats
     print_events_breakdown
-  end
-
-  def fetch_designation_or_fail(name)
-    d = Designation.find_by_name(name)
-    unless d
-      fail "Designation #{name} missing from target DB"
-    end
-    d
   end
 
   def print_events_breakdown
