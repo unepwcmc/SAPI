447e9d81801e7d1bfcc8a00daac3b57a533dc122
Stuart Watson <stuart.watson@unep-wcmc.org>
2015-07-22 13:52:04 +0100

    Added secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,10 +40,10 @@ set :ssh_options, {
 
 
 # Default value for :linked_files is []
-#set :linked_files, fetch(:linked_files, []).push('config/database.yml config/mailer_config.yml')
+set :linked_files, %w{config/database.yml config/mailer_config.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-#set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :local_shared_dirs, %w('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/downloads', 'public/uploads' 'public/cites_trade_guidelines')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
