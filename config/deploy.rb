19f37940ca047a04725f2481cbf1300cf19ee9f6
Stuart Watson <stuart.watson@unep-wcmc.org>
2015-07-24 12:30:59 +0100

    Fixed problem with task not running

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,17 +38,7 @@ set :ssh_options, {
 }
 
 
-namespace :deploy do
-desc "Rsync of downloads subfolders"
-on roles(:app), in: :sequence, wait: 5 do
-task :sync do
-execute "rsync -av --ignore-existing #{release_path}/public/downloads/ #{shared_path}/public/downloads/"
-execute "rsync -av --ignore-existing #{release_path}/public/cites_trade_guidelines/ #{shared_path}/public/cites_trade_guidelines/"
-  end
- end
-end
-
-before "deploy:symlink:shared", "rsync"
+before "deploy:symlink:shared", "rsync:sync"
 
 
 # Default value for :linked_files is []
