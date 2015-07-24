fbc6c7e5b23563269b9b0b2607bd82c2ec26a562
strtwtsn <stuart.watson@unep-wcmc.org>
2015-07-24 11:18:47 +0100

    FIx for rsync task not running

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,11 +39,15 @@ set :ssh_options, {
 
 
 namespace :deploy do
-on roles(:app) do
+desc "Rsync of downloads subfolders"
+on roles(:app), in: :sequence, wait: 5 do
+task :rsync do
 execute "rsync -av --ignore-existing #{release_path}/public/downloads/ #{shared_path}/public/downloads/"
 execute "rsync -av --ignore-existing #{release_path}/public/cites_trade_guidelines/ #{shared_path}/public/cites_trade_guidelines/"
+  end
+ end
 end
-end
+
 
 
 
