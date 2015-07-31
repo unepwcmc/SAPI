dedb8e0e708e881f23f8324722f260d46a69e781
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-07-31 11:37:25 +0100

    Updated api_documents_controller tests

--- spec/controllers/api/documents_controller_spec.rb
+++ spec/controllers/api/documents_controller_spec.rb
@@ -1,17 +1,57 @@
 require 'spec_helper'
 
 describe Api::V1::DocumentsController, :type => :controller do
-  context "GET index" do
-    before(:each) do
-      @taxon_concept = create(:taxon_concept)
-      document = create(:proposal, event: create(:cites_cop, designation: cites))
-      citation = create(:document_citation, document_id: document.id)
-      citation_taxon_concept = create(:document_citation_taxon_concept,
-        document_citation_id: citation.id, taxon_concept_id: @taxon_concept.id)
+
+  before(:each) do
+    @taxon_concept = create(:taxon_concept)
+    document = create(:proposal, is_public: true, event: create(:cites_cop, designation: cites))
+    citation = create(:document_citation, document_id: document.id)
+    create(:document_citation_taxon_concept, document_citation_id: citation.id,
+      taxon_concept_id: @taxon_concept.id)
+    document2 = create(:proposal, event: create(:cites_cop, designation: cites))
+    citation2 = create(:document_citation, document_id: document2.id)
+    create(:document_citation_taxon_concept, document_citation_id: citation2.id,
+      taxon_concept_id: @taxon_concept.id)
+  end
+
+  context "GET index returns all documents" do
+    def get_all_documents
+      get :index, taxon_concept_id: @taxon_concept.id
+      response.body.should have_json_size(2).at_path('cites_cop_docs')
     end
-    it "returns documents" do
+    context "GET index contributor" do
+      login_contributor
+
+      it "returns all documents" do
+        get_all_documents
+      end
+    end
+
+    context "GET index manager" do
+      login_admin
+
+      it "returns all documents" do
+        get_all_documents
+      end
+    end
+  end
+
+  context "GET index returns only public documents" do
+    def get_public_documents
       get :index, taxon_concept_id: @taxon_concept.id
       response.body.should have_json_size(1).at_path('cites_cop_docs')
+    end
+    context "GET index api user " do
+      login_api_user
+
+      it "returns only public documents" do
+        get_public_documents
+      end
+    end
+    context "GET index no user" do |variable|
+      it "returns only public documents" do
+        get_public_documents
+      end
     end
   end
 end
