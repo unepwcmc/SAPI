4edc10bfe44e5c5325266ac37c94db31ef5779e8
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-08-03 16:24:12 +0100

    Added 403 error page and improved response on error

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -33,8 +33,10 @@ class Api::V1::DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
     path_to_file = @document.filename.path;
-    if access_denied? || !File.exists?(path_to_file)
-      redirect_to "/"
+    if access_denied?
+      render :file => "#{Rails.root}/public/403.html",  :status => 403
+    elsif !File.exists?(path_to_file)
+      render :file => "#{Rails.root}/public/404.html",  :status => 404
     else
       send_file(
         path_to_file,
