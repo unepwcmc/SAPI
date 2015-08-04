c6701eec6dc58a6ead07d51d21bcf5f5f9ec1e9b
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-08-04 15:54:04 +0100

    commented out the rsync step

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,9 +37,7 @@ set :ssh_options, {
   forward_agent: true,
 }
 
-
-before "deploy:symlink:shared", "rsync:sync"
-
+#before "deploy:symlink:shared", "rsync:sync"
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml config/mailer_config.yml config/secrets.yml}
