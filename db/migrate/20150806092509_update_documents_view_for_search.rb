4976b286cd4f661472919aca089711205035c333
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-08-06 16:05:20 +0100

    Updated api_documents_view

---
+++ db/migrate/20150806092509_update_documents_view_for_search.rb
@@ -0,0 +1,11 @@
+class UpdateDocumentsViewForSearch < ActiveRecord::Migration
+  def up
+    execute "DROP VIEW IF EXISTS api_documents_view"
+    execute "CREATE VIEW api_documents_view AS #{view_sql('20150806092509', 'api_documents_view')}"
+  end
+
+  def down
+    execute "DROP VIEW IF EXISTS api_documents_view"
+    execute "CREATE VIEW api_documents_view AS #{view_sql('20150730153305', 'api_documents_view')}"
+  end
+end
