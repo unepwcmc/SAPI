26d170bae80e12b87af8bedce1d4ae314df602b0
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-08-10 10:26:44 +0200

    Basic usage of SQL query in controller action

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -8,7 +8,8 @@ class Api::V1::DocumentsController < ApplicationController
     documents = documents.where("adv.is_public = true") if access_denied?
 
     ec_srg_docs = documents.where("adv.event_type = 'EcSrg'")
-    cites_cop_docs = documents.where("adv.event_type = 'CitesCop'")
+    cites_cop_docs = Document.find_by_sql(get_documents_by_sql)
+    byebug
     cites_ac_docs = documents.where("adv.event_type = 'CitesAc'")
     cites_pc_docs = documents.where("adv.event_type = 'CitesPc'")
     # other docs can be docs tied to historic types of events (CITES Technical
@@ -21,7 +22,7 @@ class Api::V1::DocumentsController < ApplicationController
     )
 
     render :json => {
-      cites_cop_docs: ActiveModel::ArraySerializer.new(cites_cop_docs, each_serializer: Species::DocumentsSerializer),
+      cites_cop_docs: cites_cop_docs,
       ec_srg_docs: ActiveModel::ArraySerializer.new(ec_srg_docs, each_serializer: Species::DocumentsSerializer),
       cites_ac_docs: ActiveModel::ArraySerializer.new(cites_ac_docs, each_serializer: Species::DocumentsSerializer),
       citec_pc_docs: ActiveModel::ArraySerializer.new(cites_pc_docs, each_serializer: Species::DocumentsSerializer),
@@ -48,6 +49,29 @@ class Api::V1::DocumentsController < ApplicationController
   end
 
   private
+
+  def get_documents_by_sql event_type=nil
+    <<-SQL
+      SELECT event_name, event_date, event_type,is_public, document_type,
+        number, sort_index, proposal_outcome, primary_document_id,
+        geo_entity_names, taxon_names,
+        ARRAY_TO_JSON(
+          ARRAY_AGG_NOTNULL(
+            ROW(
+              documents.id, documents.title, language
+            )::document_language_version
+          )
+        ) AS document_language_versions
+      FROM (
+        SELECT *
+        FROM api_documents_view
+        WHERE taxon_concept_ids @> ARRAY[4521]
+      ) documents
+      GROUP BY event_name, event_date, event_type, is_public, document_type,
+        number, sort_index, proposal_outcome, primary_document_id,
+        geo_entity_names, taxon_names
+    SQL
+  end
 
   def access_denied?
     !current_user || current_user.role == User::API_USER
