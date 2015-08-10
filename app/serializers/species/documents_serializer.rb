f94cd7d7b6a531185e04939e567a9909c5ec71bc
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-08-10 20:12:40 +0200

    Updated serializer to handle new custom type

--- app/serializers/species/documents_serializer.rb
+++ app/serializers/species/documents_serializer.rb
@@ -1,11 +1,19 @@
 class Species::DocumentsSerializer < ActiveModel::Serializer
   attributes :id, :event_type, :event_name, :event_date, :title, :is_public,
     :document_type, :number, :sort_index, :language, :proposal_outcome,
-    :primary_document_id, :taxon_concept_ids, :geo_entity_ids,
+    :primary_document_id, :taxon_names, :geo_entity_names,
     :taxon_names, :geo_entity_names, :languages
 
   def document_type
     object.document_type.split(":").last
+  end
+
+  def title
+    JSON.parse(object.document_language_versions).first["title"]
+  end
+
+  def language
+    JSON.parse(object.document_language_versions).first["language"]
   end
 
   def languages
