72736cef59ef28eda607377ce34341de3f8562bb
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-10-08 23:42:42 +0100

    Replace joins with includes to fix readonly exception

--- app/models/listing_change.rb
+++ app/models/listing_change.rb
@@ -137,14 +137,14 @@ class ListingChange < ActiveRecord::Base
       )
     )
     if party_listing_distribution
-      relation = relation.joins(:party_listing_distribution).where(
+      relation = relation.includes(:party_listing_distribution).where(
         party_listing_distribution.comparison_conditions(
           party_listing_distribution.comparison_attributes.except(:listing_change_id)
         )
       )
     end
     if annotation
-      relation = relation.joins(:annotation).where(
+      relation = relation.includes(:annotation).where(
         annotation.comparison_conditions
       )
     end
