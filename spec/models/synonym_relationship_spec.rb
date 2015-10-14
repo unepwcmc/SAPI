4850b95551445cbaf6e8607c419e389f45a08b6e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-10-14 11:24:32 +0100

    removed the ability to create S, H, T taxa via nested attributes in TaxonRelationship

--- spec/models/synonym_relationship_spec.rb
+++ spec/models/synonym_relationship_spec.rb
@@ -19,42 +19,34 @@ describe TaxonRelationship do
         :taxon_name => create(:taxon_name, :scientific_name => 'lolcatus')
       )
     }
-    let(:synonym_attributes){
-      build_tc_attributes(
-        :taxon_concept,
-        :taxonomy => cites_eu,
-        :rank => species_rank,
-        :name_status => 'S',
-        :author_year => 'Hemulen 2013',
-        :full_name => 'Lolcatus lolus'
+    let(:synonym){
+      create_cites_eu_species(
+        name_status: 'S',
+        author_year: 'Hemulen 2013',
+        full_name: 'Lolcatus lolus'
       )
     }
-    let(:another_synonym_attributes){
-      build_tc_attributes(
-        :taxon_concept,
-        :taxonomy => cites_eu,
-        :rank => species_rank,
-        :name_status => 'S',
-        :author_year => 'Hemulen 2013',
-        :full_name => 'Lolcatus lolatus'
+    let(:another_synonym){
+      create_cites_eu_species(
+        name_status: 'S',
+        author_year: 'Hemulen 2013',
+        full_name: 'Lolcatus lolatus'
       )
     }
     let(:synonymy_rel){
       build(
         :taxon_relationship,
-        :taxon_relationship_type => synonym_relationship_type,
-        :taxon_concept_id => tc.id,
-        :other_taxon_concept_id => nil,
-        :other_taxon_concept_attributes => synonym_attributes
+        taxon_relationship_type: synonym_relationship_type,
+        taxon_concept_id: tc.id,
+        other_taxon_concept_id: synonym.id
       )
     }
     let(:another_synonymy_rel){
       build(
         :taxon_relationship,
-        :taxon_relationship_type => synonym_relationship_type,
-        :taxon_concept_id => another_tc.id,
-        :other_taxon_concept_id => nil,
-        :other_taxon_concept_attributes => synonym_attributes
+        taxon_relationship_type: synonym_relationship_type,
+        taxon_concept_id: another_tc.id,
+        other_taxon_concept_id: synonym.id
       )
     }
     specify {
@@ -62,25 +54,9 @@ describe TaxonRelationship do
       tc.synonyms.map(&:full_name).should include('Lolcatus lolus')
     }
     specify{
-      lambda do
-        synonymy_rel.save
-      end.should change(TaxonConcept, :count).by(1)
-    }
-    specify{
-      lambda do
-        synonymy_rel.save
-        another_synonymy_rel.save
-      end.should change(TaxonConcept, :count).by(1)
-    }
-    specify{
       synonymy_rel.save
       another_synonymy_rel.save
-      another_tc.synonyms.map(&:full_name).should include('Lolcatus lolus')
-    }
-    specify{
-      synonymy_rel.save
-      another_synonymy_rel.save
-      synonymy_rel.other_taxon_concept_attributes = another_synonym_attributes
+      synonymy_rel.other_taxon_concept = another_synonym
       synonymy_rel.save
       tc.synonyms.map(&:full_name).should include('Lolcatus lolatus')
       another_tc.synonyms.map(&:full_name).should include('Lolcatus lolus')
