dc7ec46a5867cabdaa318b915149016578b7d1a4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-10-15 14:22:29 +0100

    fixed transfer of distribution references

--- app/models/nomenclature_change/reassignment_transfer_processor.rb
+++ app/models/nomenclature_change/reassignment_transfer_processor.rb
@@ -6,6 +6,7 @@ class NomenclatureChange::ReassignmentTransferProcessor < NomenclatureChange::Re
     if reassigned_object
       reassigned_object.save(validate: false)
       post_process(reassigned_object, object_before_reassignment)
+      transfer_associations(reassigned_object, reassignable)
     end
     reassigned_object
   end
@@ -46,6 +47,47 @@ class NomenclatureChange::ReassignmentTransferProcessor < NomenclatureChange::Re
   def summary_line
     "The following associations will be transferred from #{@input.taxon_concept.full_name}
       to #{@output.display_full_name}"
+  end
+
+  protected
+
+  def transfer_associations(reassigned_object, reassignable)
+    return if reassigned_object.id == reassignable.id
+    if reassigned_object.is_a?(Distribution)
+      # that means the distribution is a duplicate and was not transferred
+      # but it might have some distribution references and taggings
+      transfer_distribution_references(reassigned_object, reassignable)
+      transfer_distribution_taggings(reassigned_object, reassignable)
+    end
+    # destroy the original object
+    reassignable.delete
+  end
+
+  def transfer_distribution_references(reassigned_object, reassignable)
+    return if reassignable.distribution_references.count == 0
+    distribution_references_to_transfer = reassignable.distribution_references
+    if reassigned_object.distribution_references.count > 0
+      distribution_references_to_transfer = distribution_references_to_transfer.
+      where(
+        'reference_id NOT IN (?)',
+        reassigned_object.distribution_references.select(:reference_id)
+          .map(&:reference_id)
+      )
+    end
+    distribution_references_to_transfer.update_all(distribution_id: reassigned_object.id)
+  end
+
+  def transfer_distribution_taggings(reassigned_object, reassignable)
+    return if reassignable.taggings.count == 0
+    taggings_to_transfer = reassignable.taggings
+    if reassigned_object.taggings.count > 0
+      taggings_to_transfer = taggings_to_transfer.
+      where(
+        'tag_id NOT IN (?)',
+        reassigned_object.taggings.select(:tag_id).map(&:tag_id)
+      )
+    end
+    taggings_to_transfer.update_all(taggable_id: reassigned_object.id)
   end
 
 end
