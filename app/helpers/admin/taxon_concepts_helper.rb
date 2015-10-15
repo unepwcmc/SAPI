a7b40b665dbdd337c6609d8f898457cbc127ba75
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-10-15 13:53:52 +0100

    Fix accepted names for trade names

--- app/helpers/admin/taxon_concepts_helper.rb
+++ app/helpers/admin/taxon_concepts_helper.rb
@@ -9,11 +9,13 @@ module Admin::TaxonConceptsHelper
           'data-rank-scope' => 'parent')
       end
     elsif ['S', 'T'].include? name_status
+      name_ids =
+        name_status == 'S' ? :accepted_name_ids : :accepted_names_for_trade_name_ids
       content_tag(:div, class: 'control-group') do
         concat content_tag(:label, 'Accepted names')
-        concat f.text_field(:accepted_name_ids, {
+        concat f.text_field(name_ids, {
           :class => 'taxon-concept-multiple',
-          :'data-name' => TaxonConcept.fetch_taxons_full_name(f.object.accepted_name_ids).to_s, 
+          :'data-name' => TaxonConcept.fetch_taxons_full_name(f.object.send(name_ids)).to_s,
           :'data-name-status' => 'A',
           :'data-name-status-filter' => ['A'].to_json,
           :'data-taxonomy-id' => f.object.taxonomy_id,
