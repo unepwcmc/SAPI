55b36f8fce3afe322dbde2101816ba122b885de6
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-12-10 15:07:24 +0000

    remove unnecessary query

--- app/controllers/admin/api_usage_controller.rb
+++ app/controllers/admin/api_usage_controller.rb
@@ -1,13 +1,11 @@
 class Admin::ApiUsageController < Admin::AdminController
   def index
     @last_30_days_requests = ApiRequest.last_30_days_requests
-    @all_requests = ApiRequest.all
     @users_by_activity = ApiRequest.top_5_most_active_users
   end
 
   def show
     @user = User.find(params[:id])
     @last_30_days_requests = @user.last_30_days_api_requests
-    @all_requests = @user.api_requests
   end
 end
