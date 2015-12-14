938be174d713b8d8187dd9699350f1e487782ae4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-12-14 11:16:50 +0000

    improved speed of stats queries to display recent 90 days

--- app/helpers/admin/api_usage_helper.rb
+++ app/helpers/admin/api_usage_helper.rb
@@ -1,4 +1,13 @@
 module Admin::ApiUsageHelper
+
+  def status_colours
+    ['#109618', '#DDDDDD', '#EE3B3B', '#990099', '#dc3912', '#000000']
+  end
+
+  def controller_colours
+    ['#dc3912', '#990099', '#109618', '#3fb0ac', '#e6af4b']
+  end
+
   # Take the hash necessary for the line graph and replace 200 with 'Successful'
   # and anything else with 'Failed' and return newly constructed hash
   def sanitise_hash_for_line_graph(hash)
