4d53d366450f888514a168f3809e88e5b6d74bf9
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-03-15 13:13:39 +0000

    added spec for primary output name status validation

--- spec/models/nomenclature_change/status_to_synonym_spec.rb
+++ spec/models/nomenclature_change/status_to_synonym_spec.rb
@@ -36,6 +36,34 @@ describe NomenclatureChange::StatusToSynonym do
         specify { expect(status_change).to have(1).error_on(:primary_output) }
       end
     end
+    context "when primary output has invalid name status" do
+      context "when primary_output" do
+        let(:status_change){
+          build(
+            :nomenclature_change_status_to_synonym,
+            :primary_output_attributes => {
+              taxon_concept_id: create_cites_eu_species(name_status: 'A').id
+            },
+            status: NomenclatureChange::StatusToAccepted::PRIMARY_OUTPUT
+          )
+        }
+        specify { expect(status_change).to have(1).error_on(:primary_output) }
+      end
+    end
+    context "when primary output has valid name status" do
+      context "when primary_output" do
+        let(:status_change){
+          build(
+            :nomenclature_change_status_to_synonym,
+            :primary_output_attributes => {
+              taxon_concept_id: create_cites_eu_species(name_status: 'N').id
+            },
+            status: NomenclatureChange::StatusToAccepted::PRIMARY_OUTPUT
+          )
+        }
+        specify { expect(status_change).to have(0).errors_on(:primary_output) }
+      end
+    end
     context "when required secondary output missing" do
       context "when relay" do
         let(:status_change){
