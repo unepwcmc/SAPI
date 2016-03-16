e47cc0d8454fb0d45d367b0c4c7263dd80e49f96
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-03-16 17:00:55 +0000

    removed commented code and fixed indentation

--- spec/factories/ranks.rb
+++ spec/factories/ranks.rb
@@ -1,90 +1,46 @@
-  # def find_or_create_rank(name)
-  #   send("#{name.downcase}_rank")
-  # end
+def attributes_for_rank(name)
+  send("attributes_for_#{name.downcase}")
+end
 
-  # def kingdom_rank
-  #   Rank.find_by_name(Rank::KINGDOM) || Rank.new(attributes_for_kingdom)
-  # end
+def attributes_for_kingdom
+  { name: Rank::KINGDOM, taxonomic_position: '1', fixed_order: true }
+end
 
-  # def phylum_rank
-  #   Rank.find_by_name(Rank::PHYLUM) || Rank.new(attributes_for_phylum)
-  # end
+def attributes_for_phylum
+  { name: Rank::PHYLUM, taxonomic_position: '2', fixed_order: true }
+end
 
-  # def class_rank
-  #   Rank.find_by_name(Rank::CLASS) || Rank.new(attributes_for_class)
-  # end
+def attributes_for_class
+  { name: Rank::CLASS, taxonomic_position: '3', fixed_order: true }
+end
 
-  # def order_rank
-  #   Rank.find_by_name(Rank::ORDER) || Rank.new(attributes_for_order)
-  # end
+def attributes_for_order
+  { name: Rank::ORDER, taxonomic_position: '4', fixed_order: false }
+end
 
-  # def family_rank
-  #   Rank.find_by_name(Rank::FAMILY) || Rank.new(attributes_for_family)
-  # end
+def attributes_for_family
+  { name: Rank::FAMILY, taxonomic_position: '5', fixed_order: false }
+end
 
-  # def subfamily_rank
-  #   Rank.find_by_name(Rank::SUBFAMILY) || Rank.new(attributes_for_subfamily)
-  # end
+def attributes_for_subfamily
+  { name: Rank::SUBFAMILY, taxonomic_position: '5.1', fixed_order: false }
+end
 
-  # def genus_rank
-  #   Rank.find_by_name(Rank::GENUS) || Rank.new(attributes_for_genus)
-  # end
+def attributes_for_genus
+  { name: Rank::GENUS, taxonomic_position: '6', fixed_order: false }
+end
 
-  # def species_rank
-  #   Rank.find_by_name(Rank::SPECIES) || Rank.new(attributes_for_species)
-  # end
+def attributes_for_species
+  { name: Rank::SPECIES, taxonomic_position: '7', fixed_order: false }
+end
 
-  # def subspecies_rank
-  #   Rank.find_by_name(Rank::SUBSPECIES) || Rank.new(attributes_for_subspecies)
-  # end
+def attributes_for_subspecies
+  { name: Rank::SUBSPECIES, taxonomic_position: '7.1', fixed_order: false }
+end
 
-  # def variety_rank
-  #   Rank.find_by_name(Rank::VARIETY) || Rank.new(attributes_for_variety)
-  # end
-
-  def attributes_for_rank(name)
-    send("attributes_for_#{name.downcase}")
-  end
-
-  def attributes_for_kingdom
-    { name: Rank::KINGDOM, taxonomic_position: '1', fixed_order: true }
-  end
-
-  def attributes_for_phylum
-    { name: Rank::PHYLUM, taxonomic_position: '2', fixed_order: true }
-  end
-
-  def attributes_for_class
-    { name: Rank::CLASS, taxonomic_position: '3', fixed_order: true }
-  end
-
-  def attributes_for_order
-    { name: Rank::ORDER, taxonomic_position: '4', fixed_order: false }
-  end
-
-  def attributes_for_family
-    { name: Rank::FAMILY, taxonomic_position: '5', fixed_order: false }
-  end
-
-  def attributes_for_subfamily
-    { name: Rank::SUBFAMILY, taxonomic_position: '5.1', fixed_order: false }
-  end
-
-  def attributes_for_genus
-    { name: Rank::GENUS, taxonomic_position: '6', fixed_order: false }
-  end
-
-  def attributes_for_species
-    { name: Rank::SPECIES, taxonomic_position: '7', fixed_order: false }
-  end
-
-  def attributes_for_subspecies
-    { name: Rank::SUBSPECIES, taxonomic_position: '7.1', fixed_order: false }
-  end
-
-  def attributes_for_variety
-    { name: Rank::VARIETY, taxonomic_position: '7.2', fixed_order: false }
-  end
+def attributes_for_variety
+  { name: Rank::VARIETY, taxonomic_position: '7.2', fixed_order: false }
+end
 
 FactoryGirl.define do
   factory :rank do
