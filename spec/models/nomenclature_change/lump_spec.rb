3e476e7ca3921afde2ef6fca793d4c9c265ebac1
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-03-18 16:55:31 +0000

    Remove unnecessary test for different ranks

--- spec/models/nomenclature_change/lump_spec.rb
+++ spec/models/nomenclature_change/lump_spec.rb
@@ -61,21 +61,5 @@ describe NomenclatureChange::Lump do
         specify { expect(lump).to have(1).errors_on(:inputs) }
       end
     end
-    context "when output has different rank than inputs" do
-      let(:lump){
-        build(:nomenclature_change_lump,
-          :status => NomenclatureChange::Lump::OUTPUTS,
-          :inputs_attributes => {
-            0 => {:taxon_concept_id => create_cites_eu_subspecies.id},
-            1 => {:taxon_concept_id => create_cites_eu_subspecies.id}
-          },
-          :output_attributes => {
-            :taxon_concept_id => create_cites_eu_species.id,
-            :new_rank_id => create(:rank, name: Rank::SPECIES).id
-          }
-        )
-      }
-      specify { expect(lump).to have(1).errors_on(:output) }
-    end
   end
 end
