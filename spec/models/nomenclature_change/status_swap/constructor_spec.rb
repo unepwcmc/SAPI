4cdd8ec6df66a96fab9c4b803defb1e0558ddac5
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-03-23 16:13:18 +0000

    Fix tests

--- spec/models/nomenclature_change/status_swap/constructor_spec.rb
+++ spec/models/nomenclature_change/status_swap/constructor_spec.rb
@@ -37,27 +37,21 @@ describe NomenclatureChange::StatusSwap::Constructor do
   end
 
   describe :build_output_notes do
-    let(:primary_output){ status_change.primary_output }
-    let(:secondary_output){ status_change.secondary_output }
+    let(:new_accepted_name){ status_change.new_accepted_name }
     before(:each) do
-      @old_primary_output_note = primary_output.internal_note
-      @old_secondary_output_note = secondary_output.note_en
+      @old_new_accepted_name_note = new_accepted_name.note_en
       constructor.build_output_notes
     end
     let(:status_change){ a_to_s_with_swap }
     context "when previously no notes in place" do
-      specify{ expect(primary_output.internal_note).to be_blank }
-      specify{ expect(secondary_output.note_en).not_to be_blank }
+      specify{ expect(new_accepted_name.internal_note).to be_blank }
+      specify{ expect(new_accepted_name.note_en).not_to be_blank }
     end
     context "when previously notes in place" do
-      let(:primary_output){
-        create(:nomenclature_change_output, nomenclature_change: status_change, internal_note: 'blah')
-      }
-      let(:secondary_output){
+      let(:new_accepted_name){
         create(:nomenclature_change_output, nomenclature_change: status_change, note_en: 'blah')
       }
-      specify{ expect(primary_output.internal_note).to eq(@old_primary_output_note) }
-      specify{ expect(secondary_output.note_en).to eq(@old_secondary_output_note) }
+      specify{ expect(new_accepted_name.note_en).to eq(@old_new_accepted_name_note) }
     end
   end
 
