b363368e36913d16468a8ee76a0e85564c7b562d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-03-30 23:13:58 +0200

    refactored common funtionality from geo entity controllers into a search class with low-level caching

--- app/controllers/api/v1/geo_entities_controller.rb
+++ app/controllers/api/v1/geo_entities_controller.rb
@@ -1,27 +1,9 @@
 class Api::V1::GeoEntitiesController < ApplicationController
 
-  # TODO refactor this into a search class with low level caching
-  # needs to handle both geo entity types selection and is_current
-  # perhaps a visibility parameter (e.g. checklist, s+, admin trade)
   def index
-    locale = if params[:locale] &&
-      ['en', 'es', 'fr'].include?(params[:locale])
-      params[:locale]
-    else
-      I18n.locale
-    end
-    geo_entity_types_set = GeoEntityType::SETS.has_key?(params[:geo_entity_types_set]) &&
-      params[:geo_entity_types_set] || GeoEntityType::DEFAULT_SET
-    geo_entity_types = GeoEntityType::SETS[geo_entity_types_set]
-    @geo_entities = GeoEntity.includes(:geo_entity_type).order("name_#{locale}")
-    if GeoEntityType::CURRENT_ONLY_SETS.include?(geo_entity_types_set)
-      @geo_entities = @geo_entities.current
-    end
-    unless geo_entity_types.empty?
-      @geo_entities = @geo_entities.
-        joins(:geo_entity_type).
-        where(:"geo_entity_types.name" => geo_entity_types)
-    end
+    @geo_entities = GeoEntitySearch.new(
+      params.slice(:geo_entity_types_set, :locale)
+    ).results
     render :json => @geo_entities,
       :each_serializer => Species::GeoEntitySerializer,
       :meta => {:total => @geo_entities.count}
--- app/controllers/checklist/geo_entities_controller.rb
+++ app/controllers/checklist/geo_entities_controller.rb
@@ -1,20 +1,10 @@
-#TODO remove this once Checklist is upgraded to use the API
 class Checklist::GeoEntitiesController < ApplicationController
 
   #override the serializer by using render :text, old ember-data won't handle json root
   def index
-    geo_entity_types = if params[:geo_entity_types_set] &&
-      GeoEntityType::SETS.has_key?(params[:geo_entity_types_set])
-                         GeoEntityType::SETS[params[:geo_entity_types_set]]
-                       else
-                         GeoEntityType::SETS[GeoEntityType::DEFAULT_SET]
-                       end
-    @geo_entities = GeoEntity.current.order(:"name_#{I18n.locale}")
-    unless geo_entity_types.empty?
-      @geo_entities = @geo_entities.
-        joins(:geo_entity_type).
-        where(:"geo_entity_types.name" => geo_entity_types)
-    end
+    @geo_entities = GeoEntitySearch.new(
+      params.slice(:geo_entity_types_set, :locale)
+    ).results
     render :text => @geo_entities.to_json
   end
 
