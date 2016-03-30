d0a13f672725fe905cbbe5ab77c0a8147e60a95d
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-03-30 22:08:09 +0200

    Remove old documents refresh worker and library

--- app/models/document_observer.rb
+++ app/models/document_observer.rb
@@ -2,12 +2,10 @@ class DocumentObserver < ActiveRecord::Observer
 
   def after_save(document)
     DocumentSearch.increment_cache_iterator
-    RefreshDocumentsWorker.perform_async
   end
 
   def after_destroy(document)
     DocumentSearch.increment_cache_iterator
-    RefreshDocumentsWorker.perform_async
   end
 
 end
--- app/workers/refresh_documents_worker.rb
+++
@@ -1,8 +0,0 @@
-class RefreshDocumentsWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: :admin, backtrace: 50, unique: :until_and_while_executing
-
-  def perform
-    DocumentSearch.refresh
-  end
-end
