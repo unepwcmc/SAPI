ed904ddc029d8caffb5c3e6d4b7cff439fdc8a8d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-03-30 23:13:58 +0200

    refactored common funtionality from geo entity controllers into a search class with low-level caching

---
+++ app/models/geo_entity_observer.rb
@@ -0,0 +1,11 @@
+class GeoEntityObserver < ActiveRecord::Observer
+
+  def after_save(geo_entity)
+    GeoEntitySearch.increment_cache_iterator
+  end
+
+  def after_destroy(geo_entity)
+    GeoEntitySearch.increment_cache_iterator
+  end
+
+end
