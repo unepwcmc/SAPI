4bf607a1e380698b76ceffd2ce8185a4262a27df
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-04-06 19:08:53 +0200

    removed parent_name_compatible validation

--- spec/models/taxon_concept/validation_spec.rb
+++ spec/models/taxon_concept/validation_spec.rb
@@ -27,31 +27,6 @@ describe TaxonConcept do
       }
       specify { tc.should have(1).error_on(:parent_id) }
     end
-    context "parent name is incompatible" do
-      let(:genus_tc){
-        create_genus(
-          :taxonomy_id => cites_eu.id,
-          :taxon_name => build(:taxon_name, :scientific_name => 'Foobarus')
-        )
-      }
-      let(:another_genus_tc){
-        create_genus(
-          :taxonomy_id => cites_eu.id,
-          :taxon_name => build(:taxon_name, :scientific_name => 'Foobaria')
-        )
-      }
-      let(:tc) {
-        create_species(
-          :taxonomy_id => cites_eu.id,
-          :parent_id => genus_tc.id
-        )
-      }
-      let(:tc_with_incompatible_parent){
-        tc.parent = another_genus_tc
-        tc
-      }
-      specify { tc_with_incompatible_parent.should have(1).error_on(:parent_id) }
-    end
     context "parent is not an accepted name" do
       let(:genus_tc){
         create_genus(
