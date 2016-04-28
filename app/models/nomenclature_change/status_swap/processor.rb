1af57581d21047893fe9f90741bb4c8ab292e34d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-04-28 12:20:57 +0100

    simplified swap processor logic (needs_to_relay always true, needs_to_receive always false)

--- app/models/nomenclature_change/status_swap/processor.rb
+++ app/models/nomenclature_change/status_swap/processor.rb
@@ -7,15 +7,10 @@ class NomenclatureChange::StatusSwap::Processor < NomenclatureChange::Processor
   # A subprocessor needs to respond to #run
   def prepare_chain
     chain = []
-    output = if @nc.needs_to_relay_associations?
-      @secondary_output
-    elsif @nc.needs_to_receive_associations?
-      @primary_output
-    end
     chain << NomenclatureChange::OutputTaxonConceptProcessor.new(@primary_output)
     chain << NomenclatureChange::OutputTaxonConceptProcessor.new(@secondary_output)
 
-    chain << reassignment_processor(output)
+    chain << reassignment_processor(@secondary_output)
 
     chain << if @primary_output.new_name_status == 'A'
       linked_names = @secondary_output ? [@secondary_output] : []
