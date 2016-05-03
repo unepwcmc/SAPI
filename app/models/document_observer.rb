65bdebab0e0d0ebde711fc85cbcf6565c9a8aae9
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-05-03 10:15:35 +0100

    caching and invalidating cache of documents download

--- app/models/document_observer.rb
+++ app/models/document_observer.rb
@@ -1,13 +1,19 @@
 class DocumentObserver < ActiveRecord::Observer
 
   def after_save(document)
-    DocumentSearch.increment_cache_iterator
-    RefreshDocumentsWorker.perform_async
+    clear_cache
   end
 
   def after_destroy(document)
+    clear_cache
+  end
+
+  private
+
+  def clear_cache
     DocumentSearch.increment_cache_iterator
     RefreshDocumentsWorker.perform_async
+    DownloadsCacheCleanupWorker.perform_async(:documents)
   end
 
 end
