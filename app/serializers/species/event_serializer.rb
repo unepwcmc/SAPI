0111c377ae96e25ec3b1a81f8db0029e11c6dfed
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-05-04 14:14:06 +0100

    Attach effective_at to name in serialiser

--- app/serializers/species/event_serializer.rb
+++ app/serializers/species/event_serializer.rb
@@ -1,3 +1,8 @@
 class Species::EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :type
+
+  def name
+    name = object.name
+    "#{name} #{object.effective_at.strftime('(%B %Y)')}" if object.effective_at
+  end
 end
