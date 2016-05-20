c1262a5263d9e3f46d009a4afe8a8bb26a49ce8a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-05-20 11:06:35 +0100

    added new page to reorder documents

--- app/controllers/admin/event_documents_controller.rb
+++ app/controllers/admin/event_documents_controller.rb
@@ -2,4 +2,6 @@ class Admin::EventDocumentsController < Admin::SimpleCrudController
   defaults :resource_class => Document,
     :collection_name => 'documents', :instance_name => 'document'
   belongs_to :event
+
+  def reorder; end
 end
