ef1a0a64226919dd786e4176550116cf91a24c3b
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-10-25 19:10:09 +0200

    check for citation duplicates in reassignment processors

--- app/models/document_citation.rb
+++ app/models/document_citation.rb
@@ -34,4 +34,29 @@ class DocumentCitation < ActiveRecord::Base
     dc.document.touch
   end
 
+  def duplicates(comparison_attributes_override = {})
+    taxon_concept_id = comparison_attributes_override.delete(:taxon_concept_id)
+
+    relation = DocumentCitation.where(document_id: self.document_id).
+      includes(:document_citation_taxon_concepts).
+      where('document_citation_taxon_concepts.taxon_concept_id' => taxon_concept_id)
+    geo_entities_ids = document_citation_geo_entities.pluck(:geo_entity_id)
+    if !geo_entities_ids.empty?
+      relation = relation.joins(
+        ActiveRecord::Base.send(:sanitize_sql_array, [
+          "JOIN (
+            SELECT document_citation_id, CASE
+              WHEN ARRAY_AGG(geo_entity_id) @> ARRAY[:geo_entities_ids]::INT[] THEN TRUE
+              ELSE FALSE
+            END AS geo_entities_match
+            FROM document_citation_geo_entities
+            GROUP BY document_citation_id
+          ) s ON s.document_citation_id = document_citations.id AND s.geo_entities_match",
+          geo_entities_ids: geo_entities_ids
+        ])
+      )
+    end
+    relation
+  end
+
 end
