5a00e3503f17184484abfc56db8cf5059840998d
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-01-03 15:55:34 +0000

    Fix serializers nil error

--- app/serializers/trade/annual_report_upload_serializer.rb
+++ app/serializers/trade/annual_report_upload_serializer.rb
@@ -7,11 +7,11 @@ class Trade::AnnualReportUploadSerializer < ActiveModel::Serializer
   end
 
   def created_at
-    object.created_at.strftime("%d/%m/%y")
+    object.created_at && object.created_at.strftime("%d/%m/%y")
   end
 
   def updated_at
-    object.updated_at.strftime("%d/%m/%y")
+    object.updated_at && object.updated_at.strftime("%d/%m/%y")
   end
 
   def created_by
