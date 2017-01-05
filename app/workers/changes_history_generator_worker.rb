ed7cf2c6c5395e12177d34401d344dee66b26ad4
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-01-05 14:19:13 +0000

    Rescue errors when uploading changelog to S3

--- app/workers/changes_history_generator_worker.rb
+++ app/workers/changes_history_generator_worker.rb
@@ -17,13 +17,18 @@ class ChangesHistoryGeneratorWorker
     user = User.find(user_id)
     tempfile = Trade::ChangelogCsvGenerator.call(aru, user)
 
-    s3 = Aws::S3::Resource.new
-    filename = "trade/annual_report_upload/#{aru.id}/changelog.csv"
-    bucket_name = Rails.application.secrets.aws['bucket_name']
-    obj = s3.bucket(bucket_name).object(filename)
-    obj.upload_file(tempfile.path)
+    begin
+      s3 = Aws::S3::Resource.new
+      filename = "trade/annual_report_upload/#{aru.id}/changelog.csv"
+      bucket_name = Rails.application.secrets.aws['bucket_name']
+      obj = s3.bucket(bucket_name).object(filename)
+      obj.upload_file(tempfile.path)
 
-    aru.update_attributes(aws_storage_path: obj.public_url)
+      aru.update_attributes(aws_storage_path: obj.public_url)
+    rescue => e
+      Rails.logger.info("Something went wrong while uploading file to S3")
+      Rails.logger.info(e)
+    end
 
     NotificationMailer.changelog(user, aru, tempfile).deliver
 
