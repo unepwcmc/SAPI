d87e5d58902e05a53acb3259f9aed490cd4f3114
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-01-10 10:16:57 +0000

    Remove obsolete method in mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,18 +1,5 @@
 class NotificationMailer < ApplicationMailer
 
-  def validation_result(user, aru, validation_report, validation_report_csv_file)
-    @user = user
-    @aru = aru
-    @status = validation_report[:CITESReportResult][:Status]
-    @message = validation_report[:CITESReportResult][:Message]
-    @has_errors = @aru.validation_report.present?
-    if @has_errors
-      attachments["validation_report_#{@aru.id}.csv"] = File.read(validation_report_csv_file.path)
-u
-    end
-    mail(to: @user.email, subject: 'CITES Report validation result')
-  end
-
   def changelog(user, aru, csv_file)
     @user = user
     @aru = aru
