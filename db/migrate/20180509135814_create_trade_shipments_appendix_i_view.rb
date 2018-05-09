92c82f54e85d044d6c988c7a1c3594a3ec1bf444
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-05-09 17:05:48 +0100

    Create view about appendix I compliance

---
+++ db/migrate/20180509135814_create_trade_shipments_appendix_i_view.rb
@@ -0,0 +1,10 @@
+class CreateTradeShipmentsAppendixIView < ActiveRecord::Migration
+  def up
+    execute "DROP VIEW IF EXISTS trade_shipments_appendix_i_view"
+    execute "CREATE VIEW trade_shipments_appendix_i_view AS #{view_sql('20180509135814', 'trade_shipments_appendix_i_view')}"
+  end
+
+  def down
+    execute "DROP VIEW IF EXISTS trade_shipments_appendix_i_view"
+  end
+end
---
+++ db/views/trade_shipments_appendix_i_view/20180509135814.sql
@@ -0,0 +1,13 @@
+SELECT ts.*
+FROM trade_shipments_with_taxa_view ts
+INNER JOIN trade_codes s ON ts.source_id = s.id
+INNER JOIN trade_codes p ON ts.purpose_id = p.id
+INNER JOIN listing_changes lc ON ts.taxon_concept_id = lc.taxon_concept_id
+INNER JOIN species_listings sl ON lc.species_listing_id = sl.id
+WHERE ts.appendix = 'I'
+  AND lc.is_current
+  AND ts.year >= to_char(lc.effective_at, 'YYYY')::int
+  AND p.type = 'Purpose'
+  AND p.code = 'T'
+  AND s.type = 'Source'
+  AND s.code = 'W'
