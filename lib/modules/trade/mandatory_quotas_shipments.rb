fb6395e2e0099dc11a83f8b59f9e31541f34f932
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-05-30 13:01:06 +0100

    Dynamically generate quotas query based on quotas file

---
+++ lib/modules/trade/mandatory_quotas_shipments.rb
@@ -0,0 +1,92 @@
+class Trade::MandatoryQuotasShipments
+  attr_reader :query
+
+  QUOTAS_PATH = 'lib/data/quotas.csv'
+
+  ATTRIBUTES = [:start_date, :end_date, :taxon_concept_id, :iso_code2, :quota,
+                :unit, :term, :source, :purpose]
+
+  def initialize
+    @query = "#{select} #{from} #{joins} WHERE"
+    length = CSV.read(QUOTAS_PATH).length
+    CSV.foreach(QUOTAS_PATH, headers: true) do |row|
+      @query << "(#{where(row)})"
+      #Conjunction if not EOF
+      @query << " OR " if $. < length
+    end
+  end
+
+  private
+
+  def select
+    """
+      SELECT ts.*, ge.iso_code2 AS exporter, source.code AS source,
+             purpose.code AS purpose, unit.code AS unit, term.code AS term
+    """.gsub("\n", '')
+  end
+
+  def from
+    'FROM trade_shipments_with_taxa_view ts'
+  end
+
+  def joins
+    """
+      INNER JOIN geo_entities AS ge ON ge.id = ts.exporter_id
+      LEFT OUTER JOIN trade_codes source ON ts.source_id = source.id
+      LEFT OUTER JOIN trade_codes purpose ON ts.purpose_id = purpose.id
+      LEFT OUTER JOIN trade_codes unit ON ts.unit_id = unit.id
+      LEFT OUTER JOIN trade_codes term ON ts.term_id = term.id
+    """.gsub("\n", '')
+  end
+
+  def where(row)
+    ATTRIBUTES.map { |a| send("parse_#{a.to_s}", row[a.to_s]) }.join(' AND ')
+  end
+
+  def parse_start_date(date)
+    year = date.split('/').last.to_i
+    "ts.year >= #{year}"
+  end
+
+  def parse_end_date(date)
+    year = date == 'Present' ? Date.today.year  : date.split('/').last.to_i
+    "ts.year <= #{year}"
+  end
+
+  def parse_iso_code2(iso)
+    "ge.iso_code2 = '#{iso}'"
+  end
+
+  def parse_taxon_concept_id(tc)
+    "ts.taxon_concept_id = #{tc}"
+  end
+
+  def parse_unit(unit)
+    parse_trade_code(unit, 'unit')
+  end
+
+  def parse_term(term)
+    parse_trade_code(term, 'term')
+  end
+
+  def parse_purpose(purpose)
+    parse_trade_code(purpose, 'purpose')
+  end
+
+  def parse_source(source)
+    parse_trade_code(source, 'source')
+  end
+
+  def parse_trade_code(code, type)
+    #Return TRUE to prevent empty conditions and a malformed query
+    return 'TRUE' if code == 'All' || code.blank?
+
+    codes = code.split(';')
+    where = codes.map { |c| "#{type}.code = '#{c.strip}'" }.join(' OR ')
+    "(#{where})"
+  end
+
+  def parse_quota(quota)
+    "ts.quantity >= #{quota}"
+  end
+end
