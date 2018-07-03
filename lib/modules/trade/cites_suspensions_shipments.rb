303f121e1b534a0a72f2f0057eeb1c8b2c1f9e40
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-07-03 13:22:15 +0100

    Add exemptions for cites suspensions compliance query

---
+++ lib/modules/trade/cites_suspensions_shipments.rb
@@ -0,0 +1,217 @@
+class Trade::CitesSuspensionsShipments
+
+  attr_accessor :query
+
+  EXEMPTIONS_PATH = 'lib/data/exemptions.csv'.freeze
+
+  VIEW_DIR = 'db/views/trade_shipments_cites_suspensions_view'.freeze
+
+  ATTRIBUTES = [
+    :start_date, :end_date, :taxon_concept_id, :iso_code2,
+    :unit, :term, :source, :purpose
+  ]
+
+  def initialize
+    @query = exceptions_query
+  end
+
+  def generate_view(timestamp)
+    Dir.mkdir(VIEW_DIR) unless Dir.exists?(VIEW_DIR)
+    File.open("#{VIEW_DIR}/#{timestamp}.sql", 'w') { |f| f.write(@query) }
+  end
+
+  private
+
+  def exceptions_query
+    <<-SQL
+      SELECT DISTINCT *
+      FROM(
+        -- Fetch all shipments where taxon concepts are specified in CITES Suspension.
+        -- It considers also the geo_entity if specified in the Suspension as well as sources and purposes if any.
+        (
+          SELECT ts.id, ts.year, ts.appendix, ts.taxon_concept_id,
+                 ts.taxon_concept_full_name AS taxon,
+                 ts.taxon_concept_class_name AS class,
+                 ts.taxon_concept_order_name AS order,
+                 ts.taxon_concept_family_name AS family,
+                 ts.taxon_concept_genus_name AS genus,
+                 terms.name_en AS term,
+                 CASE WHEN ts.reported_by_exporter IS FALSE THEN ts.quantity
+                      ELSE NULL
+                 END AS importer_reported_quantity,
+                 CASE WHEN ts.reported_by_exporter IS TRUE THEN ts.quantity
+                      ELSE NULL
+                 END AS exporter_reported_quantity,
+                 units.name_en AS unit, exporters.iso_code2 AS exporter, importers.iso_code2 AS importer, NULL AS origin,
+                 purposes.name_en AS purpose, sources.name_en AS source, ts.import_permit_number AS import_permit,
+                 ts.export_permit_number AS export_permit, ts.origin_permit_number AS origin_permit, 'Suspension' AS issue_type,
+                 start_notifications.subtype AS details_of_compliance_issue,
+                 start_notifications.effective_at AS compliance_type_start_date,
+                 ts.taxon_concept_full_name AS compliance_type_taxon,
+                 ranks.name AS compliance_type_taxonomic_rank,
+                 end_notifications.effective_at AS compliance_type_end_date,
+                 start_notifications.name AS suspension_start_notification, end_notifications.name AS suspension_end_notification,
+                 tr_tc.notes AS notes
+          FROM trade_shipments_with_taxa_view ts
+          INNER JOIN taxon_concepts_and_ancestors_mview tca ON ts.taxon_concept_id = tca.taxon_concept_id
+          INNER JOIN trade_restrictions tr_tc ON tr_tc.taxon_concept_id = tca.ancestor_taxon_concept_id
+          INNER JOIN events start_notifications ON tr_tc.start_notification_id  = start_notifications.id
+          INNER JOIN geo_entities importers ON ts.importer_id = importers.id
+          INNER JOIN geo_entities exporters ON ts.exporter_id = exporters.id
+          INNER JOIN ranks ON ranks.id = ts.taxon_concept_rank_id
+          LEFT OUTER JOIN geo_entities suspension_countries ON tr_tc.geo_entity_id = suspension_countries.id
+          LEFT OUTER JOIN events end_notifications ON tr_tc.end_notification_id = end_notifications.id
+          LEFT OUTER JOIN trade_restriction_sources tr_s ON tr_tc.id = tr_s.trade_restriction_id
+          LEFT OUTER JOIN trade_restriction_purposes tr_p ON tr_tc.id = tr_p.trade_restriction_id
+          LEFT OUTER JOIN trade_codes sources ON sources.id = tr_s.source_id
+          LEFT OUTER JOIN trade_codes purposes ON purposes.id = tr_p.purpose_id
+          LEFT OUTER JOIN trade_codes units ON units.id = ts.unit_id
+          LEFT OUTER JOIN trade_codes terms ON terms.id = ts.term_id
+          WHERE tr_tc.type = 'CitesSuspension' AND
+          start_notifications.type = 'CitesSuspensionNotification' AND
+          end_notifications.type = 'CitesSuspensionNotification' AND
+          (
+            ts.year BETWEEN EXTRACT(YEAR FROM tr_tc.start_date) AND
+            CASE WHEN tr_tc.end_date IS NULL THEN EXTRACT(YEAR FROM current_date)
+            ELSE EXTRACT(YEAR FROM tr_tc.end_date)
+            END
+          )AND
+          (tr_tc.geo_entity_id IS NULL OR (tr_tc.geo_entity_id = ts.exporter_id OR tr_tc.geo_entity_id = ts.importer_id AND tr_tc.applies_to_import)) AND
+          (tr_s.source_id = ts.source_id OR tr_s.id IS NULL) AND (tr_p.purpose_id = ts.purpose_id OR tr_p.id IS NULL) AND country_of_origin_id IS NULL
+        )
+
+        UNION
+
+        -- Fetch all shipments where geo entities are specified in CITES Suspension, both for importer and exporter.
+        -- It considers also taxon concepts if specified in the Suspension as well as sources and purposes if any.
+
+        (
+          SELECT ts.id, ts.year, ts.appendix, ts.taxon_concept_id,
+                 ts.taxon_concept_full_name AS taxon,
+                 ts.taxon_concept_class_name AS class,
+                 ts.taxon_concept_order_name AS order,
+                 ts.taxon_concept_family_name AS family,
+                 ts.taxon_concept_genus_name AS genus,
+                 terms.name_en AS term,
+                 CASE WHEN ts.reported_by_exporter IS FALSE THEN ts.quantity
+                      ELSE NULL
+                 END AS importer_reported_quantity,
+                 CASE WHEN ts.reported_by_exporter IS TRUE THEN ts.quantity
+                      ELSE NULL
+                 END AS exporter_reported_quantity,
+                 units.name_en AS unit, exporters.iso_code2 AS exporter, importers.iso_code2 AS importer, NULL AS origin,
+                 purposes.name_en AS purpose, sources.name_en AS source, ts.import_permit_number AS import_permit,
+                 ts.export_permit_number AS export_permit, ts.origin_permit_number AS origin_permit, 'Suspension' AS issue_type,
+                 start_notifications.subtype AS details_of_compliance_issue,
+                 start_notifications.effective_at AS start_date,
+                 ts.taxon_concept_full_name AS compliance_type_taxon,
+                 ranks.name AS compliance_type_taxonomic_rank,
+                 end_notifications.effective_at AS end_date,
+                 start_notifications.name AS suspension_start_notification, end_notifications.name AS suspension_end_notification,
+                 tr_ge.notes AS notes
+          FROM trade_shipments_with_taxa_view ts
+          INNER JOIN trade_restrictions tr_ge ON tr_ge.geo_entity_id = ts.exporter_id OR (tr_ge.geo_entity_id = ts.importer_id AND tr_ge.applies_to_import)
+          INNER JOIN events start_notifications ON tr_ge.start_notification_id  = start_notifications.id
+          INNER JOIN geo_entities importers ON ts.importer_id = importers.id
+          INNER JOIN geo_entities exporters ON ts.exporter_id = exporters.id
+          LEFT OUTER JOIN ranks ON ranks.id = ts.taxon_concept_rank_id
+          LEFT OUTER JOIN geo_entities suspension_countries ON tr_ge.geo_entity_id = suspension_countries.id
+          LEFT OUTER JOIN events end_notifications ON tr_ge.end_notification_id = end_notifications.id
+          LEFT OUTER JOIN trade_restriction_sources tr_s ON tr_ge.id = tr_s.trade_restriction_id
+          LEFT OUTER JOIN trade_restriction_purposes tr_p ON tr_ge.id = tr_p.trade_restriction_id
+          LEFT OUTER JOIN trade_codes sources ON sources.id = tr_s.source_id
+          LEFT OUTER JOIN trade_codes purposes ON purposes.id = tr_p.purpose_id
+          LEFT OUTER JOIN trade_codes units ON units.id = ts.unit_id
+          LEFT OUTER JOIN trade_codes terms ON terms.id = ts.term_id
+          WHERE tr_ge.type = 'CitesSuspension' AND
+          (tr_ge.taxon_concept_id IS NULL OR tr_ge.taxon_concept_id = ts.taxon_concept_id) AND
+          (
+            ts.year BETWEEN EXTRACT(YEAR FROM tr_ge.start_date) AND
+            CASE WHEN tr_ge.end_date IS NULL THEN EXTRACT(YEAR FROM current_date)
+            ELSE EXTRACT(YEAR FROM tr_ge.end_date)
+            END
+          )AND
+          (tr_s.source_id = ts.source_id OR tr_s.id IS NULL) AND (tr_p.purpose_id = ts.purpose_id OR tr_p.id IS NULL) AND country_of_origin_id IS NULL
+        )
+      ) AS s
+
+      WHERE s.id NOT IN (
+        SELECT ts.id
+        FROM trade_shipments_with_taxa_view ts
+        INNER JOIN trade_restrictions tr_tc ON tr_tc.taxon_concept_id = ts.taxon_concept_id
+        INNER JOIN events start_notifications ON tr_tc.start_notification_id  = start_notifications.id
+        INNER JOIN geo_entities importers ON ts.importer_id = importers.id
+        INNER JOIN geo_entities exporters ON ts.exporter_id = exporters.id
+        INNER JOIN ranks ON ranks.id = ts.taxon_concept_rank_id
+        LEFT OUTER JOIN geo_entities suspension_countries ON tr_tc.geo_entity_id = suspension_countries.id
+        LEFT OUTER JOIN events end_notifications ON tr_tc.end_notification_id = end_notifications.id
+        LEFT OUTER JOIN trade_restriction_sources tr_s ON tr_tc.id = tr_s.trade_restriction_id
+        LEFT OUTER JOIN trade_restriction_purposes tr_p ON tr_tc.id = tr_p.trade_restriction_id
+        LEFT OUTER JOIN trade_codes sources ON sources.id = tr_s.source_id
+        LEFT OUTER JOIN trade_codes purposes ON purposes.id = tr_p.purpose_id
+        LEFT OUTER JOIN trade_codes units ON units.id = ts.unit_id
+        LEFT OUTER JOIN trade_codes terms ON terms.id = ts.term_id
+        WHERE #{except}
+      )
+
+      ORDER BY s.year, s.class, s.order, s.family, s.genus, s.taxon, s.term
+    SQL
+  end
+
+  def except
+    where = []
+    CSV.foreach(EXEMPTIONS_PATH, headers: true) do |row|
+      @row = row
+      where << "(#{ATTRIBUTES.map { |a| send("parse_#{a.to_s}", row[a.to_s]) }.join(' AND ')})"
+    end
+    where.join('OR')
+  end
+
+  def parse_start_date(date)
+    year = date.split('/').last.to_i
+    "ts.year >= #{year}"
+  end
+
+  def parse_end_date(date)
+    year = date.blank? ? Date.today.year  : date.split('/').last.to_i
+    "ts.year <= #{year}"
+  end
+
+  def parse_iso_code2(iso)
+    return 'TRUE' if iso.upcase == 'All' || iso.blank?
+    "#{imp_or_exp_country}.iso_code2 = '#{iso}'"
+  end
+
+  def parse_taxon_concept_id(tc)
+    return 'TRUE' unless tc.is_a? Numeric
+    "ts.taxon_concept_id = #{tc}"
+  end
+
+  def parse_unit(unit)
+    parse_trade_code(unit, 'units')
+  end
+
+  def parse_term(term)
+    parse_trade_code(term, 'terms')
+  end
+
+  def parse_purpose(purpose)
+    parse_trade_code(purpose, 'purposes')
+  end
+
+  def parse_source(source)
+    parse_trade_code(source, 'sources')
+  end
+
+  def parse_trade_code(code, type)
+    #Return TRUE to prevent empty conditions and a malformed query
+    return 'TRUE' if code.upcase == 'ALL' || code.blank?
+
+    codes = code.split(';').map(&:strip)
+    "#{type}.code IN (#{codes.map{|c| "'#{c}'"}.join(',')})"
+  end
+
+  def imp_or_exp_country
+    @row['applies_to_import'].present? ? 'importers' : 'exporters'
+  end
+end
