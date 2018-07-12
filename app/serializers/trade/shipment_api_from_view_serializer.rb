824c8884e4f90d26317996ddd37ac0a624930902
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-12 18:27:39 +0100

    add trade shipments attribute to serializer

--- app/serializers/trade/shipment_api_from_view_serializer.rb
+++ app/serializers/trade/shipment_api_from_view_serializer.rb
@@ -1,7 +1,17 @@
 class Trade::ShipmentApiFromViewSerializer < ActiveModel::Serializer
-  attributes :id
+   attributes :id , :year, :appendix, :taxon, :class, :order, :family, :genus,
+              :term, :importer_reported_quantity, :exporter_reported_quantity,
+              :unit, :importer, :exporter, :origin, :purpose, :source,
+              :compliance_type_taxonomic_rank,
+              :import_permit,
+              :export_permit, :origin_permit
 
-  def id
-    object.id || object.shipment_id
+  def importer_reported_quantity
+    object.attributes['importer_reported_quantity'] || object.attributes['importer_quantity']
   end
+
+  def exporter_reported_quantity
+    object.attributes['exporter_reported_quantity'] || object.attributes['exporter_quantity']
+  end
+
 end
