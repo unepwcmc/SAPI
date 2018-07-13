b30bdacf7be337cae918bac39cd9b2d597a74baf
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-13 16:42:22 +0100

    add page parameter to shipments controller

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::ShipmentsController < ApplicationController
   private
 
   def search_params
-    params.permit(:compliance_type, :per_page)
+    params.permit(:compliance_type, :page, :per_page)
   end
 
   def authenticate
