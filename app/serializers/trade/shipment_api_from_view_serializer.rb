6c2268de0aae2eedbd1aa5f5c3ce1e36e3a47181
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-07-16 17:17:27 +0100

    Add back rank in serializer

--- app/serializers/trade/shipment_api_from_view_serializer.rb
+++ app/serializers/trade/shipment_api_from_view_serializer.rb
@@ -2,7 +2,8 @@ class Trade::ShipmentApiFromViewSerializer < ActiveModel::Serializer
    attributes :id , :year, :appendix, :taxon, :class, :order, :family, :genus,
               :term, :importer_reported_quantity, :exporter_reported_quantity,
               :unit, :importer, :importer_iso, :exporter, :exporter_iso, :origin, :purpose, :source,
-              :import_permit, :export_permit, :origin_permit, :issue_type
+              :import_permit, :export_permit, :origin_permit, :issue_type,
+              :compliance_type_taxonomic_rank
 
   def importer_reported_quantity
     object.attributes['importer_reported_quantity'] || object.attributes['importer_quantity']
