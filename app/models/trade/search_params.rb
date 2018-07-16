6f85237e895c70530b7fa794895f14df90e8de97
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-16 11:57:52 +0100

    remove unuseful non compliant view parameter

--- app/models/trade/search_params.rb
+++ app/models/trade/search_params.rb
@@ -51,8 +51,7 @@ class Trade::SearchParams < Hash
   COMPLIANCE_TYPES_VIEWS = {
     appendix_i: "trade_shipments_appendix_i_mview",
     trade_suspensions: "trade_shipments_cites_suspensions_mview",
-    mandatory_quotas: "trade_shipments_mandatory_quotas_mview",
-    non_compliant_terms: "trade_shipments_non_compliant_terms_view"
+    mandatory_quotas: "trade_shipments_mandatory_quotas_mview"
   }
   def sanitise_compliance_type(compliance_type)
     compliance_type.present? ? COMPLIANCE_TYPES_VIEWS[compliance_type.to_sym] : "trade_shipments_with_taxa_view"
