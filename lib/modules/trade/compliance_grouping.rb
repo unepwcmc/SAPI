208f91c33449bebe122799cbd6ac3c78cef41114
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-07-16 15:56:15 +0100

    Improve grouping by adding fields and several query types

--- lib/modules/trade/compliance_grouping.rb
+++ lib/modules/trade/compliance_grouping.rb
@@ -5,8 +5,11 @@ class Trade::ComplianceGrouping
   ATTRIBUTES = {
     id: 'id',
     year: 'year',
+    appendix: 'appendix',
     importer: 'importer',
+    importer_iso: 'importer_iso',
     exporter: 'exporter',
+    exporter_iso: 'exporter_iso',
     term: 'term',
     unit: 'unit',
     purpose: 'purpose',
@@ -14,67 +17,132 @@ class Trade::ComplianceGrouping
     taxon: 'taxon',
     genus: 'genus',
     family: 'family',
-    class: 'class'
+    class: 'class',
+    issue_type: 'issue_type'
   }
+
+  COUNTRIES = 183.freeze
 
   # Example usage
   # Group by year considering compliance types:
-  # Trade::ComplianceGrouping.new('year', {all: false})
-  # Group by importer across all shipments and limit result to 5 records
-  # Trade::ComplianceGrouping.new('importer', {all: true, limit: 5})
-  # Group by importer across all shipments for a given year
-  # Trade::ComplianceGrouping.new('importer', {all: true, attributes: ['year']})
+  # Trade::ComplianceGrouping.new('year', {attributes: ['issue_type']})
+  # Group by importer and limit result to 5 records
+  # Trade::ComplianceGrouping.new('importer', {limit: 5})
   def initialize(group, opts={})
     @group = sanitise_group(group)
     @attributes = sanitise_params(opts[:attributes])
+    @condition = opts[:condition] || 'TRUE'
     @limit = sanitise_limit(opts[:limit])
-    @all = opts[:all]
-    @query = "#{non_compliant_shipments}#{group_query}"
+    @query = group_query
+  end
+
+  def run
+    db.execute(@query)
+  end
+
+  def json_by_year(data, opts={})
+    return data unless data.first["year"]
+
+    # Custom group_by
+    years = data.map { |d| d["year"] }.uniq
+    json = []
+    years.map do |year|
+      partials = data.select { |d| d["year"] == year }
+      values = partials.map do |partial|
+        hash = {}
+        opts.each { |key, value| hash.merge!({"#{key}": partial[value]}) }
+        hash.merge({
+          value: partial['cnt'],
+          percent: partial['percent']
+        })
+      end
+      json << ({ "#{year}": values })
+    end
+    json
+  end
+
+  def countries_reported_range(year)
+    years = [year - 1, year, year + 1]
+    hash = {}
+    years.map do |y|
+      data = countries_reported(y)
+      hash[year] ||= []
+      hash[year] << data
+    end
+    hash
   end
 
   private
 
   def group_query
-    # If @all is true it means we are considering all shipment at once,
-    # without taking care of their compliance type.
-    # If @all is false instead, we are also grouping by compliance type
-    # and considering shipments separately.
-    compliance_type = @all ? nil : 'compliance_type'
-    columns = [@group, @attributes, compliance_type].flatten.compact.uniq.join(',')
+    columns = [@group, @attributes].flatten.compact.uniq.join(',')
     <<-SQL
-      SELECT #{columns}, cnt, 100.0*cnt/(SUM(cnt) OVER ()) AS percent
-      FROM (
-        SELECT #{columns}, COUNT(*) AS cnt
-        FROM non_compliant_shipments
-        GROUP BY #{columns}
-      ) counts
+      #{non_compliant_shipments}
+      SELECT #{columns}, COUNT(*) AS cnt, 100.0*COUNT(*)/(SUM(COUNT(*)) OVER (PARTITION BY year)) AS percent
+      FROM non_compliant_shipments
+      WHERE #{@condition}
+      GROUP BY #{columns}
       ORDER BY percent DESC
       #{limit}
     SQL
   end
 
   def non_compliant_shipments
-    # For the reason stated above, if @all is true we need just UNION,
-    # otherwise, in order to consider the shipments separately by compliance type,
-    # we need UNION ALL, which allows duplicated shipments.
+    # We need UNION ALL, which allows duplicated shipments.
     <<-SQL
       WITH non_compliant_shipments AS (
         (
-          SELECT #{ATTRIBUTES.values.join(',')}, 'appendixI' AS compliance_type
+          SELECT #{ATTRIBUTES.values.join(',')}
           FROM trade_shipments_appendix_i_mview
         )
-        #{@all ? 'UNION' : 'UNION ALL'}
+        UNION ALL
         (
-          SELECT #{ATTRIBUTES.values.join(',')}, 'quotas' AS compliance_type
+          SELECT #{ATTRIBUTES.values.join(',')}
           FROM trade_shipments_mandatory_quotas_mview
         )
-        #{@all ? 'UNION' : 'UNION ALL'}
+        UNION ALL
         (
-          SELECT #{ATTRIBUTES.values.join(',')}, 'suspension' AS compliance_type
+          SELECT #{ATTRIBUTES.values.join(',')}
           FROM trade_shipments_cites_suspensions_mview
         )
       )
     SQL
+  end
+
+  def countries_reported(year)
+    sql = <<-SQL
+      #{non_compliant_shipments}
+      SELECT COUNT(*) AS cnt
+      FROM(
+        (
+          SELECT DISTINCT importer AS country, importer_iso AS iso
+          FROM non_compliant_shipments
+          WHERE year = #{year}
+        )
+        UNION
+        (
+          SELECT DISTINCT exporter AS country, exporter_iso AS iso
+          FROM non_compliant_shipments
+          WHERE year = #{year}
+        )
+      ) AS countries
+    SQL
+    countries_reported = db.execute(sql).first['cnt'].to_i
+
+    sql = <<-SQL
+      #{non_compliant_shipments}
+      SELECT COUNT(*) AS cnt
+      FROM non_compliant_shipments
+      WHERE year = #{year}
+    SQL
+    issues_reported = db.execute(sql).first['cnt'].to_i
+
+    {
+      year: year,
+      issuesReported: issues_reported,
+      countriesReported: countries_reported,
+      countriesYetToReport: COUNTRIES-countries_reported
+    }
   end
 
   def limit
@@ -92,5 +160,21 @@ class Trade::ComplianceGrouping
 
   def sanitise_limit(limit)
     limit.is_a?(Integer) ? limit : nil
+  end
+
+  def sanitise_condition(condition)
+    # TODO
+    return nil if condition.blank?
+    condition.map do |key, value|
+      if value.is_a?(Array)
+        "#{ATTRIBUTES[key]} IN (#{value.join(',')})"
+      else
+        "#{ATTRIBUTES[key]} = #{value}"
+      end
+    end.join(' AND ')
+  end
+
+  def db
+    ActiveRecord::Base.connection
   end
 end
