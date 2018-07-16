a5b877636043c26968faaf0e717fcdff6eb1528c
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-16 16:09:52 +0100

    add issue type attributes to shipments api serializer

--- app/serializers/trade/shipment_api_from_view_serializer.rb
+++ app/serializers/trade/shipment_api_from_view_serializer.rb
@@ -2,9 +2,7 @@ class Trade::ShipmentApiFromViewSerializer < ActiveModel::Serializer
    attributes :id , :year, :appendix, :taxon, :class, :order, :family, :genus,
               :term, :importer_reported_quantity, :exporter_reported_quantity,
               :unit, :importer, :exporter, :origin, :purpose, :source,
-              :compliance_type_taxonomic_rank,
-              :import_permit,
-              :export_permit, :origin_permit
+              :import_permit, :export_permit, :origin_permit, :issue_type
 
   def importer_reported_quantity
     object.attributes['importer_reported_quantity'] || object.attributes['importer_quantity']
