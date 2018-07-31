eb1246046eadf56e5b13b23f0b52d1c53a1f47f7
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-31 14:01:35 +0100

    refactor and move build hash method from controller to a module

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -33,7 +33,7 @@ class Api::V1::ShipmentsController < ApplicationController
   def search_query
     query = Trade::ComplianceGrouping.new('year', {attributes: sanitized_attributes, condition: "year = #{params[:year]}"})
     data = query.run
-    @search_data = build_hash(data)
+    @search_data = query.build_hash(data, params)
     render :json => @search_data
   end
 
@@ -59,69 +59,6 @@ class Api::V1::ShipmentsController < ApplicationController
       @grouped_data = record
     end
     @grouped_data
-  end
-
-  def build_hash(data)
-    hash, json = {}, []
-    if params[:group_by].include?('commodity')
-      keys = data.values.map { |d| d[1] }.uniq
-      keys.map do |k|
-        partial = data.values.select { |d| d[1] == k }
-        values = partial.map { |el| hash.merge({ cnt: el[2].to_i }) }
-        json << ({ "#{k}": values[0] })
-      end
-    elsif params[:group_by].include?('exporting')
-      query = Trade::ComplianceGrouping.new('year', {attributes: GROUPING_ATTRIBUTES[:importing], condition: "year = #{params[:year]}"}).run
-      all_data = data.values + query.values
-      keys = all_data.map { |d| d[1] }.uniq
-      keys.map do |k|
-        exp_partial = data.values.select { |d| d[1] == k }
-        exp_values = exp_partial.map do |el|
-          id = el[3]
-          tot_exp = total_ships_exp_cnt(id)
-          hash.merge({
-            cnt: el[4].to_i,
-            total_cnt: tot_exp
-          })
-        end
-        imp_partial = query.values.select { |d| d[1] == k }
-        imp_values = imp_partial.map do |el|
-          id = el[3]
-          tot_imp = total_ships_imp_cnt(id)
-          hash.merge({
-            cnt: el[4].to_i,
-            total_cnt: tot_imp
-          })
-        end
-        values = if exp_values.empty?
-                   imp_values[0]
-                 elsif imp_values.empty?
-                   exp_values[0]
-                 else
-                   exp_values[0].merge(imp_values[0]){ |_x, a, b| a + b }
-                 end
-        values = values.merge(percentage: (values[:cnt]*100.0/values[:total_cnt]).round(2))
-        json << ({ "#{k}": values })
-      end
-    else
-      keys = data.values.map { |d| d[1] }.uniq
-      keys.map do |k|
-        partial = data.values.select { |d| d[1] == k }
-        values = partial.map { |el| hash.merge({ appendix: el[2], cnt: el[3].to_i }) }
-        json << ({ "#{k}": values[0] })
-      end
-    end
-    json
-  end
-
-  def total_ships_exp_cnt(id)
-    query_exp = "SELECT COUNT(*) FROM trade_shipments_with_taxa_view WHERE exporter_id = #{id} GROUP BY exporter_id"
-    ActiveRecord::Base.connection.execute(query_exp).values.flatten.first.to_i
-  end
-
-  def total_ships_imp_cnt(id)
-    query_imp = "SELECT COUNT(*) FROM trade_shipments_with_taxa_view WHERE importer_id = #{id} GROUP BY importer_id"
-    ActiveRecord::Base.connection.execute(query_imp).values.flatten.first.to_i
   end
 
   def authenticate
