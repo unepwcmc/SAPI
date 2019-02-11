e8f2d3ebbaff4f446f91b223e4ca7c8b67d914ce
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-02-11 16:29:09 +0000

    Add full trade db download link

--- app/controllers/cites_trade/home_controller.rb
+++ app/controllers/cites_trade/home_controller.rb
@@ -2,7 +2,10 @@ class CitesTrade::HomeController < CitesTradeController
 
   def index
     respond_to do |format|
-      format.html { @years = (1975..Trade::Shipment.maximum('year')).to_a.reverse }
+      format.html {
+        @years = (1975..Trade::Shipment.maximum('year')).to_a.reverse
+        @full_download_link = Rails.application.secrets['cites_trade_full_download']
+      }
     end
   end
 
