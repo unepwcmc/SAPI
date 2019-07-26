6a668ca9c35f194709badb27d57e48862aff837d
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-07-26 15:58:06 +0100

    Year should be an integer, not a string

--- db/migrate/20190724131538_create_trade_plus_static.rb
+++ db/migrate/20190724131538_create_trade_plus_static.rb
@@ -1,7 +1,7 @@
 class CreateTradePlusStatic < ActiveRecord::Migration
   def change
     create_table :trade_plus_static do |t|
-      t.string :year
+      t.integer :year
       t.string :appendix
       t.string :taxon_name
       t.integer :taxon_id
