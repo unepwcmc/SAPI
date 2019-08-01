652d8aa9ba4b6cbc3138efa993a3275a4782b79c
lucacug <luca.cugliari@unep-wcmc.org>
2019-08-01 14:52:48 +0100

    cache the request and order alphabetically the filters

--- app/controllers/api/v1/trade_plus_filters_controller.rb
+++ app/controllers/api/v1/trade_plus_filters_controller.rb
@@ -4,11 +4,22 @@ class Api::V1::TradePlusFiltersController < ApplicationController
   ATTRIBUTES = %w[importer exporter origin term
                   source purpose appendix unit].freeze
   def index
-    res = ActiveRecord::Base.connection.execute(query)
-    render :json => JSON.parse(res.first['filters'])
+    filters = Rails.cache.fetch('trade_plus_filters', expires_in: 1.week) do
+                res = ActiveRecord::Base.connection.execute(query)
+                response_ordering(res)
+              end
+    render :json => filters
   end
 
   private
+
+  def response_ordering(response)
+    result = {}
+    JSON.parse(response.first['filters']).each do |k, v|
+      result[k] = v.sort_by { |i| i['name'].to_s }
+    end
+    result
+  end
 
   def query
     <<-SQL
@@ -27,7 +38,7 @@ class Api::V1::TradePlusFiltersController < ApplicationController
   def select_query
     query= []
     ATTRIBUTES.each do |attr|
-      query << "#{attr}_id" << "#{attr}"
+      query << "#{attr}" << "#{attr}_id"
       query << "#{attr}_iso" if ['exporter', 'importer', 'origin'].include? attr
     end
     query << 'group_name' << 'year' << 'taxon_name' << 'taxon_id'
@@ -37,7 +48,7 @@ class Api::V1::TradePlusFiltersController < ApplicationController
   def inner_query
     query = ''
     ATTRIBUTES.each do |attr|
-      if %w[group_name term unit year].include? attr
+      if %w[term unit].include? attr
         query << "json_agg(DISTINCT(json_build_object('name', #{attr}, 'id', #{attr})::jsonb)) AS #{attr.pluralize},"
       elsif %w[importer exporter origin].include? attr
         query << "json_agg(
@@ -58,7 +69,9 @@ class Api::V1::TradePlusFiltersController < ApplicationController
       end
 
     end
-    query << "json_agg(DISTINCT(json_build_object('name', taxon_name, 'id', taxon_id)::jsonb)) AS taxa"
+    query << "json_agg(DISTINCT(json_build_object('name', taxon_name, 'id', taxon_id)::jsonb)) AS taxa,"
+    query << "json_agg(DISTINCT(json_build_object('name', group_name, 'id', group_name)::jsonb)) AS taxonomic_groups,"
+    query << "json_agg(DISTINCT(json_build_object('name', year, 'id', year)::jsonb)) AS years"
     query
   end
 end
