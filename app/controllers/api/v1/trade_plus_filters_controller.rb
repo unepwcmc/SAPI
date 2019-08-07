7118f6b570f311fafbc7155338800fdae558ab43
lucacug <luca.cugliari@unep-wcmc.org>
2019-08-07 11:53:23 +0100

    handle null values on the filters response

--- app/controllers/api/v1/trade_plus_filters_controller.rb
+++ app/controllers/api/v1/trade_plus_filters_controller.rb
@@ -16,7 +16,19 @@ class Api::V1::TradePlusFiltersController < ApplicationController
   def response_ordering(response)
     result = {}
     JSON.parse(response.first['filters']).each do |k, v|
-      result[k] = v.sort_by { |i| i['name'].to_s }
+      case k
+      when 'sources', 'purposes'
+        v.map do |value|
+          value['id'], value['name'] = 'null', 'Unreported' if value['id'].nil?
+        end
+        v
+      when 'units'
+        v.map do |value|
+          value['id'], value['name'] = 'null', 'Number of items' if value['id'].empty?
+        end
+        v
+      end
+      result[k] = v.sort_by { |i| i['name'].to_s.downcase }
     end
     result
   end
