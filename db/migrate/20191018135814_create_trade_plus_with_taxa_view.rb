9328e0ce1882162e3f65976b308c9d89e29f9b4d
lucacug <luca.cugliari@unep-wcmc.org>
2019-10-30 17:14:49 +0000

    create trade_plus_shipments_view as parent of all other views

--- db/migrate/20191018135814_create_trade_plus_with_taxa_view.rb
+++ db/migrate/20191018135814_create_trade_plus_with_taxa_view.rb
@@ -1,7 +1,7 @@
 class CreateTradePlusWithTaxaView < ActiveRecord::Migration
   def up
     execute "DROP VIEW IF EXISTS trade_plus_with_taxa_view"
-    execute "CREATE VIEW trade_plus_with_taxa_view AS #{view_sql('20191024123326', 'trade_plus_with_taxa_view')}"
+    execute "CREATE VIEW trade_plus_with_taxa_view AS #{view_sql('20191030154249', 'trade_plus_with_taxa_view')}"
   end
 
   def down
