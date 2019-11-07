8f3d8ff14741ce24ffcd04c0155b4eabe5ec6e68
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-11-07 13:36:17 +0000

    Revert materialized view migration back to normal view

--- db/migrate/20191023135814_create_trade_plus_complete_mview.rb
+++ db/migrate/20191023135814_create_trade_plus_complete_mview.rb
@@ -1,10 +1,10 @@
 class CreateTradePlusCompleteMview < ActiveRecord::Migration
   def up
-    execute "DROP MATERIALIZED VIEW IF EXISTS trade_plus_complete_mview"
-    execute "CREATE MATERIALIZED VIEW trade_plus_complete_mview AS #{view_sql('20191023141810', 'trade_plus_complete_mview')}"
+    execute "DROP VIEW IF EXISTS trade_plus_complete_view"
+    execute "CREATE VIEW trade_plus_complete_view AS #{view_sql('20191023141810', 'trade_plus_complete_view')}"
   end
 
   def down
-    execute "DROP MATERIALIZED VIEW IF EXISTS trade_plus_complete_mview"
+    execute "DROP VIEW IF EXISTS trade_plus_complete_view"
   end
 end
--- db/views/trade_plus_complete_view/20191023141810.sql
+++ db/views/trade_plus_complete_view/20191023141810.sql
@@ -1,5 +1,5 @@
 SELECT ts.*,
-       ts.term_quantity_unit[1] AS term_code,
+       ts.term_quantity_unit[1] AS term_id,
        CASE WHEN ts.reported_by_exporter IS FALSE
          THEN ts.term_quantity_unit[2]
          ELSE NULL
@@ -8,10 +8,10 @@ SELECT ts.*,
          THEN ts.term_quantity_unit[2]
          ELSE NULL
        END AS exporter_reported_quantity,
-       ts.term_quantity_unit[3] AS unit_code,
-       terms.id AS term_id,
+       ts.term_quantity_unit[3] AS unit_id,
+       terms.code AS term_code,
        terms.name_en AS term,
-       units.id AS unit_id,
+       units.code AS unit_code,
        units.name_en AS unit
 FROM trade_plus_formatted_data_view ts
 LEFT OUTER JOIN trade_codes terms ON terms.id = ts.term_quantity_unit[1]
