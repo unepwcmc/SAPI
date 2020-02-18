a92b983f2ea96fbe40b1d5ad0816fac01ce29d39
lucacug <luca.cugliari@unep-wcmc.org>
2020-02-18 16:18:24 +0000

    add language among permitted params in document search

--- app/models/document_search_params.rb
+++ app/models/document_search_params.rb
@@ -10,6 +10,7 @@ class DocumentSearchParams < Hash
       event_type: sanitise_string(params['event_type']),
       document_type: sanitise_string(params['document_type']),
       title_query: sanitise_string(params['title_query']),
+      language: sanitise_string(params['locale']),
       document_date_start: (Date.parse(params['document_date_start']) rescue nil),
       document_date_end: (Date.parse(params['document_date_end']) rescue nil),
       taxon_concepts_ids: sanitise_integer_array(params['taxon_concepts_ids']),
