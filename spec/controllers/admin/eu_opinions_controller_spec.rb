7e677d39dfc5cc7b8760b20f8ed411e06bad8595
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-05-15 12:09:33 +0100

    Add SRG hitory to create test to check it works correctly

--- spec/controllers/admin/eu_opinions_controller_spec.rb
+++ spec/controllers/admin/eu_opinions_controller_spec.rb
@@ -9,24 +9,24 @@ describe Admin::EuOpinionsController do
 
   describe "GET index" do
     it "renders the index template" do
-      get :index, :taxon_concept_id => @taxon_concept.id
+      get :index, taxon_concept_id: @taxon_concept.id
       response.should render_template("index")
     end
     it "renders the taxon_concepts_layout" do
-      get :index, :taxon_concept_id => @taxon_concept.id
+      get :index, taxon_concept_id: @taxon_concept.id
       response.should render_template('layouts/taxon_concepts')
     end
   end
 
   describe "GET new" do
     it "renders the new template" do
-      get :new, :taxon_concept_id => @taxon_concept.id
+      get :new, taxon_concept_id: @taxon_concept.id
       response.should render_template('new')
     end
     it "assigns @geo_entities (country and territory) with two objects" do
-      territory = create(:geo_entity, :geo_entity_type_id => territory_geo_entity_type.id)
-      country = create(:geo_entity)
-      get :new, :taxon_concept_id => @taxon_concept.id
+      create(:geo_entity, :geo_entity_type_id => territory_geo_entity_type.id)
+      create(:geo_entity)
+      get :new, taxon_concept_id: @taxon_concept.id
       assigns(:geo_entities).size.should == 2
     end
   end
@@ -35,25 +35,27 @@ describe Admin::EuOpinionsController do
     context "when successful" do
       before do
         @eu_decision_type = create(:eu_decision_type)
+        @srg_history = create(:srg_history)
       end
       it "redirects to the EU Opinions index" do
         post :create,
-          :eu_opinion => {
-            :eu_decision_type_id => @eu_decision_type.id,
-            :start_date => Date.today,
-            :geo_entity_id => create(
-              :geo_entity, :geo_entity_type_id => country_geo_entity_type.id
+          eu_opinion: {
+            eu_decision_type_id: @eu_decision_type.id,
+            srg_history_id: @srg_history.id,
+            start_date: Date.today,
+            geo_entity_id: create(
+              :geo_entity, geo_entity_type_id: country_geo_entity_type.id
             )
           },
-          :taxon_concept_id => @taxon_concept.id
+          taxon_concept_id: @taxon_concept.id
         response.should redirect_to(admin_taxon_concept_eu_opinions_url(@taxon_concept.id))
       end
     end
 
     context "when not successful" do
       it "renders new" do
-        post :create, :eu_opinion => {},
-          :taxon_concept_id => @taxon_concept.id
+        post :create, eu_opinion: {},
+          taxon_concept_id: @taxon_concept.id
         response.should render_template("new")
       end
     end
@@ -63,16 +65,16 @@ describe Admin::EuOpinionsController do
     before(:each) do
       @eu_opinion = create(
         :eu_opinion,
-        :taxon_concept_id => @taxon_concept.id
+        taxon_concept_id: @taxon_concept.id
       )
     end
     it "renders the edit template" do
-      get :edit, :id => @eu_opinion.id, :taxon_concept_id => @taxon_concept.id
+      get :edit, id: @eu_opinion.id, taxon_concept_id: @taxon_concept.id
       response.should render_template('edit')
     end
     it "assigns @geo_entities" do
-      territory = create(:geo_entity, :geo_entity_type_id => territory_geo_entity_type.id)
-      get :edit, :id => @eu_opinion.id, :taxon_concept_id => @taxon_concept.id
+      territory = create(:geo_entity, geo_entity_type_id: territory_geo_entity_type.id)
+      get :edit, id: @eu_opinion.id, taxon_concept_id: @taxon_concept.id
       assigns(:geo_entities).should include(territory)
     end
   end
@@ -81,7 +83,7 @@ describe Admin::EuOpinionsController do
     before(:each) do
       @eu_opinion = create(
         :eu_opinion,
-        :taxon_concept_id => @taxon_concept.id
+        taxon_concept_id: @taxon_concept.id
       )
       @eu_decision_type = create(:eu_decision_type)
     end
@@ -89,11 +91,11 @@ describe Admin::EuOpinionsController do
     context "when successful" do
       it "renders taxon_concepts EU Opinions page" do
         put :update,
-          :eu_opinion => {
-            :eu_decision_type_id => @eu_decision_type.id
+          eu_opinion: {
+            eu_decision_type_id: @eu_decision_type.id
           },
-          :id => @eu_opinion.id,
-          :taxon_concept_id => @taxon_concept.id
+          id: @eu_opinion.id,
+          taxon_concept_id: @taxon_concept.id
         response.should redirect_to(
           admin_taxon_concept_eu_opinions_url(@taxon_concept)
         )
@@ -103,11 +105,11 @@ describe Admin::EuOpinionsController do
     context "when not successful" do
       it "renders new" do
         put :update,
-          :eu_opinion => {
-            :eu_decision_type_id => nil
+          eu_opinion: {
+            eu_decision_type_id: nil
           },
-          :id => @eu_opinion.id,
-          :taxon_concept_id => @taxon_concept.id
+          id: @eu_opinion.id,
+          taxon_concept_id: @taxon_concept.id
         response.should render_template('new')
       end
     end
@@ -117,12 +119,12 @@ describe Admin::EuOpinionsController do
     before(:each) do
       @eu_opinion = create(
         :eu_opinion,
-        :taxon_concept_id => @taxon_concept.id
+        taxon_concept_id: @taxon_concept.id
       )
     end
     it "redirects after delete" do
-      delete :destroy, :id => @eu_opinion.id,
-        :taxon_concept_id => @taxon_concept.id
+      delete :destroy, id: @eu_opinion.id,
+        taxon_concept_id: @taxon_concept.id
       response.should redirect_to(
         admin_taxon_concept_eu_opinions_url(@taxon_concept)
       )
