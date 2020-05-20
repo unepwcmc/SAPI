b6c4dd3dfc864ade935fb5fd3b98df10fb5e4db1
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-05-20 11:53:31 +0100

    Fix wrong view timestamp

--- db/migrate/20200514150717_add_srg_history_to_eu_decisions_view.rb
+++ db/migrate/20200514150717_add_srg_history_to_eu_decisions_view.rb
@@ -15,7 +15,7 @@ class AddSrgHistoryToEuDecisionsView < ActiveRecord::Migration
 
   def down
     execute "DROP VIEW IF EXISTS api_eu_decisions_view"
-    execute "CREATE VIEW api_eu_decisions_view AS #{view_sql('20150210140508', 'api_eu_decisions_view')}"
+    execute "CREATE VIEW api_eu_decisions_view AS #{view_sql('20150126161813', 'api_eu_decisions_view')}"
 
     execute "DROP VIEW IF EXISTS eu_decisions_view"
     execute "CREATE VIEW eu_decisions_view AS #{view_sql('20150210140508', 'eu_decisions_view')}"
