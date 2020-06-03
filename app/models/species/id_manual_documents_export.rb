fa9353406406a980c73f99a56b08406f58c0e357
lucacug <luca.cugliari@unep-wcmc.org>
2020-06-03 10:11:23 +0100

    add id materials inventory download via the admin interface

--- app/models/species/id_manual_documents_export.rb
+++ app/models/species/id_manual_documents_export.rb
@@ -30,7 +30,11 @@ class Species::IdManualDocumentsExport < Species::CsvCopyExport
       :taxon_names,
       :geo_entity_names,
       "to_char(created_at, 'DD/MM/YYYY')",
-      "to_char(updated_at, 'DD/MM/YYYY')"
+      "to_char(updated_at, 'DD/MM/YYYY')",
+      :created_by_id,
+      :created_by,
+      :updated_by_id,
+      :updated_by
     ]
   end
 
@@ -47,7 +51,11 @@ class Species::IdManualDocumentsExport < Species::CsvCopyExport
       'Taxon names',
       'Country names',
       'Created at',
-      'Updated at'
+      'Updated at',
+      'Created by id',
+      'Created by',
+      'Updated by id',
+      'Updated by'
     ]
   end
 
