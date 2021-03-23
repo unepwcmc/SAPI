faf954ec989f32a6a5eb74c78d38eddb06a537e6
Will Kocur <will.kocur@unep-wcmc.org>
2021-03-23 13:55:42 +0000

    add uglifier to staging deploy config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -17,6 +17,7 @@ SAPI::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.js_compressor = :uglifier
 
   # Generate digests for assets URLs
   config.assets.digest = true
