295f7beba6231b8b5bbbfff289fead6c8e55680d
lucacug <luca.cugliari@unep-wcmc.org>
2021-10-06 11:05:43 +0100

    remove unused sql view and add migration to use new ones and hopefully the psql function

--- db/migrate/20210823165358_add_multilingual_and_fix_formatting_issue_on_trade_plus_view.rb
+++ db/migrate/20210823165358_add_multilingual_and_fix_formatting_issue_on_trade_plus_view.rb
@@ -7,11 +7,13 @@ class AddMultilingualAndFixFormattingIssueOnTradePlusView < ActiveRecord::Migrat
     execute "DROP VIEW IF EXISTS trade_plus_group_view"
     execute "DROP VIEW IF EXISTS trade_plus_shipments_view"
     execute "CREATE VIEW trade_plus_shipments_view AS #{view_sql('20200206150700', 'trade_plus_shipments_view')}"
-    execute "CREATE VIEW trade_plus_group_view AS #{view_sql('20210823165358', 'trade_plus_group_view')}"
-    execute "CREATE VIEW trade_plus_formatted_data_view AS #{view_sql('20210823165358', 'trade_plus_formatted_data_view')}"
-    execute "CREATE VIEW trade_plus_formatted_data_final_view AS #{view_sql('20210823165358', 'trade_plus_formatted_data_final_view')}"
+    execute "CREATE VIEW trade_plus_group_view AS #{view_sql('20211005144857', 'trade_plus_group_view')}"
+    execute "CREATE VIEW trade_plus_formatted_data_view AS #{view_sql('20211005144857', 'trade_plus_formatted_data_view')}"
+    execute "CREATE VIEW trade_plus_formatted_data_final_view AS #{view_sql('20211005144857', 'trade_plus_formatted_data_final_view')}"
     execute "CREATE VIEW trade_plus_complete_view AS #{view_sql('20200707183829', 'trade_plus_complete_view')}"
-    execute "CREATE MATERIALIZED VIEW trade_plus_complete_mview AS SELECT * FROM trade_plus_complete_view"
+    # execute "CREATE MATERIALIZED VIEW trade_plus_complete_mview AS SELECT * FROM trade_plus_complete_view"
+    execute File.read(Rails.root.join("db/mviews/010_rebuild_trade_plus_mviews.sql"))
+    execute "SELECT * FROM rebuild_trade_plus_complete_mview()"
   end
 
   def down
@@ -22,7 +24,7 @@ class AddMultilingualAndFixFormattingIssueOnTradePlusView < ActiveRecord::Migrat
     execute "DROP VIEW IF EXISTS trade_plus_group_view"
     execute "DROP VIEW IF EXISTS trade_plus_shipments_view"
     execute "CREATE VIEW trade_plus_shipments_view AS #{view_sql('20200206150700', 'trade_plus_shipments_view')}"
-    execute "CREATE VIEW trade_plus_group_view AS #{view_sql('20210115103657', 'trade_plus_group_view')}"
+    execute "CREATE VIEW trade_plus_group_view AS #{view_sql('20210823165358', 'trade_plus_group_view')}"
     execute "CREATE VIEW trade_plus_formatted_data_view AS #{view_sql('20210106174249', 'trade_plus_formatted_data_view')}"
     execute "CREATE VIEW trade_plus_formatted_data_final_view AS #{view_sql('20210106174249', 'trade_plus_formatted_data_final_view')}"
     execute "CREATE VIEW trade_plus_complete_view AS #{view_sql('20191209215129', 'trade_plus_complete_view')}"
