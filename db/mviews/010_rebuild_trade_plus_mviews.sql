b1f3199f500d3fe333de136a8ced98a56dca3a42
lucacug <luca.cugliari@unep-wcmc.org>
2021-10-06 10:28:15 +0100

    remove comment from all taxon concept mview refresh line

--- db/mviews/010_rebuild_trade_plus_mviews.sql
+++ db/mviews/010_rebuild_trade_plus_mviews.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE FUNCTION rebuild_trade_plus_complete_mview() RETURNS void
     DROP MATERIALIZED VIEW IF EXISTS trade_plus_complete_mview CASCADE;
     -- Below is used for child taxa queries
     RAISE INFO 'Refreshing all taxon concepts and ancestors materialized view';
-    -- REFRESH MATERIALIZED VIEW all_taxon_concepts_and_ancestors_mview;
+    REFRESH MATERIALIZED VIEW all_taxon_concepts_and_ancestors_mview;
 
     RAISE INFO 'Creating Trade Plus complete materialized view';
     CREATE MATERIALIZED VIEW trade_plus_complete_mview AS
