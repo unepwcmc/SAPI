25f64c67b67e748c938a1478de175d0cfe50f362
lucacug <luca.cugliari@unep-wcmc.org>
2021-10-12 11:19:23 +0100

    pass locale param to filter class

--- app/controllers/api/v1/trade_plus_filters_controller.rb
+++ app/controllers/api/v1/trade_plus_filters_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::TradePlusFiltersController < ApplicationController
   respond_to :json
 
   def index
-    filters_service = Trade::TradePlusFilters
-    filters = Rails.cache.fetch('trade_plus_filters', expires_in: 1.week) do
+    filters_service = Trade::TradePlusFilters.new(params['locale'])
+    filters = Rails.cache.fetch(['trade_plus_filters', params], expires_in: 1.week) do
       res = ActiveRecord::Base.connection.execute(filters_service.query)
       filters_service.response_ordering(res)
     end
