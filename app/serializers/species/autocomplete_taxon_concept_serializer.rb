ec085a0ee8720a9d3b8fff1cfcabe89e2d6cb695
lucacug <luca.cugliari@unep-wcmc.org>
2022-08-08 16:01:58 +0100

    feat: add author year attribute to autocomplete api response

--- app/serializers/species/autocomplete_taxon_concept_serializer.rb
+++ app/serializers/species/autocomplete_taxon_concept_serializer.rb
@@ -1,5 +1,5 @@
 class Species::AutocompleteTaxonConceptSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :rank_name, :name_status, :matching_names
+  attributes :id, :full_name, :author_year, :rank_name, :name_status, :matching_names
 
   def rank_name
     rank_with_locale = "rank_display_name_#{I18n.locale.to_s}"
