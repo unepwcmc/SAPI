9e92681459468fa26b9a83110e3c49573f3b5293
lucacug <luca.cugliari@unep-wcmc.org>
2022-08-19 14:31:51 +0100

    spec: fix broken spec adding the event or document to the eu_opinions and cover intersessional doc cases

--- spec/controllers/admin/eu_opinions_controller_spec.rb
+++ spec/controllers/admin/eu_opinions_controller_spec.rb
@@ -5,6 +5,7 @@ describe Admin::EuOpinionsController do
 
   before do
     @taxon_concept = create(:taxon_concept)
+    @eu_regulation = create(:ec_srg)
   end
 
   describe "GET index" do
@@ -37,18 +38,40 @@ describe Admin::EuOpinionsController do
         @eu_decision_type = create(:eu_decision_type)
         @srg_history = create(:srg_history)
       end
-      it "redirects to the EU Opinions index" do
-        post :create,
-          eu_opinion: {
-            eu_decision_type_id: @eu_decision_type.id,
-            srg_history_id: @srg_history.id,
-            start_date: Date.today,
-            geo_entity_id: create(
-              :geo_entity, geo_entity_type_id: country_geo_entity_type.id
-            )
-          },
-          taxon_concept_id: @taxon_concept.id
-        response.should redirect_to(admin_taxon_concept_eu_opinions_url(@taxon_concept.id))
+      context "when intersessional document is present" do
+        before do
+          @document = create(:commission_note)
+        end
+        it "redirects to the EU Opinions index" do
+          post :create,
+            eu_opinion: {
+              eu_decision_type_id: @eu_decision_type.id,
+              srg_history_id: @srg_history.id,
+              start_date: Date.today,
+              document_id: @document.id,
+              geo_entity_id: create(
+                :geo_entity, geo_entity_type_id: country_geo_entity_type.id
+              )
+            },
+            taxon_concept_id: @taxon_concept.id
+          response.should redirect_to(admin_taxon_concept_eu_opinions_url(@taxon_concept))
+        end
+      end
+      context "when event is present" do
+        it "redirects to the EU Opinions index" do
+          post :create,
+            eu_opinion: {
+              eu_decision_type_id: @eu_decision_type.id,
+              srg_history_id: @srg_history.id,
+              start_date: Date.today,
+              start_event_id: @eu_regulation.id,
+              geo_entity_id: create(
+                :geo_entity, geo_entity_type_id: country_geo_entity_type.id
+              )
+            },
+            taxon_concept_id: @taxon_concept.id
+          response.should redirect_to(admin_taxon_concept_eu_opinions_url(@taxon_concept))
+        end
       end
     end
 
@@ -65,16 +88,17 @@ describe Admin::EuOpinionsController do
     before(:each) do
       @eu_opinion = create(
         :eu_opinion,
-        taxon_concept_id: @taxon_concept.id
+        taxon_concept_id: @taxon_concept.id,
+        start_event_id: @eu_regulation.id
       )
     end
     it "renders the edit template" do
-      get :edit, id: @eu_opinion.id, taxon_concept_id: @taxon_concept.id
+      get :edit, id: @eu_opinion.id, taxon_concept_id: @taxon_concept.id, start_event_id: @eu_regulation.id
       response.should render_template('edit')
     end
     it "assigns @geo_entities" do
       territory = create(:geo_entity, geo_entity_type_id: territory_geo_entity_type.id)
-      get :edit, id: @eu_opinion.id, taxon_concept_id: @taxon_concept.id
+      get :edit, id: @eu_opinion.id, taxon_concept_id: @taxon_concept.id, start_event_id: @eu_regulation.id
       assigns(:geo_entities).should include(territory)
     end
   end
@@ -83,10 +107,12 @@ describe Admin::EuOpinionsController do
     before(:each) do
       @eu_opinion = create(
         :eu_opinion,
-        taxon_concept_id: @taxon_concept.id
+        taxon_concept_id: @taxon_concept.id,
+        start_event_id: @eu_regulation.id
       )
       @eu_decision_type = create(:eu_decision_type)
       @srg_history = create(:srg_history)
+      @document = create(:commission_note)
     end
 
     context "when successful" do
@@ -94,7 +120,8 @@ describe Admin::EuOpinionsController do
         it "renders taxon_concepts EU Opinions page" do
           put :update,
             eu_opinion: {
-              eu_decision_type_id: @eu_decision_type.id
+              eu_decision_type_id: @eu_decision_type.id,
+              start_event_id: @eu_regulation.id
             },
             id: @eu_opinion.id,
             taxon_concept_id: @taxon_concept.id
@@ -107,9 +134,41 @@ describe Admin::EuOpinionsController do
       context "when eu decision type is not present" do
         it "renders taxon_concepts EU Opinions page" do
           put :update,
+          eu_opinion: {
+            eu_decision_type_id: nil,
+            srg_history_id: @srg_history.id,
+            start_event_id: @eu_regulation.id
+          },
+          id: @eu_opinion.id,
+          taxon_concept_id: @taxon_concept.id
+          response.should redirect_to(
+            admin_taxon_concept_eu_opinions_url(@taxon_concept)
+          )
+        end
+      end
+
+      context "when event is present" do
+        it "renders taxon_concepts EU Opinions page" do
+          put :update,
             eu_opinion: {
-              eu_decision_type_id: nil,
-              srg_history_id: @srg_history.id
+              eu_decision_type_id: @eu_decision_type.id,
+              start_event_id: @eu_regulation.id
+            },
+            id: @eu_opinion.id,
+            taxon_concept_id: @taxon_concept.id
+          response.should redirect_to(
+            admin_taxon_concept_eu_opinions_url(@taxon_concept)
+          )
+        end
+      end
+
+      context "when event is not present" do
+        it "renders taxon_concepts EU Opinions page" do
+          put :update,
+            eu_opinion: {
+              eu_decision_type_id: @eu_decision_type.id,
+              start_event_id: nil,
+              document_id: @document.id
             },
             id: @eu_opinion.id,
             taxon_concept_id: @taxon_concept.id
@@ -126,6 +185,7 @@ describe Admin::EuOpinionsController do
           put :update,
             eu_opinion: {
               eu_decision_type_id: @eu_decision_type.id,
+              start_event_id: @eu_regulation.id,
               start_date: nil
             },
             id: @eu_opinion.id,
@@ -140,6 +200,7 @@ describe Admin::EuOpinionsController do
             eu_opinion: {
               eu_decision_type_id: nil,
               srg_history_id: @srg_history.id,
+              start_event_id: @eu_regulation.id,
               start_date: nil
             },
             id: @eu_opinion.id,
@@ -162,18 +223,78 @@ describe Admin::EuOpinionsController do
         response.should render_template('new')
       end
     end
+
+    context "when event is present" do
+      it "renders new" do
+        put :update,
+          eu_opinion: {
+            eu_decision_type_id: @eu_decision_type.id,
+            start_event_id: @eu_regulation.id,
+            start_date: nil
+          },
+          id: @eu_opinion.id,
+          taxon_concept_id: @taxon_concept.id
+        response.should render_template('new')
+      end
+    end
+
+    context "when event is not present" do
+      it "renders new" do
+        put :update,
+          eu_opinion: {
+            eu_decision_type_id: @eu_decision_type.id,
+            start_event_id: nil,
+            document_id: @document.id,
+            start_date: nil
+          },
+          id: @eu_opinion.id,
+          taxon_concept_id: @taxon_concept.id
+        response.should render_template('new')
+      end
+    end
+
+    context "when both event and intersessional doc are empty" do
+      it "renders new" do
+        put :update,
+          eu_opinion: {
+            eu_decision_type_id: @eu_decision_type.id,
+            start_event_id: nil,
+            document_id: nil,
+            start_date: nil
+          },
+          id: @eu_opinion.id,
+          taxon_concept_id: @taxon_concept.id
+        response.should render_template('new')
+      end
+    end
+
+    context "when both event and intersessional doc are present" do
+      it "renders new" do
+        put :update,
+          eu_opinion: {
+            eu_decision_type_id: @eu_decision_type.id,
+            start_event_id: @eu_regulation.id,
+            document_id: @document.id,
+            start_date: nil
+          },
+          id: @eu_opinion.id,
+          taxon_concept_id: @taxon_concept.id
+        response.should render_template('new')
+      end
+    end
   end
 
   describe "DELETE destroy" do
     before(:each) do
       @eu_opinion = create(
         :eu_opinion,
-        taxon_concept_id: @taxon_concept.id
+        taxon_concept_id: @taxon_concept.id,
+        start_event_id: @eu_regulation.id
       )
     end
     it "redirects after delete" do
       delete :destroy, id: @eu_opinion.id,
-        taxon_concept_id: @taxon_concept.id
+        taxon_concept_id: @taxon_concept.id, start_event_id: @eu_regulation.id
       response.should redirect_to(
         admin_taxon_concept_eu_opinions_url(@taxon_concept)
       )
