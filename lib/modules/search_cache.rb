3c83d4adbf9856e28d06a598c351fa4c47039b94
sergio <s.marrocoli@gmail.com>
2022-08-26 13:31:29 +0100

    fix: cache cached keys to allow memcache to delete them (wip)

--- lib/modules/search_cache.rb
+++ lib/modules/search_cache.rb
@@ -12,17 +12,31 @@ module SearchCache
 
   def cached_results
     Rails.cache.fetch(results_cache_key, :expires_in => 24.hours) do
+      add_key_to_cached_keys(results_cache_key) # if correct key type
       results
     end
   end
 
   def cached_total_cnt
     Rails.cache.fetch(total_cnt_cache_key, :expires_in => 24.hours) do
+      add_key_to_cached_keys(total_cnt_cache_key)
       total_cnt
     end
   end
 
   private
+
+  def add_key_to_cached_keys(cache_key)
+    ##
+    # Memcache does not permit mass deletes or returning all keys so
+    # we need to cache the list of cache keys so we can iterate over them
+    # in order to mass clear cache after updates to e.g. record names
+    # via the admin interface.
+    key = :something_cool
+    cached_keys = Rails.cache.read(key) || []
+    cached_keys << cache_key
+    Rails.cache.write(key, cached_keys.compact, expires_in: 48.hours)
+  end
 
   def generic_cache_key(suffix)
     raw_key = @options.merge(:locale => I18n.locale).to_a.sort.
@@ -39,5 +53,4 @@ module SearchCache
   def total_cnt_cache_key
     generic_cache_key('total_cnt')
   end
-
 end
