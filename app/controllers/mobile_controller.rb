57bac39d2daa82be9239c77e4c3f419d42899363
Dagmara <dagmarakukla@gmail.com>
2022-09-07 11:30:58 +0100

    feat: add terms&cond content

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -2,6 +2,7 @@ class MobileController < ApplicationController
   def terms_and_conditions
   end
 
-  def privacy_policy
-  end
+  # TODO: uncomment when privacy policy content is updated
+  # def privacy_policy
+  # end
 end
