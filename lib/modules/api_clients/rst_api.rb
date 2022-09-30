12b019cf969059ae547aeef946fb4dec0920a1d7
lucacug <luca.cugliari@unep-wcmc.org>
2022-09-30 16:38:06 +0100

    feat: add rst api caller

---
+++ lib/modules/api_clients/rst_api.rb
@@ -0,0 +1,29 @@
+module ApiClients::RstApi
+  class << self
+    include HTTParty
+
+    BASE_URI = Rails.application.secrets[:rst_api_base_url]
+    PUBLIC_CASES_ENDPOINT = 'case/publicData/getCases'
+    COUNTRY_ENDPOINT = '/countries/countryId'
+    RETRIES = 3
+
+    # @see https://api-cites-rst.leman.un-icc.cloud/api/#/case/CaseController_readCases
+    def get_cases(page = 1, per_page = 25)
+      RETRIES.times do |i|
+        res = HTTParty.post("#{BASE_URI}/#{PUBLIC_CASES_ENDPOINT}?page=#{page}&limit=#{per_page}&sortBy=id&sortOrder=ASC&relationalData=yes", {})
+        break res if res.created?
+        # data['items'] => cases
+        # data['links']['next'] -> next page
+      end
+    end
+
+    # @see https://api-cites-rst.leman.un-icc.cloud/api/#/countries/CountriesController_getCountry
+    def get_country(country_id)
+      RETRIES.times do |i|
+        res = HTTParty.get("#{BASE_URI}/#{COUNTRY_ENDPOINT}?countryId=#{country_id}")
+        break res if res.ok?
+        # country_name -> data['nicname']
+      end
+    end
+  end
+end
