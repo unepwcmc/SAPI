1b1445b8c515b4186003f165d7c841bbc84994eb
lucacug <luca.cugliari@unep-wcmc.org>
2022-10-06 00:08:37 +0100

    chore: change name to final rst import method

--- lib/modules/import/rst/rst_cases.rb
+++ lib/modules/import/rst/rst_cases.rb
@@ -1,6 +1,6 @@
 module Import::Rst::RstCases
   class << self
-    def import_process
+    def import_all
       rst_cases         = Import::Rst::Fetcher.get_all_cases
       formatted_data    = Import::Rst::Formatter.format_data(rst_cases)
       rst_country_data  = Import::Rst::CountryDataHandler.merge_country_data(formatted_data)
