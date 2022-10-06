f86f5ba87bab24ab2a90eb01a3e5c7a5f0e70a4b
lucacug <luca.cugliari@unep-wcmc.org>
2022-10-06 23:28:40 +0100

    fix: move next back into the loop and multiple fixes to the rst importer module

--- lib/modules/import/rst/importer.rb
+++ lib/modules/import/rst/importer.rb
@@ -4,44 +4,49 @@ module Import::Rst::Importer
     def import(data)
       data.map do |item|
         taxon_concept = map_taxon_concept(item)
-        geo_entity    = map_geo_entity(item)
-        event         = map_event(item)
+        unless taxon_concept
+          Rails.logger.error "Species #{item['species']['name']} for case #{item['id']} not found, skipping... "
+          next
+        end
 
-        rst_process   = CitesRstProcess.find_or_initialize_by(case_id: item['id'])
-        Rails.logger.info "Importing RST case #{item['id']}..."
+        geo_entity = map_geo_entity(item)
+        unless geo_entity
+          Rails.logger.error "Country #{item['country_iso2']} for case #{item['id']} not found, skipping... "
+          next
+        end
+
+        event = map_event(item)
+
+        rst_process = CitesRstProcess.find_or_initialize_by(case_id: item['id'])
+        action = rst_process.id ? 'Updating' : 'Importing'
+        Rails.logger.info "#{action} RST process with case_id #{item['id']}..."
+
         rst_process.update!(
-          taxon_concept: taxon_concept,
-          geo_entity: geo_entity,
-          event: event,
+          taxon_concept_id: taxon_concept.id,
+          geo_entity_id: geo_entity.id,
+          start_event_id: event.try(:id),
           status: item['status'],
-          start_date: item['startDate'],
-          resolution: 'Significant Trade'
+          start_date: item['startDate']
         )
       end
+    end
 
-      private
+    private
 
-      def map_taxon_concept(item)
-        taxon_concept = MTaxonConcept.find_by(taxonomy_id: 1, full_name: item['species']['name'])
-        unless taxon_concept
-          Rails.logger.info "Species #{item['species']['name']} for case #{item['id']} not found, skipping... "
-          next
-        end
-      end
+    def map_taxon_concept(item)
+      MTaxonConcept.find_by(taxonomy_id: 1, full_name: item['species']['name'])
+    end
 
-      def map_geo_entity(item)
-        geo_entity = GeoEntity.find_by(iso_2: item['country_iso2'])
-        unless geo_entity
-          Rails.logger.info "Country #{item['country_iso2']} for case #{item['id']} not found, skipping... "
-          next
-        end
-      end
+    def map_geo_entity(item)
+      GeoEntity.find_by(iso_code2: item['country_iso2'])
+    end
 
-      def map_event(item)
-        event = Event.where("type IN (:event_type) AND name :event_name",
-          event_type: ['CitesAc', 'CitesPc'], event_name: item['meeting']['name'])
-        Rails.logger.info "Event #{item['meeting']['name']} for case #{item['id']} not found" unless event
-      end
+    def map_event(item)
+      event = Event.where("type IN (:event_type) AND name = :event_name",
+        event_type: ['CitesAc', 'CitesPc'], event_name: item['meeting']['name']).first
+
+      Rails.logger.info "Event #{item['meeting']['name']} for case #{item['id']} not found" unless event
+      event
     end
   end
 end
