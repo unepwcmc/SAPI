c1ef609309533f2b277ccc98061d10fe18450530
lucacug <luca.cugliari@unep-wcmc.org>
2022-10-07 14:30:49 +0100

    feat: make sure export all CITES processes from the admin interface

--- app/models/species/cites_processes_export.rb
+++ app/models/species/cites_processes_export.rb
@@ -1,10 +1,10 @@
 class Species::CitesProcessesExport < Species::CsvCopyExport
 
   def query
-    rel = CitesCaptivityProcess.joins("LEFT JOIN taxon_concepts ON
+    rel = CitesProcess.joins("LEFT JOIN taxon_concepts ON
             taxon_concept_id = taxon_concepts.id LEFT JOIN geo_entities ON
             geo_entity_id = geo_entities.id LEFT JOIN events ON
-            start_event_id = events.id").order('taxon_concepts.id','cites_captivity_processes.id')
+            start_event_id = events.id").order('taxon_concepts.id','cites_processes.id')
     rel.select(sql_columns)
   end
 
@@ -15,21 +15,21 @@ class Species::CitesProcessesExport < Species::CsvCopyExport
   end
 
   def table_name
-    'cites_captivity_processes'
+    'cites_processes'
   end
 
   def sql_columns
-    columns = [
-      :'cites_captivity_processes.id', :taxon_concept_id, :'taxon_concepts.full_name', 
-      :resolution, :'geo_entities.name_en', :'events.name', "to_char(start_date, 'DD/MM/YYYY')", 
-      :status, :'cites_captivity_processes.notes'
+    [
+      :'cites_processes.id', :taxon_concept_id, :'taxon_concepts.full_name',
+      :resolution, :'geo_entities.name_en', :'events.name', "to_char(start_date, 'DD/MM/YYYY')",
+      :status, :'cites_processes.document', :'cites_processes.notes'
     ]
   end
 
   def csv_column_headers
-    headers = [
-      'Id', 'TaxonConcept id', 'TaxonConcept name', 'Resolution', 'Country', 'Event', 
-      'Event date', 'Status', 'Notes'
+    [
+      'Id', 'TaxonConcept id', 'TaxonConcept name', 'Resolution', 'Country', 'Event',
+      'Event date', 'Status', 'Document', 'Notes'
     ]
   end
 
