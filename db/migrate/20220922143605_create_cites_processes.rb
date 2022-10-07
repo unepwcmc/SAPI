eb652686c3cb03535a7e9211e810c2dae6f97a56
lucacug <luca.cugliari@unep-wcmc.org>
2022-10-07 12:31:31 +0100

    feat: add document free text field to cites processes

--- db/migrate/20220922143605_create_cites_processes.rb
+++ db/migrate/20220922143605_create_cites_processes.rb
@@ -7,6 +7,7 @@ class CreateCitesProcesses < ActiveRecord::Migration
       t.references :start_event
       t.datetime :start_date
       t.string :status # change to Enum type after migrating to rails 4.1
+      t.text :document
       t.string :type
       t.integer :created_by_id
       t.integer :updated_by_id
