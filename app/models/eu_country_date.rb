bd8c8c7d53f9500385f36961699a54677bb961a4
lucacug <luca.cugliari@unep-wcmc.org>
2022-10-14 15:08:06 +0100

    chore: remove unused attr accessible

--- app/models/eu_country_date.rb
+++ app/models/eu_country_date.rb
@@ -1,11 +1,10 @@
 class EuCountryDate < ActiveRecord::Base
-  attr_accessible :eu_accession_year, :eu_exit_year, :geo_entity
   belongs_to :geo_entity
   validates :geo_entity, :presence => true
   validate :is_country
 
   private
-  
+
   def is_country
     unless self.geo_entity.is_country?
       error.add(:geo_entity, "Must be a country")
