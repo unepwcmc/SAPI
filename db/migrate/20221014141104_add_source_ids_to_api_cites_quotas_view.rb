7a6addd5663c92b70a09f67ff43df090726b0289
Will Kocur <willkocur@gmail.com>
2022-11-11 09:40:26 +0000

    fix: make suspension and quota view migrations reversible

--- db/migrate/20221014141104_add_source_ids_to_api_cites_quotas_view.rb
+++ db/migrate/20221014141104_add_source_ids_to_api_cites_quotas_view.rb
@@ -1,6 +1,11 @@
 class AddSourceIdsToApiCitesQuotasView < ActiveRecord::Migration
-  def change
+  def up
     execute "DROP VIEW IF EXISTS api_cites_quotas_view"
     execute "CREATE VIEW api_cites_quotas_view AS #{view_sql('20221014151355', 'api_cites_quotas_view')}"
+  end
+
+  def down
+    execute "DROP VIEW IF EXISTS api_cites_quotas_view"
+    execute "CREATE VIEW api_cites_quotas_view AS #{view_sql('20150512222755', 'api_cites_quotas_view')}"
   end
 end
--- db/migrate/20221014141105_add_source_ids_to_api_cites_suspensions_view.rb
+++ db/migrate/20221014141105_add_source_ids_to_api_cites_suspensions_view.rb
@@ -1,6 +1,11 @@
 class AddSourceIdsToApiCitesSuspensionsView < ActiveRecord::Migration
-  def change
+  def up
     execute "DROP VIEW IF EXISTS api_cites_suspensions_view"
     execute "CREATE VIEW api_cites_suspensions_view AS #{view_sql('20221014151355', 'api_cites_suspensions_view')}"
+  end
+
+  def down
+    execute "DROP VIEW IF EXISTS api_cites_suspensions_view"
+    execute "CREATE VIEW api_cites_suspensions_view AS #{view_sql('20150518122737', 'api_cites_suspensions_view')}"
   end
 end
