078b7e32d759ffeac648c56079983c974044fc5e
sergio marrocoli <s.marrocoli@gmail.com>
2023-06-30 11:21:08 +0100

    feat: use try instead of rescue in synonym and accepted name serializers

--- app/serializers/species/accepted_name_serializer.rb
+++ app/serializers/species/accepted_name_serializer.rb
@@ -2,6 +2,6 @@ class Species::AcceptedNameSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :author_year, :rank
 
   def rank
-    object.rank.name rescue nil
+    object.rank.try(:name)
   end
 end
