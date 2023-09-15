3238c58f7e10020143ca4edb2ae4f0d55bdac6f4
sergio marrocoli <s.marrocoli@gmail.com>
2023-09-15 14:00:00 +0100

    feat: Add index to optimise LIKE search, and allow matches anywhere in trade_permit 'number' string, instead of just the start

---
+++ db/migrate/20230915113624_add_trigram_index_to_trade_permit_numbers.rb
@@ -0,0 +1,20 @@
+class AddTrigramIndexToTradePermitNumbers < ActiveRecord::Migration
+  def up
+    # Index to optimise LIKE queries
+    if Rails.env.staging? or Rails.env.production?
+      puts "Please add extension by hand: CREATE EXTENSION pg_trgm"
+    else
+      execute "CREATE EXTENSION IF NOT EXISTS pg_trgm"
+    end
+    execute 'CREATE INDEX trade_permits_number_trigm_idx ON trade_permits USING gin (upper(number) gin_trgm_ops)'
+  end
+
+  def down
+    execute 'DROP INDEX trade_permits_number_trigm_idx'
+    if Rails.env.staging? or Rails.env.production?
+      puts "Please drop extension by hand: DROP EXTENSION pg_trgm"
+    else
+      execute "DROP EXTENSION pg_trgm"
+    end
+  end
+end
