ac7dbbaab113ef0b3710a8a9216ec7a1aa3fee70
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-01-08 14:45:29 +0000

    Fix test failure(s) on Rails 4.1 where update_all no longer takes two arguments

--- app/models/document_collection_order.rb
+++ app/models/document_collection_order.rb
@@ -23,10 +23,11 @@ class DocumentCollectionOrder
 
   def update(id_sort_index_hash)
     id_sort_index_hash.each do |id, sort_index|
-      Document.update_all(
+      Document.where([
+        'id = :id OR primary_language_document_id = :id',
+        id: id
+      ]).update_all(
         { sort_index: sort_index, updated_at: DateTime.now },
-        ['id = :id OR primary_language_document_id = :id',
-        id: id]
       )
     end
     DocumentSearch.clear_cache
