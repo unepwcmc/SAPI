4a2477a9e4d7d706cb287674199b84125a7de934
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-01-10 15:33:13 +0000

    Requiring `rspec/autorun` when running RSpec via the `rspec` command is deprecated.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ SimpleCov.start 'rails'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'sidekiq/testing'
 require 'capybara/rspec'
 require 'capybara/rails'
