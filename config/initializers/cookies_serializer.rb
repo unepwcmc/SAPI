4cd3e0fa7584b7aa886d3c189fd5c1eeb11ab1c7
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-01-19 10:05:29 +0000

    rails app:update

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# https://guides.rubyonrails.org/v4.1/upgrading_ruby_on_rails.html#cookies-serializer
+# Specify a serializer for the signed and encrypted cookie jars.
+# Valid options are :json, :marshal, and :hybrid.
+# @see https://guides.rubyonrails.org/v4.1/upgrading_ruby_on_rails.html#cookies-serializer
 Rails.application.config.action_dispatch.cookies_serializer = :hybrid
