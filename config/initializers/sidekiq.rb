fcc75f4c50c2ed5e8d0cd6143f3fa5e5b39bd9f1
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-01-19 13:13:22 +0000

    upgrade sidekiq and related gems

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
 require 'sidekiq'
 require 'sidekiq-status'
+require "sidekiq-unique-jobs"
 
 Sidekiq.configure_client do |config|
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware unless Rails.env.test?
+    chain.add SidekiqUniqueJobs::Middleware::Client
   end
   config.redis = {
     url: Rails.application.secrets.redis['url']
@@ -13,11 +15,15 @@ end
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes
+    chain.add SidekiqUniqueJobs::Middleware::Server
   end
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware unless Rails.env.test?
+    chain.add SidekiqUniqueJobs::Middleware::Client
   end
   config.redis = {
     url: Rails.application.secrets.redis['url']
   }
+
+  SidekiqUniqueJobs::Server.configure(config)
 end
