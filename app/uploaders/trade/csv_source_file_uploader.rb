8c004a7c68d14837996f4796dc5882b3d432bf8e
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-01-19 12:07:11 +0000

    upgrade gems for rails 5

--- app/uploaders/trade/csv_source_file_uploader.rb
+++ app/uploaders/trade/csv_source_file_uploader.rb
@@ -73,7 +73,7 @@ class Trade::CsvSourceFileUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
+  def extension_allowlist
     %w(csv)
   end
 
