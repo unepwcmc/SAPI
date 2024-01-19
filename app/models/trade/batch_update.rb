7f9cf288c95c0f1524ea3bc915692219ed638c4b
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-01-19 16:10:00 +0000

    https://gitlab.com/gitlab-org/gitlab-foss/-/merge_requests/19792/diffs

--- app/models/trade/batch_update.rb
+++ app/models/trade/batch_update.rb
@@ -20,7 +20,7 @@ class Trade::BatchUpdate
     affected_shipments = nil
     Trade::Shipment.transaction do
       affected_shipments = @shipments.count
-      @shipments.update_all(update_params)
+      @shipments.update_all(update_params.to_h)
       DownloadsCacheCleanupWorker.perform_async(:shipments)
     end
     PermitCleanupWorker.perform_async(disconnected_permits_ids)
