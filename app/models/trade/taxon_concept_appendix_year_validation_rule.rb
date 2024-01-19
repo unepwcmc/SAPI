e841b6b8aa6a57fa6989685e65575c4583fa1604
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-01-19 17:06:33 +0000

    rollback part of 894a3e9bf49a4a7aa0e98954dbe12b4ea526c483

--- app/models/trade/taxon_concept_appendix_year_validation_rule.rb
+++ app/models/trade/taxon_concept_appendix_year_validation_rule.rb
@@ -21,11 +21,11 @@ class Trade::TaxonConceptAppendixYearValidationRule < Trade::InclusionValidation
     return nil unless shipment_in_scope?(shipment)
     # if it is, check if it has a match in valid values view
     v = Arel::Table.new(valid_values_view)
-    appendix_node = v['appendix'].eq(Arel::Nodes::Quoted.new(shipment.appendix))
+    appendix_node = v['appendix'].eq(shipment.appendix)
     effective_from = Arel::Nodes::NamedFunction.new "DATE_PART", [Arel::Nodes.build_quoted("year"), v['effective_from']]
     effective_to = Arel::Nodes::NamedFunction.new "DATE_PART", [Arel::Nodes.build_quoted("year"), v['effective_to']]
     year_node = effective_from.lteq(shipment.year).and(effective_to.gteq(shipment.year).or(effective_to.eq(nil)))
-    taxon_concept_node = v['taxon_concept_id'].eq(Arel::Nodes::Quoted.new(shipment.taxon_concept_id))
+    taxon_concept_node = v['taxon_concept_id'].eq(shipment.taxon_concept_id)
     conditions = appendix_node.and(year_node).and(taxon_concept_node)
     return nil if Trade::Shipment.find_by_sql(v.project(Arel.star).where(conditions)).any?
     error_message
@@ -41,11 +41,11 @@ class Trade::TaxonConceptAppendixYearValidationRule < Trade::InclusionValidation
   end
 
   def taxon_concept_join_node(s, v)
-    s['taxon_concept_id'].eq(Arel::Nodes::Quoted.new(v['taxon_concept_id']))
+    s['taxon_concept_id'].eq(v['taxon_concept_id'])
   end
 
   def appendix_join_node(s, v)
-    s['appendix'].eq(Arel::Nodes::Quoted.new(v['appendix']))
+    s['appendix'].eq(v['appendix'])
   end
 
   # Difference from superclass: rather than equality, check if appendix
