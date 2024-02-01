f5aede0996294cbfac56161491a4854a7b5c6653
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-02-01 18:08:09 +0000

    https://thoughtbot.com/blog/rails-6-warning-message-upgrade

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -1,26 +1,4 @@
-# Migrated to Strong Parameters
-# ActsAsTaggableOn::Tag.class_eval do
-#   attr_accessible :name
-# end
-
-# ActsAsTaggableOn::Tagging.class_eval do
-#   attr_accessible :tag_id, :context, :taggable, :taggable_id, :taggable_type, :tagger_id, :tagger_type
-# end
-
-
-
-# TODO
-# DEPRECATION WARNING: Initialization autoloaded the constant ComparisonAttributes.
-#
-# Being able to do this is deprecated. Autoloading during initialization is going
-# to be an error condition in future versions of Rails.
-#
-# Reloading does not reboot the application, and therefore code executed during
-# initialization does not run again. So, if you reload ComparisonAttributes, for example,
-# the expected changes won't be reflected in that stale Module object.
-#
-# `config.autoloader` is set to `classic`. This autoloaded constant would have been unloaded if `config.autoloader` had been set to `:zeitwerk`.
-#
-# Please, check the "Autoloading and Reloading Constants" guide for solutions.
-#  (called from <top (required)> at /SAPI/config/environment.rb:5)
-ActsAsTaggableOn::Tagging.send :include, ComparisonAttributes
+# https://thoughtbot.com/blog/rails-6-warning-message-upgrade
+Rails.application.reloader.to_prepare do
+  ActsAsTaggableOn::Tagging.send :include, ComparisonAttributes
+end
