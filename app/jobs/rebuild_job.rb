07c58b989d7aed0fc496c2e92709917eeff95128
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-02-05 16:17:08 +0000

    fix: split rebuild job into daily and weekly, as we only want to run it weekly on production

--- app/jobs/rebuild_job.rb
+++ app/jobs/rebuild_job.rb
@@ -2,11 +2,6 @@ class RebuildJob < ApplicationJob
   queue_as :admin
 
   def perform(*args)
-    if Rails.env.production?
-      # Only run on Saturday in production
-      Sapi.rebuild if Date.today.saturday?
-    else
-      Sapi.rebuild
-    end
+    Sapi.rebuild
   end
 end
