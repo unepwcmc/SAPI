d968716473ebf57d1e0b8163138299fd79b34040
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-02-05 17:25:37 +0000

    Try to fix SQL issue after upgrade

--- app/services/country_dictionary.rb
+++ app/services/country_dictionary.rb
@@ -3,10 +3,9 @@ class CountryDictionary
 
   def initialize
     @collection = GeoEntity.
-      select([
-        :"geo_entities.id", :name_en, :name_es, :name_fr,
-        :"UPPER(geo_entities.iso_code2) AS iso_code2"
-      ]).
+      select(
+        :id, :name_en, :name_es, :name_fr, 'UPPER(geo_entities.iso_code2) AS iso_code2'
+      ).
       joins(:geo_entity_type).
       where(:"geo_entity_types.name" => [GeoEntityType::COUNTRY, GeoEntityType::TERRITORY]).
       all
