b3fe0f999b2df01db6c2f098add9533e2e0ee511
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-02-08 10:36:32 +0000

    build the cache in background through sidekiq, with unique job, to prevent many request coming from that endpoint, which generate a lot of slow SQL (cache not ready) and take down the server.

--- app/controllers/api/v1/trade_plus_filters_controller.rb
+++ app/controllers/api/v1/trade_plus_filters_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::TradePlusFiltersController < ApplicationController
   respond_to :json
 
-  def index
-    locale = params['locale'] || I18n.default_locale.to_s
-    filters_service = Trade::TradePlusFilters.new(locale)
-    filters = Rails.cache.fetch(['trade_plus_filters', locale], expires_in: 4.weeks) do
-      res = ApplicationRecord.connection.execute(filters_service.query)
-      filters_service.response_ordering(res)
-    end
+  CACHE_KEY_PREFIX = 'trade_plus_filters_with_locale_'
 
-    render :json => filters
+  def index
+    cache_key = "#{CACHE_KEY_PREFIX}#{I18n.locale}"
+    filters = Rails.cache.read(cache_key)
+    if filters.nil?
+      TradePlusFiltersWorker.perform_async(I18n.locale)
+      head 500 # Cache not ready yet.
+    else
+      render :json => filters
+    end
   end
 end
