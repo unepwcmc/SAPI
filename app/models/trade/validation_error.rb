ae8cac99c816c236ac6f916d221d8904c1217448
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-03-04 12:15:10 +0000

    fix: add a validation on Trade::ValidationError model to ensure JSON type of matching_criteria

--- app/models/trade/validation_error.rb
+++ app/models/trade/validation_error.rb
@@ -1,4 +1,10 @@
 class Trade::ValidationError < ApplicationRecord
+  include ActiveModel::Validations
+
+  validates_each :matching_criteria, allow_blank: true do |record, attr, value|
+    record.errors.add attr, "must be a Hash, got a #{value.class.name}" if !value.is_a? Hash
+  end
+
   belongs_to :annual_report_upload, class_name: 'Trade::AnnualReportUpload'
   belongs_to :validation_rule, class_name: 'Trade::ValidationRule'
 
