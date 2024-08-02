3429640596604bf6f048ddd4db772be362001531
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2024-08-02 14:52:36 +0100

    Remove old files, compare with railsdiff.org

--- config/initializers/cookies_serializer.rb
+++
@@ -1,6 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Specify a serializer for the signed and encrypted cookie jars.
-# Valid options are :json, :marshal, and :hybrid.
-# @see https://guides.rubyonrails.org/v4.1/upgrading_ruby_on_rails.html#cookies-serializer
-Rails.application.config.action_dispatch.cookies_serializer = :hybrid
