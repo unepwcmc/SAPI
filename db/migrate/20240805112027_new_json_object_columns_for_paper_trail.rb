ccccfe131fb477d9f9d1e52e0f1b5403d7562961
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-08-05 14:02:51 +0100

    chore: write "down" migration for NewJsonObjectColumnsForPaperTrail"

--- db/migrate/20240805112027_new_json_object_columns_for_paper_trail.rb
+++ db/migrate/20240805112027_new_json_object_columns_for_paper_trail.rb
@@ -12,6 +12,20 @@ class NewJsonObjectColumnsForPaperTrail < ActiveRecord::Migration[7.0]
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    safety_assured {
+      execute %Q{
+        UPDATE taxon_concept_versions SET object_yml = '---' || chr(10) || object::JSON::TEXT || chr(10) WHERE object IS NOT NULL AND object_yml IS NULL
+        UPDATE versions SET object_yml = '---' || chr(10) || object::JSON::TEXT || chr(10) WHERE object IS NOT NULL AND object_yml IS NULL
+        UPDATE versions SET object_changes_yml = '---' || chr(10) || object_changes::JSON::TEXT || chr(10) WHERE object_changes IS NOT NULL AND object_changes_yml IS NULL
+      }
+
+      drop_column :taxon_concept_versions, :object
+      drop_column :versions, :object
+      drop_column :versions, :object_changes
+
+      rename_column :taxon_concept_versions, :object_yml, :object
+      rename_column :versions, :object_yml, :object
+      rename_column :versions, :object_changes_yml, :object_changes
+    }
   end
 end
