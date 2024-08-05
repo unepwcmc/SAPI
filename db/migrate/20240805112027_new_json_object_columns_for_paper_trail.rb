689ef9939614627f2b9f270335f3fc3224931840
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-08-05 16:58:29 +0100

    fix: NewJsonObjectColumnsForPaperTrail down patch

--- db/migrate/20240805112027_new_json_object_columns_for_paper_trail.rb
+++ db/migrate/20240805112027_new_json_object_columns_for_paper_trail.rb
@@ -14,14 +14,14 @@ class NewJsonObjectColumnsForPaperTrail < ActiveRecord::Migration[7.0]
   def down
     safety_assured {
       execute %Q{
-        UPDATE taxon_concept_versions SET object_yml = '---' || chr(10) || object::JSON::TEXT || chr(10) WHERE object IS NOT NULL AND object_yml IS NULL
-        UPDATE versions SET object_yml = '---' || chr(10) || object::JSON::TEXT || chr(10) WHERE object IS NOT NULL AND object_yml IS NULL
-        UPDATE versions SET object_changes_yml = '---' || chr(10) || object_changes::JSON::TEXT || chr(10) WHERE object_changes IS NOT NULL AND object_changes_yml IS NULL
+        UPDATE taxon_concept_versions SET object_yml = '---' || chr(10) || object::JSON::TEXT || chr(10) WHERE object IS NOT NULL AND object_yml IS NULL;
+        UPDATE versions SET object_yml = '---' || chr(10) || object::JSON::TEXT || chr(10) WHERE object IS NOT NULL AND object_yml IS NULL;
+        UPDATE versions SET object_changes_yml = '---' || chr(10) || object_changes::JSON::TEXT || chr(10) WHERE object_changes IS NOT NULL AND object_changes_yml IS NULL;
       }
 
-      drop_column :taxon_concept_versions, :object
-      drop_column :versions, :object
-      drop_column :versions, :object_changes
+      remove_column :taxon_concept_versions, :object
+      remove_column :versions, :object
+      remove_column :versions, :object_changes
 
       rename_column :taxon_concept_versions, :object_yml, :object
       rename_column :versions, :object_yml, :object
