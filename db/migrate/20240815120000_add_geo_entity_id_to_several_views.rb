a7ab3311e8c9c2219a8fbba25355d577be57f31e
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-08-15 17:31:49 +0100

    fix: also apply changes to api_eu_listing_changes_view

--- db/migrate/20240815120000_add_geo_entity_id_to_several_views.rb
+++ db/migrate/20240815120000_add_geo_entity_id_to_several_views.rb
@@ -11,12 +11,16 @@ class AddGeoEntityIdToSeveralViews < ActiveRecord::Migration[4.2]
         );
       SQL
 
+      execute 'DROP VIEW IF EXISTS api_cites_listing_changes_view;'
+      execute 'DROP VIEW IF EXISTS api_eu_listing_changes_view;'
       execute 'DROP VIEW IF EXISTS api_cites_quotas_view;'
       execute 'DROP VIEW IF EXISTS api_cites_suspensions_view;'
       execute 'DROP VIEW IF EXISTS api_eu_decisions_view;'
       execute 'DROP VIEW IF EXISTS taxon_concepts_distributions_view;'
 
+
       execute "CREATE VIEW api_cites_listing_changes_view AS #{view_sql('20240815120000', 'api_cites_listing_changes_view')}"
+      execute "CREATE VIEW api_eu_listing_changes_view AS #{view_sql('20240815120000', 'api_eu_listing_changes_view')}"
       execute "CREATE VIEW api_cites_quotas_view AS #{view_sql('20240815120000', 'api_cites_quotas_view')}"
       execute "CREATE VIEW api_cites_suspensions_view AS #{view_sql('20240815120000', 'api_cites_suspensions_view')}"
       execute "CREATE VIEW api_eu_decisions_view AS #{view_sql('20240815120000', 'api_eu_decisions_view')}"
@@ -36,12 +40,15 @@ class AddGeoEntityIdToSeveralViews < ActiveRecord::Migration[4.2]
       SQL
 
       execute 'DROP VIEW IF EXISTS api_cites_listing_changes_view;'
+      execute 'DROP VIEW IF EXISTS api_eu_listing_changes_view;'
       execute 'DROP VIEW IF EXISTS api_cites_quotas_view;'
       execute 'DROP VIEW IF EXISTS api_cites_suspensions_view;'
       execute 'DROP VIEW IF EXISTS api_eu_decisions_view;'
       execute 'DROP VIEW IF EXISTS taxon_concepts_distributions_view;'
 
+
       execute "CREATE VIEW api_cites_listing_changes_view AS #{view_sql('20230509172742', 'api_cites_listing_changes_view')}"
+      execute "CREATE VIEW api_eu_listing_changes_view AS #{view_sql('20141230193844', 'api_eu_listing_changes_view')}"
       execute "CREATE VIEW api_cites_quotas_view AS #{view_sql('20221014151355', 'api_cites_quotas_view')}"
       execute "CREATE VIEW api_cites_suspensions_view AS #{view_sql('20240724113700', 'api_cites_suspensions_view')}"
       execute "CREATE VIEW api_eu_decisions_view AS #{view_sql('20220808165846', 'api_eu_decisions_view')}"
