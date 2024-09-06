707758ac7a1b337a1ae5f9d611247ebfd3fa62dd
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-09-06 15:33:47 +0100

    fix: on deletion of a Cites CoP, also destroy its hash annotations

--- app/models/events/cites_cop.rb
+++ app/models/events/cites_cop.rb
@@ -32,9 +32,14 @@
 
 class CitesCop < Event
   include Deletable
+
   # Migrated to controller (Strong Parameters)
   # attr_accessible :is_current
-  has_many :hash_annotations, class_name: 'Annotation', foreign_key: :event_id
+
+  has_many :hash_annotations,
+    class_name: 'Annotation',
+    dependent: :destroy,
+    foreign_key: :event_id
 
   validate :designation_is_cites
   validates :effective_at, presence: true
