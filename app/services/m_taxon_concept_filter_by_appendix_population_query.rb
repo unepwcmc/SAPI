9a018d92cd299cded8ba5d1654e329f1a377242d
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-10-15 14:23:45 +0100

    fix: syntax error arising from comments in sql

--- app/services/m_taxon_concept_filter_by_appendix_population_query.rb
+++ app/services/m_taxon_concept_filter_by_appendix_population_query.rb
@@ -12,20 +12,31 @@ class MTaxonConceptFilterByAppendixPopulationQuery < MTaxonConceptFilterByAppend
     unless [ 'CITES', 'EU', 'CMS' ].include? designation_name
       designation_name = 'CITES'
     end
+
     listing_changes_mview = "#{designation_name.downcase}_listing_changes_mview"
+
     initialize_species_listings_conditions(designation_name)
 
+    # Filter all current additions where either
+    #
+    # - The listing does not specify which population the listing applies to
+    #   (and thus applies to all)
+    # - at least one member in common between the `listed_geo_entities_ids` of
+    #   the listing and and `@geo_entities_ids`.
+    #
+    # And where the listing change does not specify excluded populations or
+    # whose excluded populations (in the case of genus-level listings, where
+    # the species in question is known to have such a population) do not include
+    # all members of `@geo_entities_ids`
     @relation.joins(
-      <<-SQL
+      <<-SQL.squish
       JOIN (
         SELECT taxon_concept_id
         FROM #{listing_changes_mview} lc
         JOIN #{@table} tc ON lc.taxon_concept_id = tc.id
         WHERE is_current = 't' AND change_type_name = 'ADDITION'
         AND (
-          -- is listed in some of those entities
           listed_geo_entities_ids && ARRAY[#{@geo_entities_in_clause}]
-          -- or is listed everywhere
           OR ARRAY_UPPER(listed_geo_entities_ids, 1) IS NULL
         ) AND NOT excluded_geo_entities_ids @> array_intersect(countries_ids_ary, ARRAY[#{@geo_entities_in_clause}])
         #{"AND species_listing_id IN (#{@species_listings_in_clause})" unless @appendix_abbreviations.empty?}
