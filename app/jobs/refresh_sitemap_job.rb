a805b8c8675bfa7aec46d4a6aa9a77def141f7c1
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-12-08 10:32:25 +0000

    fix: allow two more long-running jobs to have a longer lock wait timeout

--- app/jobs/refresh_sitemap_job.rb
+++ app/jobs/refresh_sitemap_job.rb
@@ -2,7 +2,15 @@ class RefreshSitemapJob < ApplicationJob
   queue_as :admin
 
   def perform(*args)
-    # https://github.com/kjvarga/sitemap_generator/issues/231
-    SitemapGenerator::Interpreter.run
+    ActiveRecord::Base.transaction do
+      connection = ActiveRecord::Base.connection
+      # Within the current transaction, increase the lock_timeout. The default
+      # postgres value is 0 (infinite) but config/database.yml sets this to a
+      # lower value.
+      connection.execute("SET LOCAL lock_timeout='30s';")
+
+      # https://github.com/kjvarga/sitemap_generator/issues/231
+      SitemapGenerator::Interpreter.run
+    end
   end
 end
--- app/jobs/sunday_cleanup_job.rb
+++ app/jobs/sunday_cleanup_job.rb
@@ -2,9 +2,21 @@ class SundayCleanupJob < ApplicationJob
   queue_as :admin
 
   def perform(*args)
-    # rake "dashboard_stats:cache:update"
-    DashboardStatsCache.update_dashboard_stats
+    ActiveRecord::Base.transaction do
+      connection = ActiveRecord::Base.connection
+      # Within the current transaction, increase the lock_timeout. The default
+      # postgres value is 0 (infinite) but config/database.yml sets this to a
+      # lower value.
+      connection.execute("SET LOCAL lock_timeout='30s';")
 
+      # rake "dashboard_stats:cache:update"
+      DashboardStatsCache.update_dashboard_stats
+
+      cleanup_orphaned_records
+    end
+  end
+
+  def cleanup_orphaned_records
     # rake "db:common_names:cleanup"
     Rails.logger.warn '### rake db:common_names:cleanup'
 
