1bf80f059d5c053bf0dba34bb8ac1be91d897e8a
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2025-05-19 11:30:39 +0100

    fix: make it possible to run the down migration for DocumentFilenameColumnDeprecated

--- db/migrate/20250513134147_document_filename_column_deprecated.rb
+++ db/migrate/20250513134147_document_filename_column_deprecated.rb
@@ -1,5 +1,22 @@
+##
+# Make the filename column nullable on Document; after this, uploads will go
+# into S3 storage rather than being kept on the disk of the web server.
+#
+# Note that the rollback 'down' migration is DESTRUCTIVE - all documents created
+# since the change will be deleted, as they can no longer be represented in the
+# previous schema.
+
 class DocumentFilenameColumnDeprecated < ActiveRecord::Migration[7.1]
   def change
-    change_column_null :documents, :filename, true
+    reversible do |dir|
+      dir.up do
+        change_column_null :documents, :filename, true
+      end
+
+      dir.down do
+        Document.where(filename: nil).destroy_all
+        change_column_null :documents, :filename, false
+      end
+    end
   end
 end
