a1deada06bb819f22e6517c53190df38a91bf285
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2025-05-22 11:48:53 +0100

    chore: fix a bug where db restores would sometimes fail due to failure to resolve public.squish function

--- db/helpers/000_helpers.sql
+++ db/helpers/000_helpers.sql
@@ -9,15 +9,6 @@ AS $FUNCTION$
     );
 $FUNCTION$;
 
-CREATE OR REPLACE FUNCTION squish_null(TEXT) RETURNS TEXT
-  LANGUAGE SQL IMMUTABLE
-  AS $$
-    SELECT CASE WHEN SQUISH($1) = '' THEN NULL ELSE SQUISH($1) END;
-  $$;
-
-COMMENT ON FUNCTION squish_null(TEXT) IS
-  'Squishes whitespace characters in a string and returns null for empty string';
-
 -- This function previously had a different signature - ensure that the old version is gone
 DROP FUNCTION IF EXISTS full_name_with_spp(rank_name VARCHAR(255), full_name VARCHAR(255));
 
