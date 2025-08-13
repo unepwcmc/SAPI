be1cbb27197caefa1aa63c1589df8035ea900999
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2025-08-13 17:22:20 +0100

    feat: write a view to make 003_rebuild_designation_all_listing_changes_mview redundant

--- db/mviews/003_rebuild_designation_all_listing_changes_mview.sql
+++ db/mviews/003_rebuild_designation_all_listing_changes_mview.sql
@@ -1,9 +1,135 @@
-  DROP FUNCTION IF EXISTS rebuild_designation_all_listing_changes_mview(
-    taxonomy taxonomies, designation designations
-  );
-  CREATE OR REPLACE FUNCTION rebuild_designation_all_listing_changes_mview(
-    taxonomy taxonomies, designation designations, events_ids INT[]
-  ) RETURNS void
+CREATE OR REPLACE VIEW all_listing_changes_mview AS
+  -- affected_taxon_concept -- is a taxon concept that is affected by this listing change,
+  -- even though it might not have an explicit connection to it
+  -- (i.e. it''s an ancestor''s listing change)
+  WITH designations_and_intervals AS (
+    SELECT
+      designations.id          designation_id,
+      designations.name        designation_name,
+      designations.taxonomy_id taxonomy_id,
+      intervals.start_date     interval_start_date,
+      intervals.end_date       interval_end_date,
+      intervals.events_ids     interval_events_ids
+    FROM designations
+    LEFT JOIN eu_regulations_applicability_view intervals
+      ON designations.name = 'EU'
+  ), listing_changes_with_exceptions AS (
+    -- the purpose of this CTE is to aggregate excluded taxon concept ids
+    SELECT
+      listing_changes.id,
+      change_types.designation_id,
+      change_types.name AS change_type_name,
+      listing_changes.taxon_concept_id,
+      listing_changes.species_listing_id,
+      listing_changes.change_type_id,
+      listing_changes.inclusion_taxon_concept_id,
+      listing_changes.event_id,
+      listing_changes.effective_at::DATE,
+      listing_changes.is_current,
+      ARRAY_AGG_NOTNULL(taxonomic_exceptions.taxon_concept_id) AS excluded_taxon_concept_ids
+    FROM listing_changes
+    LEFT JOIN listing_changes taxonomic_exceptions
+    ON listing_changes.id = taxonomic_exceptions.parent_id
+    AND listing_changes.taxon_concept_id != taxonomic_exceptions.taxon_concept_id
+    JOIN change_types ON change_types.id = listing_changes.change_type_id
+    JOIN designations_and_intervals
+      ON designations_and_intervals.designation_id = change_types.designation_id
+      AND (
+        designations_and_intervals IS NULL
+        OR ARRAY_UPPER(designations_and_intervals.interval_events_ids, 1) IS NULL
+        OR listing_changes.event_id = ANY(designations_and_intervals.interval_events_ids)
+      )
+    GROUP BY
+      listing_changes.id,
+      change_types.designation_id,
+      change_types.name,
+      listing_changes.taxon_concept_id,
+      listing_changes.species_listing_id,
+      listing_changes.change_type_id,
+      listing_changes.inclusion_taxon_concept_id,
+      listing_changes.event_id,
+      listing_changes.effective_at::DATE,
+      listing_changes.is_current
+  ), aggregate_lc AS (
+  -- the purpose of this CTE is to aggregate listed and excluded populations
+  SELECT lc.id,
+    lc.designation_id,
+    lc.change_type_name,
+    lc.taxon_concept_id,
+    lc.species_listing_id,
+    lc.change_type_id,
+    lc.inclusion_taxon_concept_id,
+    lc.event_id,
+    lc.effective_at,
+    lc.is_current,
+    lc.excluded_taxon_concept_ids,
+    party_distribution.geo_entity_id AS party_id,
+    ARRAY_AGG_NOTNULL(listing_distributions.geo_entity_id) AS listed_geo_entities_ids,
+    ARRAY_AGG_NOTNULL(excluded_distributions.geo_entity_id) AS excluded_geo_entities_ids
+  FROM listing_changes_with_exceptions lc
+  LEFT JOIN listing_distributions
+    ON lc.id = listing_distributions.listing_change_id
+    AND NOT listing_distributions.is_party
+  LEFT JOIN listing_distributions party_distribution
+    ON lc.id = party_distribution.listing_change_id
+    AND party_distribution.is_party
+  LEFT JOIN listing_changes population_exceptions
+    ON lc.id = population_exceptions.parent_id
+    AND lc.taxon_concept_id = population_exceptions.taxon_concept_id
+  LEFT JOIN listing_distributions excluded_distributions
+    ON population_exceptions.id = excluded_distributions.listing_change_id
+    AND NOT excluded_distributions.is_party
+  GROUP BY
+    lc.id,
+    lc.designation_id,
+    lc.change_type_name,
+    lc.taxon_concept_id,
+    lc.species_listing_id,
+    lc.change_type_id,
+    lc.inclusion_taxon_concept_id,
+    lc.event_id,
+    lc.effective_at,
+    lc.is_current,
+    party_distribution.geo_entity_id,
+    lc.excluded_taxon_concept_ids
+)
+SELECT
+  lc.*,
+  tc.taxon_concept_id AS affected_taxon_concept_id,
+  COALESCE(itc.tree_distance, tc.tree_distance) tree_distance,
+  -- the following ROW_NUMBER call will assign chronological order to listing changes
+  -- in scope of the affected taxon concept and a particular designation
+  ROW_NUMBER() OVER (
+    PARTITION BY tc.taxon_concept_id, designation_id
+    ORDER BY effective_at,
+    CASE
+      WHEN change_type_name = 'DELETION' THEN 0
+      WHEN change_type_name = 'RESERVATION_WITHDRAWAL' THEN 1
+      WHEN change_type_name = 'ADDITION' THEN 2
+      WHEN change_type_name = 'RESERVATION' THEN 3
+      WHEN change_type_name = 'EXCEPTION' THEN 4
+    END,
+    tc.tree_distance
+    -- ??? OR would it be better to
+    -- COALESCE(itc.tree_distance, tc.tree_distance) tree_distance
+  )::INT AS timeline_position
+FROM aggregate_lc lc
+JOIN taxon_concepts_and_ancestors_mview tc
+  ON lc.taxon_concept_id = tc.ancestor_taxon_concept_id
+LEFT JOIN taxon_concepts_and_ancestors_mview itc
+  ON lc.inclusion_taxon_concept_id = itc.ancestor_taxon_concept_id
+  AND lc.taxon_concept_id = itc.taxon_concept_id
+;
+
+
+DROP FUNCTION IF EXISTS rebuild_designation_all_listing_changes_mview(
+  taxonomy taxonomies, designation designations
+);
+
+
+CREATE OR REPLACE FUNCTION rebuild_designation_all_listing_changes_mview(
+  taxonomy taxonomies, designation designations, events_ids INT[]
+) RETURNS void
   LANGUAGE plpgsql
   AS $$
   DECLARE
