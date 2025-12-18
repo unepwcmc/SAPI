7952233baf873ac7f77f6b14375272d9670360a7
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2025-12-18 11:48:21 +0000

    morewop

--- db/mviews/003_rebuild_designation_all_listing_changes_mview.sql
+++ db/mviews/003_rebuild_designation_all_listing_changes_mview.sql
@@ -1,7 +1,7 @@
-CREATE OR REPLACE VIEW all_listing_changes_mview AS
-  -- affected_taxon_concept -- is a taxon concept that is affected by this listing change,
-  -- even though it might not have an explicit connection to it
-  -- (i.e. it''s an ancestor''s listing change)
+CREATE OR REPLACE VIEW all_listing_changes_view AS
+  -- affected_taxon_concept is a taxon concept that is affected by this listing
+  -- change, even though it might not have an explicit connection to it
+  -- (i.e. it is an ancestor's listing change).
   WITH designations_and_intervals AS (
     SELECT
       designations.id          designation_id,
@@ -96,6 +96,8 @@ CREATE OR REPLACE VIEW all_listing_changes_mview AS
 SELECT
   lc.*,
   tc.taxon_concept_id AS affected_taxon_concept_id,
+  -- Make the tree distance reflect distance from inclusion
+  -- TODO TEST Rhinopittecus roxellana
   COALESCE(itc.tree_distance, tc.tree_distance) tree_distance,
   -- the following ROW_NUMBER call will assign chronological order to listing changes
   -- in scope of the affected taxon concept and a particular designation
@@ -121,33 +123,11 @@ LEFT JOIN taxon_concepts_and_ancestors_mview itc
   AND lc.taxon_concept_id = itc.taxon_concept_id
 ;
 
-
 DROP FUNCTION IF EXISTS rebuild_designation_all_listing_changes_mview(
   taxonomy taxonomies, designation designations
 );
 
-
-CREATE OR REPLACE FUNCTION rebuild_designation_all_listing_changes_mview(
-  taxonomy taxonomies, designation designations, events_ids INT[]
-) RETURNS void
-  LANGUAGE plpgsql
-  AS $$
-  DECLARE
-    all_lc_table_name TEXT;
-    tmp_lc_table_name TEXT;
-    tc_table_name TEXT;
-    sql TEXT;
-  BEGIN
-    SELECT listing_changes_mview_name('all', designation.name, events_ids)
-    INTO all_lc_table_name;
-    SELECT listing_changes_mview_name('tmp', designation.name, events_ids)
-    INTO tmp_lc_table_name;
-
-    SELECT LOWER(taxonomy.name) || '_taxon_concepts_and_ancestors_view' INTO tc_table_name;
-
-    EXECUTE 'DROP TABLE IF EXISTS ' || tmp_lc_table_name || ' CASCADE';
-
-    sql := 'CREATE TEMP TABLE ' || tmp_lc_table_name || ' AS
+CREATE OR REPLACE VIEW all_listing_changes_view AS
     -- affected_taxon_concept -- is a taxon concept that is affected by this listing change,
     -- even though it might not have an explicit connection to it
     -- (i.e. it''s an ancestor''s listing change)
@@ -225,63 +205,87 @@ CREATE OR REPLACE FUNCTION rebuild_designation_all_listing_changes_mview(
       lc.effective_at,
       lc.is_current,
       party_distribution.geo_entity_id,
-      lc.excluded_taxon_concept_ids';
+      lc.excluded_taxon_concept_ids
+;
 
-    EXECUTE sql;
+CREATE OR REPLACE FUNCTION rebuild_designation_all_listing_changes_mview(
+  taxonomy taxonomies,
+  designation designations,
+  events_ids INT[]
+) RETURNS void
+  LANGUAGE plpgsql
+AS $rebuild_designation_all_listing_changes_mview$
+  DECLARE
+    all_lc_table_name TEXT;
+    tmp_lc_table_name TEXT;
+    tc_table_name TEXT;
+    sql TEXT;
+  BEGIN
+    SELECT listing_changes_mview_name('all', designation.name, events_ids)
+      INTO all_lc_table_name;
+    SELECT listing_changes_mview_name('tmp', designation.name, events_ids)
+      INTO tmp_lc_table_name;
 
-    EXECUTE 'CREATE INDEX ON ' || tmp_lc_table_name || ' (taxon_concept_id)';
-    -- for the current listing calculation
-    EXECUTE 'CREATE INDEX ON ' || tmp_lc_table_name || ' (taxon_concept_id, is_current, change_type_name, inclusion_taxon_concept_id)';
+    SELECT LOWER(taxonomy.name) || '_taxon_concepts_and_ancestors_view'
+      INTO tc_table_name;
 
-    EXECUTE 'DROP TABLE IF EXISTS ' || all_lc_table_name || ' CASCADE';
+    -- First, build the temp table
+    EXECUTE format(
+      $format$
+        CREATE TABLE %I AS
+          SELECT *
+          FROM all_listing_changes_view
+          WHERE designation_id = %L
+            %S
+      $format$,
+      tmp_lc_table_name,
+      designation.id,
+      CASE
+        WHEN array_length(events_ids, 1) > 0
+        THEN format(
+          'AND event_ids = %L::INT[]'
+          event_ids
+        )
+        ELSE ''
+      END
+    );
 
-    sql := 'CREATE TEMP TABLE ' || all_lc_table_name || ' AS
-    SELECT
-      lc.*,
-      tc.taxon_concept_id AS affected_taxon_concept_id,
-      tc.tree_distance,
-      -- the following ROW_NUMBER call will assign chronological order to listing changes
-      -- in scope of the affected taxon concept and a particular designation
-      ROW_NUMBER() OVER (
-          PARTITION BY tc.taxon_concept_id, designation_id
-          ORDER BY effective_at,
-          CASE
-            WHEN change_type_name = ''DELETION'' THEN 0
-            WHEN change_type_name = ''RESERVATION_WITHDRAWAL'' THEN 1
-            WHEN change_type_name = ''ADDITION'' THEN 2
-            WHEN change_type_name = ''RESERVATION'' THEN 3
-            WHEN change_type_name = ''EXCEPTION'' THEN 4
-          END,
-          tree_distance
-      )::INT AS timeline_position
-    FROM ' || tmp_lc_table_name || ' lc
-    JOIN ' || tc_table_name || ' tc
-    ON lc.taxon_concept_id = tc.ancestor_taxon_concept_id';
+    -- Then, drop the old table and swap in the temp table.
+    EXECUTE format(
+      $format$
+        DROP TABLE IF EXISTS %1$I CASCADE;
+        ALTER %2$I RENAME TO %1$I;
+      $format$,
+      all_lc_table_name,
+      tmp_lc_table_name
+    );
 
-    EXECUTE sql;
-
-    EXECUTE 'CREATE INDEX ON ' || all_lc_table_name || ' (designation_id, timeline_position, affected_taxon_concept_id)';
-    EXECUTE 'CREATE INDEX ON ' || all_lc_table_name || ' (affected_taxon_concept_id, inclusion_taxon_concept_id)';
-    EXECUTE 'CREATE INDEX ON ' || all_lc_table_name || ' (id, affected_taxon_concept_id)';
-
-    -- make the tree distance reflect distance from inclusion (Rhinopittecus roxellana)
-    sql := 'UPDATE ' || all_lc_table_name
-    || ' SET tree_distance = tc.tree_distance
-    FROM ' || all_lc_table_name || ' alc
-    JOIN ' || tc_table_name || ' tc
-    ON alc.inclusion_taxon_concept_id = tc.ancestor_taxon_concept_id
-    AND alc.affected_taxon_concept_id = tc.taxon_concept_id
-    WHERE alc.id = ' || all_lc_table_name || '.id
-    AND alc.affected_taxon_concept_id = ' || all_lc_table_name || '.affected_taxon_concept_id';
-
-    EXECUTE sql;
-
+    EXECUTE format(
+      $format$
+        CREATE INDEX ON %1$I (taxon_concept_id) WHERE is_current;
+        CREATE INDEX ON %1$I (taxon_concept_id);
+        CREATE INDEX ON %1$I (
+          taxon_concept_id,
+          is_current,
+          change_type_name,
+          inclusion_taxon_concept_id
+        );
+        CREATE INDEX ON %1%I (designation_id, timeline_position, affected_taxon_concept_id)
+        CREATE INDEX ON %1%I (affected_taxon_concept_id, inclusion_taxon_concept_id)
+        CREATE INDEX ON %1%I (id, affected_taxon_concept_id)
+        CREATE INDEX ON %1$I (affected_taxon_concept_id, id);
+      $format$,
+      all_lc_table_name
+    );
   END;
-  $$;
+$rebuild_designation_all_listing_changes_mview$;
 
-  COMMENT ON FUNCTION rebuild_designation_all_listing_changes_mview(
-    taxonomy taxonomies, designation designations, events_ids INT[]
-  ) IS
-  'Procedure to create a helper table with all listing changes
-  + their included / excluded populations
-  + tree distance between affected taxon concept and the taxon concept this listing change applies to.';
+COMMENT ON FUNCTION rebuild_designation_all_listing_changes_mview(
+  taxonomy taxonomies,
+  designation designations,
+  events_ids INT[]
+) IS $comment$
+Procedure to create a helper table with all listing changes
++ their included / excluded populations
++ tree distance between affected taxon concept and the taxon concept this listing change applies to.
+$comment$;
