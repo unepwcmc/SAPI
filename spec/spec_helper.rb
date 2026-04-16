ef49619742e73b28ffb05672cc6d0e043dde2acf
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2026-04-16 10:22:05 +0100

    fix: force test env in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+
 require 'simplecov'
 require 'coveralls'
 
@@ -14,8 +16,9 @@ SimpleCov.start 'rails' do
   add_group 'Serializers', 'app/serializers'
 end
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= 'test'
+# CHANGED
+ENV['RAILS_ENV'] = 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'sidekiq/testing'
