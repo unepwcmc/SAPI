5a94f076fa0b662bef282c39b25780282a4fb0b7
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2026-09-08 14:48:34 +0100

    fix: strip leading and trailing spaces from taxon names and author_year

--- config/initializers/00_constants.rb
+++ config/initializers/00_constants.rb
@@ -2,3 +2,8 @@
 # This is overly conservative, there are no doubt many punctuation characters
 # which are also not going to break.
 PDF_SAFE_REGEX = /\A[\x20-\x7f\p{Latin}‘’“”–]*\z/
+MESSAGE_SHOULD_BE_PDF_SAFE = 'should only contain PDF-safe characters'
+
+# Strings which match this regex have no leading or trailing spaces
+TRIMMED_REGEX = /\A(?!\s).*(?<!\s)\z/
+MESSAGE_SHOULD_BE_TRIMMED = 'should only contain PDF-safe characters'
